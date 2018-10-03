27d5853c2b0c24981bdcbde854b2d55d84bba7ae
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-03 19:10:48 +0200

    Add Hooks::Hooks() constructor

---
+++ Osiris/Hooks.cpp
@@ -0,0 +1,6 @@
+#include "Hooks.h"
+
+Hooks::Hooks()
+{
+
+}
--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -5,6 +5,8 @@
 #include <d3d9.h>
 
 class Hooks {
+public:
+    Hooks();
 private:
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
