2fa3cf5d77af06b87db03dda999362df5439fe42
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-03 19:26:51 +0200

    Initialize originalPresent and originalReset in Hooks constructor

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -4,5 +4,6 @@
 
 Hooks::Hooks()
 {
-
+    originalPresent = reinterpret_cast<HRESULT(STDMETHODCALLTYPE*)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)>(memory.present);
+    originalReset = reinterpret_cast<HRESULT(STDMETHODCALLTYPE*)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)>(memory.reset);
 }
