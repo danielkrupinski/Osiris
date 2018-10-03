db8e5228c59aa2253d8ca82f1fbd763938dfab2a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-03 18:57:56 +0200

    Update function signature in originalPresent

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -6,5 +6,5 @@
 
 class Hooks {
 private:
-    std::function<HRESULT(const RECT* pSourceRect, const RECT* pDestRect, HWND hDestWindowOverride, const RGNDATA* pDirtyRegion)> originalPresent;
+    std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
 };
