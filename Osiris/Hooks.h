52fb3b7d6bf25515c94d6164d2b7618e8a349145
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-04 17:28:58 +0200

    Add Hooks::hookedReset method

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -13,4 +13,5 @@ private:
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
     HRESULT STDMETHODCALLTYPE hookedPresent(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
+    HRESULT STDMETHODCALLTYPE hookedReset(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
 };
