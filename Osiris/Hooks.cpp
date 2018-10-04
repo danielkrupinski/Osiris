fd2b386f908a50e33577bc457150f451b0877cb4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-04 17:03:57 +0200

    Include imgui/imgui.h

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -1,9 +1,15 @@
-#include "Hooks.h"
+#include "imgui/imgui.h"
 
+#include "Hooks.h"
 #include "Memory.h"
 
 Hooks::Hooks()
 {
     originalPresent = reinterpret_cast<HRESULT(STDMETHODCALLTYPE*)(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)>(memory.present);
     originalReset = reinterpret_cast<HRESULT(STDMETHODCALLTYPE*)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)>(memory.reset);
+}
+
+LRESULT STDMETHODCALLTYPE hookedWndProc(HWND window, UINT messageType, WPARAM wParam, LPARAM lParam)
+{
+
 }
