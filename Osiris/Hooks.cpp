25f4a1bdaccc5b5bed551937283367b66ab84fe5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-04 17:17:45 +0200

    Update Hooks::hookedWndProc definition

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -1,5 +1,6 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
 
 #include "Hooks.h"
 #include "Memory.h"
@@ -10,7 +11,47 @@ Hooks::Hooks()
     originalReset = reinterpret_cast<HRESULT(STDMETHODCALLTYPE*)(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)>(memory.reset);
 }
 
-LRESULT STDMETHODCALLTYPE hookedWndProc(HWND window, UINT messageType, WPARAM wParam, LPARAM lParam)
+LRESULT STDMETHODCALLTYPE Hooks::hookedWndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-
+    ImGuiIO& io = ImGui::GetIO();
+    switch (msg) {
+    case WM_LBUTTONDOWN:
+        io.MouseDown[0] = true;
+        return true;
+    case WM_LBUTTONUP:
+        io.MouseDown[0] = false;
+        return true;
+    case WM_RBUTTONDOWN:
+        io.MouseDown[1] = true;
+        return true;
+    case WM_RBUTTONUP:
+        io.MouseDown[1] = false;
+        return true;
+    case WM_MBUTTONDOWN:
+        io.MouseDown[2] = true;
+        return true;
+    case WM_MBUTTONUP:
+        io.MouseDown[2] = false;
+        return true;
+    case WM_MOUSEWHEEL:
+        io.MouseWheel += GET_WHEEL_DELTA_WPARAM(wParam) > 0 ? +1.0f : -1.0f;
+        return true;
+    case WM_MOUSEMOVE:
+        io.MousePos.x = (signed short)(lParam);
+        io.MousePos.y = (signed short)(lParam >> 16);
+        return true;
+    case WM_KEYDOWN:
+        if (wParam < 256)
+            io.KeysDown[wParam] = 1;
+        return true;
+    case WM_KEYUP:
+        if (wParam < 256)
+            io.KeysDown[wParam] = 0;
+        return true;
+    case WM_CHAR:
+        if (wParam > 0 && wParam < 0x10000)
+            io.AddInputCharacter((unsigned short)wParam);
+        return true;
+    }
+    return CallWindowProc(originalWndProc, window, msg, wParam, lParam);
 }
