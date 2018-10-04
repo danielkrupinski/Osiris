7bf63bc1fb475dd8bb65f5150f812ce1caa016f8
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-04 17:01:25 +0200

    Add Hooks::hookedWndProc method

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -9,6 +9,7 @@ public:
     Hooks();
 private:
     WNDPROC originalWndProc;
+    LRESULT STDMETHODCALLTYPE hookedWndProc(HWND, UINT, WPARAM, LPARAM);
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
     HRESULT STDMETHODCALLTYPE hookedPresent(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
