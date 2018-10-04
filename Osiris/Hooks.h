f41f21146531e2a5f583861569a36249828d2af5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-04 16:34:56 +0200

    Add hookedPresent method of Hooks class

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -9,5 +9,6 @@ public:
     Hooks();
 private:
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
+    HRESULT STDMETHODCALLTYPE hookedPresent(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
     std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
 };
