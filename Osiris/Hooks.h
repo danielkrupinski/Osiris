e3343a3059bfb7448eb62a4852bbb5c31ae86319
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-05 16:33:10 +0200

    Update Hooks.h

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -1,19 +1,15 @@
 #pragma once
 
-#include <functional>
+#include <type_traits>
 
 #include <d3d9.h>
 
-class Hooks {
+class Hooks final {
 public:
     Hooks();
-private:
     WNDPROC originalWndProc;
-    LRESULT STDMETHODCALLTYPE hookedWndProc(HWND, UINT, WPARAM, LPARAM);
-    std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
-    HRESULT STDMETHODCALLTYPE hookedPresent(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
-    std::function<HRESULT STDMETHODCALLTYPE(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
-    HRESULT STDMETHODCALLTYPE hookedReset(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
+    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*)> originalPresent;
+    std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
 };
 
 extern Hooks hooks;
