67c9742b6179da996816fd0716607ce9e7a60e46
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-05 18:55:30 +0200

    Initialize path to config path in Config::Config()

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -4,5 +4,8 @@
 
 Config::Config(const std::string& name)
 {
-
+    char buff[MAX_PATH]{ };
+    if (SUCCEEDED(SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, 0, buff))) {
+        path = buff + std::string{ '\\' } + name;
+    }
 }
