c55b8b33be9e0e808ae0e1cbff1f78030a893bcd
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-06 15:38:48 +0200

    Add menu tabs handling

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -19,5 +19,31 @@ void GUI::render()
 
     static enum { AIMBOT, VISUALS, MISC } tab{ MISC };
  
+    ImGui::PushItemWidth(165);
+    if (ImGui::BeginChild("##tabchild", ImVec2(0, 440), true)) {
+        switch (tab) {
+        case AIMBOT:
+            break;
+        case VISUALS:
+            break;
+        case MISC:
+            break;
+        }
+    }
+    
+    ImGui::EndChild();
+    ImGui::PopItemWidth();
+
+    if (ImGui::Button("Aimbot", ImVec2(152, 20)))
+        tab = AIMBOT;
+    ImGui::SameLine();
+
+    if (ImGui::Button("Visuals", ImVec2(152, 20)))
+        tab = VISUALS;
+    ImGui::SameLine();
+
+    if (ImGui::Button("Misc", ImVec2(152, 20)))
+        tab = MISC;
+
     ImGui::End();
 }
