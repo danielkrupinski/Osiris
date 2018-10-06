af72e64590a2b83ccede9286f8d3346ddffea9c3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-06 16:06:37 +0200

    Change int arguments to floats where callee expects floats

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -14,13 +14,13 @@ void GUI::render()
     ImGui::StyleColorsLight();
     ImGuiStyle& style = ImGui::GetStyle();
     style.Alpha = 0.9f;
-    ImGui::SetNextWindowSize(ImVec2(650, 500));
+    ImGui::SetNextWindowSize(ImVec2(650.0f, 500.0f));
     ImGui::Begin("Osiris", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize);
 
     static enum { AIMBOT, VISUALS, MISC } tab{ MISC };
  
     ImGui::PushItemWidth(165);
-    if (ImGui::BeginChild("#tabs", ImVec2(0, 440), true)) {
+    if (ImGui::BeginChild("#tabs", ImVec2(0.0f, 440.0f), true)) {
         switch (tab) {
         case AIMBOT:
             ImGui::Text("Aimbot tab welcomes!");
@@ -37,15 +37,15 @@ void GUI::render()
     ImGui::EndChild();
     ImGui::PopItemWidth();
 
-    if (ImGui::Button("Aimbot", ImVec2(207, 20)))
+    if (ImGui::Button("Aimbot", ImVec2(207.0f, 20.0f)))
         tab = AIMBOT;
     ImGui::SameLine();
 
-    if (ImGui::Button("Visuals", ImVec2(207, 20)))
+    if (ImGui::Button("Visuals", ImVec2(207.0f, 20.0f)))
         tab = VISUALS;
     ImGui::SameLine();
 
-    if (ImGui::Button("Misc", ImVec2(207, 20)))
+    if (ImGui::Button("Misc", ImVec2(207.0f, 20.0f)))
         tab = MISC;
 
     ImGui::End();
