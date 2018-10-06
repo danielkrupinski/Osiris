722561887214b217086fcfa78a8fd5804cbc773a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-06 19:33:17 +0200

    Include <Windows.h>

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -1,3 +1,5 @@
+#include <Windows.h>
+
 #include "Interfaces.h"
 
 Interfaces::Interfaces()
@@ -11,7 +13,7 @@ void* Interfaces::find(const std::string& module, const std::string& name)
 
     void* Interface = nullptr;
 
-    CreateInterfaceFn createInterface = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA(module), "CreateInterface");
+    CreateInterfaceFn createInterface = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA(module.c_str()), "CreateInterface");
 
     for (int i = 1; i < 100; i++) {
         std::string interfaceName{ name + std::string{ '0' } + std::to_string(i) };
