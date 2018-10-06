a8aeb0cba87ae85fb5ecf57e6ea9b4888cb92dec
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-06 20:04:36 +0200

    Add basic SDK

--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -2,8 +2,11 @@
 
 #include <string>
 
+#include "SDK/engine.h"
+
 class Interfaces final {
 public:
     Interfaces();
     void* find(const std::string&, const std::string&);
+    CEngine* engine;
 };
