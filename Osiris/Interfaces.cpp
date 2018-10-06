0c26d196281cace10d79f04e493d175475e67e5b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-06 19:32:40 +0200

    Update Interfaces::find() method

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -7,5 +7,22 @@ Interfaces::Interfaces()
 
 void* Interfaces::find(const std::string& module, const std::string& name)
 {
-    return nullptr;
+    typedef void* (*CreateInterfaceFn)(const char* Name, int* ReturnCode);
+
+    void* Interface = nullptr;
+
+    CreateInterfaceFn createInterface = (CreateInterfaceFn)GetProcAddress(GetModuleHandleA(module), "CreateInterface");
+
+    for (int i = 1; i < 100; i++) {
+        std::string interfaceName{ name + std::string{ '0' } + std::to_string(i) };
+        Interface = (void*)createInterface(interfaceName.c_str(), nullptr);
+        if (Interface)
+            break;
+
+        interfaceName = name + "00" + std::to_string(i);
+        Interface = (void*)createInterface(interfaceName.c_str(), nullptr);
+        if (Interface)
+            break;
+    }
+    return Interface;
 }
