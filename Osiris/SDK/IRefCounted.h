8d9c7049570d01201f491eef01839c3a03cf9228
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-07 17:30:57 +0200

    Update IRefCounted.h

--- Osiris/SDK/IRefCounted.h
+++ Osiris/SDK/IRefCounted.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <Windows.h>
+
+class IRefCounted {
+private:
+    volatile long refCount;
+
+public:
+    virtual void destructor(char bDelete) = 0;
+    virtual bool OnFinalRelease() = 0;
+
+    void unreference() {
+        if (InterlockedDecrement(&refCount) == 0 && OnFinalRelease()) {
+            destructor(1);
+        }
+    }
+};
