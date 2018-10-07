3b29e359f3811c74ce293b20d41b58fae2fe244b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-07 16:10:45 +0200

    Remove SDK again

--- Osiris/SDK/misc/vfunc.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template<typename FuncType>
-__forceinline static FuncType CallVFunction(void* ppClass, int index)
-{
-    int* pVTable = *(int**)ppClass;
-    int dwAddress = pVTable[index];
-    return (FuncType)(dwAddress);
-}
