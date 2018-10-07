c22e1345c18d5136db73a1b92f46907bd545f580
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-07 17:13:06 +0200

    Add player_info_t typedef

--- Osiris/SDK/IVEngineClient.h
+++ Osiris/SDK/IVEngineClient.h
@@ -5,6 +5,29 @@
 #include "Vector.h"
 #include "VMatrix.h"
 
+typedef struct player_info_s {
+    __int64         unknown;            //0x0000 
+    union
+    {
+        __int64       steamID64;          //0x0008 - SteamID64
+        struct
+        {
+            __int32     xuid_low;
+            __int32     xuid_high;
+        };
+    };
+    char            szName[128];        //0x0010 - Player Name
+    int             userId;             //0x0090 - Unique Server Identifier
+    char            szSteamID[20];      //0x0094 - STEAM_X:Y:Z
+    char            pad_0x00A8[0x10];   //0x00A8
+    unsigned long   iSteamID;           //0x00B8 - SteamID 
+    char            szFriendsName[128];
+    bool            fakeplayer;
+    bool            ishltv;
+    unsigned int    customfiles[4];
+    unsigned char   filesdownloaded;
+} player_info_t;
+
 class IVEngineClient {
 public:
     virtual int                   GetIntersectingSurfaces(const model_t *model, const Vector &vCenter, const float radius, const bool bOnlyVisibleSurfaces, SurfInfo *pInfos, const int nMaxInfos) = 0;
