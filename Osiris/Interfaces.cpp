42bd83349d52b9b896be3239250014b788ce98de
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-10 19:23:37 +0200

    Initialize clieant pointer with VClient interface

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -7,6 +7,7 @@ Interfaces::Interfaces()
 {
     engineClient = reinterpret_cast<EngineClient*>(find("engine.dll", "VEngineClient"));
     clientEntityList = reinterpret_cast<ClientEntityList*>(find("client_panorama.dll", "VClientEntityList"));
+    client = reinterpret_cast<Client*>(find("client_panorama.dll", "VClient"));
 }
 
 void* Interfaces::find(const std::string& module, const std::string& name)
