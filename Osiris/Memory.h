a9362cead0d1761670b153a18aac50cf81e37a23
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-12 20:16:52 +0200

    Remove C_LocalPlayer class

--- Osiris/Memory.h
+++ Osiris/Memory.h
@@ -5,21 +5,6 @@
 #include "SDK/ClientMode.h"
 #include "SDK/GlobalVars.h"
 
-class C_LocalPlayer
-{
-    friend bool operator==(const C_LocalPlayer& lhs, void* rhs);
-public:
-    C_LocalPlayer() : m_local(nullptr) {}
-
-    operator bool() const { return *m_local != nullptr; }
-    operator BaseEntity*() const { return *m_local; }
-
-    BaseEntity* operator->() { return *m_local; }
-
-private:
-    BaseEntity** m_local;
-};
-
 class Memory final {
 public:
     Memory();
