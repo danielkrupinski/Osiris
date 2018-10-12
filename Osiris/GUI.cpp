f96a61b60e4b9c83af764b064102328d0f8eaf70
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-12 19:04:22 +0200

    Add ImGuiWindowFlags_NoScrollWithMouse flag to main imgui window

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -15,7 +15,7 @@ GUI::GUI()
 void GUI::render()
 {
     ImGui::SetNextWindowSize(ImVec2(650.0f, 500.0f));
-    ImGui::Begin("Osiris", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar);
+    ImGui::Begin("Osiris", nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
 
     static enum { AIMBOT, VISUALS, MISC } tab{ MISC };
  
