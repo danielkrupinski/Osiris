0b38110cdc55e9f1fa19bfa97d1c2501008de19e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-13 17:31:26 +0200

    Encrypt strings in Interfaces.cpp

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -7,15 +7,15 @@
 
 Interfaces::Interfaces()
 {
-    engineClient = reinterpret_cast<EngineClient*>(find("engine.dll", "VEngineClient"));
-    clientEntityList = reinterpret_cast<ClientEntityList*>(find("client_panorama.dll", "VClientEntityList"));
-    client = reinterpret_cast<Client*>(find("client_panorama.dll", "VClient"));
+    engineClient = reinterpret_cast<EngineClient*>(find(xorstr_("engine.dll"), xorstr_("VEngineClient")));
+    clientEntityList = reinterpret_cast<ClientEntityList*>(find(xorstr_("client_panorama.dll"), xorstr_("VClientEntityList")));
+    client = reinterpret_cast<Client*>(find(xorstr_("client_panorama.dll"), xorstr_("VClient")));
 }
 
 void* Interfaces::find(const std::string& module, const std::string& name)
 {
     void* foundInterface = nullptr;
-    std::add_pointer_t<void* (const char* name, int* returnCode)> createInterface = reinterpret_cast<decltype(createInterface)>(GetProcAddress(GetModuleHandleA(module.c_str()), "CreateInterface"));
+    std::add_pointer_t<void* (const char* name, int* returnCode)> createInterface = reinterpret_cast<decltype(createInterface)>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
 
     for (int i = 1; i < 100; i++) {
         std::string interfaceName{ name + std::string{ '0' } + std::to_string(i) };
@@ -23,7 +23,7 @@ void* Interfaces::find(const std::string& module, const std::string& name)
         if (foundInterface)
             break;
 
-        interfaceName = name + "00" + std::to_string(i);
+        interfaceName = name + xorstr_("00") + std::to_string(i);
         foundInterface = (void*)createInterface(interfaceName.c_str(), nullptr);
         if (foundInterface)
             break;
