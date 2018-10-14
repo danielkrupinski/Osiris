620dc1c73b4d7dce13c4d5b8804e1695ee2ced3b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-14 19:07:02 +0200

    Update autostrafe

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -7,121 +7,142 @@
 #include "../Memory.h"
 #include "Misc.h"
 
-/* void Misc::autoStrafe(UserCmd* cmd)
-{
 #define RAD2DEG( x ) ( ( float )( x ) * ( float )( 180.0f / ( float )( M_PI ) ) )
-    if (GetAsyncKeyState(VK_SPACE)) {
-        auto viewAngles = cmd->
-        static int old_yaw;
+#define CheckIfNonValidNumber(x) (fpclassify(x) == FP_INFINITE || fpclassify(x) == FP_NAN || fpclassify(x) == FP_SUBNORMAL)
 
-        auto get_velocity_degree = [](float length_2d) {
-            auto tmp = RAD2DEG(atan(30.f / length_2d));
+float normalizeYaw(float value)
+{
+    while (value > 180)
+        value -= 360.f;
 
-            if (CheckIfNonValidNumber(tmp) || tmp > 90.f)
-                return 90.f;
+    while (value < -180)
+        value += 360.f;
+    return value;
+}
 
-            else if (tmp < 0.f)
-                return 0.f;
-
-            else
-                return tmp;
-        };
-
-        auto velocity = G::LocalPlayer->GetVelocity();
-        velocity.z = 0;
-
-        static auto flip = false;
-        auto turn_direction_modifier = (flip) ? 1.f : -1.f;
-        flip = !flip;
-
-        if (*((*memory.localPlayer)->getFlags()) & FL_ONGROUND || G::LocalPlayer->GetMoveType() == MOVETYPE_LADDER)
-            return;
-
-        if (cmd->forwardmove > 0.f)
-            cmd->forwardmove = 0.f;
-
-        auto velocity_length_2d = velocity.Length2D();
-
-        auto strafe_angle = RAD2DEG(atan(15.f / velocity_length_2d));
-
-        if (strafe_angle > 90.f)
-            strafe_angle = 90.f;
-
-        else if (strafe_angle < 0.f)
-            strafe_angle = 0.f;
-
-        Vector Buffer(0, G::StrafeAngle.y - old_yaw, 0);
-        Buffer.y = Math::NormalizeYaw(Buffer.y);
-
-        int yaw_delta = Buffer.y;
-        old_yaw = G::StrafeAngle.y;
-
-        if (yaw_delta > 0.f)
-            G::UserCmd->sidemove = -450.f;
-
-        else if (yaw_delta < 0.f)
-            G::UserCmd->sidemove = 450.f;
-
-        auto abs_yaw_delta = abs(yaw_delta);
-
-        if (abs_yaw_delta <= strafe_angle || abs_yaw_delta >= 30.f) {
-            Vector velocity_angles;
-            Math::VectorAngles(velocity, velocity_angles);
-
-            Buffer = Vector(0, G::StrafeAngle.y - velocity_angles.y, 0);
-            Buffer.y = Math::NormalizeYaw(Buffer.y);
-            int velocityangle_yawdelta = Buffer.y;
-
-            auto velocity_degree = get_velocity_degree(velocity_length_2d) * Clientvariables->Misc.Retrack;
-
-            if (velocityangle_yawdelta <= velocity_degree || velocity_length_2d <= 15.f) {
-                if (-(velocity_degree) <= velocityangle_yawdelta || velocity_length_2d <= 15.f) {
-                    G::StrafeAngle.y += (strafe_angle * turn_direction_modifier);
-                    G::UserCmd->sidemove = 450.f * turn_direction_modifier;
-                }
-
-                else {
-                    G::StrafeAngle.y = velocity_angles.y - velocity_degree;
-                    G::UserCmd->sidemove = 450.f;
-                }
-            }
-
-            else {
-                G::StrafeAngle.y = velocity_angles.y + velocity_degree;
-                cmd->sidemove = -450.f;
-            }
-        }
-
-        cmd->buttons &= ~(IN_MOVELEFT | IN_MOVERIGHT | IN_FORWARD | IN_BACK);
-
-        if (cmd->sidemove <= 0.0)
-            cmd->buttons |= IN_MOVELEFT;
-        else
-            cmd->buttons |= IN_MOVERIGHT;
-
-        if (cmd->forwardmove <= 0.0)
-            cmd->buttons |= IN_BACK;
-        else
-            cmd->buttons |= IN_FORWARD;
-
+void vectorAngles(const Vector& vecForward, Vector& vecAngles)
+{
+    Vector vecView;
+    if (vecForward[1] == 0.f && vecForward[0] == 0.f) {
+        vecView[0] = 0.f;
+        vecView[1] = 0.f;
     }
-} */
+    else {
+        vecView[1] = atan2(vecForward[1], vecForward[0]) * 180.f / M_PI;
+
+        if (vecView[1] < 0.f)
+            vecView[1] += 360;
+
+        vecView[2] = sqrt(vecForward[0] * vecForward[0] + vecForward[1] * vecForward[1]);
+
+        vecView[0] = atan2(vecForward[2], vecView[2]) * 180.f / M_PI;
+    }
+
+    vecAngles[0] = -vecView[0];
+    vecAngles[1] = vecView[1];
+    vecAngles[2] = 0.f;
+}
 
 void Misc::autoStrafe(UserCmd* cmd)
 {
     if (config.misc.autoStrafe) {
-        bool bKeysPressed = true;
-        if (GetAsyncKeyState(0x41) || GetAsyncKeyState(0x57) || GetAsyncKeyState(0x53) || GetAsyncKeyState(0x44)) bKeysPressed = false;
+        if (GetAsyncKeyState(VK_SPACE)) {
+            static int old_yaw;
 
-        if ((GetAsyncKeyState(VK_SPACE) && !(*((*memory.localPlayer)->getFlags()) & FL_ONGROUND)) && bKeysPressed) {
-            cmd->forwardmove = (1550.f * 5) / (*memory.localPlayer)->getVelocity().Length2D();
-            cmd->sidemove = (cmd->command_number % 2) == 0 ? -450.f : 450.f;
-            if (cmd->forwardmove > 450.f)
-                cmd->forwardmove = 450.f;
+            auto get_velocity_degree = [](float length_2d) {
+                auto tmp = RAD2DEG(atan(30.f / length_2d));
+
+                if (CheckIfNonValidNumber(tmp) || tmp > 90.f)
+                    return 90.f;
+
+                else if (tmp < 0.f)
+                    return 0.f;
+
+                else
+                    return tmp;
+            };
+
+            auto velocity = (*memory.localPlayer)->getVelocity();
+            velocity.z = 0;
+
+            static auto flip = false;
+            auto turn_direction_modifier = (flip) ? 1.f : -1.f;
+            flip = !flip;
+
+            if (*((*memory.localPlayer)->getFlags()) & FL_ONGROUND || (*memory.localPlayer)->getMoveType() == MOVETYPE_LADDER)
+                return;
+
+            if (cmd->forwardmove > 0.f)
+                cmd->forwardmove = 0.f;
+
+            auto velocity_length_2d = velocity.Length2D();
+
+            auto strafe_angle = RAD2DEG(atan(15.f / velocity_length_2d));
+
+            if (strafe_angle > 90.f)
+                strafe_angle = 90.f;
+
+            else if (strafe_angle < 0.f)
+                strafe_angle = 0.f;
+
+            Vector strafeAngle = cmd->viewangles;
+            Vector Buffer(0, strafeAngle.y - old_yaw, 0);
+            Buffer.y = normalizeYaw(Buffer.y);
+
+            int yaw_delta = Buffer.y;
+            old_yaw = strafeAngle.y;
+
+            if (yaw_delta > 0.f)
+                cmd->sidemove = -450.f;
+
+            else if (yaw_delta < 0.f)
+                cmd->sidemove = 450.f;
+
+            auto abs_yaw_delta = abs(yaw_delta);
+
+            if (abs_yaw_delta <= strafe_angle || abs_yaw_delta >= 30.f) {
+                Vector velocity_angles;
+                vectorAngles(velocity, velocity_angles);
+
+                Buffer = Vector(0, strafeAngle.y - velocity_angles.y, 0);
+                Buffer.y = normalizeYaw(Buffer.y);
+                int velocityangle_yawdelta = Buffer.y;
+
+                auto velocity_degree = get_velocity_degree(velocity_length_2d) * 1.0f;// Clientvariables->Misc.Retrack;
+
+                if (velocityangle_yawdelta <= velocity_degree || velocity_length_2d <= 15.f) {
+                    if (-(velocity_degree) <= velocityangle_yawdelta || velocity_length_2d <= 15.f) {
+                        strafeAngle.y += (strafe_angle * turn_direction_modifier);
+                        cmd->sidemove = 450.f * turn_direction_modifier;
+                    }
+
+                    else {
+                        strafeAngle.y = velocity_angles.y - velocity_degree;
+                        cmd->sidemove = 450.f;
+                    }
+                }
+
+                else {
+                    strafeAngle.y = velocity_angles.y + velocity_degree;
+                    cmd->sidemove = -450.f;
+                }
+            }
+
+            cmd->buttons &= ~(IN_MOVELEFT | IN_MOVERIGHT | IN_FORWARD | IN_BACK);
+
+            if (cmd->sidemove <= 0.0)
+                cmd->buttons |= IN_MOVELEFT;
+            else
+                cmd->buttons |= IN_MOVERIGHT;
+
+            if (cmd->forwardmove <= 0.0)
+                cmd->buttons |= IN_BACK;
+            else
+                cmd->buttons |= IN_FORWARD;
+
         }
     }
 }
-
 
 void Misc::bunnyHop(UserCmd* cmd)
 {
--- Osiris/SDK/UserCmd.h
+++ Osiris/SDK/UserCmd.h
@@ -8,7 +8,7 @@ public:
     int pad;
     int     command_number;     // 0x04 For matching server and client commands for debugging
     int     tick_count;         // 0x08 the tick the client created this command
-    QAngle  viewangles;         // 0x0C Player instantaneous view angles.
+    Vector  viewangles;         // 0x0C Player instantaneous view angles.
     Vector  aimdirection;       // 0x18
     float   forwardmove;        // 0x24
     float   sidemove;           // 0x28
