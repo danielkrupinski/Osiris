e1c67684be79502ca886d69654967eef7a93a77b
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-14 10:56:45 +0200

    Update xor enctyption on GUI.cpp

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -17,22 +17,22 @@ GUI::GUI()
 void GUI::render()
 {
     ImGui::SetNextWindowSize(ImVec2(650.0f, 500.0f));
-    ImGui::Begin(xorstr("Osiris").crypt_get(), nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoTitleBar);
+    ImGui::Begin(xorstr_("Osiris"), nullptr, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoTitleBar);
 
     static enum { AIMBOT, VISUALS, MISC } tab{ MISC };
  
     ImGui::PushItemWidth(165);
-    if (ImGui::BeginChild(xorstr("#tabs").crypt_get(), ImVec2(0.0f, 460.0f), true)) {
+    if (ImGui::BeginChild(xorstr_("#tabs"), ImVec2(0.0f, 460.0f), true)) {
         switch (tab) {
         case AIMBOT:
             if (interfaces.engineClient->IsInGame())
-                ImGui::Text(xorstr("Player health: %d").crypt_get(), (*memory.localPlayer)->getHealth());
+                ImGui::Text(xorstr_("Player health: %d"), (*memory.localPlayer)->getHealth());
             break;
         case VISUALS:
-            ImGui::Text(xorstr("Visuals tab welcomes!").crypt_get());
+            ImGui::Text(xorstr_("Visuals tab welcomes!"));
             break;
         case MISC:
-            ImGui::Text(xorstr("Misc tab welcomes!").crypt_get());
+            ImGui::Text(xorstr_("Misc tab welcomes!"));
             ImGui::Checkbox(xorstr_("Bunny hop"), &config.misc.bunnyHop);
             break;
         }
@@ -41,15 +41,15 @@ void GUI::render()
     ImGui::EndChild();
     ImGui::PopItemWidth();
 
-    if (ImGui::Button(xorstr("Aimbot").crypt_get(), ImVec2(207.0f, 20.0f)))
+    if (ImGui::Button(xorstr_("Aimbot"), ImVec2(207.0f, 20.0f)))
         tab = AIMBOT;
     ImGui::SameLine();
 
-    if (ImGui::Button(xorstr("Visuals").crypt_get(), ImVec2(207.0f, 20.0f)))
+    if (ImGui::Button(xorstr_("Visuals"), ImVec2(207.0f, 20.0f)))
         tab = VISUALS;
     ImGui::SameLine();
 
-    if (ImGui::Button(xorstr("Misc").crypt_get(), ImVec2(207.0f, 20.0f)))
+    if (ImGui::Button(xorstr_("Misc"), ImVec2(207.0f, 20.0f)))
         tab = MISC;
 
     ImGui::End();
