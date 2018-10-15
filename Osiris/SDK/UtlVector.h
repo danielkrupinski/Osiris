544dc9f81db3eddb92cd5fb20b4ea441aa2658e4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-15 19:30:42 +0200

    Update UtlVector.h

--- Osiris/SDK/UtlVector.h
+++ Osiris/SDK/UtlVector.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "UtlMemory.h"
+
+template <typename T, typename A = UtlMemory<T>>
+class UtlVector {
+public:
+    typedef A Allocator;
+
+    T& operator[](int i) {
+        return m_Memory[i];
+    };
+
+    Allocator m_Memory;
+    int m_Size;
+    T* m_pElements;
+};
