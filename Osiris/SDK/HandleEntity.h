ca6d8b9b8842c31e9e22110ed9e836efe4c58808
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-15 20:12:35 +0200

    Update HandleEntity.h

--- Osiris/SDK/HandleEntity.h
+++ Osiris/SDK/HandleEntity.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "BaseHandle.h"
+
+class HandleEntity {
+public:
+    virtual ~HandleEntity() {}
+    virtual void SetRefEHandle(const BaseHandle &handle) = 0;
+    virtual const BaseHandle& GetRefEHandle() const = 0;
+};
