723fe38ad4986b35e066781db1e5f737511f6b05
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-15 19:28:41 +0200

    Update UtlMemory.h

--- Osiris/SDK/UtlMemory.h
+++ Osiris/SDK/UtlMemory.h
@@ -1,1 +1,14 @@
 #pragma once
+
+template <typename T>
+class UtlMemory {
+public:
+    T& operator[](int i)
+    {
+        return m_pMemory[i];
+    };
+
+    T* m_pMemory;
+    int m_nAllocationCount;
+    int m_nGrowSize;
+};
