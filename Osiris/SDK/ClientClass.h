3bb71b65c831afbc4efd283c23c4c3354d1d5f16
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-15 20:06:05 +0200

    Update ClientClass.h

--- Osiris/SDK/ClientClass.h
+++ Osiris/SDK/ClientClass.h
@@ -1,1 +1,17 @@
 #pragma once
+
+//class ClientClass;
+class IClientNetworkable;
+
+typedef ClientNetworkable* (*CreateClientClassFn)(int entnum, int serialNum);
+typedef ClientNetworkable* (*CreateEventFn)();
+
+class ClientClass {
+public:
+    CreateClientClassFn      m_pCreateFn;
+    CreateEventFn            m_pCreateEventFn;
+    char*                    m_pNetworkName;
+    void* /* RecvTable* */               m_pRecvTable;
+    ClientClass*             m_pNext;
+    ClassId                  m_ClassID;
+};
