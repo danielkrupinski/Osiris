16a6bdbb05244c18db49e289f43fe611d9f61aa3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-24 18:04:50 +0200

    Update ClieantEntity class

--- Osiris/SDK/ClientEntity.h
+++ Osiris/SDK/ClientEntity.h
@@ -4,8 +4,11 @@
 #include "ClientRenderable.h"
 #include "ClientThinkable.h"
 #include "ClientUnknown.h"
+#include "../Utils.h"
 
 class ClientEntity : public ClientUnknown, public ClientRenderable, public ClientNetworkable, public ClientThinkable {
 public:
     virtual void Release(void) = 0;
+
+    bool isWeapon() { return callVirtualFunction<bool(__thiscall*)(ClientEntity*)>(this, 160)(this); }
 };
