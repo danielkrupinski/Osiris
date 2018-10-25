f38ac9f089917f721069ad97a51e9743a512fa17
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-25 18:22:55 +0200

    Update void Misc::skybox()

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -39,5 +39,6 @@ void Misc::disablePostProcessing()
 
 void Misc::skybox()
 {
-
+    if (config.misc.skybox != "Default")
+        reinterpret_cast<void (__fastcall*)(const char*)>(memory.loadSky)(config.misc.skybox.c_str());
 }
