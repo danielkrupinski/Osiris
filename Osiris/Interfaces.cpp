263359dbf840b809100087f9ee15d698b98bb3d6
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-28 15:19:28 +0100

    Add try...catch block in Interfaces constructor

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -10,11 +10,16 @@
 
 Interfaces::Interfaces()
 {
-    engineClient = reinterpret_cast<EngineClient*>(find(xorstr_("engine.dll"), xorstr_("VEngineClient")));
-    clientEntityList = reinterpret_cast<ClientEntityList*>(find(xorstr_("client_panorama.dll"), xorstr_("VClientEntityList")));
-    client = reinterpret_cast<Client*>(find(xorstr_("client_panorama.dll"), xorstr_("VClient")));
-    surface = reinterpret_cast<Surface*>(find(xorstr_("vguimatsurface.dll"), xorstr_("VGUI_Surface")));
-    cvar = reinterpret_cast<Cvar*>(find(xorstr_("vstdlib.dll"), xorstr_("VEngineCvar")));
+    try {
+        engineClient = reinterpret_cast<EngineClient*>(find(xorstr_("engine.dll"), xorstr_("VEngineClient")));
+        clientEntityList = reinterpret_cast<ClientEntityList*>(find(xorstr_("client_panorama.dll"), xorstr_("VClientEntityList")));
+        client = reinterpret_cast<Client*>(find(xorstr_("client_panorama.dll"), xorstr_("VClient")));
+        surface = reinterpret_cast<Surface*>(find(xorstr_("vguimatsurface.dll"), xorstr_("VGUI_Surface")));
+        cvar = reinterpret_cast<Cvar*>(find(xorstr_("vstdlib.dll"), xorstr_("VEngineCvar")));
+    }
+    catch (std::runtime_error& e) {
+
+    }
 }
 
 void* Interfaces::find(const std::string& module, const std::string& name)
@@ -23,7 +28,7 @@ void* Interfaces::find(const std::string& module, const std::string& name)
     std::add_pointer_t<void* (const char* name, int* returnCode)> createInterface = reinterpret_cast<decltype(createInterface)>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
 
     for (int i = 1; i < 100; i++) {
-        std::string interfaceName{ name + std::string{ '0' } + std::to_string(i) };
+        std::string interfaceName{ name + std::string{ '0' } +std::to_string(i) };
         foundInterface = (void*)createInterface(interfaceName.c_str(), nullptr);
         if (foundInterface)
             break;
