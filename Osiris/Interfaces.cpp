fcfd8e4e9c8c7937a7ff67783d29548faf4db2e0
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-29 16:02:58 +0100

    Make Interfaces::find() template function

--- Osiris/Interfaces.cpp
+++ Osiris/Interfaces.cpp
@@ -22,25 +22,3 @@ Interfaces::Interfaces()
         std::exit(EXIT_FAILURE);
     }
 }
-
-void* Interfaces::find(const std::string& module, const std::string& name)
-{
-    void* foundInterface = nullptr;
-    std::add_pointer_t<void* (const char* name, int* returnCode)> createInterface = reinterpret_cast<decltype(createInterface)>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
-
-    for (int i = 1; i < 100; i++) {
-        std::string interfaceName{ name + std::string{ '0' } +std::to_string(i) };
-        foundInterface = (void*)createInterface(interfaceName.c_str(), nullptr);
-        if (foundInterface)
-            break;
-
-        interfaceName = name + xorstr_("00") + std::to_string(i);
-        foundInterface = (void*)createInterface(interfaceName.c_str(), nullptr);
-        if (foundInterface)
-            break;
-    }
-
-    if (!foundInterface)
-        throw std::runtime_error(std::string{ xorstr_("Could not find ") } +name + std::string{ xorstr_(" in ") } + module + std::string{ xorstr_("!") });
-    return foundInterface;
-}
--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -19,7 +19,29 @@ public:
     Surface* surface;
     Cvar* cvar;
 private:
-    void* find(const std::string&, const std::string&);
+    template <typename T>
+    auto find(const std::string& module, const std::string& name)
+    {
+        T* foundInterface = nullptr;
+        std::add_pointer_t<void* (const char* name, int* returnCode)> createInterface = reinterpret_cast<decltype(createInterface)>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
+
+        for (int i = 1; i < 100; i++) {
+            std::string interfaceName{ name + std::string{ '0' } +std::to_string(i) };
+            foundInterface = createInterface(interfaceName.c_str(), nullptr);
+            if (foundInterface)
+                break;
+
+            interfaceName = name + xorstr_("00") + std::to_string(i);
+            foundInterface = createInterface(interfaceName.c_str(), nullptr);
+            if (foundInterface)
+                break;
+        }
+
+        if (!foundInterface)
+            throw std::runtime_error(std::string{ xorstr_("Could not find ") } +name + std::string{ xorstr_(" in ") } +module + std::string{ xorstr_("!") });
+        return foundInterface;
+    }
+
 };
 
 extern Interfaces interfaces;
