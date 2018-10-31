f1ba5174b6cb9f14ab45136000926c6f09e4b66e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-31 11:38:11 +0100

    Update void runTriggerbot()

--- Osiris/Hacks/Triggerbot.cpp
+++ Osiris/Hacks/Triggerbot.cpp
@@ -1,6 +1,18 @@
+#include "../Interfaces.h"
+#include "../Memory.h"
 #include "Triggerbot.h"
 
 void runTriggerbot()
 {
-
+    if (GetAsyncKeyState(VK_MENU) & 1) {
+        auto inCrosshair = (*memory.localPlayer)->getCrosshairID();
+        if (inCrosshair > 0 && inCrosshair <= 64) {
+            auto target = interfaces.clientEntityList->getClientEntity(inCrosshair);
+            if (!reinterpret_cast<ClientEntity*>(target)->IsDormant() && target->getTeamNumber() != (*memory.localPlayer)->getTeamNumber())
+            {
+                constexpr int IN_ATTACK{ 1 };
+                cmd->buttons |= IN_ATTACK;
+            }
+        }
+    }
 }
