4c1e51cb850a2ed9dea91c2abcb15198dfdfecd1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-31 14:59:41 +0100

    Further Interfaces::find() optimizations

--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -28,17 +28,14 @@ private:
     template <typename T>
     auto find(const std::string& module, const std::string& name)
     {
-        T* foundInterface = nullptr;
         auto createInterface = reinterpret_cast<std::add_pointer_t<T* (const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
 
         for (int i = 1; i < 100; i++) {
             std::stringstream interfaceName;
             interfaceName << name << std::setfill('0') << std::setw(3) << i;
-            foundInterface = createInterface(interfaceName.str().c_str(), nullptr);
-            if (foundInterface)
+            if (T* foundInterface = createInterface(interfaceName.str().c_str(), nullptr))
                 return foundInterface;
         }
-
         throw std::runtime_error(std::string{ xorstr_("Could not find ") } + name + std::string{ xorstr_(" in ") } + module + std::string{ xorstr_("!") });
     }
 
