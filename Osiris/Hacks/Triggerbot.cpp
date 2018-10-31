442760f2d94210d763ef8ae1c6d69d848be76297
Daniel Krupiński <danielkrupinski@outlook.com>
2018-10-31 18:34:39 +0100

    Check if triggerbot is enabled in runTriggerbot()

--- Osiris/Hacks/Triggerbot.cpp
+++ Osiris/Hacks/Triggerbot.cpp
@@ -1,20 +1,23 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#include "../Config.h"
 #include "../Interfaces.h"
 #include "../Memory.h"
 #include "Triggerbot.h"
 
 void runTriggerbot(UserCmd* cmd)
 {
-    if (GetAsyncKeyState(VK_MENU)) {
-        auto inCrosshair = (*memory.localPlayer)->getCrosshairID();
-        if (inCrosshair > 0 && inCrosshair <= 64) {
-            auto target = interfaces.clientEntityList->getClientEntity(inCrosshair);
-            if (!reinterpret_cast<ClientEntity*>(target)->IsDormant() && target->getTeamNumber() != (*memory.localPlayer)->getTeamNumber())
-            {
-                constexpr int IN_ATTACK{ 1 };
-                cmd->buttons |= IN_ATTACK;
+    if (config.triggerbot.enabled) {
+        if (GetAsyncKeyState(VK_MENU)) {
+            auto inCrosshair = (*memory.localPlayer)->getCrosshairID();
+            if (inCrosshair > 0 && inCrosshair <= 64) {
+                auto target = interfaces.clientEntityList->getClientEntity(inCrosshair);
+                if (!reinterpret_cast<ClientEntity*>(target)->IsDormant() && target->getTeamNumber() != (*memory.localPlayer)->getTeamNumber())
+                {
+                    constexpr int IN_ATTACK{ 1 };
+                    cmd->buttons |= IN_ATTACK;
+                }
             }
         }
     }
