b3424bcdaddb24c4f1f2c1cecdbee6c7b80926c7
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-01 19:10:01 +0100

    Adjust "Misc" window size

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -79,7 +79,7 @@ void GUI::render()
     }
 
     if (window.misc) {
-        ImGui::SetNextWindowSize(ImVec2(240.0f, 180.0f));
+        ImGui::SetNextWindowSize(ImVec2(240.0f, 240.0f));
         ImGui::Begin(xorstr_("Misc"), &window.misc, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
         ImGui::PushItemWidth(120.0f);
         ImGui::Checkbox(xorstr_("Bunny hop"), &config.misc.bunnyHop);
