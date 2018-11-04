15422ba697dfcfcd56a6301bd9e0cdc4b9149270
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-04 18:02:59 +0100

    Add findMaterial() method to MaterialSystem class

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -1,5 +1,12 @@
 #pragma once
 
-class MaterialSystem {
+#include "Material.h"
+#include "Utils.h"
 
+class MaterialSystem {
+public:
+    Material* findMaterial(const char* pMaterialName, const char* pTextureGroupName, bool complain = true, const char* pComplainPrefix = nullptr)
+    {
+        return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, pMaterialName, pTextureGroupName, complain, pComplainPrefix);
+    }
 };
