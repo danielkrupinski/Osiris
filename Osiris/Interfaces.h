3f8e88698f7f82690b980077011f6701ac6c23b4
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-04 12:00:21 +0100

    Shorten Intefaces::find() function

--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -35,9 +35,7 @@ private:
         auto createInterface = reinterpret_cast<std::add_pointer_t<T* (const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleA(module.c_str()), xorstr_("CreateInterface")));
 
         for (int i = 1; i < 100; i++) {
-            std::stringstream interfaceName;
-            interfaceName << name << std::setfill('0') << std::setw(3) << i;
-            if (T* foundInterface = createInterface(interfaceName.str().c_str(), nullptr))
+            if (T* foundInterface = createInterface((std::stringstream{ } << name << std::setfill('0') << std::setw(3) << i).str().c_str(), nullptr))
                 return foundInterface;
         }
         throw std::runtime_error(std::string{ xorstr_("Could not find ") } + name + std::string{ xorstr_(" in ") } + module + std::string{ xorstr_("!") });
