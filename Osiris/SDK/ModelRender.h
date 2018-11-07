7116353d678c9e431f114aa4babd64eb85d4bec1
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-07 17:10:14 +0100

    Make ModelRender::forceMaterialOverride() public method

--- Osiris/SDK/ModelRender.h
+++ Osiris/SDK/ModelRender.h
@@ -3,6 +3,7 @@
 #include "Utils.h"
 
 class ModelRender {
+public:
     void forceMaterialOverride(Material* newMaterial, int nOverrideType = 0, int nOverrides = 0)
     {
         callVirtualFunction<void(__thiscall*)(void*, Material*, int, int)>(this, 1)(this, newMaterial, nOverrideType, nOverrides);
