cb543f3a0fa43551c71e234400b9f5a840cb2552
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-07 18:53:52 +0100

    Add ModelRenderInfo struct

--- Osiris/SDK/ModelRender.h
+++ Osiris/SDK/ModelRender.h
@@ -2,6 +2,23 @@
 
 #include "Utils.h"
 
+struct ModelRenderInfo {
+    Vector                  origin;
+    QAngle                  angles;
+    char                    pad[4];
+    ClientRenderable*      pRenderable;
+    const model_t*          pModel;
+    const matrix3x4_t*      pModelToWorld;
+    const matrix3x4_t*      pLightingOffset;
+    const Vector*           pLightingOrigin;
+    int                     flags;
+    int                     entity_index;
+    int                     skin;
+    int                     body;
+    int                     hitboxset;
+    ModelInstanceHandle_t   instance;
+};
+
 class ModelRender {
 public:
     void forceMaterialOverride(Material* newMaterial, int nOverrideType = 0, int nOverrides = 0)
