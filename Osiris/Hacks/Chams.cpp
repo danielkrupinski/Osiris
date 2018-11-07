a9dff4655aae551c8d4dd12e515cf06773eb61c2
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-07 20:24:37 +0100

    Update Chams::Chams()

---
+++ Osiris/Hacks/Chams.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+
+#include "Chams.h"
+
+Chams::Chams()
+{
+    std::ofstream("csgo\\materials\\simple_regular.vmt") <<        
+R"#("VertexLitGeneric"
+{
+  "$basetexture" "vgui/white_additive"
+  "$ignorez"      "0"
+  "$envmap"       ""
+  "$nofog"        "1"
+  "$model"        "1"
+  "$nocull"       "0"
+  "$selfillum"    "1"
+  "$halflambert"  "1"
+  "$znearer"      "0"
+  "$flat"         "1"
+}
+)#";
+}
--- Osiris/Hacks/Chams.h
+++ Osiris/Hacks/Chams.h
@@ -1,5 +1,6 @@
 #pragma once
 
 class Chams final {
-
+public:
+    Chams();
 };
