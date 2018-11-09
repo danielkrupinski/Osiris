8e4d0e569c86308b48baf57e688c8a5bc313ce1a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-09 20:02:48 +0100

    Apply correct colors to entities

--- Osiris/Hacks/Chams.cpp
+++ Osiris/Hacks/Chams.cpp
@@ -35,13 +35,16 @@ void Chams::render(const ModelRenderInfo& pInfo)
         if (strstr(pInfo.pModel->szName, "models/player") != nullptr) {
             auto entity = interfaces.clientEntityList->getClientEntity(pInfo.entity_index);
             if (entity && entity->isAlive()) {
+                auto material = interfaces.materialSystem->findMaterial("dev/glow_color", "Model textures");
+                material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
+                material->alphaModulate(config.chams.alpha);
                 if (entity->getTeamNumber() != (*memory.localPlayer)->getTeamNumber()) {
-                    auto material = interfaces.materialSystem->findMaterial("dev/glow_color", "Model textures");
-                    material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
-                    material->alphaModulate(config.chams.alpha);
-                    material->colorModulate(1.0f, 0.08f, 0.58f);
-                    interfaces.modelRender->forceMaterialOverride(material);
+                    material->colorModulate(config.chams.enemiesColor[0], config.chams.enemiesColor[1], config.chams.enemiesColor[2]);
                 }
+                else {
+                    material->colorModulate(config.chams.alliesColor[0], config.chams.alliesColor[1], config.chams.alliesColor[2]);
+                }
+                interfaces.modelRender->forceMaterialOverride(material);
             }
         }
     }
