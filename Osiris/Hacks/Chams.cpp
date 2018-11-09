061fe4cf704e52b817a6d8d490c4024dc83d0070
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-09 18:58:17 +0100

    Change chams material to dev/glow_color

--- Osiris/Hacks/Chams.cpp
+++ Osiris/Hacks/Chams.cpp
@@ -36,7 +36,7 @@ void Chams::render(const ModelRenderInfo& pInfo)
             auto entity = interfaces.clientEntityList->getClientEntity(pInfo.entity_index);
             if (entity && entity->isAlive()) {
                 if (entity->getTeamNumber() != (*memory.localPlayer)->getTeamNumber()) {
-                    auto material = interfaces.materialSystem->findMaterial("simple_regular", "Model textures");
+                    auto material = interfaces.materialSystem->findMaterial("dev/glow_color", "Model textures");
                     material->alphaModulate(config.chams.alpha);
                     material->colorModulate(1.0f, 0.08f, 0.58f);
                     interfaces.modelRender->forceMaterialOverride(material);
