5e87871be3a3175e2e6f68084b97343797e88937
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-11 18:28:27 +0100

    Create RenderView class with setColorModulation() method

--- Osiris/SDK/RenderView.h
+++ Osiris/SDK/RenderView.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "Utils.h"
+
+class RenderView {
+    void setColorModulation(const float* colors)
+    {
+        return callVirtualFunction<void(__thiscall*)(void*, const float*)>(this, 6)(this, colors);
+    }
+};
