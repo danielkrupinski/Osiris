0021c22334c023264a39b090c569588db6c5e068
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-12 09:10:33 +0100

    Add RenderView::setBlend() method

--- Osiris/SDK/RenderView.h
+++ Osiris/SDK/RenderView.h
@@ -4,8 +4,13 @@
 
 class RenderView {
 public:
-    void setColorModulation(float const* colors)
+    void setBlend(float alpha)
     {
-        return callVirtualFunction<void(__thiscall*)(void*, float const*)>(this, 6)(this, colors);
+        return callVirtualFunction<void(__thiscall*)(void*, float)>(this, 4)(this, alpha);
+    }
+
+    void setColorModulation(const float* colors)
+    {
+        return callVirtualFunction<void(__thiscall*)(void*, const float*)>(this, 6)(this, colors);
     }
 };
