2161752da211b65c8ad95b4d3c3ae40a72183f9d
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-15 19:56:13 +0100

    Remove unnecessary parentheses

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -192,9 +192,9 @@ std::uintptr_t* Hooks::Vmt::findFreeDataPage(const std::string& module, std::siz
         if (VirtualQuery(address, &mbi, sizeof(mbi)) == sizeof(mbi) && mbi.AllocationBase && mbi.BaseAddress &&
             mbi.State == MEM_COMMIT && !(mbi.Protect & PAGE_GUARD) && mbi.Protect != PAGE_NOACCESS)
         {
-            if ((mbi.Protect & DataProtection) && mbi.RegionSize >= vmt_size)
+            if (mbi.Protect & DataProtection && mbi.RegionSize >= vmt_size)
             {
-                return ((mbi.Protect & DataProtection) && mbi.RegionSize >= vmt_size) ? true : false;
+                return mbi.Protect & DataProtection && mbi.RegionSize >= vmt_size ? true : false;
             }
         }
         return false;
