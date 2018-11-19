91f4b04a293f277720ad29c79995c3c573b24f8c
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-19 15:53:38 +0100

    Move window struct to class declaration as private member

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -10,13 +10,6 @@
 
 void GUI::render()
 {
-    static struct {
-        bool glow{ false };
-        bool chams{ false };
-        bool triggerbot{ false };
-        bool misc{ false };
-    } window;
-
     if (ImGui::BeginMainMenuBar()) {
         if (ImGui::MenuItem(xorstr_("Glow"))) {
             window.glow = true;
--- Osiris/GUI.h
+++ Osiris/GUI.h
@@ -5,6 +5,13 @@ public:
     void render();
     void renderGlowWindow();
     bool isOpen{ false };
+private:
+    struct {
+        bool glow{ false };
+        bool chams{ false };
+        bool triggerbot{ false };
+        bool misc{ false };
+    } window;
 };
 
 extern GUI gui;
