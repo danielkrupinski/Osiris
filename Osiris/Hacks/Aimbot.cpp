0d7630347dfb824bcd2986e0a86ed7753bdbab51
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-24 14:51:45 +0100

    Add static int findTarget(UserCmd* cmd, Vector& position)

--- Osiris/Hacks/Aimbot.cpp
+++ Osiris/Hacks/Aimbot.cpp
@@ -1,4 +1,20 @@
+#include "../Interfaces.h"
+#include "../Memory.h"
+#include "../SDK/UserCmd.h"
+#include "../SDK/Vector.h"
+
 #include "Aimbot.h"
+
+static int findTarget(UserCmd* cmd, Vector& position)
+{
+    for (int i = 1; i < interfaces.engineClient->GetMaxClients(); i++) {
+        auto entity = interfaces.clientEntityList->getClientEntity(i);
+        if (!entity || !entity->isAlive() || entity == *memory.localPlayer
+            || entity->getTeamNumber() == (*memory.localPlayer)->getTeamNumber())
+            continue;
+        return i;
+    }
+}
 
 void Aimbot::run()
 {
