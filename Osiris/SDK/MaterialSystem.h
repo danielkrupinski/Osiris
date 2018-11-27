c9d47541b81e5c4a9cb0d3b2ab5726325e078318
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-27 20:30:41 +0100

    Change type of MaterialSystem::findMaterial() argument materialName to const std::string&

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -5,8 +5,8 @@
 
 class MaterialSystem {
 public:
-    Material* findMaterial(std::string_view materialName, const char* pTextureGroupName, bool complain = true, const char* pComplainPrefix = nullptr)
+    Material* findMaterial(const std::string& materialName, const char* pTextureGroupName, bool complain = true, const char* pComplainPrefix = nullptr)
     {
-        return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, materialName.data(), pTextureGroupName, complain, pComplainPrefix);
+        return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, materialName.c_str(), pTextureGroupName, complain, pComplainPrefix);
     }
 };
