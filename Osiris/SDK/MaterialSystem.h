a59f3b810d87bb00ea0e0d4d28c2bfb819a20183
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-27 20:14:17 +0100

    Update MaterialSystem::findMaterial() to take materialName as std::string_view

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -5,8 +5,8 @@
 
 class MaterialSystem {
 public:
-    Material* findMaterial(const char* pMaterialName, const char* pTextureGroupName, bool complain = true, const char* pComplainPrefix = nullptr)
+    Material* findMaterial(std::string_view materialName, const char* pTextureGroupName, bool complain = true, const char* pComplainPrefix = nullptr)
     {
-        return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, pMaterialName, pTextureGroupName, complain, pComplainPrefix);
+        return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, materialName.data(), pTextureGroupName, complain, pComplainPrefix);
     }
 };
