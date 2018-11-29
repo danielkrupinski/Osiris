8f5499e2899f04fed24e8697bdc4861a4c8f94ca
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-29 18:32:59 +0100

    Make MaterialSystem::findMaterial() materialName argument constant

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -5,7 +5,7 @@
 
 class MaterialSystem {
 public:
-    Material* findMaterial(std::string_view materialName)
+    Material* findMaterial(const std::string_view materialName)
     {
         return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, materialName.data(), nullptr, true, nullptr);
     }
