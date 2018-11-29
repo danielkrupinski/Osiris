7b0b68d7f38e29f270f2112e50943b1328ae495a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-11-29 18:36:14 +0100

    Make MaterialSystem::findMaterial() constexpr

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -5,7 +5,7 @@
 
 class MaterialSystem {
 public:
-    Material* findMaterial(const std::string_view materialName) noexcept
+    constexpr Material* findMaterial(const std::string_view materialName) noexcept
     {
         return callVirtualFunction<Material*(__thiscall*)(void*, const char*, const char*, bool, const char*)>(this, 84)(this, materialName.data(), nullptr, true, nullptr);
     }
