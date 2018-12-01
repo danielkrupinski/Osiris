14a2032a6a5aa51c928339ded90733313a6295cc
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-01 17:08:25 +0100

    Add Vector::operator=() overload

--- Osiris/SDK/Vector.h
+++ Osiris/SDK/Vector.h
@@ -1,11 +1,12 @@
 #pragma once
 
 struct Vector {
-    Vector(const float* array)
+    Vector& operator=(const float* array) noexcept
     {
         x = array[0];
         y = array[1];
         z = array[2];
+        return *this;
     }
     float x, y, z;
 };
