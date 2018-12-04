c4099c1415e3a1592fa1a7e434725aaf4b68085a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-04 18:40:02 +0100

    Remove old combo code from GUI::renderMiscWindow()

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -69,21 +69,21 @@ void GUI::renderGlowWindow() noexcept
         ImGui::SliderFloat("Thickness", &config.glow.thickness, 0.0f, 1.0f);
         ImGui::SliderFloat("Alpha", &config.glow.alpha, 0.0f, 1.0f);
         ImGui::SliderInt("Style", &config.glow.style, 0, 3);
-		ImGui::Checkbox("##Allies", &config.glow.allies);
-		ImGui::SameLine(0.0f, 5.0f);
-		ImGui::ColorEdit3("Allies", config.glow.alliesColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
+        ImGui::Checkbox("##Allies", &config.glow.allies);
+        ImGui::SameLine(0.0f, 5.0f);
+        ImGui::ColorEdit3("Allies", config.glow.alliesColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
         ImGui::NextColumn();
-		ImGui::Checkbox("##Weapons", &config.glow.weapons);
-		ImGui::SameLine(0.0f, 5.0f);
+        ImGui::Checkbox("##Weapons", &config.glow.weapons);
+        ImGui::SameLine(0.0f, 5.0f);
         ImGui::ColorEdit3("Weapons", config.glow.weaponsColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
-		ImGui::Checkbox("##C4", &config.glow.C4);
-		ImGui::SameLine(0.0f, 5.0f);
+        ImGui::Checkbox("##C4", &config.glow.C4);
+        ImGui::SameLine(0.0f, 5.0f);
         ImGui::ColorEdit3("C4", config.glow.C4Color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
-		ImGui::Checkbox("##PlantedC4", &config.glow.plantedC4);
-		ImGui::SameLine(0.0f, 5.0f);
+        ImGui::Checkbox("##PlantedC4", &config.glow.plantedC4);
+        ImGui::SameLine(0.0f, 5.0f);
         ImGui::ColorEdit3("Planted C4", config.glow.plantedC4Color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
-		ImGui::Checkbox("##Chickens", &config.glow.chickens);
-		ImGui::SameLine(0.0f, 5.0f);
+        ImGui::Checkbox("##Chickens", &config.glow.chickens);
+        ImGui::SameLine(0.0f, 5.0f);
         ImGui::ColorEdit3("Chickens", config.glow.chickensColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
         ImGui::Checkbox("##Enemies", &config.glow.enemies);
         ImGui::SameLine(0.0f, 5.0f);
@@ -142,19 +142,8 @@ void GUI::renderMiscWindow() noexcept
         ImGui::Checkbox("No smoke", &config.misc.noSmoke);
         if (!config.misc.noSmoke)
             ImGui::Checkbox("Wireframe smoke", &config.misc.wireframeSmoke);
-        static const std::string skyboxes[]{ "Default", "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam" };
-        //constexpr std::string& currentSkybox = config.misc.skybox;
+        // static const std::string skyboxes[]{ "Default", "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam" };
         ImGui::Combo("Skybox", &config.misc.skybox, "Default\0cs_baggage_skybox_\0cs_tibet\0embassy\0italy\0jungle\0nukeblank\0office\0sky_cs15_daylight01_hdr\0sky_cs15_daylight02_hdr\0sky_cs15_daylight03_hdr\0sky_cs15_daylight04_hdr\0sky_csgo_cloudy01\0sky_csgo_night_flat\0sky_csgo_night02\0sky_day02_05_hdr\0sky_day02_05\0sky_dust\0sky_l4d_rural02_ldr\0sky_venice\0vertigo_hdr\0vertigo\0vertigoblue_hdr\0vietnam");
-        //if (ImGui::BeginCombo("Skybox", currentSkybox.c_str())) {
-        //    for (const auto& a : skyboxes) {
-         //       bool isSelected = (currentSkybox == a);
-         //       if (ImGui::Selectable(a.c_str(), isSelected))
-         //           currentSkybox = a;
-         //       if (isSelected)
-         //           ImGui::SetItemDefaultFocus();
-         //   }
-         //   ImGui::EndCombo();
-        //}
         ImGui::SliderInt("Viewmodel FOV", &config.misc.viewmodelFov, 0, 130);
         ImGui::ColorEdit3("World color", config.misc.worldColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
         ImGui::End();
