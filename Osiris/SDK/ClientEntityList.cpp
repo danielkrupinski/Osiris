3a7cffd88d24c1c7cfea14d2adc3c774b1bcbf13
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-04 18:41:14 +0100

    Delete ClientEntityList.cpp

--- Osiris/SDK/ClientEntityList.cpp
+++
@@ -1,7 +0,0 @@
-#include "ClientEntityList.h"
-#include "Utils.h"
-
-BaseEntity* ClientEntityList::getClientEntity(int index)
-{
-    return callVirtualFunction<BaseEntity*(__thiscall*)(void*, int)>(this, 3)(this, index);
-}
