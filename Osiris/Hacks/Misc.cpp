e082661d190a049ac47992089de33425baa38d96
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-04 18:26:56 +0100

    Update Misc::skybox() method

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -40,8 +40,10 @@ void Misc::disablePostProcessing() noexcept
 
 void Misc::skybox() noexcept
 {
-    if (config.misc.skybox != "Default")
-        reinterpret_cast<void(__fastcall*)(const char*)>(memory.loadSky)(config.misc.skybox.c_str());
+    constexpr std::string_view skyboxes[]{ "Default", "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam" };
+
+    if (config.misc.skybox)
+        reinterpret_cast<void(__fastcall*)(const char*)>(memory.loadSky)(skyboxes[config.misc.skybox].data());
 }
 
 void Misc::clanTag() noexcept
