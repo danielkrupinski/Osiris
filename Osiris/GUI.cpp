3846a4db9193fb39bcae0e52e96de2af87fd5010
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-06 19:15:21 +0100

    Implement simplified one-line checkbox and color picker in GUI::checkBoxAndColorPicker()

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -17,7 +17,17 @@ void GUI::render() noexcept
 
 void GUI::checkBoxAndColorPicker(const std::string_view name)
 {
-
+	ImGui::Checkbox(std::string{ "##" }.append(name).c_str(), &config.glow.weapons);
+	ImGui::SameLine(0.0f, 5.0f);
+	bool openPopup = ImGui::ColorButton(std::string{ "##" }.append(name).append("ColorButton").c_str(), ImColor{ config.glow.weaponsColor[0], config.glow.weaponsColor[1], config.glow.weaponsColor[2] }, ImGuiColorEditFlags_NoPicker | ImGuiColorEditFlags_AlphaPreviewHalf);
+	ImGui::SameLine(0.0f, 5.0f);
+	ImGui::Text(name.data());
+	if (openPopup)
+		ImGui::OpenPopup(std::string{ "##" }.append(name).append("Popup").c_str());
+	if (ImGui::BeginPopup(std::string{ "##" }.append(name).append("Popup").c_str())) {
+		ImGui::ColorPicker3(std::string{ "##" }.append(name).append("Picker").c_str(), config.glow.weaponsColor, ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_NoSmallPreview);
+		ImGui::EndPopup();
+	}
 }
 
 void GUI::renderMenuBar() noexcept
