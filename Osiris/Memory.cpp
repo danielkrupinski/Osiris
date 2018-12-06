f7cda462fcd4173882be803d0bff18ec6454f4cd
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-06 22:12:17 +0100

    Remove legacy code

--- Osiris/Memory.cpp
+++ Osiris/Memory.cpp
@@ -8,19 +8,6 @@
 
 Memory::Memory() noexcept
 {
-    /*
-    present = findPattern("gameoverlayrenderer.dll", "FF 15 ? ? ? ? 8B F8 85 DB") + 2;
-    reset = findPattern("gameoverlayrenderer.dll", "C7 45 ? ? ? ? ? FF 15 ? ? ? ? 8B F8") + 9;
-    localPlayer = *reinterpret_cast<BaseEntity***>(findPattern("client_panorama.dll", "8B 0D ? ? ? ? 83 FF FF 74 07") + 2);
-    clientMode = **reinterpret_cast<ClientMode***>((*reinterpret_cast<std::uintptr_t**>(interfaces.client))[10] + 5);
-    input = *reinterpret_cast<Input**>(findPattern("client_panorama.dll", "B9 ? ? ? ? 8B 40 38 FF D0 84 C0 0F 85") + 1);
-    glowObjectManager = *reinterpret_cast<GlowObjectManager**>(findPattern("client_panorama.dll", "0F 11 05 ? ? ? ? 83 C8 01") + 3);
-    disablePostProcessing = *reinterpret_cast<bool**>(findPattern("client_panorama.dll", "80 3D ? ? ? ? ? 53 56 57 0F 85") + 2);
-    loadSky = findPattern("engine.dll", "55 8B EC 81 EC ? ? ? ? 56 57 8B F9 C7 45");
-    setClanTag = findPattern("engine.dll", "53 56 57 8B DA 8B F9 FF 15");
-    smokeCount = *reinterpret_cast<int**>(findPattern("client_panorama.dll", "8B 1D ? ? ? ? 56 33 F6 57 85 DB") + 2);
-    */
-
 	try {
 		present = findPattern("gameoverlayrenderer.dll", "\xFF\x15.{4}\x8B\xF8\x85\xDB") + 2;
 		reset = findPattern("gameoverlayrenderer.dll", "\xC7\x45.{5}\xFF\x15.{4}\x8B\xF8") + 9;
