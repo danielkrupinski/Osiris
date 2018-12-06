85e33a1a49347c06d8ca44f834ea7210ff282b6f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-06 20:06:44 +0100

    Make void GUI::checkBoxAndColorPicker(const std::string_view name, bool* enable, float* color) const

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -15,7 +15,7 @@ void GUI::render() noexcept
     renderMiscWindow();
 }
 
-void GUI::checkBoxAndColorPicker(const std::string_view name, bool* enable, float* color)
+void GUI::checkBoxAndColorPicker(const std::string_view name, bool* enable, float* color) const
 {
 	ImGui::Checkbox(std::string{ "##" }.append(name).c_str(), enable);
 	ImGui::SameLine(0.0f, 5.0f);
