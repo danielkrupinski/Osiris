593c4f964fa1f406e7ae4e0b090924d2d8cfd2b5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-16 17:21:01 +0100

    Delete HandleEntity.h

--- Osiris/SDK/HandleEntity.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class HandleEntity {
-public:
-    virtual ~HandleEntity() {}
-    virtual void SetRefEHandle(const long& handle) = 0;
-    virtual const long& GetRefEHandle() const = 0;
-};
