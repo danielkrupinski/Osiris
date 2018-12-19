14fcae4497f50a3e35b1b4c089d152b01d536353
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-19 18:51:32 +0100

    Update void Triggerbot::run(UserCmd* cmd) noexcept

--- Osiris/Hacks/Triggerbot.cpp
+++ Osiris/Hacks/Triggerbot.cpp
@@ -10,12 +10,12 @@ void Triggerbot::run(UserCmd* cmd) noexcept
 {
     if (config.triggerbot.enabled) {
         static auto lastTime = std::chrono::steady_clock::now();
-        if (GetAsyncKeyState(VK_MENU) || !config.triggerbot.onKey
+        if ((GetAsyncKeyState(VK_MENU) || !config.triggerbot.onKey)
             && std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - lastTime).count() >= config.triggerbot.shotDelay) {
             auto inCrosshair = (*memory.localPlayer)->getCrosshairID();
             if (inCrosshair > 0 && inCrosshair <= 64) {
                 auto target = interfaces.entityList->getClientEntity(inCrosshair);
-                if (/* !reinterpret_cast<ClientEntity*>(target)->IsDormant() && */target->isEnemy()) {
+                if (target->isEnemy()) {
                     constexpr int IN_ATTACK{ 1 };
                     cmd->buttons |= IN_ATTACK;
                 }
