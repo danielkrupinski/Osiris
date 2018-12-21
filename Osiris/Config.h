6796287b31e4b453fc1290b670e973319b9d9cae
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-21 20:32:35 +0100

    Add Config::chams.shader int

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -34,6 +34,7 @@ public:
     struct {
         bool enabled{ false };
         float alpha{ 1.0f };
+        int shader{ 0 };
         bool wireframe{ false };
         bool allies{ false };
         float alliesColor[3]{ 0.0f, 1.0f, 0.0f };
