479d2bf372977d3270bed51487875f6292ebb881
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-23 18:15:45 +0100

    Remove legacy code

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -16,32 +16,6 @@ void Misc::bunnyHop(UserCmd* cmd) noexcept
                 cmd->buttons &= ~IN_JUMP;
             }
         }
-        /*
-        constexpr int ON_GROUND{ 1 };
-        constexpr int IN_JUMP{ 2 };
-
-        static auto jumped = false;
-        static auto fake = false;
-        if (!jumped && fake) {
-            fake = false;
-            cmd->buttons |= IN_JUMP;
-        }
-        else if (cmd->buttons & IN_JUMP) {
-            if (*((*memory.localPlayer)->getFlags()) & ON_GROUND) {
-                jumped = true;
-                fake = true;
-            }
-            else {
-                cmd->buttons &= ~IN_JUMP;
-                jumped = false;
-            }
-        }
-        else {
-            jumped = false;
-            fake = false;
-        }
-    }
-    */
     }
 }
 
