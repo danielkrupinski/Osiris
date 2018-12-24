8a0920eb3a6b3e133509a2994251c142648799ca
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-24 19:31:03 +0100

    Create InputSystem::enableInput() method

--- Osiris/SDK/InputSystem.h
+++ Osiris/SDK/InputSystem.h
@@ -3,5 +3,8 @@
 #include "Utils.h"
 
 class InputSystem {
-    
+    void enableInput(bool enable)
+    {
+        callVirtualFunction<void(__thiscall*)(void*, bool)>(this, 11)(this, enable);
+    }
 };
