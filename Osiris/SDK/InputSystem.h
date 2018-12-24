0f73f78b0aa74eb1675bc64b978eb6b07b68559f
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-24 19:53:35 +0100

    Create InputSystem::resetInputState()

--- Osiris/SDK/InputSystem.h
+++ Osiris/SDK/InputSystem.h
@@ -3,8 +3,14 @@
 #include "Utils.h"
 
 class InputSystem {
+public:
     void enableInput(bool enable)
     {
         callVirtualFunction<void(__thiscall*)(void*, bool)>(this, 11)(this, enable);
+    }
+
+    void resetInputState()
+    {
+        callVirtualFunction<void(__thiscall*)(void*)>(this, 39)(this);
     }
 };
