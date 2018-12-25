3e505be39619d290d3416ebcf1aa4a597f722a23
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-25 13:59:49 +0100

    Enable imgui cursor drawing

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -48,6 +48,7 @@ static HRESULT __stdcall hookedPresent(IDirect3DDevice9* device, const RECT* src
         ImGuiIO& io = ImGui::GetIO();
         io.IniFilename = nullptr;
         io.LogFilename = nullptr;
+        io.MouseDrawCursor = true;
         char buffer[MAX_PATH];
         GetWindowsDirectoryA(buffer, MAX_PATH);
         io.Fonts->AddFontFromFileTTF(std::string{ buffer + std::string{ "\\Fonts\\Tahoma.ttf" } }.c_str(), 16.0f);
