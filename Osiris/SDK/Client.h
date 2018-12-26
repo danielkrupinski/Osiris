682959eed27abf02163c953b4bfbfe0a3fa0a4d3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-26 15:30:46 +0100

    Add Client::getAllClasses() method

--- Osiris/SDK/Client.h
+++ Osiris/SDK/Client.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include "ClientClass.h"
+#include "Utils.h"
+
+class Client final {
+public:
+    ClientClass* getAllClasses()
+    {
+        return callVirtualFunction<ClientClass*(__thiscall*)()>(this, 8)();
+    }
+};
