1b679069ac5182b7c7ffad750a1bfc497f9b273a
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-26 13:28:36 +0100

    Add Client::activateMouse() method

--- Osiris/SDK/Client.h
+++ Osiris/SDK/Client.h
@@ -1,5 +1,11 @@
 #pragma once
 
-class Client final {
+#include "Utils.h"
 
+class Client final {
+public:
+    void activateMouse()
+    {
+        callVirtualFunction<void(__thiscall*)()>(this, 14)();
+    }
 };
