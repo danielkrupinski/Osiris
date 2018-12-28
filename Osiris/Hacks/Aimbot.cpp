68a6f1b7381bd4a9601eedd3cfbd0d5897958582
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-28 20:58:06 +0100

    Add static Vector calculateAngleBetween(Vector source, Vector destination) function to Aimbot.cpp

--- Osiris/Hacks/Aimbot.cpp
+++ Osiris/Hacks/Aimbot.cpp
@@ -1,15 +1,30 @@
+#include "Aimbot.h"
 #include "../Interfaces.h"
 #include "../Memory.h"
 #include "../SDK/UserCmd.h"
 #include "../SDK/Vector.h"
 
-#include "Aimbot.h"
+static Vector calculateAngleBetween(Vector source, Vector destination)
+{
+    Vector delta = source - destination;
+    float hyp = delta.length2D();
+
+    Vector angles;
+    angles.y = std::atanf(delta.y / delta.x) * 57.2957795131f;
+    angles.x = std::atanf(-delta.z / hyp) * -57.2957795131f;
+    angles.z = 0.0f;
+
+    if (delta.x >= 0.0f)
+        angles.y += 180.0f;
+
+    return angles;
+}
 
 static int findTarget(UserCmd* cmd, Vector& position)
 {
     for (int i = 1; i < interfaces.engine->getMaxClients(); i++) {
         auto entity = interfaces.entityList->getClientEntity(i);
-        if (!entity || !entity->isDormant() || !entity->isAlive() || !entity->isEnemy())
+        if (!entity || entity->isDormant() || !entity->isAlive() || !entity->isEnemy())
             continue;
     }
 }
