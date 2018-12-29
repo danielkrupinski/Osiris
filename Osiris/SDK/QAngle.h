41c04e0873316ed8260bd429d3e136bb820494a5
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-29 14:35:18 +0100

    Add operator- overload to Qangle class

--- Osiris/SDK/QAngle.h
+++ Osiris/SDK/QAngle.h
@@ -1,6 +1,14 @@
 #pragma once
 
 struct QAngle final {
+    constexpr QAngle& operator-(const QAngle& v) noexcept
+    {
+        pitch -= v.pitch;
+        yaw -= v.yaw;
+        roll -= v.roll;
+        return *this;
+    }
+
     float pitch;
     float yaw;
     float roll;
