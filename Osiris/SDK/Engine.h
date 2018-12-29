7870baee9c0f36b701396b2b7b83e4db67122640
Daniel Krupiński <danielkrupinski@outlook.com>
2018-12-29 15:40:01 +0100

    Add Engine::setViewAngles() method

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "QAngle.h"
 #include "Utils.h"
 
 class Engine {
 public:
+    void setViewAngles(const QAngle& angles)
+    {
+        callVirtualFunction<void(__thiscall*)(void*, const QAngle&)>(this, 19)(this, angles);
+    }
+
 	int getMaxClients()
 	{
 		return callVirtualFunction<int(__thiscall*)(void*)>(this, 20)(this);
