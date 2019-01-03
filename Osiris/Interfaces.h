8d52043d29b04bae1bdf546ee820cf5408d95e53
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-03 19:34:26 +0100

    Change std::stringstream to std::ostringstream

--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -35,7 +35,7 @@ private:
         const auto createInterface = reinterpret_cast<std::add_pointer_t<T*(const char* name, int* returnCode)>>(GetProcAddress(GetModuleHandleA(module.c_str()), "CreateInterface"));
 
         for (int i = 1; i < 100; i++)
-            if (T* foundInterface = createInterface((std::stringstream{ } << name << std::setfill('0') << std::setw(3) << i).str().c_str(), nullptr))
+            if (T* foundInterface = createInterface((std::ostringstream{ } << name << std::setfill('0') << std::setw(3) << i).str().c_str(), nullptr))
                 return foundInterface;
 
         throw std::runtime_error("Could not find " + name + " in " + module + "!");
