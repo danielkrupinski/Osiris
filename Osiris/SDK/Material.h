020fbdd58bceec76525e238d38a9589ce4c61d5f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-04 20:10:28 +0100

    Remove unused methods from Material class

--- Osiris/SDK/Material.h
+++ Osiris/SDK/Material.h
@@ -42,23 +42,8 @@ public:
         callVirtualFunction<void(__thiscall*)(void*)>(this, 12)(this);
     }
 
-    void alphaModulate(float alpha)
-    {
-        callVirtualFunction<void(__thiscall*)(void*, float)>(this, 27)(this, alpha);
-    }
-
-    void colorModulate(float r, float g, float b)
-    {
-        callVirtualFunction<void(__thiscall*)(void*, float, float, float)>(this, 28)(this, r, g, b);
-    }
-
     void setMaterialVarFlag(MaterialVar flag, bool isOn)
     {
         callVirtualFunction<void(__thiscall*)(void*, MaterialVar, bool)>(this, 29)(this, flag, isOn);
-    }
-
-    void setShader(const char* shaderName)
-    {
-        callVirtualFunction<void(__thiscall*)(void*, const char*)>(this, 34)(this, shaderName);
     }
 };
