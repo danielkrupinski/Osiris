82e8cd0b4cd8d1245cf78044f6b6166ed7c0fa09
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-05 13:52:09 +0100

    Make Engine class methods noexcept

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -5,17 +5,17 @@
 
 class Engine {
 public:
-    void setViewAngles(const QAngle& angles)
+    auto setViewAngles(const QAngle& angles) noexcept
     {
         callVirtualFunction<void(__thiscall*)(void*, const QAngle&)>(this, 19)(this, angles);
     }
 
-	int getMaxClients()
+	auto getMaxClients() noexcept
 	{
 		return callVirtualFunction<int(__thiscall*)(void*)>(this, 20)(this);
 	}
 
-	bool isInGame()
+	auto isInGame() noexcept
 	{
 		return callVirtualFunction<bool(__thiscall*)(void*)>(this, 26)(this);
 	}
