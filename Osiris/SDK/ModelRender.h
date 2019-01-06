cde390e11f55299a853f2d3a9726e002de15569a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-06 12:09:41 +0100

    Add Model and ModelRenderInfo structs

--- Osiris/SDK/ModelRender.h
+++ Osiris/SDK/ModelRender.h
@@ -3,6 +3,38 @@
 #include "Material.h"
 #include "Utils.h"
 
+using matrix3x4 = float[3][4];
+
+struct Model {
+    void* handle;
+    char name[260];
+    int loadFlags;
+    int serverCount;
+    int type;
+    int flags;
+    Vector  vecMins;
+    Vector  vecMaxs;
+    float   radius;
+};
+
+struct ModelRenderInfo
+{
+    Vector origin;
+    QAngle angles;
+    std::byte pad[4];
+    void* renderable;
+    const Model* model;
+    const matrix3x4* modelToWorld;
+    const matrix3x4* lightingOffset;
+    const Vector* lightingOrigin;
+    int flags;
+    int entityIndex;
+    int skin;
+    int body;
+    int hitboxset;
+    // ModelInstanceHandle_t   instance;
+};
+
 class ModelRender {
 public:
     constexpr void forceMaterialOverride(Material* newMaterial) noexcept
