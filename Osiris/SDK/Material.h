9d4a69e625a5b3620d88753b7c333cc495c816ae
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-06 14:32:11 +0100

    Bring back alphaModulate() and colorModulate() methods of Material class

--- Osiris/SDK/Material.h
+++ Osiris/SDK/Material.h
@@ -15,6 +15,16 @@ public:
         callVirtualFunction<void(__thiscall*)(void*)>(this, 12)(this);
     }
 
+    void alphaModulate(float alpha)
+    {
+        callVirtualFunction<void(__thiscall*)(void*, float)>(this, 27)(this, alpha);
+    }
+
+    void colorModulate(float r, float g, float b)
+    {
+        callVirtualFunction<void(__thiscall*)(void*, float, float, float)>(this, 28)(this, r, g, b);
+    }
+
     void setMaterialVarFlag(MaterialVar flag, bool isOn)
     {
         callVirtualFunction<void(__thiscall*)(void*, MaterialVar, bool)>(this, 29)(this, flag, isOn);
