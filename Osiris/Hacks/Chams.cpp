8096aad8275ef85540fc6b176f5ea7040e3b1e7b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-07 19:18:14 +0100

    Remove old Chams::render() function

--- Osiris/Hacks/Chams.cpp
+++ Osiris/Hacks/Chams.cpp
@@ -6,62 +6,6 @@
 #include "../Interfaces.h"
 #include "../Memory.h"
 
-[[deprecated]] void Chams::render() noexcept
-{
-    if (config.chams.enabled) {
-        static auto vertex = interfaces.materialSystem->findMaterial("dev/glow_rim3d");
-        static auto unlit = interfaces.materialSystem->findMaterial("debug/debugdrawflat");
-        static bool isInitialized = false;
-
-        if (!isInitialized) {
-            vertex->incrementReferenceCount();
-            unlit->incrementReferenceCount();
-            isInitialized = true;
-        }
-
-        auto material = config.chams.flat ? unlit : vertex;
-
-        material->setMaterialVarFlag(MaterialVar::WIREFRAME, config.chams.wireframe);
-        interfaces.renderView->setBlend(config.chams.alpha);
-
-        for (int i = 1; i < interfaces.engine->getMaxClients(); ++i) {
-            auto entity = interfaces.entityList->getClientEntity(i);
-
-            if (entity && !entity->isDormant() && entity->isAlive()) {
-                if (entity->isEnemy()) {
-                    if (config.chams.enemies) {
-                        interfaces.renderView->setColorModulation(config.chams.enemiesColor);
-                        material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
-                        interfaces.modelRender->forceMaterialOverride(material);
-                        entity->drawModel(1, 255);
-                    }
-                    if (config.chams.visibleEnemies) {
-                        interfaces.renderView->setColorModulation(config.chams.visibleEnemiesColor);
-                        material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
-                        interfaces.modelRender->forceMaterialOverride(material);
-                        entity->drawModel(1, 255);
-                    }
-                }
-                else {
-                    if (config.chams.allies) {
-                        interfaces.renderView->setColorModulation(config.chams.alliesColor);
-                        material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
-                        interfaces.modelRender->forceMaterialOverride(material);
-                        entity->drawModel(1, 255);
-                    }
-                    if (config.chams.visibleAllies) {
-                        interfaces.renderView->setColorModulation(config.chams.visibleAlliesColor);
-                        material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
-                        interfaces.modelRender->forceMaterialOverride(material);
-                        entity->drawModel(1, 255);
-                    }
-                }
-            }
-        }
-        interfaces.modelRender->forceMaterialOverride(nullptr);
-    }
-}
-
 void Chams::renderDME(void* ctx, void* state, const ModelRenderInfo& info, matrix3x4* customBoneToWorld)
 {
     if (config.chams.enabled) {
