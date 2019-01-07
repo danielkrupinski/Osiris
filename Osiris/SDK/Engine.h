c969dadb085f00af7fbed0a367e715970ec8f9d8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-07 16:52:06 +0100

    Add getViewAngles() method to Engine class

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -5,6 +5,11 @@
 
 class Engine {
 public:
+    auto getViewAngles(QAngle& angles) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, QAngle&)>(this, 18)(this, angles);
+    }
+
     auto setViewAngles(const QAngle& angles) noexcept
     {
         callVirtualFunction<void(__thiscall*)(void*, const QAngle&)>(this, 19)(this, angles);
