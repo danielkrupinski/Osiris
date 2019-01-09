95e699658f80f31c7a0dd12b52d04a68d64971b8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-09 21:24:13 +0100

    Make Chams::renderPlayers() noexcept

--- Osiris/Hacks/Chams.cpp
+++ Osiris/Hacks/Chams.cpp
@@ -37,45 +37,43 @@ void Chams::render(void* ctx, void* state, const ModelRenderInfo& info, matrix3x
     }
 }
 
-void Chams::renderPlayers(void* ctx, void* state, const ModelRenderInfo& info, matrix3x4* customBoneToWorld)
+void Chams::renderPlayers(void* ctx, void* state, const ModelRenderInfo& info, matrix3x4* customBoneToWorld) noexcept
 {
-    if (config.chams.enabled) {
-        auto entity = interfaces.entityList->getClientEntity(info.entityIndex);
+    auto entity = interfaces.entityList->getClientEntity(info.entityIndex);
 
-        if (entity && !entity->isDormant() && entity->isAlive()) {
-            auto material = config.chams.flat ? flat : normal;
-            material->alphaModulate(config.chams.alpha);
-            material->setMaterialVarFlag(MaterialVar::WIREFRAME, config.chams.wireframe);
+    if (entity && !entity->isDormant() && entity->isAlive()) {
+        auto material = config.chams.flat ? flat : normal;
+        material->alphaModulate(config.chams.alpha);
+        material->setMaterialVarFlag(MaterialVar::WIREFRAME, config.chams.wireframe);
 
-            if (entity->isEnemy()) {
-                if (config.chams.occludedEnemies) {
-                    material->colorModulate(config.chams.occludedEnemiesColor);
-                    material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
-                    interfaces.modelRender->forceMaterialOverride(material);
-                    hooks.modelRender.getOriginal<void(__thiscall*)(ModelRender*, void*, void*, const ModelRenderInfo&, matrix3x4*)>(21)(interfaces.modelRender, ctx, state, info, customBoneToWorld);
-                    if (!config.chams.visibleEnemies)
-                        interfaces.modelRender->forceMaterialOverride(nullptr);
-                }
-                if (config.chams.visibleEnemies) {
-                    material->colorModulate(config.chams.visibleEnemiesColor);
-                    material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
-                    interfaces.modelRender->forceMaterialOverride(material);
-                }
+        if (entity->isEnemy()) {
+            if (config.chams.occludedEnemies) {
+                material->colorModulate(config.chams.occludedEnemiesColor);
+                material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
+                interfaces.modelRender->forceMaterialOverride(material);
+                hooks.modelRender.getOriginal<void(__thiscall*)(ModelRender*, void*, void*, const ModelRenderInfo&, matrix3x4*)>(21)(interfaces.modelRender, ctx, state, info, customBoneToWorld);
+                if (!config.chams.visibleEnemies)
+                    interfaces.modelRender->forceMaterialOverride(nullptr);
             }
-            else {
-                if (config.chams.occludedAllies) {
-                    material->colorModulate(config.chams.occludedAlliesColor);
-                    material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
-                    interfaces.modelRender->forceMaterialOverride(material);
-                    hooks.modelRender.getOriginal<void(__thiscall*)(ModelRender*, void*, void*, const ModelRenderInfo&, matrix3x4*)>(21)(interfaces.modelRender, ctx, state, info, customBoneToWorld);
-                    if (!config.chams.visibleAllies)
-                        interfaces.modelRender->forceMaterialOverride(nullptr);
-                }
-                if (config.chams.visibleAllies) {
-                    material->colorModulate(config.chams.visibleAlliesColor);
-                    material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
-                    interfaces.modelRender->forceMaterialOverride(material);
-                }
+            if (config.chams.visibleEnemies) {
+                material->colorModulate(config.chams.visibleEnemiesColor);
+                material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
+                interfaces.modelRender->forceMaterialOverride(material);
+            }
+        }
+        else {
+            if (config.chams.occludedAllies) {
+                material->colorModulate(config.chams.occludedAlliesColor);
+                material->setMaterialVarFlag(MaterialVar::IGNOREZ, true);
+                interfaces.modelRender->forceMaterialOverride(material);
+                hooks.modelRender.getOriginal<void(__thiscall*)(ModelRender*, void*, void*, const ModelRenderInfo&, matrix3x4*)>(21)(interfaces.modelRender, ctx, state, info, customBoneToWorld);
+                if (!config.chams.visibleAllies)
+                    interfaces.modelRender->forceMaterialOverride(nullptr);
+            }
+            if (config.chams.visibleAllies) {
+                material->colorModulate(config.chams.visibleAlliesColor);
+                material->setMaterialVarFlag(MaterialVar::IGNOREZ, false);
+                interfaces.modelRender->forceMaterialOverride(material);
             }
         }
     }
--- Osiris/Hacks/Chams.h
+++ Osiris/Hacks/Chams.h
@@ -6,7 +6,7 @@ class Chams {
 public:
     Chams() noexcept;
     void render(void*, void*, const ModelRenderInfo&, matrix3x4*) noexcept;
-    void renderPlayers(void*, void*, const ModelRenderInfo&, matrix3x4*);
+    void renderPlayers(void*, void*, const ModelRenderInfo&, matrix3x4*) noexcept;
     void renderWeapons(void*, void*, const ModelRenderInfo&, matrix3x4*) noexcept;
 private:
     Material* normal;
