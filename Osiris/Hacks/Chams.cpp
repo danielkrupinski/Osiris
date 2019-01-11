93df28a6fed000025885fd69ae2f323fae2dff84
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-11 18:03:36 +0100

    Add arms to ignored models

--- Osiris/Hacks/Chams.cpp
+++ Osiris/Hacks/Chams.cpp
@@ -26,6 +26,7 @@ void Chams::render(void* ctx, void* state, const ModelRenderInfo& info, matrix3x
         if (modelName.find("models/player") != std::string_view::npos)
             renderPlayers(ctx, state, info, customBoneToWorld);
         else if ((modelName.find("models/weapons/v_") != std::string_view::npos)
+            && (modelName.find("arms") == std::string_view::npos)
             && (modelName.find("tablet") == std::string_view::npos)
             && (modelName.find("parachute") == std::string_view::npos))
             renderWeapons(info.entityIndex);
