0d3ecea31726b78a5e175f7174cc2e29ea4dc361
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-13 15:16:31 +0100

    Update Visuals::removeSleeves()

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -1,8 +1,9 @@
+#include "../Config.h"
 #include "Visuals.h"
 
 bool Visuals::removeSleeves(const std::string_view modelName) noexcept
 {
-    return modelName.find("sleeve") != std::string_view::npos;
+    return config.visuals.noSleeves && (modelName.find("sleeve") != std::string_view::npos);
 }
 
 void Visuals::thirdperson()
