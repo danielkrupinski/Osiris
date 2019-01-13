8387f73f846ad87c6312a2bd6b13782a8abc1ccc
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-13 15:08:57 +0100

    Add void Visuals::removeSleeves()

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -1,5 +1,10 @@
 #include "Visuals.h"
 
+void Visuals::removeSleeves(const std::string_view modelName) noexcept
+{
+
+}
+
 void Visuals::thirdperson()
 {
 
--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string_view>
+
 namespace Visuals {
+    void removeSleeves(const std::string_view) noexcept;
     void thirdperson();
 };
