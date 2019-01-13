bcc7e8616d15983ba11c98532f7f0d6d1b172e4f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-13 15:54:25 +0100

    Add bool Visuals::removeHands() function

--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -3,6 +3,7 @@
 #include <string_view>
 
 namespace Visuals {
+    bool removeHands(const std::string_view) noexcept;
     bool removeSleeves(const std::string_view) noexcept;
     void thirdperson();
 };
