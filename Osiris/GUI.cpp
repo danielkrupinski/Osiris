96e0a6b4d010775a36ed59b776ba62d5a0c4ba55
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-13 16:32:32 +0100

    Add "No hands" checkbox to "Visuals" window

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -145,6 +145,7 @@ void GUI::renderVisualsWindow() noexcept
     if (window.visuals) {
         ImGui::SetNextWindowSize(ImVec2(200.0f, 200.0f));
         ImGui::Begin("Visuals", &window.visuals, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+        ImGui::Checkbox("No hands", &config.visuals.noHands);
         ImGui::Checkbox("No sleeves", &config.visuals.noSleeves);
         ImGui::End();
     }
