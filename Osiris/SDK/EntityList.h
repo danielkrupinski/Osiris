2cdad1c75dcaa0ab45799c450e191c528bc70317
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-14 19:12:57 +0100

    Add EntityList::getClientEntityFromHandle(int handle) method

--- Osiris/SDK/EntityList.h
+++ Osiris/SDK/EntityList.h
@@ -9,6 +9,11 @@ public:
         return callVirtualFunction<class Entity*(__thiscall*)(void*, int)>(this, 3)(this, index);
     }
 
+    constexpr class Entity* getClientEntityFromHandle(int handle) noexcept
+    {
+        return callVirtualFunction<class Entity*(__thiscall*)(void*, int)>(this, 4)(this, handle);
+    }
+
     constexpr int getHighestEntityIndex() noexcept
     {
         return callVirtualFunction<int(__thiscall*)(void*)>(this, 6)(this);
