9578b69d9dcd75a217332f73b64681dd1aa4bb3a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-14 19:12:16 +0100

    Add more classes to enum class ClassId

--- Osiris/SDK/ClassId.h
+++ Osiris/SDK/ClassId.h
@@ -4,5 +4,12 @@ enum class ClassId {
     C4 = 32,
     Chicken = 34,
     CSPlayer = 38,
-    PlantedC4 = 126
+    Deagle = 44,
+    PlantedC4 = 126,
+    Elite = 235,
+    FiveSeven = 237,
+    Glock = 241,
+    P2000 = 242,
+    P250 = 254,
+    Tec9 = 264
 };
