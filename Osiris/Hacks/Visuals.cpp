3ffc47531744ee7e75915376b32c3b6d15286b6d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-18 19:26:13 +0100

    Move Visuals::removeHands() and Visuals::removeSleeves() to header to allow constexpr use on these functions

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -2,16 +2,6 @@
 #include "../Memory.h"
 #include "Visuals.h"
 
-bool Visuals::removeHands(const std::string_view modelName) noexcept
-{
-    return config.visuals.noHands && (modelName.find("arms") != std::string_view::npos);
-}
-
-bool Visuals::removeSleeves(const std::string_view modelName) noexcept
-{
-    return config.visuals.noSleeves && (modelName.find("sleeve") != std::string_view::npos);
-}
-
 void Visuals::thirdperson()
 {
     static bool isInThirdperson{ true };
--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -3,7 +3,14 @@
 #include <string_view>
 
 namespace Visuals {
-    bool removeHands(const std::string_view) noexcept;
-    bool removeSleeves(const std::string_view) noexcept;
+    constexpr bool removeHands(const std::string_view modelName) noexcept
+    {
+        return config.visuals.noHands && (modelName.find("arms") != std::string_view::npos);
+    }
+
+    constexpr bool removeSleeves(const std::string_view modelName) noexcept
+    {
+        return config.visuals.noSleeves && (modelName.find("sleeve") != std::string_view::npos);
+    }
     void thirdperson();
 };
