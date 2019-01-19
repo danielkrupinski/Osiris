1a384c51b978afd43b060747e0893042ed00d3db
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-19 14:55:28 +0100

    Implement configuration load and save using cereal binary serialization

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <ShlObj.h>
 
+#include "cereal/archives/binary.hpp"
 #include "json/json.h"
 
 #include "Config.h"
@@ -20,187 +21,24 @@ void Config::load()
 {
     std::ifstream in{ path };
 
-    if (!in.is_open())
+    if (!in.good())
         return;
 
-    Json::Value config;
-    in >> config;
+    cereal::BinaryInputArchive inputArchive{ in };
+    inputArchive(aimbot, triggerbot, glow, chams, visuals, misc);
     in.close();
-
-    aimbot.enabled = config["aimbot"]["enabled"].asBool();
-    aimbot.silent = config["aimbot"]["silent"].asBool();
-    aimbot.fov = config["aimbot"]["fov"].asFloat();
-    aimbot.smooth = config["aimbot"]["smooth"].asFloat();
-
-    triggerbot.enabled = config["triggerbot"]["enabled"].asBool();
-    triggerbot.onKey = config["triggerbot"]["onKey"].asBool();
-    triggerbot.key = config["triggerbot"]["key"].asInt();
-    triggerbot.shotDelay = config["triggerbot"]["shotDelay"].asInt();
-
-    glow.enabled = config["glow"]["enabled"].asBool();
-    glow.thickness = config["glow"]["thickness"].asFloat();
-    glow.alpha = config["glow"]["alpha"].asFloat();
-    glow.style = config["glow"]["style"].asInt();
-
-    glow.allies = config["glow"]["allies"].asBool();
-    glow.alliesColor[0] = config["glow"]["alliesColor"][0].asFloat();
-    glow.alliesColor[1] = config["glow"]["alliesColor"][1].asFloat();
-    glow.alliesColor[2] = config["glow"]["alliesColor"][2].asFloat();
-
-    glow.enemies = config["glow"]["enemies"].asBool();
-    glow.enemiesColor[0] = config["glow"]["enemiesColor"][0].asFloat();
-    glow.enemiesColor[1] = config["glow"]["enemiesColor"][1].asFloat();
-    glow.enemiesColor[2] = config["glow"]["enemiesColor"][2].asFloat();
-
-    glow.weapons = config["glow"]["weapons"].asBool();
-    glow.weaponsColor[0] = config["glow"]["weaponsColor"][0].asFloat();
-    glow.weaponsColor[1] = config["glow"]["weaponsColor"][1].asFloat();
-    glow.weaponsColor[2] = config["glow"]["weaponsColor"][2].asFloat();
-
-    glow.C4 = config["glow"]["C4"].asBool();
-    glow.C4Color[0] = config["glow"]["C4Color"][0].asFloat();
-    glow.C4Color[1] = config["glow"]["C4Color"][1].asFloat();
-    glow.C4Color[2] = config["glow"]["C4Color"][2].asFloat();
-
-    glow.plantedC4 = config["glow"]["plantedC4"].asBool();
-    glow.plantedC4Color[0] = config["glow"]["plantedC4Color"][0].asFloat();
-    glow.plantedC4Color[1] = config["glow"]["plantedC4Color"][1].asFloat();
-    glow.plantedC4Color[2] = config["glow"]["plantedC4Color"][2].asFloat();
-
-    glow.chickens = config["glow"]["chickens"].asBool();
-    glow.chickensColor[0] = config["glow"]["chickensColor"][0].asFloat();
-    glow.chickensColor[1] = config["glow"]["chickensColor"][1].asFloat();
-    glow.chickensColor[2] = config["glow"]["chickensColor"][2].asFloat();
-
-    chams.enabled = config["chams"]["enabled"].asBool();
-    chams.alpha = config["chams"]["alpha"].asFloat();
-    chams.flat = config["chams"]["flat"].asBool();
-    chams.wireframe = config["chams"]["wireframe"].asBool();
-
-    chams.visibleAllies = config["chams"]["visibleAllies"].asBool();
-    chams.visibleAlliesColor[0] = config["chams"]["visibleAlliesColor"][0].asFloat();
-    chams.visibleAlliesColor[1] = config["chams"]["visibleAlliesColor"][1].asFloat();
-    chams.visibleAlliesColor[2] = config["chams"]["visibleAlliesColor"][2].asFloat();
-
-    chams.occludedAllies = config["chams"]["occludedAllies"].asBool();
-    chams.occludedAlliesColor[0] = config["chams"]["occludedAlliesColor"][0].asFloat();
-    chams.occludedAlliesColor[1] = config["chams"]["occludedAlliesColor"][1].asFloat();
-    chams.occludedAlliesColor[2] = config["chams"]["occludedAlliesColor"][2].asFloat();
-
-    chams.occludedEnemies = config["chams"]["occludedEnemies"].asBool();
-    chams.occludedEnemiesColor[0] = config["chams"]["occludedEnemiesColor"][0].asFloat();
-    chams.occludedEnemiesColor[1] = config["chams"]["occludedEnemiesColor"][1].asFloat();
-    chams.occludedEnemiesColor[2] = config["chams"]["occludedEnemiesColor"][2].asFloat();
-
-    chams.visibleEnemies = config["chams"]["visibleEnemies"].asBool();
-    chams.visibleEnemiesColor[0] = config["chams"]["visibleEnemiesColor"][0].asFloat();
-    chams.visibleEnemiesColor[1] = config["chams"]["visibleEnemiesColor"][1].asFloat();
-    chams.visibleEnemiesColor[2] = config["chams"]["visibleEnemiesColor"][2].asFloat();
-
-    misc.bunnyHop = config["misc"]["bunnyHop"].asBool();
-    misc.disablePostProcessing = config["misc"]["disablePostProcessing"].asBool();
-    misc.flashReduction = config["misc"]["flashReduction"].asInt();
-    misc.inverseRagdollGravity = config["misc"]["inverseRagdollGravity"].asBool();
-    misc.fastDuck = config["misc"]["fastDuck"].asBool();
-    misc.noSmoke = config["misc"]["noSmoke"].asBool();
-    misc.wireframeSmoke = config["misc"]["wireframeSmoke"].asBool();
-    misc.skybox = config["misc"]["skybox"].asInt();
-    misc.viewmodelFov = config["misc"]["viewmodelFov"].asInt();
-    misc.worldColor[0] = config["misc"]["worldColor"][0].asFloat();
-    misc.worldColor[1] = config["misc"]["worldColor"][1].asFloat();
-    misc.worldColor[2] = config["misc"]["worldColor"][2].asFloat();
-    misc.autoPistol = config["misc"]["autoPistol"].asBool();
 }
 
 void Config::save() const
 {
-    Json::Value config;
+    std::ofstream out{ path };
 
-    config["aimbot"]["enabled"] = aimbot.enabled;
-    config["aimbot"]["silent"] = aimbot.silent;
-    config["aimbot"]["fov"] = aimbot.fov;
-    config["aimbot"]["smooth"] = aimbot.smooth;
+    if (!out.good())
+        return;
 
-    config["triggerbot"]["enabled"] = triggerbot.enabled;
-    config["triggerbot"]["onKey"] = triggerbot.onKey;
-    config["triggerbot"]["key"] = triggerbot.key;
-    config["triggerbot"]["shotDelay"] = triggerbot.shotDelay;
-
-    config["glow"]["enabled"] = glow.enabled;
-    config["glow"]["thickness"] = glow.thickness;
-    config["glow"]["alpha"] = glow.alpha;
-    config["glow"]["style"] = glow.style;
-
-    config["glow"]["allies"] = glow.allies;
-    config["glow"]["alliesColor"][0] = glow.alliesColor[0];
-    config["glow"]["alliesColor"][1] = glow.alliesColor[1];
-    config["glow"]["alliesColor"][2] = glow.alliesColor[2];
-
-    config["glow"]["enemies"] = glow.enemies;
-    config["glow"]["enemiesColor"][0] = glow.enemiesColor[0];
-    config["glow"]["enemiesColor"][1] = glow.enemiesColor[1];
-    config["glow"]["enemiesColor"][2] = glow.enemiesColor[2];
-
-    config["glow"]["weapons"] = glow.weapons;
-    config["glow"]["weaponsColor"][0] = glow.weaponsColor[0];
-    config["glow"]["weaponsColor"][1] = glow.weaponsColor[1];
-    config["glow"]["weaponsColor"][2] = glow.weaponsColor[2];
-
-    config["glow"]["C4"] = glow.C4;
-    config["glow"]["C4Color"][0] = glow.C4Color[0];
-    config["glow"]["C4Color"][1] = glow.C4Color[1];
-    config["glow"]["C4Color"][2] = glow.C4Color[2];
-
-    config["glow"]["plantedC4"] = glow.plantedC4;
-    config["glow"]["plantedC4Color"][0] = glow.plantedC4Color[0];
-    config["glow"]["plantedC4Color"][1] = glow.plantedC4Color[1];
-    config["glow"]["plantedC4Color"][2] = glow.plantedC4Color[2];
-
-    config["glow"]["chickens"] = glow.chickens;
-    config["glow"]["chickensColor"][0] = glow.chickensColor[0];
-    config["glow"]["chickensColor"][1] = glow.chickensColor[1];
-    config["glow"]["chickensColor"][2] = glow.chickensColor[2];
-
-    config["chams"]["enabled"] = chams.enabled;
-    config["chams"]["alpha"] = chams.alpha;
-    config["chams"]["flat"] = chams.flat;
-    config["chams"]["wireframe"] = chams.wireframe;
-
-    config["chams"]["visibleAllies"] = chams.visibleAllies;
-    config["chams"]["visibleAlliesColor"][0] = chams.visibleAlliesColor[0];
-    config["chams"]["visibleAlliesColor"][1] = chams.visibleAlliesColor[1];
-    config["chams"]["visibleAlliesColor"][2] = chams.visibleAlliesColor[2];
-
-    config["chams"]["occludedAllies"] = chams.occludedAllies;
-    config["chams"]["occludedAlliesColor"][0] = chams.occludedAlliesColor[0];
-    config["chams"]["occludedAlliesColor"][1] = chams.occludedAlliesColor[1];
-    config["chams"]["occludedAlliesColor"][2] = chams.occludedAlliesColor[2];
-
-    config["chams"]["visibleEnemies"] = chams.visibleEnemies;
-    config["chams"]["visibleEnemiesColor"][0] = chams.visibleEnemiesColor[0];
-    config["chams"]["visibleEnemiesColor"][1] = chams.visibleEnemiesColor[1];
-    config["chams"]["visibleEnemiesColor"][2] = chams.visibleEnemiesColor[2];
-
-    config["chams"]["occludedEnemies"] = chams.occludedEnemies;
-    config["chams"]["occludedEnemiesColor"][0] = chams.occludedEnemiesColor[0];
-    config["chams"]["occludedEnemiesColor"][1] = chams.occludedEnemiesColor[1];
-    config["chams"]["occludedEnemiesColor"][2] = chams.occludedEnemiesColor[2];
-
-    config["misc"]["bunnyHop"] = misc.bunnyHop;
-    config["misc"]["disablePostProcessing"] = misc.disablePostProcessing;
-    config["misc"]["flashReduction"] = misc.flashReduction;
-    config["misc"]["inverseRagdollGravity"] = misc.inverseRagdollGravity;
-    config["misc"]["fastDuck"] = misc.fastDuck;
-    config["misc"]["noSmoke"] = misc.noSmoke;
-    config["misc"]["wireframeSmoke"] = misc.wireframeSmoke;
-    config["misc"]["skybox"] = misc.skybox;
-    config["misc"]["viewmodelFov"] = misc.viewmodelFov;
-    config["misc"]["worldColor"][0] = misc.worldColor[0];
-    config["misc"]["worldColor"][1] = misc.worldColor[1];
-    config["misc"]["worldColor"][2] = misc.worldColor[2];
-    config["misc"]["autoPistol"] = misc.autoPistol;
-
-    std::ofstream{ path } << config;
+    cereal::BinaryOutputArchive outputArchive{ out };
+    outputArchive(aimbot, triggerbot, glow, chams, visuals, misc);
+    out.close();
 }
 
 void Config::reset()
