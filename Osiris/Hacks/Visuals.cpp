7a62ff053628c83b8eade78493d37e75d9087ff6
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-20 10:48:39 +0100

    Return back to firstperson mode if local player is dead

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -7,6 +7,8 @@ void Visuals::thirdperson()
     static bool isInThirdperson{ true };
     if (GetAsyncKeyState(config.visuals.thirdpersonKey))
         isInThirdperson = !isInThirdperson;
-    if (memory.input->isCameraInThirdPerson = (config.visuals.thirdperson && isInThirdperson))
+    if (memory.input->isCameraInThirdPerson =
+        (config.visuals.thirdperson && isInThirdperson
+            && interfaces.entityList->getClientEntity(interfaces.engine->getLocalPlayer())->isAlive()))
         memory.input->cameraOffset.z = static_cast<float>(config.visuals.thirdpersonDistance);
 }
