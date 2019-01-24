40c9b3a5991fe0c5e8929f37dda74537f6aaa33f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-24 18:59:24 +0100

    Correct aimbot smoothing

--- Osiris/Hacks/Aimbot.cpp
+++ Osiris/Hacks/Aimbot.cpp
@@ -31,7 +31,6 @@ static constexpr int getBoneId() noexcept
 static QAngle calculateAngleBetween(const Vector& source, const Vector& destination) noexcept
 {
     Vector delta = source - destination;
-    delta /= config.aimbot.smooth;
     float hyp = delta.length2D();
     constexpr auto radiansToDegrees = [](float radians) noexcept { return radians * 180 / static_cast<float>(M_PI); };
     QAngle angles;
@@ -80,7 +79,7 @@ void Aimbot::run(UserCmd* cmd)
             auto entity = interfaces.entityList->getEntity(target);
             auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer());
             auto angle = calculateAngleBetween(localPlayer->getEyeOrigin(), entity->getBonePosition(getBoneId()));
-            cmd->viewangles = angle;
+            cmd->viewangles += angle / config.aimbot.smooth;
             if (!config.aimbot.silent)
                 interfaces.engine->setViewAngles(angle);
         }
