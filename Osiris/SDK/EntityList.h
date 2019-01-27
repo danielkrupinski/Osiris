3d2b4312755260b1d5221d3aed631f7aa69e5922
Daniel Krupiński <danielkrupinski@outlook.com>
2019-01-27 15:18:58 +0100

    Change return types of EntityList class methods to auto

--- Osiris/SDK/EntityList.h
+++ Osiris/SDK/EntityList.h
@@ -4,17 +4,17 @@
 
 class EntityList {
 public:
-    constexpr class Entity* getEntity(int index) noexcept
+    constexpr auto getEntity(int index) noexcept
     {
         return callVirtualFunction<class Entity*(__thiscall*)(void*, int)>(this, 3)(this, index);
     }
 
-    constexpr class Entity* getClientEntityFromHandle(int handle) noexcept
+    constexpr auto getClientEntityFromHandle(int handle) noexcept
     {
         return callVirtualFunction<class Entity*(__thiscall*)(void*, int)>(this, 4)(this, handle);
     }
 
-    constexpr int getHighestEntityIndex() noexcept
+    constexpr auto getHighestEntityIndex() noexcept
     {
         return callVirtualFunction<int(__thiscall*)(void*)>(this, 6)(this);
     }
