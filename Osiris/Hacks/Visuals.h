449ab0da25ba9f65ba214d499e7821859e428f6c
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-02 19:04:56 +0100

    Add Visuals::removeWeapons() method

--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -14,6 +14,15 @@ namespace Visuals {
     {
         return config.visuals.noSleeves && (modelName.find("sleeve") != std::string_view::npos);
     }
+
+    constexpr bool removeWeapons(const std::string_view modelName) noexcept
+    {
+        return config.visuals.noWeapons && modelName.find("models/weapons/v_") != std::string_view::npos
+            && modelName.find("arms") == std::string_view::npos
+            && modelName.find("tablet") == std::string_view::npos
+            && modelName.find("parachute") == std::string_view::npos
+            && modelName.find("fists") == std::string_view::npos;
+    }
     void modifySmoke() noexcept;
     void thirdperson();
 };
