2bb0874ac042883dc90700566edcafe9a2171dcc
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-04 15:38:19 +0100

    Make Engine class methods constexpr

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -6,32 +6,32 @@
 
 class Engine {
 public:
-    auto getLocalPlayer() noexcept
+    constexpr auto getLocalPlayer() noexcept
     {
         return callVirtualFunction<int(__thiscall*)(void*)>(this, 12)(this);
     }
 
-    auto getViewAngles(Vector& angles) noexcept
+    constexpr auto getViewAngles(Vector& angles) noexcept
     {
         callVirtualFunction<void(__thiscall*)(void*, Vector&)>(this, 18)(this, angles);
     }
 
-    auto setViewAngles(const Vector& angles) noexcept
+    constexpr auto setViewAngles(const Vector& angles) noexcept
     {
         callVirtualFunction<void(__thiscall*)(void*, const Vector&)>(this, 19)(this, angles);
     }
 
-	auto getMaxClients() noexcept
+	constexpr auto getMaxClients() noexcept
 	{
 		return callVirtualFunction<int(__thiscall*)(void*)>(this, 20)(this);
 	}
 
-	auto isInGame() noexcept
+	constexpr auto isInGame() noexcept
 	{
 		return callVirtualFunction<bool(__thiscall*)(void*)>(this, 26)(this);
 	}
 
-    auto getNetworkChannel() noexcept
+    constexpr auto getNetworkChannel() noexcept
     {
         return callVirtualFunction<NetworkChannel*(__thiscall*)(void*)>(this, 78)(this);
     }
