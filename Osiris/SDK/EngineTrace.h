6d5fb7ce0573fbc8c6e9a1ab81e49c9530284ac5
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-04 19:49:55 +0100

    Add required structs to EngineTrace.h

--- Osiris/SDK/EngineTrace.h
+++ Osiris/SDK/EngineTrace.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <cstddef>
 #include "Utils.h"
+
+
+struct Ray {
+    std::byte pad[80];
+};
+
+class TraceFilter { };
+
+struct Trace {
+    std::byte pad[76];
+    class Entity* entity;
+};
 
 class EngineTrace {
 public:
-    constexpr void traceRay(const int& ray, unsigned int mask, void* filter, void* trace) noexcept
+    constexpr void traceRay(const Ray& ray, unsigned int mask, TraceFilter& filter, Trace& trace) noexcept
     {
-        callVirtualFunction<void(__thiscall*)(void*, const int&, unsigned int, void*, void*)>(this, 5)(this, ray, mask, filter, trace);
+        callVirtualFunction<void(__thiscall*)(void*, const Ray&, unsigned int, TraceFilter& , Trace&) > (this, 5)(this, ray, mask, filter, trace);
     }
 };
