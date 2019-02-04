f05c720042b05c3d2a5202e1aeb50056e6eeacfa
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-04 19:35:08 +0100

    Add EngineTrace class

--- Osiris/SDK/EngineTrace.h
+++ Osiris/SDK/EngineTrace.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Utils.h"
+
+class EngineTrace {
+public:
+    constexpr void traceRay(const int& ray, unsigned int mask, void* filter, void* trace) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, const int&, unsigned int, void*, void*)>(this, 5)(this, ray, mask, filter, trace);
+    }
+};
