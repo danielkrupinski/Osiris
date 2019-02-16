4022a26ddaf6edd6cb494f466ac6e5ca675022fc
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-16 19:58:12 +0100

    Add ModelInfo class

--- Osiris/SDK/ModelInfo.h
+++ Osiris/SDK/ModelInfo.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <string_view>
+#include "Utils.h"
+
+class ModelInfo final {
+public:
+    constexpr auto getModel(const std::string_view name) noexcept
+    {
+        return callVirtualFunction<int(__thiscall*)(void*, const char*)>(this, 2)(this, name.data());
+    }
+};
