76274752af6e01be226771a4cbc731038a36d87d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-19 15:45:19 +0100

    Add "Enabled" checkbox to "Knife changer" window

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -242,6 +242,7 @@ void GUI::renderKnifeChangerWindow() noexcept
     if (window.knifeChanger) {
         ImGui::SetNextWindowSize({ 300.0f, 300.0f });
         ImGui::Begin("Knife changer", &window.knifeChanger, windowFlags);
+        ImGui::Checkbox("Enabled", &config.knifeChanger.enabled);
         ImGui::Combo("Knife", &config.knifeChanger.knife, "Default\0Bayonet\0Bowie\0Butterfly\0Falchion\0Flip\0Gut\0Huntsman\0Karambit\0M9 Bayonet\0Shadow Daggers\0Navaja\0Stiletto\0Talon\0Ursus\0Golden");
         ImGui::End();
     }
