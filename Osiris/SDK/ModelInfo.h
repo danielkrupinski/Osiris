cbaffdc21045ed076f7a1fa3191c89fe3c1be4e4
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-19 17:01:57 +0100

    Rename ModelInfo::getModel() to ModelInfo::getModelIndex()

--- Osiris/SDK/ModelInfo.h
+++ Osiris/SDK/ModelInfo.h
@@ -5,7 +5,7 @@
 
 class ModelInfo final {
 public:
-    constexpr auto getModel(const std::string_view name) noexcept
+    constexpr auto getModelIndex(const std::string_view name) noexcept
     {
         return callVirtualFunction<int(__thiscall*)(void*, const char*)>(this, 2)(this, name.data());
     }
