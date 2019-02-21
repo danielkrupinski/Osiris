f48536423d3894d211039281ca99e518753067c9
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-21 19:14:57 +0100

    Supply some methods to Surface class

--- Osiris/SDK/Surface.h
+++ Osiris/SDK/Surface.h
@@ -1,5 +1,36 @@
 #pragma once
 
-class Surface final {
+#include "Utils.h"
 
+class Surface final {
+public:
+    constexpr void setDrawColor(int r, int g, int b, int a) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, int, int, int, int)>(this, 14)(this, r, g, b, a);
+    }
+
+    constexpr void drawFilledRect(int x0, int y0, int x1, int y1) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, int, int, int, int)>(this, 16)(this, x0, y0, x1, y1);
+    }
+
+    constexpr void setTextFont(unsigned long font) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, unsigned long)>(this, 23)(this, font);
+    }
+
+    constexpr void setTextColor(int r, int g, int b, int a) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, int, int, int, int)>(this, 24)(this, r, g, b, a);
+    }
+
+    constexpr void setTextPosition(int x, int y) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, int, int)>(this, 26)(this, x, y);
+    }
+
+    constexpr void printText(const wchar_t* text, int textLength, int drawType = 0) noexcept
+    {
+        callVirtualFunction<void(__thiscall*)(void*, const wchar_t*, int, int)>(this, 28)(this, text, textLength, drawType);
+    }
 };
