246a52be4fea1a87d3fc5d079e753f0a199cf5ad
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-21 18:00:33 +0100

    Add Panel class with Panel::getName() method

--- Osiris/SDK/Panel.h
+++ Osiris/SDK/Panel.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <string_view>
+#include "Utils.h"
+
+class Panel {
+public:
+    constexpr auto getName(unsigned int panel) noexcept
+    {
+        return std::string_view{ callVirtualFunction<const char* (__thiscall*)(void*, unsigned int)>(this, 36)(this, panel) };
+    }
+};
