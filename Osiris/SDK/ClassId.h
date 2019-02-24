7bba193f1830f3e357d6ef2370a39a415a1c8535
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-24 17:53:55 +0100

    Update ClassId enum

--- Osiris/SDK/ClassId.h
+++ Osiris/SDK/ClassId.h
@@ -8,10 +8,16 @@ enum class ClassId {
     Knife = 105,
     KnifeGG = 106,
     PlantedC4 = 126,
+    Aug = 228,
+    Awp = 229,
     Elite = 235,
     FiveSeven = 237,
+    G3sg1 = 238,
     Glock = 241,
     P2000 = 242,
     P250 = 254,
+    Scar20 = 257,
+    Sg553 = 261,
+    Ssg08 = 262,
     Tec9 = 264
 };
