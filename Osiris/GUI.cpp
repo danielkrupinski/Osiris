1a09d47452602333421177baf6f9876cbd093c0e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-02-27 19:43:19 +0100

    Add "Planting" category to chams

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -177,17 +177,17 @@ void GUI::renderChamsWindow() noexcept
         ImGui::Begin("Chams", &window.chams, windowFlags);
         static int currentCategory{ 0 };
         ImGui::PushItemWidth(110.0f);
-        ImGui::Combo("##1", &currentCategory, "Allies\0Enemies\0Defusing\0Local player\0Weapons\0Hands");
+        ImGui::Combo("##1", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0Hands\0");
         static int currentItem{ 0 };
 
-        if (currentCategory <= 2) {
+        if (currentCategory <= 3) {
             ImGui::SameLine();
             static int currentType{ 0 };
             ImGui::Combo("##2", &currentType, "Visible\0Occluded");
             currentItem = currentCategory * 2 + currentType;
         }
         else {
-            currentItem = currentCategory + 3;
+            currentItem = currentCategory + 4;
         }
 
         ImGui::Checkbox("Enabled", &config.chams.chams[currentItem].enabled);
