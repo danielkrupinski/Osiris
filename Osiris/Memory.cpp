1bfd0d91949ea3a4025c4151af035d7081d36dd4
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-14 19:19:23 +0100

    Find globalVars pointer using client interface instead of pattern scanning

--- Osiris/Memory.cpp
+++ Osiris/Memory.cpp
@@ -13,7 +13,7 @@ Memory::Memory() noexcept
         reset = findPattern_2("gameoverlayrenderer", "\xC7\x45?????\xFF\x15????\x8B\xF8") + 9;
         clientMode = **reinterpret_cast<ClientMode***>((*reinterpret_cast<uintptr_t**>(interfaces.client))[10] + 5);
         input = *reinterpret_cast<Input**>((*reinterpret_cast<uintptr_t**>(interfaces.client))[16] + 1);
-        globalVars = **reinterpret_cast<GlobalVars***>(findPattern_2("client_panorama", "\xA1?????\x8B\x40\x10") + 1);
+        globalVars = **reinterpret_cast<GlobalVars***>((*reinterpret_cast<uintptr_t**>(interfaces.client))[11] + 10);
         glowObjectManager = *reinterpret_cast<GlowObjectManager**>(findPattern_2("client_panorama", "\x0F\x11\x05????\x83\xC8\x01") + 3);
         disablePostProcessing = *reinterpret_cast<bool**>(findPattern_2("client_panorama", "\x80\x3D?????\x53\x56\x57\x0F\x85") + 2);
         loadSky = findPattern_2("engine", "\x55\x8B\xEC\x81\xEC????\x56\x57\x8B\xF9\xC7\x45");
