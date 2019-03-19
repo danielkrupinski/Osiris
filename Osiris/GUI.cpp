b50fcefaf3d6c4783f35338f1c819155e5a8b523
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-19 17:19:05 +0100

    Add support for additional mouse buttons and use ImGui::PushID()

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -32,7 +32,7 @@ void GUI::hotkey(int& key) noexcept
 
         for (int i = 0; i < IM_ARRAYSIZE(io.MouseDown); i++)
             if (ImGui::IsMouseDown(i))
-                key = i == 2 ? 4 : i + 1;
+                key = i + (i > 1 ? 2 : 1);
     }
 }
 
@@ -67,7 +67,8 @@ void GUI::renderAimbotWindow() noexcept
         ImGui::Begin("Aimbot", &window.aimbot, windowFlags);
         static int currentCategory{ 0 };
         ImGui::PushItemWidth(110.0f);
-        ImGui::Combo("##1", &currentCategory, "All\0Pistols\0Heavy\0SMG\0Rifles");
+        ImGui::PushID(0);
+        ImGui::Combo("", &currentCategory, "All\0Pistols\0Heavy\0SMG\0Rifles");
         ImGui::SameLine();
         static int currentWeapon{ 0 };
 
@@ -78,25 +79,29 @@ void GUI::renderAimbotWindow() noexcept
             break;
         case 1: {
             static int currentPistol{ 0 };
-            ImGui::Combo("##2", &currentPistol, "Glock-18\0P2000\0USP-S\0Dual Berettas\0P250\0Tec-9\0Five-Seven\0CZ-75\0Desert Eagle\0Revolver");
+            ImGui::PushID(1);
+            ImGui::Combo("", &currentPistol, "Glock-18\0P2000\0USP-S\0Dual Berettas\0P250\0Tec-9\0Five-Seven\0CZ-75\0Desert Eagle\0Revolver");
             currentWeapon = currentPistol + 1;
             break;
         }
         case 2: {
             static int currentHeavy{ 0 };
-            ImGui::Combo("##3", &currentHeavy, "Nova\0XM1014\0Sawed-off\0MAG-7\0M249\0Negev");
+            ImGui::PushID(2);
+            ImGui::Combo("", &currentHeavy, "Nova\0XM1014\0Sawed-off\0MAG-7\0M249\0Negev");
             currentWeapon = currentHeavy + 11;
             break;
         }
         case 3: {
             static int currentSmg{ 0 };
-            ImGui::Combo("##4", &currentSmg, "Mac-10\0MP9\0MP7\0MP5-SD\0UMP-45\0P90\0PP-Bizon");
+            ImGui::PushID(3);
+            ImGui::Combo("", &currentSmg, "Mac-10\0MP9\0MP7\0MP5-SD\0UMP-45\0P90\0PP-Bizon");
             currentWeapon = currentSmg + 17;
             break;
         }
         case 4: {
             static int currentRifle{ 0 };
-            ImGui::Combo("##5", &currentRifle, "Galil AR\0Famas\0AK-47\0M4A4\0M4A1-S\0SSG-08\0SG-553\0AUG\0AWP\0G3SG1\0SCAR-20");
+            ImGui::PushID(4);
+            ImGui::Combo("", &currentRifle, "Galil AR\0Famas\0AK-47\0M4A4\0M4A1-S\0SSG-08\0SG-553\0AUG\0AWP\0G3SG1\0SCAR-20");
             currentWeapon = currentRifle + 24;
             break;
         }
@@ -109,10 +114,14 @@ void GUI::renderAimbotWindow() noexcept
         ImGui::Checkbox("Auto shot", &config.aimbot.weapons[currentWeapon].autoShot);
         ImGui::Combo("Bone", &config.aimbot.weapons[currentWeapon].bone, "Head\0Neck\0Sternum\0Chest\0Stomach\0Pelvis");
         ImGui::PushItemWidth(240.0f);
-        ImGui::SliderFloat("##6", &config.aimbot.weapons[currentWeapon].fov, 0.0f, 255.0f, "Fov: %.2f");
-        ImGui::SliderFloat("##7", &config.aimbot.weapons[currentWeapon].smooth, 1.0f, 100.0f, "Smooth: %.2f");
-        ImGui::SliderFloat("##8", &config.aimbot.weapons[currentWeapon].recoilControlX, 0.0f, 1.0f, "Recoil control x: %.2f");
-        ImGui::SliderFloat("##9", &config.aimbot.weapons[currentWeapon].recoilControlY, 0.0f, 1.0f, "Recoil control y: %.2f");
+        ImGui::PushID(5);
+        ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].fov, 0.0f, 255.0f, "Fov: %.2f");
+        ImGui::PushID(6);
+        ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].smooth, 1.0f, 100.0f, "Smooth: %.2f");
+        ImGui::PushID(7);
+        ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].recoilControlX, 0.0f, 1.0f, "Recoil control x: %.2f");
+        ImGui::PushID(9);
+        ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].recoilControlY, 0.0f, 1.0f, "Recoil control y: %.2f");
         ImGui::End();
     }
 }
@@ -127,7 +136,8 @@ void GUI::renderTriggerbotWindow() noexcept
         ImGui::SameLine();
         hotkey(config.triggerbot.key);
         ImGui::PushItemWidth(160.0f);
-        ImGui::SliderInt("##1", &config.triggerbot.shotDelay, 0, 250, "Shot delay: %d ms");
+        ImGui::PushID(0);
+        ImGui::SliderInt("", &config.triggerbot.shotDelay, 0, 250, "Shot delay: %d ms");
         ImGui::End();
     }
 }
@@ -140,21 +150,27 @@ void GUI::renderGlowWindow() noexcept
         ImGui::Begin("Glow", &window.glow, windowFlags);
         static int currentCategory{ 0 };
         ImGui::PushItemWidth(110.0f);
-        ImGui::Combo("##1", &currentCategory, "Allies\0Enemies\0Local player\0Weapons\0C4\0Planted C4\0Chickens\0");
+        ImGui::PushID(0);
+        ImGui::Combo("", &currentCategory, "Allies\0Enemies\0Local player\0Weapons\0C4\0Planted C4\0Chickens\0");
         ImGui::Checkbox("Enabled", &config.glow[currentCategory].enabled);
         bool openPopup = ImGui::ColorButton("Color", ImColor{ config.glow[currentCategory].color[0], config.glow[currentCategory].color[1], config.glow[currentCategory].color[2] }, ImGuiColorEditFlags_NoTooltip);
         ImGui::SameLine(0.0f, 5.0f);
         ImGui::Text("Color");
+        ImGui::PushID(1);
         if (openPopup)
-            ImGui::OpenPopup("##5");
-        if (ImGui::BeginPopup("##5")) {
-            ImGui::ColorPicker3("##6", config.glow[currentCategory].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
+            ImGui::OpenPopup("");
+        if (ImGui::BeginPopup("")) {
+            ImGui::PushID(2);
+            ImGui::ColorPicker3("", config.glow[currentCategory].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
             ImGui::EndPopup();
         }
         ImGui::PushItemWidth(220.0f);
-        ImGui::SliderFloat("##2", &config.glow[currentCategory].thickness, 0.0f, 1.0f, "Thickness: %.2f");
-        ImGui::SliderFloat("##3", &config.glow[currentCategory].alpha, 0.0f, 1.0f, "Alpha: %.2f");
-        ImGui::SliderInt("##4", &config.glow[currentCategory].style, 0, 3, "Style: %d");
+        ImGui::PushID(3);
+        ImGui::SliderFloat("", &config.glow[currentCategory].thickness, 0.0f, 1.0f, "Thickness: %.2f");
+        ImGui::PushID(4);
+        ImGui::SliderFloat("", &config.glow[currentCategory].alpha, 0.0f, 1.0f, "Alpha: %.2f");
+        ImGui::PushID(5);
+        ImGui::SliderInt("", &config.glow[currentCategory].style, 0, 3, "Style: %d");
         ImGui::End();
     }
 }
@@ -166,13 +182,15 @@ void GUI::renderChamsWindow() noexcept
         ImGui::Begin("Chams", &window.chams, windowFlags);
         static int currentCategory{ 0 };
         ImGui::PushItemWidth(110.0f);
-        ImGui::Combo("##1", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0Hands\0");
+        ImGui::PushID(0);
+        ImGui::Combo("", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0Hands\0");
         static int currentItem{ 0 };
 
         if (currentCategory <= 3) {
             ImGui::SameLine();
             static int currentType{ 0 };
-            ImGui::Combo("##2", &currentType, "Visible\0Occluded");
+            ImGui::PushID(1);
+            ImGui::Combo("", &currentType, "Visible\0Occluded");
             currentItem = currentCategory * 2 + currentType;
         }
         else {
@@ -186,14 +204,17 @@ void GUI::renderChamsWindow() noexcept
         bool openPopup = ImGui::ColorButton("Color", ImColor{ config.chams[currentItem].color[0], config.chams[currentItem].color[1], config.chams[currentItem].color[2] }, ImGuiColorEditFlags_NoTooltip);
         ImGui::SameLine(0.0f, 5.0f);
         ImGui::Text("Color");
+        ImGui::PushID(2);
         if (openPopup)
-            ImGui::OpenPopup("##3");
-        if (ImGui::BeginPopup("##3")) {
-            ImGui::ColorPicker3("##4", config.chams[currentItem].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
+            ImGui::OpenPopup("");
+        if (ImGui::BeginPopup("")) {
+            ImGui::PushID(3);
+            ImGui::ColorPicker3("", config.chams[currentItem].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
             ImGui::EndPopup();
         }
         ImGui::PushItemWidth(220.0f);
-        ImGui::SliderFloat("##5", &config.chams[currentItem].alpha, 0.0f, 1.0f, "Alpha: %.2f");
+        ImGui::PushID(4);
+        ImGui::SliderFloat("", &config.chams[currentItem].alpha, 0.0f, 1.0f, "Alpha: %.2f");
         ImGui::End();
     }
 }
@@ -221,10 +242,14 @@ void GUI::renderVisualsWindow() noexcept
         ImGui::SameLine();
         hotkey(config.visuals.thirdpersonKey);
         ImGui::PushItemWidth(290.0f);
-        ImGui::SliderInt("##1", &config.visuals.thirdpersonDistance, 0, 1000, "Thirdperson distance: %d");
-        ImGui::SliderInt("##2", &config.visuals.viewmodelFov, -60, 60, "Viewmodel FOV: %d");
-        ImGui::SliderInt("##3", &config.visuals.flashReduction, 0, 100, "Flash reduction: %d%%");
-        ImGui::SliderFloat("##4", &config.visuals.brightness, 0.0f, 1.0f, "Brightness: %.2f");
+        ImGui::PushID(0);
+        ImGui::SliderInt("", &config.visuals.thirdpersonDistance, 0, 1000, "Thirdperson distance: %d");
+        ImGui::PushID(1);
+        ImGui::SliderInt("", &config.visuals.viewmodelFov, -60, 60, "Viewmodel FOV: %d");
+        ImGui::PushID(2);
+        ImGui::SliderInt("", &config.visuals.flashReduction, 0, 100, "Flash reduction: %d%%");
+        ImGui::PushID(3);
+        ImGui::SliderFloat("", &config.visuals.brightness, 0.0f, 1.0f, "Brightness: %.2f");
         ImGui::PopItemWidth();
         ImGui::Combo("Skybox", &config.visuals.skybox, "Default\0cs_baggage_skybox_\0cs_tibet\0embassy\0italy\0jungle\0nukeblank\0office\0sky_cs15_daylight01_hdr\0sky_cs15_daylight02_hdr\0sky_cs15_daylight03_hdr\0sky_cs15_daylight04_hdr\0sky_csgo_cloudy01\0sky_csgo_night_flat\0sky_csgo_night02\0sky_day02_05_hdr\0sky_day02_05\0sky_dust\0sky_l4d_rural02_ldr\0sky_venice\0vertigo_hdr\0vertigo\0vertigoblue_hdr\0vietnam");
         ImGui::ColorEdit3("World color", config.visuals.worldColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
@@ -252,7 +277,8 @@ void GUI::renderMiscWindow() noexcept
         ImGui::Checkbox("Bunny hop", &config.misc.bunnyHop);
         static char buffer[16];
         ImGui::PushItemWidth(120.0f);
-        ImGui::InputText("##1", buffer, IM_ARRAYSIZE(buffer));
+        ImGui::PushID(0);
+        ImGui::InputText("", buffer, IM_ARRAYSIZE(buffer));
         ImGui::SameLine();
         if (ImGui::Button("Set clantag")) {
             Misc::setClanTag(buffer);
