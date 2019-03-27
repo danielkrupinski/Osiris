5adb7fed149a467bfee83cb75d91327506ae95f8
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-27 20:05:55 +0100

    Update GameUI::messageBox()

--- Osiris/SDK/GameUI.h
+++ Osiris/SDK/GameUI.h
@@ -6,6 +6,6 @@ class GameUI {
 public:
     constexpr void messageBox(const char* title, const char* text) noexcept
     {
-        callVirtualFunction<void(__thiscall*)(void*, const char*, const char*, bool, bool, const char*, const char*, const char*, const char*)>(this, 20)(this, title, text, true, false, "", "", "", "");
+        callVirtualFunction<void(__thiscall*)(void*, const char*, const char*, bool, bool, const char*, const char*, const char*, const char*, void*)>(this, 20)(this, title, text, true, false, nullptr, nullptr, nullptr, nullptr, nullptr);
     }
 };
