a3ab59bc8b814f34a831d592dbd9044bd40f4fdf
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-28 19:08:13 +0100

    Update GUI.cpp

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -61,6 +61,7 @@ void GUI::renderAimbotWindow() noexcept
         ImGui::PushItemWidth(110.0f);
         ImGui::PushID(0);
         ImGui::Combo("", &currentCategory, "All\0Pistols\0Heavy\0SMG\0Rifles");
+        ImGui::PopID();
         ImGui::SameLine();
         static int currentWeapon{ 0 };
 
@@ -73,6 +74,7 @@ void GUI::renderAimbotWindow() noexcept
             static int currentPistol{ 0 };
             ImGui::PushID(1);
             ImGui::Combo("", &currentPistol, "Glock-18\0P2000\0USP-S\0Dual Berettas\0P250\0Tec-9\0Five-Seven\0CZ-75\0Desert Eagle\0Revolver");
+            ImGui::PopID();
             currentWeapon = currentPistol + 1;
             break;
         }
@@ -80,6 +82,7 @@ void GUI::renderAimbotWindow() noexcept
             static int currentHeavy{ 0 };
             ImGui::PushID(2);
             ImGui::Combo("", &currentHeavy, "Nova\0XM1014\0Sawed-off\0MAG-7\0M249\0Negev");
+            ImGui::PopID();
             currentWeapon = currentHeavy + 11;
             break;
         }
@@ -87,6 +90,7 @@ void GUI::renderAimbotWindow() noexcept
             static int currentSmg{ 0 };
             ImGui::PushID(3);
             ImGui::Combo("", &currentSmg, "Mac-10\0MP9\0MP7\0MP5-SD\0UMP-45\0P90\0PP-Bizon");
+            ImGui::PopID();
             currentWeapon = currentSmg + 17;
             break;
         }
@@ -94,6 +98,7 @@ void GUI::renderAimbotWindow() noexcept
             static int currentRifle{ 0 };
             ImGui::PushID(4);
             ImGui::Combo("", &currentRifle, "Galil AR\0Famas\0AK-47\0M4A4\0M4A1-S\0SSG-08\0SG-553\0AUG\0AWP\0G3SG1\0SCAR-20");
+            ImGui::PopID();
             currentWeapon = currentRifle + 24;
             break;
         }
@@ -108,12 +113,16 @@ void GUI::renderAimbotWindow() noexcept
         ImGui::PushItemWidth(240.0f);
         ImGui::PushID(5);
         ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].fov, 0.0f, 255.0f, "Fov: %.2f");
+        ImGui::PopID();
         ImGui::PushID(6);
         ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].smooth, 1.0f, 100.0f, "Smooth: %.2f");
+        ImGui::PopID();
         ImGui::PushID(7);
         ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].recoilControlX, 0.0f, 1.0f, "Recoil control x: %.2f");
+        ImGui::PopID();
         ImGui::PushID(9);
         ImGui::SliderFloat("", &config.aimbot.weapons[currentWeapon].recoilControlY, 0.0f, 1.0f, "Recoil control y: %.2f");
+        ImGui::PopID();
         ImGui::End();
     }
 }
@@ -128,7 +137,6 @@ void GUI::renderTriggerbotWindow() noexcept
         ImGui::SameLine();
         hotkey(config.triggerbot.key);
         ImGui::PushItemWidth(160.0f);
-        ImGui::PushID(0);
         ImGui::SliderInt("", &config.triggerbot.shotDelay, 0, 250, "Shot delay: %d ms");
         ImGui::End();
     }
@@ -144,25 +152,44 @@ void GUI::renderGlowWindow() noexcept
         ImGui::PushItemWidth(110.0f);
         ImGui::PushID(0);
         ImGui::Combo("", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0C4\0Planted C4\0Chickens\0");
-        ImGui::Checkbox("Enabled", &config.glow[currentCategory].enabled);
-        bool openPopup = ImGui::ColorButton("Color", ImColor{ config.glow[currentCategory].color[0], config.glow[currentCategory].color[1], config.glow[currentCategory].color[2] }, ImGuiColorEditFlags_NoTooltip);
+        ImGui::PopID();
+        static int currentItem{ 0 };
+        if (currentCategory <= 3) {
+            ImGui::SameLine();
+            static int currentType{ 0 };
+            ImGui::PushID(1);
+            ImGui::Combo("", &currentType, "Visible\0Occluded");
+            ImGui::PopID();
+            currentItem = currentCategory * 2 + currentType;
+        }
+        else {
+            currentItem = currentCategory + 4;
+        }
+
+        ImGui::Checkbox("Enabled", &config.glow[currentItem].enabled);
+        bool openPopup = ImGui::ColorButton("Color", ImColor{ config.glow[currentItem].color[0], config.glow[currentItem].color[1], config.glow[currentItem].color[2] }, ImGuiColorEditFlags_NoTooltip);
         ImGui::SameLine(0.0f, 5.0f);
         ImGui::Text("Color");
-        ImGui::PushID(1);
+        ImGui::PushID(2);
         if (openPopup)
             ImGui::OpenPopup("");
         if (ImGui::BeginPopup("")) {
-            ImGui::PushID(2);
-            ImGui::ColorPicker3("", config.glow[currentCategory].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
+            ImGui::PushID(3);
+            ImGui::ColorPicker3("", config.glow[currentItem].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
+            ImGui::PopID();
             ImGui::EndPopup();
         }
+        ImGui::PopID();
         ImGui::PushItemWidth(220.0f);
-        ImGui::PushID(3);
-        ImGui::SliderFloat("", &config.glow[currentCategory].thickness, 0.0f, 1.0f, "Thickness: %.2f");
         ImGui::PushID(4);
-        ImGui::SliderFloat("", &config.glow[currentCategory].alpha, 0.0f, 1.0f, "Alpha: %.2f");
+        ImGui::SliderFloat("", &config.glow[currentItem].thickness, 0.0f, 1.0f, "Thickness: %.2f");
+        ImGui::PopID();
         ImGui::PushID(5);
-        ImGui::SliderInt("", &config.glow[currentCategory].style, 0, 3, "Style: %d");
+        ImGui::SliderFloat("", &config.glow[currentItem].alpha, 0.0f, 1.0f, "Alpha: %.2f");
+        ImGui::PopID();
+        ImGui::PushID(6);
+        ImGui::SliderInt("", &config.glow[currentItem].style, 0, 3, "Style: %d");
+        ImGui::PopID();
         ImGui::End();
     }
 }
@@ -176,6 +203,7 @@ void GUI::renderChamsWindow() noexcept
         ImGui::PushItemWidth(110.0f);
         ImGui::PushID(0);
         ImGui::Combo("", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0Hands\0");
+        ImGui::PopID();
         static int currentItem{ 0 };
 
         if (currentCategory <= 3) {
@@ -183,6 +211,7 @@ void GUI::renderChamsWindow() noexcept
             static int currentType{ 0 };
             ImGui::PushID(1);
             ImGui::Combo("", &currentType, "Visible\0Occluded");
+            ImGui::PopID();
             currentItem = currentCategory * 2 + currentType;
         }
         else {
@@ -190,7 +219,7 @@ void GUI::renderChamsWindow() noexcept
         }
 
         ImGui::Checkbox("Enabled", &config.chams[currentItem].enabled);
-        ImGui::Checkbox("Flat", &config.chams[currentItem].flat);
+        ImGui::Combo("Material", &config.chams[currentItem].material, "Normal\0Flat\0Palm");
         ImGui::Checkbox("Wireframe", &config.chams[currentItem].wireframe);
 
         bool openPopup = ImGui::ColorButton("Color", ImColor{ config.chams[currentItem].color[0], config.chams[currentItem].color[1], config.chams[currentItem].color[2] }, ImGuiColorEditFlags_NoTooltip);
@@ -202,11 +231,14 @@ void GUI::renderChamsWindow() noexcept
         if (ImGui::BeginPopup("")) {
             ImGui::PushID(3);
             ImGui::ColorPicker3("", config.chams[currentItem].color, ImGuiColorEditFlags_DisplayRGB | ImGuiColorEditFlags_NoSidePreview);
+            ImGui::PopID();
             ImGui::EndPopup();
         }
+        ImGui::PopID();
         ImGui::PushItemWidth(220.0f);
         ImGui::PushID(4);
         ImGui::SliderFloat("", &config.chams[currentItem].alpha, 0.0f, 1.0f, "Alpha: %.2f");
+        ImGui::PopID();
         ImGui::End();
     }
 }
@@ -236,12 +268,16 @@ void GUI::renderVisualsWindow() noexcept
         ImGui::PushItemWidth(290.0f);
         ImGui::PushID(0);
         ImGui::SliderInt("", &config.visuals.thirdpersonDistance, 0, 1000, "Thirdperson distance: %d");
+        ImGui::PopID();
         ImGui::PushID(1);
         ImGui::SliderInt("", &config.visuals.viewmodelFov, -60, 60, "Viewmodel FOV: %d");
+        ImGui::PopID();
         ImGui::PushID(2);
         ImGui::SliderInt("", &config.visuals.flashReduction, 0, 100, "Flash reduction: %d%%");
+        ImGui::PopID();
         ImGui::PushID(3);
         ImGui::SliderFloat("", &config.visuals.brightness, 0.0f, 1.0f, "Brightness: %.2f");
+        ImGui::PopID();
         ImGui::PopItemWidth();
         ImGui::Combo("Skybox", &config.visuals.skybox, "Default\0cs_baggage_skybox_\0cs_tibet\0embassy\0italy\0jungle\0nukeblank\0office\0sky_cs15_daylight01_hdr\0sky_cs15_daylight02_hdr\0sky_cs15_daylight03_hdr\0sky_cs15_daylight04_hdr\0sky_csgo_cloudy01\0sky_csgo_night_flat\0sky_csgo_night02\0sky_day02_05_hdr\0sky_day02_05\0sky_dust\0sky_l4d_rural02_ldr\0sky_venice\0vertigo_hdr\0vertigo\0vertigoblue_hdr\0vietnam");
         ImGui::ColorEdit3("World color", config.visuals.worldColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip);
@@ -269,7 +305,6 @@ void GUI::renderMiscWindow() noexcept
         ImGui::Checkbox("Bunny hop", &config.misc.bunnyHop);
         static char buffer[16];
         ImGui::PushItemWidth(120.0f);
-        ImGui::PushID(0);
         ImGui::InputText("", buffer, IM_ARRAYSIZE(buffer));
         ImGui::SameLine();
         if (ImGui::Button("Set clantag")) {
@@ -298,98 +333,51 @@ void GUI::renderConfigWindow() noexcept
         ImGui::SetNextWindowSize({ 290.0f, 190.0f });
         ImGui::Begin("Config", &window.config, windowFlags);
 
-        std::string currentConfigString("Current Config: " + config.currentConfig);
-        ImGui::Text(currentConfigString.c_str());
-
         ImGui::Columns(2, nullptr, false);
         ImGui::SetColumnOffset(1, 170.0f);
 
         ImGui::PushItemWidth(160.0f);
 
-        std::vector<std::string> configItems = config.getConfigs();
-        static int configCurrent = -1;
+        constexpr auto& configItems = config.getConfigs();
+        static int currentConfig = -1;
 
-        ImGui::ListBox("", &configCurrent, configItems, 5);
+        if (static_cast<size_t>(currentConfig) >= configItems.size())
+            currentConfig = -1;
 
-        if (configCurrent < configItems.size()) {
-            if (configCurrent != 0) {
-                char buffer[16];
-                strcpy(buffer, configItems[configCurrent].c_str());
+        ImGui::ListBox("", &currentConfig, [](void* data, int idx, const char** out_text) {
+            auto& vector = *static_cast<std::vector<std::string>*>(data);
+            *out_text = vector[idx].c_str();
+            return true;
+        }, &configItems, configItems.size(), 5);
 
-                if (ImGui::InputText("##1", buffer, IM_ARRAYSIZE(buffer), ImGuiInputTextFlags_EnterReturnsTrue)) {
-                    char* nameBuffer = buffer;
-                    char* str_end = nameBuffer + strlen(nameBuffer);
-                    while (str_end > nameBuffer && str_end[-1] == ' ')
-                        str_end--; *str_end = 0;
+        static char buffer[16];
+        if (currentConfig != -1)
+            strcpy(buffer, configItems[currentConfig].c_str());
 
-                    std::string name(nameBuffer);
-                    config.rename(configItems[configCurrent], name);
-                }
-            }
-            else {
-                ImGui::InputTextDisabled("Default");
-            }
+        ImGui::PushID(0);
+        if (ImGui::InputText("", buffer, IM_ARRAYSIZE(buffer), ImGuiInputTextFlags_EnterReturnsTrue)) {
+            if (currentConfig != -1)
+                config.rename(currentConfig, buffer);
         }
-        else {
-            ImGui::InputTextDisabled("No Config Selected");
-        }
-
-        ImGui::PopItemWidth();
+        ImGui::PopID();
         ImGui::NextColumn();
 
         ImGui::PushItemWidth(100.0f);
 
-        if (configCurrent >= configItems.size()) {
-            ImGui::ButtonDisabled("Load Selected");
-            ImGui::ButtonDisabled("Save Selected");
-            if (ImGui::Button("New Config", { 100.0f, 25.0f })) {
-                config.add(GetNewConfigName());
-            }
-            ImGui::ButtonDisabled("Delete Config");
+        if (ImGui::Button("Create config", { 100.0f, 25.0f }))
+            config.add(buffer);
+
+        if (ImGui::Button("Reset config", { 100.0f, 25.0f }))
+            config.reset();
+
+        if (currentConfig != -1) {
+            if (ImGui::Button("Load selected", { 100.0f, 25.0f }))
+                config.load(currentConfig);
+            if (ImGui::Button("Save selected", { 100.0f, 25.0f }))
+                config.save(currentConfig);
+            if (ImGui::Button("Delete selected", { 100.0f, 25.0f }))
+                config.remove(currentConfig);
         }
-        else {
-            if (ImGui::Button("Load Selected", { 100.0f, 25.0f })) {
-                config.load(configItems[configCurrent].c_str());
-            }
-
-            if (configCurrent != 0) {
-                if (ImGui::Button("Save Selected", { 100.0f, 25.0f })) {
-                    config.save(configItems[configCurrent]);
-                }
-            }
-            else {
-                ImGui::ButtonDisabled("Save Selected");
-            }
-
-            if (ImGui::Button("New Config", { 100.0f, 25.0f })) {
-                config.add(GetNewConfigName());
-            }
-
-            if (configCurrent != 0)
-            {
-                if (ImGui::Button("Delete Config", { 100.0f, 25.0f })) {
-                    config.remove(configItems[configCurrent]);
-                }
-            }
-            else {
-                ImGui::ButtonDisabled("Delete Config");
-            }
-        }
-
-        ImGui::PopItemWidth();
         ImGui::End();
     }
-}
-
-std::string GUI::GetNewConfigName(std::string name, int i) noexcept {
-    std::vector<std::string> configItems = config.getConfigs();
-
-    auto cName = std::find(configItems.begin(), configItems.end(), name + (i == 0 ? "" : " " + std::to_string(i)));
-    if (cName == configItems.end())
-        if (i == 0)
-            return name;
-        else
-            return name + " " + std::to_string(i);
-    else
-        return GetNewConfigName(name, i + 1);
 }
