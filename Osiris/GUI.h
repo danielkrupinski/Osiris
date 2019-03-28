ad382b807af2f4f359006290a5f1eb00be94c02f
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-28 19:05:26 +0100

    Update GUI.h

--- Osiris/GUI.h
+++ Osiris/GUI.h
@@ -1,9 +1,4 @@
 #pragma once
-#include <string>
-#include <vector>
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_internal.h"
 
 class GUI final {
 public:
@@ -30,38 +25,6 @@ private:
         bool misc{ false };
         bool config{ false };
     } window;
-
-    std::string GetNewConfigName(std::string name = "New Config", int i = 0) noexcept;
 };
 
 extern GUI gui;
-
-
-namespace ImGui {
-    static auto vector_getter = [](void* vec, int idx, const char** out_text) {
-        auto& vector = *static_cast<std::vector<std::string>*>(vec);
-        if (idx < 0 || idx >= static_cast<int>(vector.size())) { return false; }
-        *out_text = vector.at(idx).c_str();
-        return true;
-    };
-
-    inline bool ListBox(const char* label, int* currIndex, std::vector<std::string>& values, int height_in_items = -1) {
-        if (values.empty()) { return false; }
-        return ListBox(label, currIndex, vector_getter,
-            static_cast<void*>(&values), values.size(), height_in_items);
-    }
-
-    static void ButtonDisabled(const char* label) {
-        SetCursorPosX(ImGui::GetCursorPosX() + 9);
-        SetCursorPosY(ImGui::GetCursorPosY() + 4);
-        TextDisabled(label);
-        SetCursorPosY(ImGui::GetCursorPosY() + 5);
-    }
-
-    static void InputTextDisabled(const char* label) {
-        SetCursorPosX(ImGui::GetCursorPosX() + 4);
-        SetCursorPosY(ImGui::GetCursorPosY() + 3);
-        TextDisabled(label);
-        SetCursorPosY(ImGui::GetCursorPosY() + 4);
-    }
-}
