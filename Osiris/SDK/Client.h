9212eaa2baa66999cb5cffbaafa0ffb04581c3f0
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-14 16:18:07 +0200

    Add Client::dispatchUserMessage() method

--- Osiris/SDK/Client.h
+++ Osiris/SDK/Client.h
@@ -8,6 +8,11 @@ class Client final {
 public:
     constexpr auto getAllClasses() noexcept
     {
-        return callVirtualFunction<ClientClass*(__thiscall*)()>(this, 8)();
+        return callVirtualFunction<ClientClass*(__thiscall*)(void*)>(this, 8)(this);
+    }
+
+    constexpr bool dispatchUserMessage(int messageType, int arg, int arg1, void* data) noexcept
+    {
+        return callVirtualFunction<bool(__thiscall*)(void*, int, int, int, void*)>(this, 38)(this, messageType, arg, arg1, data);
     }
 };
