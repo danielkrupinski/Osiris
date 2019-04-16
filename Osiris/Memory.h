d1a1e8135cbff6ed1ce1da50a27b9abe067f177c
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-16 18:11:10 +0200

    Make Memory::findPattern() private method

--- Osiris/Memory.h
+++ Osiris/Memory.h
@@ -13,6 +13,25 @@ class Memory final {
 public:
     Memory() noexcept;
 
+    uintptr_t present;
+    uintptr_t reset;
+
+    class ClientMode* clientMode;
+    Input* input;
+    GlobalVars* globalVars;
+    GlowObjectManager* glowObjectManager;
+
+    bool* disablePostProcessing;
+
+    std::add_pointer_t<void __fastcall(const char*) noexcept> loadSky;
+    std::add_pointer_t<void __fastcall(const char*, const char*) noexcept> setClanTag;
+    int* smokeCount;
+    uintptr_t cameraThink;
+    std::add_pointer_t<void __cdecl(int*) noexcept> revealRanks;
+    std::add_pointer_t<bool __stdcall(const char*) noexcept> acceptMatch;
+    std::add_pointer_t<bool __cdecl(Vector, Vector, short) noexcept> lineGoesThroughSmoke;
+
+private:
     template <typename T = uintptr_t>
     auto findPattern(const char* module, std::string_view pattern, size_t offset = 0) const
     {
@@ -43,24 +62,6 @@ public:
         MessageBox(NULL, (std::ostringstream{ } << "Failed to find pattern in " << module << '!').str().c_str(), "Error", MB_OK | MB_ICONERROR);
         exit(EXIT_FAILURE);
     }
-
-    uintptr_t present;
-    uintptr_t reset;
-
-    class ClientMode* clientMode;
-    Input* input;
-    GlobalVars* globalVars;
-    GlowObjectManager* glowObjectManager;
-
-    bool* disablePostProcessing;
-
-    std::add_pointer_t<void __fastcall(const char*) noexcept> loadSky;
-    std::add_pointer_t<void __fastcall(const char*, const char*) noexcept> setClanTag;
-    int* smokeCount;
-    uintptr_t cameraThink;
-    std::add_pointer_t<void __cdecl(int*) noexcept> revealRanks;
-    std::add_pointer_t<bool __stdcall(const char*) noexcept> acceptMatch;
-    std::add_pointer_t<bool __cdecl(Vector, Vector, short) noexcept> lineGoesThroughSmoke;
 };
 
 extern Memory memory;
