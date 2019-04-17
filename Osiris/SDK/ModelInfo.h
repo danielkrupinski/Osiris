c19b668a2965f1c309f2232235e49a03fce7b401
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-17 17:11:31 +0200

    Update ModelInfo::getModelIndex()

--- Osiris/SDK/ModelInfo.h
+++ Osiris/SDK/ModelInfo.h
@@ -1,12 +1,11 @@
 #pragma once
 
-#include <string_view>
 #include "Utils.h"
 
 class ModelInfo final {
 public:
-    constexpr auto getModelIndex(const std::string_view name) noexcept
+    constexpr auto getModelIndex(const char* name) noexcept
     {
-        return callVirtualFunction<int(__thiscall*)(void*, const char*)>(this, 2)(this, name.data());
+        return callVirtualFunction<int(__thiscall*)(void*, const char*)>(this, 2)(this, name);
     }
 };
