84704331dc171548dff144746bcd82d558c00913
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-17 12:33:13 +0200

    Update UserCmd class

--- Osiris/SDK/UserCmd.h
+++ Osiris/SDK/UserCmd.h
@@ -8,23 +8,23 @@ public:
         IN_ATTACK = 1 << 0,
         IN_JUMP = 1 << 1,
         IN_ATTACK2 = 1 << 11,
+        IN_SCORE = 1 << 16,
         IN_BULLRUSH = 1 << 22
     };
     int pad;
-    int     command_number;
-    int     tick_count;
-    Vector  viewangles;
-    Vector  aimdirection;
-    float   forwardmove;
-    float   sidemove;
-    float   upmove;
-    int     buttons;
-    char    impulse;
-    int     weaponselect;
-    int     weaponsubtype;
-    int     random_seed;
-    short   mousedx;
-    short   mousedy;
-    bool    hasbeenpredicted;
-    char    pad_0x4C[0x18];
+    int command_number;
+    int tick_count;
+    Vector viewangles;
+    Vector aimdirection;
+    float forwardmove;
+    float sidemove;
+    float upmove;
+    int buttons;
+    char impulse;
+    int weaponselect;
+    int weaponsubtype;
+    int random_seed;
+    short mousedx;
+    short mousedy;
+    bool hasbeenpredicted;
 };
