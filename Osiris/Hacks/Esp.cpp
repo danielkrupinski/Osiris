832ab553426577031c20eedbfa66a9acb708f31e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-19 18:44:15 +0200

    Update worldToScreen() function

--- Osiris/Hacks/Esp.cpp
+++ Osiris/Hacks/Esp.cpp
@@ -14,25 +14,24 @@ static bool screenTransform(const Vector& in, Vector& out) noexcept
 
     float w = matrix[3][0] * in.x + matrix[3][1] * in.y + matrix[3][2] * in.z + matrix[3][3];
 
-    if (w < 0.001f) {
-        out.x *= 100000;
-        out.y *= 100000;
-        return false;
+    if (w > 0.001f) {
+        out.x /= w;
+        out.y /= w;
+        return true;
     }
-
-    out.x /= w;
-    out.y /= w;
-    return true;
+    return false;
 }
 
 static bool worldToScreen(const Vector& in, Vector& out) noexcept
 {
-    if (screenTransform(in, out)) {
+    static auto& matrix = interfaces.engine->worldToScreenMatrix();
+    float w = matrix[3][0] * in.x + matrix[3][1] * in.y + matrix[3][2] * in.z + matrix[3][3];
+
+    if (w > 0.001f) {
         const auto [width, height] = interfaces.surface->getScreenSize();
-
-        out.x = (width / 2.0f) + (out.x * width) / 2.0f;
-        out.y = (height / 2.0f) - (out.y * height) / 2.0f;
-
+        out.x = width / 2 * (1 + (matrix[0][0] * in.x + matrix[0][1] * in.y + matrix[0][2] * in.z + matrix[0][3]) / w);
+        out.y = height / 2 * (1 - (matrix[1][0] * in.x + matrix[1][1] * in.y + matrix[1][2] * in.z + matrix[1][3]) / w);
+        out.z = 0.0f;
         return true;
     }
     return false;
@@ -40,7 +39,7 @@ static bool worldToScreen(const Vector& in, Vector& out) noexcept
 
 void Esp::render() noexcept
 {
-    if (config.esp.enabled && interfaces.engine->isInGame()) {
+    if (interfaces.engine->isInGame()) {
         interfaces.surface->setDrawColor(51, 153, 255, 255);
         const auto [width, height] = interfaces.surface->getScreenSize();
         const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer());
