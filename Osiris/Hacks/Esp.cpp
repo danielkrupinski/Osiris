52f0702aaf9a22d833cd1ffb0b3236fa4770d06a
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-20 15:52:50 +0200

    Remove screenTransform() function

--- Osiris/Hacks/Esp.cpp
+++ Osiris/Hacks/Esp.cpp
@@ -4,24 +4,6 @@
 #include "../SDK/Entity.h"
 #include "../SDK/Vector.h"
 
-static bool screenTransform(const Vector& in, Vector& out) noexcept
-{
-    static auto& matrix = interfaces.engine->worldToScreenMatrix();
-
-    out.x = matrix[0][0] * in.x + matrix[0][1] * in.y + matrix[0][2] * in.z + matrix[0][3];
-    out.y = matrix[1][0] * in.x + matrix[1][1] * in.y + matrix[1][2] * in.z + matrix[1][3];
-    out.z = 0.0f;
-
-    float w = matrix[3][0] * in.x + matrix[3][1] * in.y + matrix[3][2] * in.z + matrix[3][3];
-
-    if (w > 0.001f) {
-        out.x /= w;
-        out.y /= w;
-        return true;
-    }
-    return false;
-}
-
 static bool worldToScreen(const Vector& in, Vector& out) noexcept
 {
     static auto& matrix = interfaces.engine->worldToScreenMatrix();
