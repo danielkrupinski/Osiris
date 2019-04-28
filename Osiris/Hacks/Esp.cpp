772f57852d1f3a57d2f0f749701eabb18c542572
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-28 13:02:32 +0200

    Add renderSnaplines() function for snaplines drawing

--- Osiris/Hacks/Esp.cpp
+++ Osiris/Hacks/Esp.cpp
@@ -19,10 +19,21 @@ static bool worldToScreen(const Vector& in, Vector& out) noexcept
     return false;
 }
 
+static void renderSnaplines(Entity* entity, decltype(config.esp[0])& config) noexcept
+{
+    if (config.snaplines) {
+        Vector position;
+        if (worldToScreen(entity->getProperty<Vector>("m_vecOrigin"), position)) {
+            const auto [width, height] = interfaces.surface->getScreenSize();
+            interfaces.surface->setDrawColor(config.snaplinesColor, 255);
+            interfaces.surface->drawLine(width / 2, height, static_cast<int>(position.x), static_cast<int>(position.y));
+        }
+    }
+}
+
 void Esp::render() noexcept
 {
     if (interfaces.engine->isInGame()) {
-        const auto [width, height] = interfaces.surface->getScreenSize();
         const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer());
 
         for (int i = 1; i <= interfaces.engine->getMaxClients(); i++) {
@@ -30,40 +41,25 @@ void Esp::render() noexcept
             if (!entity || entity == localPlayer || entity->isDormant()
                 || !entity->isAlive())
                 continue;
-            Vector position;
-            if (worldToScreen(entity->getProperty<Vector>("m_vecOrigin"), position)) {
-                if (!entity->isEnemy()) {
-                    if (entity->isVisible()) {
-                        if (config.esp[0].enabled) {
-                            if (config.esp[0].snaplines) {
-                                interfaces.surface->setDrawColor(config.esp[0].snaplinesColor, 255);
-                                interfaces.surface->drawLine(width / 2, height, static_cast<int>(position.x), static_cast<int>(position.y));
-                            }
-                        }
-                    } else {
-                        if (config.esp[1].enabled) {
-                            if (config.esp[1].snaplines) {
-                                interfaces.surface->setDrawColor(config.esp[1].snaplinesColor, 255);
-                                interfaces.surface->drawLine(width / 2, height, static_cast<int>(position.x), static_cast<int>(position.y));
-                            }
-                        }
+
+            if (!entity->isEnemy()) {
+                if (entity->isVisible()) {
+                    if (config.esp[0].enabled) {
+                        renderSnaplines(entity, config.esp[0]);
                     }
-                    
                 } else {
-                    if (entity->isVisible()) {
-                        if (config.esp[2].enabled) {
-                            if (config.esp[2].snaplines) {
-                                interfaces.surface->setDrawColor(config.esp[2].snaplinesColor, 255);
-                                interfaces.surface->drawLine(width / 2, height, static_cast<int>(position.x), static_cast<int>(position.y));
-                            }
-                        }
-                    } else {
-                        if (config.esp[3].enabled) {
-                            if (config.esp[3].snaplines) {
-                                interfaces.surface->setDrawColor(config.esp[3].snaplinesColor, 255);
-                                interfaces.surface->drawLine(width / 2, height, static_cast<int>(position.x), static_cast<int>(position.y));
-                            }
-                        }
+                    if (config.esp[1].enabled) {
+                        renderSnaplines(entity, config.esp[1]);
+                    }
+                }
+            } else {
+                if (entity->isVisible()) {
+                    if (config.esp[2].enabled) {
+                        renderSnaplines(entity, config.esp[2]);
+                    }
+                } else {
+                    if (config.esp[3].enabled) {
+                        renderSnaplines(entity, config.esp[3]);
                     }
                 }
             }
