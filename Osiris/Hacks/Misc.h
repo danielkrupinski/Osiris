bf73b1a04f8bc0ab3ffab928546911c43608def9
Daniel Krupiński <danielkrupinski@outlook.com>
2019-04-30 16:59:46 +0200

    Create Misc::autoReload() function

--- Osiris/Hacks/Misc.h
+++ Osiris/Hacks/Misc.h
@@ -12,13 +12,20 @@ namespace Misc {
     void recoilCrosshair() noexcept;
     void watermark() noexcept;
 
+    constexpr void autoReload(UserCmd* cmd) noexcept
+    {
+        if (config.misc.autoReload) {
+            const auto activeWeapon = interfaces.entityList->getEntityFromHandle(interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer())->getProperty<int>("m_hActiveWeapon"));
+            if (activeWeapon && getWeaponIndex(activeWeapon->getProperty<WeaponId>("m_iItemDefinitionIndex")) && !activeWeapon->getProperty<int>("m_iClip1"))
+                cmd->buttons &= ~(UserCmd::IN_ATTACK | UserCmd::IN_ATTACK2);
+        }
+    }
 
     constexpr void revealRanks(UserCmd* cmd) noexcept
     {
         if (config.misc.revealRanks && cmd->buttons & UserCmd::IN_SCORE)
             interfaces.client->dispatchUserMessage(50, 0, 0, nullptr);
     }
-
 
     constexpr void autoStrafe(UserCmd* cmd) noexcept
     {
