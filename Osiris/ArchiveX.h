d2361f4d376befe18546d9037a4e3af0bf1eeded
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-05 13:50:01 +0200

    Fix encoding in ArchiveX.h

--- Osiris/ArchiveX.h
+++ Osiris/ArchiveX.h
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2019 Daniel Krupiñski
+Copyright (c) 2019 Daniel KrupiÅ„ski
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
