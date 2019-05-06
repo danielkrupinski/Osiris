d3701fac6ce5674b27bfc5e28323493990c2501e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-06 18:51:47 +0200

    Make Entity::getProperty() const method

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -11,9 +11,9 @@
 class Entity {
 public:
     template <typename T>
-    constexpr auto getProperty(const char* name, const std::size_t offset = 0) noexcept
+    constexpr auto getProperty(const char* name, const std::size_t offset = 0) const noexcept
     {
-        return *reinterpret_cast<T*>(this + netvars[name] + offset);
+        return *reinterpret_cast<const T*>(this + netvars[name] + offset);
     }
 
     template <typename T>
