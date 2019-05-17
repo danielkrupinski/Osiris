964851222a8808567d2371f88a1ce7762d2c076e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-17 17:13:11 +0200

    Update GlobalVars struct

--- Osiris/SDK/GlobalVars.h
+++ Osiris/SDK/GlobalVars.h
@@ -7,7 +7,8 @@
 struct GlobalVars {
     const float realtime;
     const int framecount;
-    const std::byte pad[8];
+    const float absoluteFrameTime;
+    const std::byte pad[4];
     const float currenttime;
     const float frametime;
     const int maxClients;
