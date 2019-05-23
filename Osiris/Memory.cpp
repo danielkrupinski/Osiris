121c1ebd9539c0c0703b7c6d60c3bd961e8e033d
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-23 17:27:27 +0200

    Shorten several signatures

--- Osiris/Memory.cpp
+++ Osiris/Memory.cpp
@@ -16,15 +16,14 @@ Memory::Memory() noexcept
     globalVars = **reinterpret_cast<GlobalVars***>((*reinterpret_cast<uintptr_t**>(interfaces.client))[11] + 10);
     glowObjectManager = *findPattern<GlowObjectManager**>("client_panorama", "\x0F\x11\x05????\x83\xC8\x01", 3);
     disablePostProcessing = *findPattern<bool**>("client_panorama", "\x83\xEC\x4C\x80\x3D", 5);
-    loadSky = findPattern<decltype(loadSky)>("engine", "\x55\x8B\xEC\x81\xEC????\x56\x57\x8B\xF9\xC7\x45");
+    loadSky = relativeToAbsolute<decltype(loadSky)>(findPattern<int*>("engine", "\xE8????\x84\xC0\x74\x2D\xA1", 1));
     setClanTag = findPattern<decltype(setClanTag)>("engine", "\x53\x56\x57\x8B\xDA\x8B\xF9\xFF\x15");
-    lineGoesThroughSmoke = findPattern<decltype(lineGoesThroughSmoke)>("client_panorama", "\x55\x8B\xEC\x83\xEC\x08\x8B\x15????\x0F\x57\xC0");
+    lineGoesThroughSmoke = relativeToAbsolute<decltype(lineGoesThroughSmoke)>(findPattern<int*>("client_panorama", "\xE8????\x8B\x4C\x24\x30\x33\xD2", 1));
     smokeCount = *(reinterpret_cast<int**>(lineGoesThroughSmoke) + 2);
     cameraThink = findPattern<>("client_panorama", "\x85\xC0\x75\x30\x38\x86");
     acceptMatch = findPattern<decltype(acceptMatch)>("client_panorama", "\x55\x8B\xEC\x83\xE4\xF8\x8B\x4D\x08\xBA????\xE8????\x85\xC0\x75\x12");
     getSequenceActivity = findPattern<decltype(getSequenceActivity)>("client_panorama", "\x55\x8B\xEC\x53\x8B\x5D\x08\x56\x8B\xF1\x83");
     scopeArc = findPattern<>("client_panorama", "\x6A?\xFF\x50\x3C\x8B\x0D????\xFF\xB7", 5);
     scopeLens = findPattern<>("client_panorama", "\xFF\x50\x3C\x8B\x4C\x24\x20", 3);
-    int* jmp = findPattern<int*>("client_panorama", "\xE8????\x02\xC0", 1);
-    isOtherEnemy = reinterpret_cast<decltype(isOtherEnemy)>(reinterpret_cast<char*>(jmp + 1) + *jmp);
+    isOtherEnemy = relativeToAbsolute<decltype(isOtherEnemy)>(findPattern<int*>("client_panorama", "\xE8????\x02\xC0", 1));
 }
