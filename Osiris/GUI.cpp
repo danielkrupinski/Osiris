615aa1f03edb7cc386431f93c04d9d30e773d40b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-25 12:54:17 +0200

    Disable knifechanger in GUI

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -22,7 +22,7 @@ void GUI::render() noexcept
     renderChamsWindow();
     renderEspWindow();
     renderVisualsWindow();
-    renderKnifeChangerWindow();
+    //renderKnifeChangerWindow();
     renderSkinChangerWindow();
     renderMiscWindow();
     renderConfigWindow();
@@ -74,7 +74,7 @@ void GUI::renderMenuBar() noexcept
         ImGui::MenuItem("Chams", nullptr, &window.chams);
         ImGui::MenuItem("Esp", nullptr, &window.esp);
         ImGui::MenuItem("Visuals", nullptr, &window.visuals);
-        ImGui::MenuItem("Knife changer", nullptr, &window.knifeChanger);
+     //   ImGui::MenuItem("Knife changer", nullptr, &window.knifeChanger);
         ImGui::MenuItem("Skin changer", nullptr, &window.skinChanger);
         ImGui::MenuItem("Misc", nullptr, &window.misc);
         ImGui::MenuItem("Config", nullptr, &window.config);
