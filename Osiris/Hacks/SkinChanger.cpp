5d616a8b9f1e9e5530aa0b4aa77e83def8cc8021
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-26 15:29:08 +0200

    Run skinchanger only if localPlayer is alive

--- Osiris/Hacks/SkinChanger.cpp
+++ Osiris/Hacks/SkinChanger.cpp
@@ -9,8 +9,8 @@ extern void post_data_update_start(sdk::C_BasePlayer*);
 void SkinChanger::run(FrameStage stage) noexcept
 {
     if (stage == FrameStage::NET_UPDATE_POSTDATAUPDATE_START) {
-        const auto localPlayerIndex = interfaces.engine->getLocalPlayer();
-        const auto localPlayer = interfaces.entityList->getEntity(localPlayerIndex);
-        if (localPlayer) post_data_update_start(reinterpret_cast<sdk::C_BasePlayer*>(localPlayer));
+        
+        if (const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer()); localPlayer && localPlayer->isAlive())
+            post_data_update_start(reinterpret_cast<sdk::C_BasePlayer*>(localPlayer));
     }
 }
