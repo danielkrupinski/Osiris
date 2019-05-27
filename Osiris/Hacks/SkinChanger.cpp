c78f2cdcdacdb3036d84c25466ff162071fb681e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-27 11:24:28 +0200

    Add SkinChanger::scheduleHudUpdate()  function

--- Osiris/Hacks/SkinChanger.cpp
+++ Osiris/Hacks/SkinChanger.cpp
@@ -6,11 +6,28 @@
 
 extern void post_data_update_start(sdk::C_BasePlayer*);
 
+static bool hudUpdateRequired{ false };
+
 void SkinChanger::run(FrameStage stage) noexcept
 {
     if (stage == FrameStage::NET_UPDATE_POSTDATAUPDATE_START) {
         
-        if (const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer()); localPlayer && localPlayer->isAlive())
+        if (const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer()); localPlayer && localPlayer->isAlive()) {
             post_data_update_start(reinterpret_cast<sdk::C_BasePlayer*>(localPlayer));
+            if (hudUpdateRequired && !localPlayer->isDormant()) {
+                auto hudWeaponSelection = memory.findHudElement(memory.hud, "CCSGO_HudWeaponSelection");
+
+                if (auto hud_weapons = hudWeaponSelection - 0xA0) {
+                    for (int i = 0; i < *reinterpret_cast<int*>(hud_weapons + 0x80); i++)
+                        i = memory.clearHudWeapon(hud_weapons, i);
+                }
+                hudUpdateRequired = false;
+            }
+        }
     }
+}
+
+void SkinChanger::scheduleHudUpdate() noexcept
+{
+    hudUpdateRequired = true;
 }
--- Osiris/Hacks/SkinChanger.h
+++ Osiris/Hacks/SkinChanger.h
@@ -4,4 +4,5 @@
 
 namespace SkinChanger {
     void run(FrameStage) noexcept;
+    void scheduleHudUpdate() noexcept;
 }
