9b6bb7020d9a746bf85b86df8fd446f4e024fa5e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-28 18:25:09 +0200

    Add updateHud() function in SkinChanger.cpp

--- Osiris/Hacks/SkinChanger.cpp
+++ Osiris/Hacks/SkinChanger.cpp
@@ -8,19 +8,22 @@ extern void post_data_update_start(sdk::C_BasePlayer*);
 
 static bool hudUpdateRequired{ false };
 
+static constexpr void updateHud() noexcept
+{
+    if (auto hud_weapons = memory.findHudElement(memory.hud, "CCSGO_HudWeaponSelection") - 0x28) {
+        for (int i = 0; i < *(hud_weapons + 0x20); i++)
+            i = memory.clearHudWeapon(hud_weapons, i);
+    }
+    hudUpdateRequired = false;
+}
+
 void SkinChanger::run(FrameStage stage) noexcept
 {
     if (stage == FrameStage::NET_UPDATE_POSTDATAUPDATE_START) {
-        
         if (const auto localPlayer = interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer()); localPlayer && localPlayer->isAlive()) {
             post_data_update_start(reinterpret_cast<sdk::C_BasePlayer*>(localPlayer));
-            if (hudUpdateRequired && !localPlayer->isDormant()) {
-                if (auto hud_weapons = memory.findHudElement(memory.hud, "CCSGO_HudWeaponSelection") - 0x28) {
-                    for (int i = 0; i < *(hud_weapons + 0x20); i++)
-                        i = memory.clearHudWeapon(hud_weapons, i);
-                }
-                hudUpdateRequired = false;
-            }
+            if (hudUpdateRequired && !localPlayer->isDormant())
+                updateHud();
         }
     }
 }
