a86de6794b9d6eec333aab85bda15c1600ed9790
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-28 21:25:10 +0200

    Add GameEventManager class with addListener() method

--- Osiris/SDK/GameEventManager.h
+++ Osiris/SDK/GameEventManager.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Utils.h"
+
+class GameEventManager {
+public:
+    constexpr auto addListener(void* listener, const char* name) noexcept
+    {
+        return callVirtualMethod<bool, void*, const char*, bool>(this, 3, listener, name, false);
+    }
+};
