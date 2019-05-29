335f99bdb98561ab07a6906972efe9f4ab7a4a21
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-29 17:21:34 +0200

    Update GameEvent class

--- Osiris/SDK/GameEvent.h
+++ Osiris/SDK/GameEvent.h
@@ -8,4 +8,19 @@ public:
     {
         return callVirtualMethod<const char*>(this, 1);
     }
+
+    constexpr auto getInt(const char* keyName) noexcept
+    {
+        return callVirtualMethod<int, const char*>(this, 6, keyName);
+    }
+
+    constexpr auto getString(const char* keyName) noexcept
+    {
+        return callVirtualMethod<const char*, const char*>(this, 9, keyName);
+    }
+
+    constexpr auto setString(const char* keyName, const char* value) noexcept
+    {
+        callVirtualMethod<void, const char*, const char*>(this, 15, keyName, value);
+    }
 };
