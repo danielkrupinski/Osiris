5c75f9428b36132ec13f8ba1d3cda799ef18e865
Daniel Krupiński <danielkrupinski@outlook.com>
2019-05-30 18:27:08 +0200

    Update callVirtualMethod() function

--- Osiris/SDK/Utils.h
+++ Osiris/SDK/Utils.h
@@ -5,5 +5,5 @@
 template<typename T, typename ...Args>
 constexpr auto callVirtualMethod(void* classBase, int index, Args... args) noexcept
 {
-    return reinterpret_cast<T(__thiscall*)(void*, Args...)>((*reinterpret_cast<uintptr_t**>(classBase))[index])(classBase, args...);
+    return ((*reinterpret_cast<T(__thiscall***)(void*, Args...)>(classBase))[index])(classBase, args...);
 }
