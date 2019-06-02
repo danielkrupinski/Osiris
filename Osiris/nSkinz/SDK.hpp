a381bdfa5da4d58da45d837a797760572a966214
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-02 13:06:47 +0200

    Remove SDK.hpp

--- Osiris/nSkinz/SDK.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "Utilities/platform.hpp"
--- Osiris/nSkinz/Utilities/Platform.cpp
+++ Osiris/nSkinz/Utilities/Platform.cpp
@@ -22,7 +22,6 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-#include "../SDK.hpp"
 #include "platform.hpp"
 #include <vector>
 #include <algorithm>
