b777135c72075ad7b60f87583e67df8877ca3084
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-05 15:28:33 +0200

    Simplify structs in kit_parser.cpp

--- Osiris/nSkinz/kit_parser.cpp
+++ Osiris/nSkinz/kit_parser.cpp
@@ -60,7 +60,6 @@ struct Head_t
     int last_element;				//0x0018
 }; //Size=0x001C
 
-// could use CUtlString but this is just easier and CUtlString isn't needed anywhere else
 struct String_t
 {
     char* buffer;	//0x0000
@@ -71,16 +70,11 @@ struct String_t
 
 struct CPaintKit
 {
-    int id;						//0x0000
-
-    String_t name;				//0x0004
-    String_t description;		//0x0014
-    String_t item_name;			//0x0024
-    String_t material_name;		//0x0034
-    String_t image_inventory;	//0x0044
-
-    char pad_0x0054[0x8C];		//0x0054
-}; //Size=0x00E0
+    int id;
+    String_t name;
+    String_t description;
+    String_t item_name;
+};
 
 struct CStickerKit
 {
@@ -91,27 +85,6 @@ struct CStickerKit
     String_t name;
     String_t description;
     String_t item_name;
-    String_t material_name;
-    String_t image_inventory;
-
-    int tournament_event_id;
-    int tournament_team_id;
-    int tournament_player_id;
-    bool is_custom_sticker_material;
-
-    float rotate_end;
-    float rotate_start;
-
-    float scale_min;
-    float scale_max;
-
-    float wear_min;
-    float wear_max;
-
-    String_t image_inventory2;
-    String_t image_inventory_large;
-
-    std::uint32_t pad0[4];
 };
 
 auto game_data::initialize_kits() -> void
