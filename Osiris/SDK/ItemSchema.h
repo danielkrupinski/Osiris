53035c89d8f25db2cf540ba576d92c4e1ea02f70
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-05 16:00:03 +0200

    Add ItemSchema class

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#include <cstddef>
+
+template <typename Key, typename Value>
+struct Node {
+    int previousId;
+    int nextId;
+    void* _unknownPtr;
+    int _unknown;
+    Key key;
+    Value value;
+};
+
+template <typename Key, typename Value>
+struct Head {
+    Node<Key, Value>* memory;
+    int allocationCount;
+    int growSize;
+    int startElement;
+    int nextAvailable;
+    int _unknown;
+    int lastElement;
+};
+
+struct String {
+    char* buffer;
+    int capacity;
+    int growSize;
+    int length;
+};
+
+struct PaintKit {
+    int id;
+    std::byte pad[32];
+    String itemName;
+};
+
+struct StickerKit {
+    int id;
+    std::byte pad[36];
+    String itemName;
+};
+
+class ItemSchema {
+    void* vmt;
+    std::byte pad[0x28C];
+public:
+    Head<int, PaintKit*> paintKits;
+private:
+    std::byte pad1[0x8];
+public:
+    Head<int, StickerKit*> stickerKits;
+};
