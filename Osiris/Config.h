e0c95ee422dc6e9e4af3143fd1b7e0f90c5e3018
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-12 20:03:28 +0200

    Add blinking bool to chams config

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -98,6 +98,7 @@ public:
             bool enabled{ false };
             bool healthBased{ false };
             bool rainbow{ false };
+            bool blinking{ false };
             int material{ 0 };
             bool wireframe{ false };
             float color[3]{ 1.0f, 1.0f, 1.0f };
