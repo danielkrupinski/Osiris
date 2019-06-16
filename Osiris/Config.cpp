9b397f89416cfe62916d2d578a573e2073cbe92b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-16 15:05:29 +0200

    Add aimbot json config

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -2,6 +2,7 @@
 #include <ShlObj.h>
 
 #include "ArchiveX.h"
+#include "json/json.h"
 
 #include "Config.h"
 
@@ -37,6 +38,31 @@ void Config::load(size_t id) noexcept
     if (!in.good())
         return;
 
+    Json::Value json;
+
+    for (size_t i = 0; i < aimbot.size(); i++) {
+        auto& aimbotJson = json["aimbot"][i];
+        auto& aimbotConfig = aimbot[i];
+
+        aimbotConfig.enabled = aimbotJson["enabled"].asBool();
+        aimbotConfig.onKey = aimbotJson["onKey"].asBool();
+        aimbotConfig.key = aimbotJson["key"].asInt();
+        aimbotConfig.silent = aimbotJson["silent"].asBool();
+        aimbotConfig.friendlyFire = aimbotJson["friendlyFire"].asBool();
+        aimbotConfig.visibleOnly = aimbotJson["visibleOnly"].asBool();
+        aimbotConfig.scopedOnly = aimbotJson["scopedOnly"].asBool();
+        aimbotConfig.ignoreFlash = aimbotJson["ignoreFlash"].asBool();
+        aimbotConfig.ignoreSmoke = aimbotJson["ignoreSmoke"].asBool();
+        aimbotConfig.autoShot = aimbotJson["autoShot"].asBool();
+        aimbotConfig.recoilbasedFov = aimbotJson["recoilbasedFov"].asBool();
+        aimbotConfig.fov = aimbotJson["fov"].asFloat();
+        aimbotConfig.maxAngleDelta = aimbotJson["maxAngleDelta"].asFloat();
+        aimbotConfig.smooth = aimbotJson["smooth"].asFloat();
+        aimbotConfig.bone = aimbotJson["bone"].asInt();
+        aimbotConfig.recoilControlX = aimbotJson["recoilControlX"].asFloat();
+        aimbotConfig.recoilControlY = aimbotJson["recoilControlY"].asFloat();
+    }
+
     ArchiveX<std::ifstream>{ in } >> aimbot >> triggerbot >> backtrack >> glow >> chams >> esp >> visuals >> skinChanger >> misc;
     in.close();
 }
@@ -52,6 +78,31 @@ void Config::save(size_t id) const noexcept
 
     if (!out.good())
         return;
+
+    Json::Value json;
+
+    for (size_t i = 0; i < aimbot.size(); i++) {
+        auto& aimbotJson = json["aimbot"][i];
+        const auto& aimbotConfig = aimbot[i];
+
+        aimbotJson["enabled"] = aimbotConfig.enabled;
+        aimbotJson["onKey"] = aimbotConfig.onKey;
+        aimbotJson["key"] = aimbotConfig.key;
+        aimbotJson["silent"] = aimbotConfig.silent;
+        aimbotJson["friendlyFire"] = aimbotConfig.friendlyFire;
+        aimbotJson["visibleOnly"] = aimbotConfig.visibleOnly;
+        aimbotJson["scopedOnly"] = aimbotConfig.scopedOnly;
+        aimbotJson["ignoreFlash"] = aimbotConfig.ignoreFlash;;
+        aimbotJson["ignoreSmoke"] = aimbotConfig.ignoreSmoke;
+        aimbotJson["autoShot"] = aimbotConfig.autoShot;
+        aimbotJson["recoilbasedFov"] = aimbotConfig.recoilbasedFov;
+        aimbotJson["fov"] = aimbotConfig.fov;
+        aimbotJson["maxAngleDelta"] = aimbotConfig.maxAngleDelta;
+        aimbotJson["smooth"] = aimbotConfig.smooth;
+        aimbotJson["bone"] = aimbotConfig.bone;
+        aimbotJson["recoilControlX"] = aimbotConfig.recoilControlX;
+        aimbotJson["recoilControlY"] = aimbotConfig.recoilControlY;
+    }
 
     ArchiveX<std::ofstream>{ out } << aimbot << triggerbot << backtrack << glow << chams << esp << visuals << skinChanger << misc;
     out.close();
