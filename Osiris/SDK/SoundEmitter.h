ce5d5d4e248d3baa62ff5d89618895aa5deb9c99
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-22 17:16:11 +0200

    Add SoundEmitter class

--- Osiris/SDK/SoundEmitter.h
+++ Osiris/SDK/SoundEmitter.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Utils.h"
+
+class SoundEmitter {
+public:
+    constexpr auto getSoundName(int index) noexcept
+    {
+        return callVirtualMethod<const char*, int>(this, 46, index);
+    }
+};
