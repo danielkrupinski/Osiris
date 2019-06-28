1edae8ab2c3f9cadedd66af485593822d6b29807
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-28 16:08:38 +0200

    Add SurfaceData struct

--- Osiris/SDK/PhysicsSurfaceProps.h
+++ Osiris/SDK/PhysicsSurfaceProps.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <cstddef>
+
+struct SurfaceData {
+    std::byte pad[80];
+    float maxspeedfactor;
+    float jumpfactor;
+    float penetrationmodifier;
+    float damagemodifier;
+    short material;
+    bool climbable;
+};
