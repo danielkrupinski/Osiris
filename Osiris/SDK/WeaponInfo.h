1e298eb4b3f8786357d8a50db9b8440fdc06646b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-06-28 15:55:28 +0200

    Add WeaponData struct

--- Osiris/SDK/WeaponInfo.h
+++ Osiris/SDK/WeaponInfo.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <cstddef>
+
+struct WeaponData {
+    std::byte pad[200];
+    int type;
+    std::byte pad1[32];
+    bool fullAuto;
+    std::byte pad2[3];
+    int damage;
+    float armorRatio;
+    int bullets;
+    float penetration;
+    std::byte pad3[8];
+    float range;
+    float rangeModifier;
+    std::byte pad4[16];
+    bool hasSilencer;
+};
