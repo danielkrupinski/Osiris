c45d3a155ed8ece21d1d2092c5ba4e332340e9ad
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-15 20:54:55 +0200

    Add MaterialSystem::getRenderContext() method

--- Osiris/SDK/MaterialSystem.h
+++ Osiris/SDK/MaterialSystem.h
@@ -3,11 +3,17 @@
 #include "Utils.h"
 
 class Material;
+class RenderContext;
 
 class MaterialSystem {
 public:
     constexpr auto findMaterial(const char* materialName) noexcept
     {
         return callVirtualMethod<Material*, const char*, const char*, bool, const char*>(this, 84, materialName, nullptr, true, nullptr);
+    }
+
+    constexpr auto getRenderContext() noexcept
+    {
+        return callVirtualMethod<RenderContext*>(this, 115);
     }
 };
