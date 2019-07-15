7037bc6eb890e6f63cd88564a0ead898196074d6
Daniel Krupiński <danielkrupinski@outlook.com>
2019-07-15 15:17:04 +0200

    Add RenderContext class

--- Osiris/SDK/RenderContext.h
+++ Osiris/SDK/RenderContext.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <tuple>
+#include "Utils.h"
+
+class RenderContext {
+public:
+    constexpr auto release() noexcept
+    {
+        callVirtualMethod<void>(this, 1);
+    }
+
+    constexpr auto beginRender() noexcept
+    {
+        callVirtualMethod<void>(this, 2);
+    }
+
+    constexpr auto endRender() noexcept
+    {
+        callVirtualMethod<void>(this, 3);
+    }
+
+    constexpr auto getViewport() noexcept
+    {
+        int x{ }, y{ }, width{ }, height{ };
+        callVirtualMethod<void, int&, int&, int&, int&>(this, 41, x, y, width, height);
+        return std::make_pair(width, height);
+    }
+};
