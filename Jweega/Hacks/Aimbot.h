f0f8fb5e7281e629fcf2ebc1f119899632f947e8
Maksym Dilai <admin@dproject.org.ua>
2019-07-21 18:17:34 +0300

    Created functions for dynamic FOV

--- Jweega/Hacks/Aimbot.h
+++ Jweega/Hacks/Aimbot.h
@@ -4,5 +4,7 @@
 
 namespace Aimbot {
     Vector calculateRelativeAngle(const Vector& source, const Vector& destination, const Vector& viewAngles) noexcept;
+    auto calculateDistance(const Vector& localPlayer, const Vector& Entity) noexcept;
+    auto calculateAngleDifference(const Vector& viewAngles, const Vector& destination, float distance);
     void run(UserCmd*) noexcept;
 }
