af01c2122644ef801aecb6e7b01dac9204cbabae
NexSqaud <tf2g3124@gmail.com>
2019-07-30 00:53:02 +0700

    Fix tabulation

--- Osiris/Hacks/Misc.h
+++ Osiris/Hacks/Misc.h
@@ -20,8 +20,8 @@ namespace Misc {
     void watermark() noexcept;
     void prepareRevolver(UserCmd*) noexcept;
     void fastPlant(UserCmd*) noexcept;
-	void bombEvents(GameEvent*) noexcept;
-	void drawTextTimer() noexcept;
+    void bombEvents(GameEvent*) noexcept;
+    void drawTextTimer() noexcept;
     
     constexpr void fixMovement(UserCmd* cmd, float yaw) noexcept
     {
