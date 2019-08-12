91168de049e7b2709057f7fe03aa8ba6d532537b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-12 18:40:08 +0200

    Call Misc::stealNames() in createMove hook

--- Jweega/Hooks.cpp
+++ Jweega/Hooks.cpp
@@ -121,6 +121,7 @@ static bool __stdcall createMove(float inputSampleTime, UserCmd* cmd) noexcept
     Misc::autoPistol(cmd);
     Misc::autoReload(cmd);
     Misc::updateClanTag();
+    Misc::stealNames();
     Misc::revealRanks(cmd);
     Backtrack::run(cmd);
 
