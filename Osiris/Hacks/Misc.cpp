87a06f78b9abb9ed1005b1ff292275cd4d95951e
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-16 11:47:33 +0200

    Update Misc::drawBombTimer() function

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -175,7 +175,7 @@ void Misc::drawBombTimer() noexcept
             interfaces.surface->setTextColor(250.0f, 0.0f, 0.0f, 255.0f);
             auto drawPositionY{ interfaces.surface->getScreenSize().second / 2 };
             interfaces.surface->setTextPosition(5, drawPositionY);
-            auto bombText{ (std::wstringstream{ } << L"Bomb on " << (!entity->c4BombSite() ? 'A' : 'B') << L" : " << std::setprecision(3) << (std::max)(entity->c4BlowTime() - memory.globalVars->currenttime, 0.0f) << L" s").str() };
+            auto bombText{ (std::wstringstream{ } << L"Bomb on " << (!entity->c4BombSite() ? 'A' : 'B') << L" : " << std::showpoint << std::setprecision(3) << (std::max)(entity->c4BlowTime() - memory.globalVars->currenttime, 0.0f) << L" s").str() };
             drawPositionY += interfaces.surface->getTextSize(Surface::font, bombText.c_str()).second;
             interfaces.surface->printText(bombText.c_str());
             if (entity->c4Defuser() != -1) {
@@ -185,7 +185,7 @@ void Misc::drawBombTimer() noexcept
                 if (interfaces.engine->getPlayerInfo(interfaces.entityList->getEntityFromHandle(entity->c4Defuser())->index(), playerInfo)) {
                     static wchar_t name[128];
                     if (MultiByteToWideChar(CP_UTF8, 0, playerInfo.name, -1, name, 128)) {
-                        interfaces.surface->printText((std::wstringstream{ } << name << L" is defusing: " << std::setprecision(4) << (std::max)(entity->c4DefuseCountDown() - memory.globalVars->currenttime, 0.0f) << L" s").str().c_str());
+                        interfaces.surface->printText((std::wstringstream{ } << name << L" is defusing: " << std::showpoint << std::setprecision(4) << (std::max)(entity->c4DefuseCountDown() - memory.globalVars->currenttime, 0.0f) << L" s").str().c_str());
                     }
                 }
             }
