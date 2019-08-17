561e662db776785999bd2d65b227916a2a58443b
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-17 14:31:09 +0200

    Add "Max shot inaccuracy" float slider to "Aimbot" window

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -259,6 +259,7 @@ void GUI::renderAimbotWindow() noexcept
         ImGui::SliderFloat("Recoil control x", &config.aimbot[currentWeapon].recoilControlX, 0.0f, 1.0f, "%.2f");
         ImGui::SliderFloat("Recoil control y", &config.aimbot[currentWeapon].recoilControlY, 0.0f, 1.0f, "%.2f");
         ImGui::SliderFloat("Max aim inaccuracy", &config.aimbot[currentWeapon].maxAimInaccuracy, 0.0f, 1.0f, "%.5f", 2.0f);
+        ImGui::SliderFloat("Max shot inaccuracy", &config.aimbot[currentWeapon].maxShotInaccuracy, 0.0f, 1.0f, "%.5f", 2.0f);
         ImGui::Columns(1);
         if (!config.style.menuStyle)
             ImGui::End();
