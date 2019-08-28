21e9a7e11b0001ddc542e31a782f60cc7ab97af8
Maksym Dilai <admin@dproject.org.ua>
2019-08-28 08:40:53 +0300

    Revert "Add "Auto scope" to aimbot"
    This reverts commit 0551673fcdb851dec9002315b10338eaead29b28.

--- Jweega/Hacks/Aimbot.cpp
+++ Jweega/Hacks/Aimbot.cpp
@@ -210,9 +210,6 @@ void Aimbot::run(UserCmd* cmd) noexcept
             if (!config.aimbot[weaponIndex].silent)
                 interfaces.engine->setViewAngles(cmd->viewangles);
 
-            if (config.aimbot[weaponIndex].autoScope && activeWeapon->nextPrimaryAttack() <= memory.globalVars->serverTime() && activeWeapon->isSniperRifle() && !localPlayer->isScoped())
-                cmd->buttons |= UserCmd::IN_ATTACK2;
-
             if (config.aimbot[weaponIndex].autoShot && activeWeapon->nextPrimaryAttack() <= memory.globalVars->serverTime() && !clamped && activeWeapon->getInaccuracy() <= config.aimbot[weaponIndex].maxShotInaccuracy)
                 cmd->buttons |= UserCmd::IN_ATTACK;
 
