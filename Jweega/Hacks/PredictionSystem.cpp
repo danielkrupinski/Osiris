238312eadb3996af1c5761bb2983ec0f5bf26310
Maksym Dilai <admin@dproject.org.ua>
2019-08-31 11:25:53 +0300

    Removed dirty hacks from PredictionSystem as their causing bugs

--- Jweega/Hacks/PredictionSystem.cpp
+++ Jweega/Hacks/PredictionSystem.cpp
@@ -25,23 +25,11 @@ void PredictionSystem::StartPrediction(UserCmd* cmd)
     previousCurrenttime = memory.globalVars->currenttime;
     previousFrametime = memory.globalVars->frametime;
 
-    *(UserCmd**)((uintptr_t)localPlayer + 0x16E8) = cmd;
-
     memory.globalVars->currenttime = memory.globalVars->serverTime(cmd);
     memory.globalVars->frametime = memory.globalVars->intervalPerTick;
 
     if (!moveData)
         moveData = (MoveData*)(calloc(1, sizeof(MoveData)));
-
-    cmd->buttons |= *reinterpret_cast<uint32_t*>(uintptr_t(localPlayer) + 0x3310);
-    if (cmd->impulse)
-        *reinterpret_cast<uint8_t*>(uintptr_t(localPlayer) + 0x31EC) = cmd->impulse;
-
-    const auto buttonsChanged = cmd->buttons ^ *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31E8);
-    *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31DC) = *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31E8);
-    *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31E8) = cmd->buttons;
-    *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31E0) = cmd->buttons & buttonsChanged;  //m_afButtonPressed ~ The changed ones still down are "pressed"
-    *reinterpret_cast<int*>(uintptr_t(localPlayer) + 0x31E4) = buttonsChanged & ~cmd->buttons; //m_afButtonReleased ~ The ones not down are "released"
 
     memory.moveHelper->SetHost(localPlayer);
     interfaces.gameMovement->StartTrackPredictionErrors(localPlayer);
