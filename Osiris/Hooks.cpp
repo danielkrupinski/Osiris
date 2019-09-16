e5d45dff11fe15c87ed81226f3f8841988e91385
Daniel Krupiński <danielkrupinski@outlook.com>
2019-09-16 21:15:12 +0200

    Call Misc::fakeBan() in createMove hook

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -124,6 +124,7 @@ static bool __stdcall createMove(float inputSampleTime, UserCmd* cmd) noexcept
     Misc::autoReload(cmd);
     Misc::updateClanTag();
     Misc::fakeVote();
+    Misc::fakeBan();
     Misc::stealNames();
     Misc::revealRanks(cmd);
     Backtrack::run(cmd);
