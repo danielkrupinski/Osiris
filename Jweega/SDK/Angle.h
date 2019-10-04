68fa2da7f50e2b63ae45f3563b4f3bab456bed18
Maksym Dilai <admin@dproject.org.ua>
2019-10-04 15:26:59 +0300

    Updated Angle class

--- Jweega/SDK/Angle.h
+++ Jweega/SDK/Angle.h
@@ -9,13 +9,13 @@ class Angle {
     Vector get(unsigned int i) noexcept {
         switch (i) {
         case 0:
-            return Vector{ cp * cy, cp * sy, -sp };
+            return Vector{ (cp * cy), (cp * sy), (-sp) };
             break;
         case 1:
-            return Vector{ (-1 * sr * sp * cy + -1 * cr * -sy), (-1 * sr * sp * sy + -1 * cr * cy), (-1 * sr * cp) };
+            return Vector{ -1 * sr * sp * cy + -1 * cr * -sy, -1 * sr * sp * sy + -1 * cr * cy, -1 * sr * cp };
             break;
         case 2:
-            return Vector{ (cr * sp * cy + -sr * -sy), (cr * sp * sy + -sr * cy), (cr * cp) };
+            return Vector{ cr * sp * cy + -sr * -sy, cr * sp * sy + -sr * cy, cr * cp };
             break;
         default:
             return Vector{ };
@@ -28,6 +28,6 @@ public:
     Vector right{ };
     Vector up{ };
 
-    Angle(const Vector& v) noexcept :sp{ sinf(v.x) }, cp{ cosf(v.x) }, sy{ sinf(v.y) }, cy{ cosf(v.y) }, sr{ sinf(v.z) }, cr{ cosf(v.z) },
+    Angle(const Vector& v) noexcept :sp{ sinf(degreesToRadians(v.x)) }, cp{ cosf(degreesToRadians(v.x)) }, sy{ sinf(degreesToRadians(v.y)) }, cy{ cosf(degreesToRadians(v.y)) }, sr{ sinf(degreesToRadians(v.z)) }, cr{ cosf(degreesToRadians(v.z)) },
                                     forward{ get(0) }, right{ get(1) }, up{ get(2) } { }
 };
