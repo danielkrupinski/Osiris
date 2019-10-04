beb95f4bd9db693d03be9f1c4296662765c8fb22
Maksym Dilai <admin@dproject.org.ua>
2019-10-04 04:59:50 +0300

    Revert "Make AntiAim legit by modifying real angles instead of fake ones"
    This reverts commit 9258474fda237d27b2f37e123d8225e43f4f4c12.

--- Jweega/Hacks/AntiAim.cpp
+++ Jweega/Hacks/AntiAim.cpp
@@ -12,7 +12,7 @@ void AntiAim::run(UserCmd* cmd, const Vector& previousViewAngles, const Vector& 
         if (config.antiAim.pitch && cmd->viewangles.x == currentViewAngles.x)
             cmd->viewangles.x = config.antiAim.pitchAngle;
 
-        if (config.antiAim.yaw && !sendPacket && interfaces.engine->getNetworkChannel()->chokedPackets && cmd->viewangles.y == currentViewAngles.y) {
+        if (config.antiAim.yaw && sendPacket && interfaces.engine->getNetworkChannel()->chokedPackets && cmd->viewangles.y == currentViewAngles.y) {
             cmd->viewangles.y = previousViewAngles.y + interfaces.entityList->getEntity(interfaces.engine->getLocalPlayer())->getMaxDesyncAngle();
             if (fabsf(cmd->sidemove) < 5.0f) {
                 if (cmd->buttons & UserCmd::IN_DUCK)
