64d070de6f310bb7281952777f918e7ba686a8a9
Maksym Dilai <admin@dproject.org.ua>
2019-10-28 07:49:17 +0200

    Revert "Remove constexpr specifier from hookProperty and unhookProperty lambdas"
    This reverts commit f0d52f01717ba8857ef180ccb1c144f74100b812.

--- Jweega/Netvars.cpp
+++ Jweega/Netvars.cpp
@@ -222,7 +222,7 @@ void Netvars::walkTable(bool unload, const char* networkName, RecvTable* recvTab
         if (!unload) {
             offsets[hash] = uint16_t(offset + prop.offset);
 
-            constexpr auto hookProperty{ [](uint32_t hash, recvProxy& originalProxy, recvProxy proxy) noexcept {
+            constexpr auto hookProperty{ [](uint32_t hash, recvProxy& originalProxy, recvProxy proxy) constexpr noexcept {
                 if (originalProxy != proxy) {
                     proxies[hash] = originalProxy;
                     originalProxy = proxy;
@@ -232,7 +232,7 @@ void Netvars::walkTable(bool unload, const char* networkName, RecvTable* recvTab
             if (auto hook{ getHook(hash) })
                 hookProperty(hash, prop.proxy, hook);
         } else {
-            constexpr auto unhookProperty{ [](recvProxy& proxy, uint32_t hash) noexcept {
+            constexpr auto unhookProperty{ [](recvProxy& proxy, uint32_t hash) constexpr noexcept {
                 proxy = proxies[hash];
             } };
 
