b4fbf105711ac125611f42c5a6d2252022758201
Zach <52088665+ZPDMiller@users.noreply.github.com>
2019-10-28 14:52:10 +1100

    Add files via upload
    Added my changes

--- Osiris/SDK/ModelRender.h
+++ Osiris/SDK/ModelRender.h
@@ -32,7 +32,7 @@ public:
         callVirtualMethod<void, Material*, int, int>(this, 1, newMaterial, 0, 0);
     }
 
-    constexpr bool isMaterialOverridden() noexcept
+	constexpr bool isMaterialOverridden() noexcept
     {
         return callVirtualMethod<bool>(this, 2);
     }
