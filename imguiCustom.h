cfec188e35ad9e6fd1a70522acfd4751dcf8165e
Zach <52088665+ZPDMiller@users.noreply.github.com>
2019-10-28 14:17:01 +1100

    Delete imguiCustom.h

--- imguiCustom.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-namespace ImGuiCustom {
-    void colorPicker(const char* name, float color[3], bool* enable = nullptr, bool* rainbow = nullptr, float* rainbowSpeed = nullptr) noexcept;
-}
