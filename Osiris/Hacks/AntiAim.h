1e34481c0f3bf8a3c64b3844344e549da008f0aa
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2019-11-03 20:19:02 -0300

    Update AntiAim.h

--- Osiris/Hacks/AntiAim.h
+++ Osiris/Hacks/AntiAim.h
@@ -6,5 +6,6 @@ struct Vector;
 namespace AntiAim {
     void run(UserCmd*, const Vector&, const Vector&, bool&) noexcept;
 }
-
+void VectorAngles(const Vector& forward, QAngle& angles);
+void AngleVectors(const QAngle& angles, Vector& forward);
 void CorrectMovement(Vector vOldAngles, UserCmd* pCmd, float fOldForward, float fOldSidemove);
