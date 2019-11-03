c242d0b0fcbf69974174a9f35f0f8ba9d8abb4f7
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2019-11-03 20:16:39 -0300

    Update Vector.h

--- Osiris/SDK/Vector.h
+++ Osiris/SDK/Vector.h
@@ -3,8 +3,172 @@
 #include <cmath>
 
 #include "matrix3x4.h"
+class QAngle
+{
+public:
 
+	QAngle(void)
+	{
+		Init();
+	}
+	QAngle(float X, float Y, float Z)
+	{
+		Init(X, Y, Z);
+	}
+	QAngle(const float* clr)
+	{
+		Init(clr[0], clr[1], clr[2]);
+	}
+
+
+	void Init(float ix = 0.0f, float iy = 0.0f, float iz = 0.0f)
+	{
+		pitch = ix;
+		yaw = iy;
+		roll = iz;
+	}
+
+	float operator[](int i) const
+	{
+		return ((float*)this)[i];
+	}
+	float& operator[](int i)
+	{
+		return ((float*)this)[i];
+	}
+
+	QAngle& operator+=(const QAngle& v)
+	{
+		pitch += v.pitch; yaw += v.yaw; roll += v.roll;
+		return *this;
+	}
+	QAngle& operator-=(const QAngle& v)
+	{
+		pitch -= v.pitch; yaw -= v.yaw; roll -= v.roll;
+		return *this;
+	}
+	QAngle& operator*=(float fl)
+	{
+		pitch *= fl;
+		yaw *= fl;
+		roll *= fl;
+		return *this;
+	}
+	QAngle& operator*=(const QAngle& v)
+	{
+		pitch *= v.pitch;
+		yaw *= v.yaw;
+		roll *= v.roll;
+		return *this;
+	}
+	QAngle& operator/=(const QAngle& v)
+	{
+		pitch /= v.pitch;
+		yaw /= v.yaw;
+		roll /= v.roll;
+		return *this;
+	}
+	QAngle& operator+=(float fl)
+	{
+		pitch += fl;
+		yaw += fl;
+		roll += fl;
+		return *this;
+	}
+	QAngle& operator/=(float fl)
+	{
+		pitch /= fl;
+		yaw /= fl;
+		roll /= fl;
+		return *this;
+	}
+	QAngle& operator-=(float fl)
+	{
+		pitch -= fl;
+		yaw -= fl;
+		roll -= fl;
+		return *this;
+	}
+
+	QAngle& operator=(const QAngle& vOther)
+	{
+		pitch = vOther.pitch; yaw = vOther.yaw; roll = vOther.roll;
+		return *this;
+	}
+
+	QAngle operator-(void) const
+	{
+		return QAngle(-pitch, -yaw, -roll);
+	}
+	QAngle operator+(const QAngle& v) const
+	{
+		return QAngle(pitch + v.pitch, yaw + v.yaw, roll + v.roll);
+	}
+	QAngle operator-(const QAngle& v) const
+	{
+		return QAngle(pitch - v.pitch, yaw - v.yaw, roll - v.roll);
+	}
+	QAngle operator*(float fl) const
+	{
+		return QAngle(pitch * fl, yaw * fl, roll * fl);
+	}
+	QAngle operator*(const QAngle& v) const
+	{
+		return QAngle(pitch * v.pitch, yaw * v.yaw, roll * v.roll);
+	}
+	QAngle operator/(float fl) const
+	{
+		return QAngle(pitch / fl, yaw / fl, roll / fl);
+	}
+	QAngle operator/(const QAngle& v) const
+	{
+		return QAngle(pitch / v.pitch, yaw / v.yaw, roll / v.roll);
+	}
+	float Length() const
+	{
+		return sqrt(pitch * pitch + yaw * yaw + roll * roll);
+	}
+	float LengthSqr(void) const
+	{
+		return (pitch * pitch + yaw * yaw + roll * roll);
+	}
+	bool IsZero(float tolerance = 0.01f) const
+	{
+		return (pitch > -tolerance && pitch < tolerance &&
+			yaw > -tolerance && yaw < tolerance &&
+			roll > -tolerance && roll < tolerance);
+	}
+
+	float Normalize() const
+	{
+		QAngle res = *this;
+		float l = res.Length();
+		if (l != 0.0f)
+		{
+			res /= l;
+		}
+		else
+		{
+			res[0] = res[1] = res[2] = 0.0f;
+		}
+		return l;
+	}
+
+	float pitch;
+	float yaw;
+	float roll;
+};
+
+inline QAngle operator*(float lhs, const QAngle& rhs)
+{
+	return rhs * lhs;
+}
+inline QAngle operator/(float lhs, const QAngle& rhs)
+{
+	return rhs / lhs;
+}
 struct Vector {
+
     constexpr operator bool() const noexcept
     {
         return x || y || z;
@@ -57,6 +221,23 @@ struct Vector {
         return Vector{ x * mul, y * mul, z * mul };
     }
 
+	float& operator[](int i)
+	{
+		return ((float*)this)[i];
+	}
+	float operator[](int i) const
+	{
+		return ((float*)this)[i];
+	}
+	bool operator==(const Vector& src) const
+	{
+		return (src.x == x) && (src.y == y) && (src.z == z);
+	}
+	bool operator!=(const Vector& src) const
+	{
+		return (src.x != x) || (src.y != y) || (src.z != z);
+	}
+
     constexpr void normalize() noexcept
     {
         x = std::isfinite(x) ? std::remainderf(x, 360.0f) : 0.0f;
@@ -85,6 +266,5 @@ struct Vector {
                        dotProduct({ mat[1][0], mat[1][1], mat[1][2] }) + mat[1][3],
                        dotProduct({ mat[2][0], mat[2][1], mat[2][2] }) + mat[2][3] };
     }
-
     float x, y, z;
 };
