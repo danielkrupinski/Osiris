ec42fbab84f1616189eb234de4092b6c1f444a46
Zach <52088665+ZPDMiller@users.noreply.github.com>
2019-11-03 11:38:54 +1100

    Added Latest Osiris Changes

--- Osiris/imguiCustom.h
+++ Osiris/imguiCustom.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "Config.h"
 
 namespace ImGuiCustom {
     void colorPicker(const char* name, float color[3], bool* enable = nullptr, bool* rainbow = nullptr, float* rainbowSpeed = nullptr) noexcept;
+	void colorPicker(const char* name, Config::ColorToggle& colorConfig) noexcept;
 }
