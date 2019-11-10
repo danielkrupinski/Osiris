bb602d07816f1903dcd70f56d143a2642a0e0036
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2019-11-10 09:41:08 -0300

    Update AntiAim.h

--- Osiris/Hacks/AntiAim.h
+++ Osiris/Hacks/AntiAim.h
@@ -9,3 +9,5 @@ namespace AntiAim {
 void VectorAngles(const Vector& forward, QAngle& angles);
 void AngleVectors(const QAngle& angles, Vector& forward);
 void CorrectMovement(Vector vOldAngles, UserCmd* pCmd, float fOldForward, float fOldSidemove);
+int distance(Vector a, Vector b);
+Vector CalcAngle(Vector src, Vector dst);
