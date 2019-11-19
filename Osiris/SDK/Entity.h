0e2ad097a3dff800e9e5099427df605452b6481e
0xE232FE <CrynogarTM@gmail.com>
2019-11-19 09:07:00 +0100

    Update Entity.h
    vtable index Update

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -101,7 +101,7 @@ public:
     constexpr Vector getEyePosition() noexcept
     {
         Vector vec{ };
-        callVirtualMethod<void, Vector&>(this, 283, vec);
+        callVirtualMethod<void, Vector&>(this, 284, vec);
         return vec;
     }
 
@@ -170,12 +170,12 @@ public:
 
     constexpr WeaponData* getWeaponData() noexcept
     {
-        return callVirtualMethod<WeaponData*>(this, 456);
+        return callVirtualMethod<WeaponData*>(this, 457);
     }
-
+	
     constexpr float getInaccuracy() noexcept
     {
-        return callVirtualMethod<float>(this, 478);
+        return callVirtualMethod<float>(this, 479);
     }
     
     VarMap* getVarMap() noexcept
