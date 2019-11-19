b1e6dd391a515314d03cca15be0236d9347825cf
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-19 22:10:58 +0100

    Add "Paracord Knife" to the Skin Changer

--- Osiris/Netvars.cpp
+++ Osiris/Netvars.cpp
@@ -130,6 +130,7 @@ static int get_new_animation(const uint32_t model, const int sequence) noexcept
     case fnv::hash("models/weapons/v_knife_ursus.mdl"):
     case fnv::hash("models/weapons/v_knife_skeleton.mdl"):
     case fnv::hash("models/weapons/v_knife_outdoor.mdl"):
+    case fnv::hash("models/weapons/v_knife_cord.mdl"):
     {
         switch (sequence)
         {
