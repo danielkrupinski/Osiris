f8f802c0f7dfd65da1ee13045810e2e467fa90ee
Daniel Krupiński <danielkrupinski@outlook.com>
2019-12-31 21:28:52 +0100

    Do not clamp forwardmove and sidemove in Misc::fixMovement()

--- Osiris/Hacks/Misc.h
+++ Osiris/Hacks/Misc.h
@@ -45,8 +45,8 @@ namespace Misc
 
             const float forwardmove = cmd->forwardmove;
             const float sidemove = cmd->sidemove;
-            cmd->forwardmove = std::clamp(cos(degreesToRadians(yawDelta)) * forwardmove + cos(degreesToRadians(yawDelta + 90.0f)) * sidemove, -450.0f, 450.0f);
-            cmd->sidemove = std::clamp(sin(degreesToRadians(yawDelta)) * forwardmove + sin(degreesToRadians(yawDelta + 90.0f)) * sidemove, -450.0f, 450.0f);
+            cmd->forwardmove = std::cos(degreesToRadians(yawDelta)) * forwardmove + std::cos(degreesToRadians(yawDelta + 90.0f)) * sidemove;
+            cmd->sidemove = std::sin(degreesToRadians(yawDelta)) * forwardmove + std::sin(degreesToRadians(yawDelta + 90.0f)) * sidemove;
         }
     }
 
