8c64477aa4d707a857155e2a123581d36dc8bfc3
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-14 17:42:02 +0100

    Check array bounds in Misc::playHitSound()

--- Osiris/Hacks/Misc.h
+++ Osiris/Hacks/Misc.h
@@ -131,15 +131,17 @@ namespace Misc
 
     constexpr void playHitSound(GameEvent* event) noexcept
     {
-        constexpr const char* hitSounds[]{
+        if (!config.misc.hitSound || interfaces.engine->getPlayerForUserID(event->getInt("attacker")) == interfaces.engine->getLocalPlayer())
+            return;
+
+        constexpr std::array hitSounds{
             "play physics/metal/metal_solid_impact_bullet2",
             "play buttons/arena_switch_press_02",
             "play training/timer_bell",
             "play physics/glass/glass_impact_bullet1"
         };
 
-        if (config.misc.hitSound
-            && interfaces.engine->getPlayerForUserID(event->getInt("attacker")) == interfaces.engine->getLocalPlayer())
+        if (static_cast<std::size_t>(config.misc.hitSound - 1) < hitSounds.size())
             interfaces.engine->clientCmdUnrestricted(hitSounds[config.misc.hitSound - 1]);
     }
 
