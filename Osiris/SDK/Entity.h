0ae78a9630dd36655dd8f35d014aa1306ed95bb4
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-17 21:30:27 +0100

    Add Entity::body netvar

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -225,6 +225,8 @@ public:
         return *reinterpret_cast<matrix3x4*>(this + 0x444);
     }
 
+    NETVAR(body, "CBaseAnimating", "m_nBody", int)
+      
     NETVAR_OFFSET(index, "CBaseEntity", "m_bIsAutoaimTarget", 4, int)
     NETVAR(modelIndex, "CBaseEntity", "m_nModelIndex", unsigned)
     NETVAR(origin, "CBaseEntity", "m_vecOrigin", Vector)
