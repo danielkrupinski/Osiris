92024491757d6fe6bdbb750b231cb4086a4ecb0f
Laxybaxy <50335046+Laxybaxy@users.noreply.github.com>
2020-01-20 19:01:19 +0100

    Add files via upload

---
+++ Osiris.cpp
@@ -0,0 +1,13 @@
+#include "Config.h"
+#include "GUI.h"
+#include "Hooks.h"
+#include "Interfaces.h"
+#include "Memory.h"
+#include "Netvars.h"
+
+Config config{ "Osiris" };
+GUI gui;
+const Interfaces interfaces;
+Memory memory;
+Netvars netvars;
+Hooks hooks;
