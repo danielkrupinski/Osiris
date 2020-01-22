2b798b8a0422a0d05a51c39a1edee6e51ff7a9b1
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-22 16:51:28 +0100

    Use std::string for storing "Custom clantag"

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -225,7 +225,7 @@ public:
         bool bunnyHop{ false };
         bool customClanTag{ false };
         bool clocktag{ false };
-        char clanTag[16]{ "" };
+        std::string clanTag;
         bool animatedClanTag{ false };
         bool fastDuck{ false };
         bool moonwalk{ false };
