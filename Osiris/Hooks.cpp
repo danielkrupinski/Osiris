e99de35044e183fe930cdbc15e60fbabdd6e6d1b
ZerGo0 <ownone93@gmail.com>
2020-01-24 19:40:55 +0100

    Update Hooks.cpp

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -225,6 +225,7 @@ static void __stdcall paintTraverse(unsigned int panel, bool forceRepaint, bool 
         Misc::spectatorList();
         Misc::watermark();        
         Visuals::hitMarker();
+        Misc::drawBombDamage();
     }
     hooks.panel.callOriginal<void, unsigned int, bool, bool>(41, panel, forceRepaint, allowForce);
 }
