c97ecce5607c8cc5c9f6a2784c64bb0a7f25e895
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-08 20:17:54 +0100

    Add MoveHelper class

--- Osiris/SDK/MoveHelper.h
+++ Osiris/SDK/MoveHelper.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Utils.h"
+
+class Entity;
+
+class MoveHelper {
+public:
+    constexpr void setHost(Entity* host) noexcept
+    {
+        return callVirtualMethod<void>(this, 1, host);
+    }
+};
