57c007895a69aa8052658a67327fca93871c14c0
pubsense <61204148+pubsense@users.noreply.github.com>
2020-02-20 15:20:02 -0500

    Update GUI.cpp

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -380,7 +380,7 @@ void GUI::renderTriggerbotWindow(bool contentOnly) noexcept
     ImGui::SameLine();
     hotkey(config.triggerbot[currentWeapon].key);
     ImGui::Checkbox("Hitchance", &config.triggerbot[currentWeapon].hitChance);
-    ImGui::SliderInt("Ammount", &config.triggerbot[currentWeapon].hitChanceAmmount);
+    ImGui::SliderInt("Ammount", &config.triggerbot[currentWeapon].hitChanceAmmount, 0, 100, "%d %");
     ImGui::Checkbox("Friendly fire", &config.triggerbot[currentWeapon].friendlyFire);
     ImGui::Checkbox("Scoped only", &config.triggerbot[currentWeapon].scopedOnly);
     ImGui::Checkbox("Ignore flash", &config.triggerbot[currentWeapon].ignoreFlash);
