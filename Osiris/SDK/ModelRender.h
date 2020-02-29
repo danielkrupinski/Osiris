d8bbdd1ee8855808ccb05a0855c3892aa75a536f
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-29 12:52:49 +0100

    Mark ModelRender::isMaterialOverridden() method as deprecated

--- Osiris/SDK/ModelRender.h
+++ Osiris/SDK/ModelRender.h
@@ -32,7 +32,7 @@ public:
         callVirtualMethod<void, Material*, int, int>(this, 1, newMaterial, 0, 0);
     }
 
-    constexpr bool isMaterialOverridden() noexcept
+    [[deprecated]] constexpr bool isMaterialOverridden() noexcept
     {
         return callVirtualMethod<bool>(this, 2);
     }
