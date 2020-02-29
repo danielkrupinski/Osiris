daebbdb9264f922bea403aa430f6167a99e0a161
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-29 12:54:47 +0100

    Update StudioRender::isInGlow() method to check material override type if there is no overridden material

--- Osiris/SDK/StudioRender.h
+++ Osiris/SDK/StudioRender.h
@@ -3,16 +3,18 @@
 #include <cstddef>
 
 #include "Material.h"
+#include "../Memory.h"
 
 class StudioRender {
-    std::byte pad[0x250];
+    std::byte pad_0[0x250];
     Material* materialOverride;
+    std::byte pad_1[0xC];
+    int overrideType;
 public:
     bool isInGlow() noexcept
     {
         if (!materialOverride)
-            return false;
-
+            return overrideType != 0; // other than OVERRIDE_NORMAL
         return std::strstr(materialOverride->getName(), "dev/glow") != nullptr;
     }
 };
