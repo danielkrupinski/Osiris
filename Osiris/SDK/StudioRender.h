83010097bbe560c3e82176d23d885ae69bf97914
Daniel Krupiński <danielkrupinski@outlook.com>
2020-02-29 21:14:59 +0100

    Add StudioRender::forcedMaterialOverride() method

--- Osiris/SDK/StudioRender.h
+++ Osiris/SDK/StudioRender.h
@@ -6,16 +6,29 @@
 #include "Material.h"
 #include "../Memory.h"
 
+enum class OverrideType {
+    Normal = 0,
+    BuildShadows,
+    DepthWrite,
+    CustomMaterial, // weapon skins
+    SsaoDepthWrite
+};
+
 class StudioRender {
     std::byte pad_0[0x250];
     Material* materialOverride;
     std::byte pad_1[0xC];
-    int overrideType;
+    OverrideType overrideType;
 public:
+    constexpr void forcedMaterialOverride(Material* material, OverrideType type = OverrideType::Normal, int index = -1) noexcept
+    {
+        callVirtualMethod<void>(this, 33, material, type, index);
+    }
+
     bool isForcedMaterialOverride() noexcept
     {
         if (!materialOverride)
-            return overrideType == 2 || overrideType == 4; // see CStudioRenderContext::IsForcedMaterialOverride
+            return overrideType == OverrideType::DepthWrite || overrideType == OverrideType::SsaoDepthWrite; // see CStudioRenderContext::IsForcedMaterialOverride
         return std::string_view{ materialOverride->getName() }.starts_with("dev/glow");
     }
 };
