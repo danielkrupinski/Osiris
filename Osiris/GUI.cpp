93780791f9c17b1073c168a74646d41ad5b4fe0f
Dapaster <60472098+dapaster@users.noreply.github.com>
2020-03-06 08:24:36 +0800

    Update GUI.cpp

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -245,8 +245,7 @@ void GUI::renderAimbotWindow(bool contentOnly) noexcept
     ImGui::SliderFloat("Smooth", &config.aimbot[currentWeapon].smooth, 1.0f, 100.0f, "%.2f");
     ImGui::SliderFloat("Recoil control x", &config.aimbot[currentWeapon].recoilControlX, 0.0f, 1.0f, "%.2f");
     ImGui::SliderFloat("Recoil control y", &config.aimbot[currentWeapon].recoilControlY, 0.0f, 1.0f, "%.2f");
-    ImGui::SliderFloat("Max aim inaccuracy", &config.aimbot[currentWeapon].maxAimInaccuracy, 0.0f, 1.0f, "%.5f", 2.0f);
-    ImGui::SliderFloat("Max shot inaccuracy", &config.aimbot[currentWeapon].maxShotInaccuracy, 0.0f, 1.0f, "%.5f", 2.0f);
+    ImGui::SliderInt("Hit chance", &config.aimbot[currentWeapon].hitchance, 0, 100, "%d");
     ImGui::InputInt("Min damage", &config.aimbot[currentWeapon].minDamage);
     config.aimbot[currentWeapon].minDamage = std::clamp(config.aimbot[currentWeapon].minDamage, 0, 250);
     ImGui::Checkbox("Killshot", &config.aimbot[currentWeapon].killshot);
