4426eaecdf8ecfdfbea172f7b5a1d2a739114f89
Dapaster <60472098+dapaster@users.noreply.github.com>
2020-03-06 08:24:07 +0800

    Update Config.cpp

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -56,8 +56,7 @@ void Config::load(size_t id) noexcept
         if (aimbotJson.isMember("Bone")) aimbotConfig.bone = aimbotJson["Bone"].asInt();
         if (aimbotJson.isMember("Recoil control X")) aimbotConfig.recoilControlX = aimbotJson["Recoil control X"].asFloat();
         if (aimbotJson.isMember("Recoil control Y")) aimbotConfig.recoilControlY = aimbotJson["Recoil control Y"].asFloat();
-        if (aimbotJson.isMember("Max aim inaccuracy")) aimbotConfig.maxAimInaccuracy = aimbotJson["Max aim inaccuracy"].asFloat();
-        if (aimbotJson.isMember("Max shot inaccuracy")) aimbotConfig.maxShotInaccuracy = aimbotJson["Max shot inaccuracy"].asFloat();
+        if (aimbotJson.isMember("Hitchance")) aimbotConfig.hitchance = aimbotJson["Hitchance"].asInt();
         if (aimbotJson.isMember("Min damage")) aimbotConfig.minDamage = aimbotJson["Min damage"].asInt();
         if (aimbotJson.isMember("Killshot")) aimbotConfig.killshot = aimbotJson["Killshot"].asBool();
         if (aimbotJson.isMember("Between shots")) aimbotConfig.betweenShots = aimbotJson["Between shots"].asBool();
@@ -938,8 +937,7 @@ void Config::save(size_t id) const noexcept
         aimbotJson["Bone"] = aimbotConfig.bone;
         aimbotJson["Recoil control X"] = aimbotConfig.recoilControlX;
         aimbotJson["Recoil control Y"] = aimbotConfig.recoilControlY;
-        aimbotJson["Max aim inaccuracy"] = aimbotConfig.maxAimInaccuracy;
-        aimbotJson["Max shot inaccuracy"] = aimbotConfig.maxShotInaccuracy;
+        aimbotJson["Hitchance"] = aimbotConfig.hitchance;
         aimbotJson["Min damage"] = aimbotConfig.minDamage;
         aimbotJson["Killshot"] = aimbotConfig.killshot;
         aimbotJson["Between shots"] = aimbotConfig.betweenShots;
