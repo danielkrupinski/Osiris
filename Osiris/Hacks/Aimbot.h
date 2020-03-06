78c435856c176b059ac18de0e4b5513aa320bf4f
Dapaster <60472098+dapaster@users.noreply.github.com>
2020-03-06 08:21:56 +0800

    Update Aimbot.h

--- Osiris/Hacks/Aimbot.h
+++ Osiris/Hacks/Aimbot.h
@@ -5,4 +5,19 @@
 namespace Aimbot {
     Vector calculateRelativeAngle(const Vector& source, const Vector& destination, const Vector& viewAngles) noexcept;
     void run(UserCmd*) noexcept;
+
+    struct Enemies 
+    {
+        int id;
+        int health;
+        float distance;
+
+        bool operator<(const Enemies& enemy) const noexcept 
+        {
+            if (health != enemy.health)
+                return health < enemy.health;
+            return distance < enemy.distance;
+        }
+        Enemies(int id, int health, float distance) noexcept : id(id), health(health), distance(distance) { }
+    };
 }
