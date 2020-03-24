a587ec6905042c2428319508f64d0e8c543cacdb
Daniel Krupiński <danielkrupinski@outlook.com>
2020-03-24 15:00:27 +0100

    Make Visuals::skybox() invulnerable to invalid config

--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include "../Config.h"
 #include "../Interfaces.h"
 #include "../Memory.h"
@@ -57,9 +59,9 @@ namespace Visuals {
 
     constexpr void skybox() noexcept
     {
-        constexpr const char* skyboxes[]{ "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam" };
+        constexpr std::array skyboxes{ "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam" };
 
-        if (config.visuals.skybox)
+        if (static_cast<std::size_t>(config.visuals.skybox - 1) < skyboxes.size())
             memory.loadSky(skyboxes[config.visuals.skybox - 1]);
         else
             memory.loadSky(interfaces.cvar->findVar("sv_skyname")->string);
