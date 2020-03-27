7c1591a7c38a8f86b88c48dd25611204df1d95be
playday3008 <titanv3585@gmail.com>
2020-03-27 10:39:49 +0100

    Edit watermark

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -161,6 +161,9 @@ void Misc::watermark() noexcept
         interfaces.surface->setTextPosition(5, 0);
         interfaces.surface->printText(L"Osiris");
 
+        interfaces.surface->setTextPosition(5, 15);
+        interfaces.surface->printText(L"dll by PlayDay");
+
         static auto frameRate = 1.0f;
         frameRate = 0.9f * frameRate + 0.1f * memory->globalVars->absoluteFrameTime;
         const auto [screenWidth, screenHeight] = interfaces.surface->getScreenSize();
