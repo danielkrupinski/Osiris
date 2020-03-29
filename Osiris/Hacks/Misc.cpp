038bc20f0423827d2bc4e34229edce33b8389b06
xpossed <34091290+xpossed-you@users.noreply.github.com>
2020-03-29 16:51:49 +0700

    clean FPS and PING flags

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -170,18 +170,20 @@ void Misc::watermark() noexcept
         static auto frameRate = 1.0f;
         frameRate = 0.9f * frameRate + 0.1f * memory->globalVars->absoluteFrameTime;
         const auto [screenWidth, screenHeight] = interfaces->surface->getScreenSize();
-        std::wstring fps{ L"FPS: " + std::to_wstring(static_cast<int>(1 / frameRate)) };
+        std::wstring fps{ std::to_wstring(static_cast<int>(1 / frameRate)) + L" fps" };
         const auto [fpsWidth, fpsHeight] = interfaces->surface->getTextSize(Surface::font, fps.c_str());
         interfaces->surface->setTextPosition(screenWidth - fpsWidth - 5, 0);
+        interfaces->surface->setTextPosition(1233, 5);
         interfaces->surface->printText(fps.c_str());
 
         float latency = 0.0f;
         if (auto networkChannel = interfaces->engine->getNetworkChannel(); networkChannel && networkChannel->getLatency(0) > 0.0f)
             latency = networkChannel->getLatency(0);
 
-        std::wstring ping{ L"PING: " + std::to_wstring(static_cast<int>(latency * 1000)) + L" ms" };
+        std::wstring ping{ std::to_wstring(static_cast<int>(latency * 1000)) + L" ms" };
         const auto pingWidth = interfaces->surface->getTextSize(Surface::font, ping.c_str()).first;
         interfaces->surface->setTextPosition(screenWidth - pingWidth - 5, fpsHeight);
+        interfaces->surface->setTextPosition(1242, 19);
         interfaces->surface->printText(ping.c_str());
     }
 }
