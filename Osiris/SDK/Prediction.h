957e37d0a9e478a16dadaf8089c0ff5e9c0f8eed
Daniel Krupiński <danielkrupinski@outlook.com>
2020-04-02 15:28:39 +0200

    Update Prediction.h to use VIRTUAL_METHOD macro

--- Osiris/SDK/Prediction.h
+++ Osiris/SDK/Prediction.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Utils.h"
+#include "VirtualMethod.h"
 
 class Entity;
 class MoveData;
@@ -9,13 +9,6 @@ struct UserCmd;
 
 class Prediction {
 public:
-    constexpr void setupMove(Entity* localPlayer, UserCmd* cmd, MoveHelper* moveHelper, MoveData* moveData) noexcept
-    {
-        callVirtualMethod<void>(this, 20, localPlayer, cmd, moveHelper, moveData);
-    }
-
-    constexpr void finishMove(Entity* localPlayer, UserCmd* cmd, MoveData* moveData) noexcept
-    {
-        callVirtualMethod<void>(this, 21, localPlayer, cmd, moveData);
-    }
+    VIRTUAL_METHOD(void, setupMove, 20, (Entity* localPlayer, UserCmd* cmd, MoveHelper* moveHelper, MoveData* moveData), (this, localPlayer, cmd, moveHelper, moveData))
+    VIRTUAL_METHOD(void, finishMove, 21, (Entity* localPlayer, UserCmd* cmd, MoveData* moveData), (this, localPlayer, cmd, moveData))
 };
