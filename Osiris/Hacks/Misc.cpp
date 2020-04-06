danielkrupinski/Osiris

commit 8e762413303e94bf5a98f843bc4ea82c80acf9d0

Update Misc::killMessage() function

Osiris/Hacks/Misc.cpp
@@ -475,7 +475,12 @@ void Misc::killMessage(GameEvent& event) noexcept
     if (!localPlayer || !localPlayer->isAlive())
         return;
 
-    if (interfaces->engine->getPlayerForUserID(event.getInt("attacker")) != localPlayer->index() || interfaces->engine->getPlayerForUserID(event.getInt("userid")) == localPlayer->index())
+    PlayerInfo localInfo;
+
+    if (!interfaces->engine->getPlayerInfo(localPlayer->index(), localInfo))
+        return;
+
+    if (event.getInt("attacker") != localInfo.userId || event.getInt("userid") == localInfo.userId)
         return;
 
     std::string cmd = "say \"";

commit 7d6521034e37795a8826becbc90cbe3d4a1a73a9

Fix sniper crosshair bug introduced in 8e762413303e94bf5a98f843bc4ea82c80acf9d0

Osiris/Hacks/Misc.cpp
@@ -133,7 +133,7 @@ void Misc::spectatorList() noexcept
 void Misc::sniperCrosshair() noexcept
 {
     static auto showSpread = interfaces->cvar->findVar("weapon_debug_spread_show");
-    showSpread->setValue(config->misc.sniperCrosshair && localPlayer && localPlayer->isScoped() ? 3 : 0);
+    showSpread->setValue(config->misc.sniperCrosshair && localPlayer && !localPlayer->isScoped() ? 3 : 0);
 }
 
 void Misc::recoilCrosshair() noexcept