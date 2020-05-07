d5407213ecd9e4c9019b27f9b56e2aebccdd4b7f
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-07 21:40:46 +0200

    Add Entity::getPlayerName() method

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -186,6 +186,29 @@ public:
         return -1;
     }
 
+    [[nodiscard]] auto getPlayerName(bool normalize) noexcept
+    {
+        std::string playerName = "unknown";
+
+        PlayerInfo playerInfo;
+        if (!interfaces->engine->getPlayerInfo(index(), playerInfo))
+            return playerName;
+
+        playerName = playerInfo.name;
+
+        if (normalize) {
+            if (wchar_t wide[128]; MultiByteToWideChar(CP_UTF8, 0, playerInfo.name, 128, wide, 128)) {
+                if (wchar_t wideNormalized[128]; NormalizeString(NormalizationKC, wide, -1, wideNormalized, 128)) {
+                    if (char nameNormalized[128]; WideCharToMultiByte(CP_UTF8, 0, wideNormalized, -1, nameNormalized, 128, nullptr, nullptr))
+                        playerName = nameNormalized;
+                }
+            }
+        }
+
+        playerName.erase(std::remove(playerName.begin(), playerName.end(), '\n'), playerName.cend());
+        return playerName;
+    }
+
     NETVAR(body, "CBaseAnimating", "m_nBody", int)
     NETVAR(hitboxSet, "CBaseAnimating", "m_nHitboxSet", int)
 
