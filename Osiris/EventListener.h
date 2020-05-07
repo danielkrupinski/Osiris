b5acaa02a8c6527b5f9e4113706b635473f23a13
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-07 21:33:19 +0200

    Add EventListener class definition

--- Osiris/EventListener.h
+++ Osiris/EventListener.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <memory>
+
+#include "SDK/GameEvent.h"
+
+class EventListener : public GameEventListener {
+public:
+    EventListener() noexcept;
+    ~EventListener();
+    void fireGameEvent(GameEvent* event);
+};
+
+inline std::unique_ptr<EventListener> eventListener;
