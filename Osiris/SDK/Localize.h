1b28a423f0b162399a1092b99f1735be095debed
Daniel Krupiński <danielkrupinski@outlook.com>
2020-05-08 08:57:19 +0200

    Add Localize::findAsUTF8() method

--- Osiris/SDK/Localize.h
+++ Osiris/SDK/Localize.h
@@ -5,4 +5,5 @@
 class Localize {
 public:
     VIRTUAL_METHOD(const wchar_t*, find, 12, (const char* tokenName), (this, tokenName))
+    VIRTUAL_METHOD(const char*, findAsUTF8, 47, (const char* tokenName), (this, tokenName))
 };
