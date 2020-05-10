3fa01747142adfdf237fd180d9982071db666b59
Cyk-Fad <62870429+Cyk-Fad@users.noreply.github.com>
2020-05-10 02:17:22 +0000

    Slow Walk update
    Slow Walk update

--- Osiris/Hacks/AntiAim.cpp
+++ Osiris/Hacks/AntiAim.cpp
@@ -5,9 +5,58 @@
 #include "../SDK/EntityList.h"
 #include "../SDK/NetworkChannel.h"
 #include "../SDK/UserCmd.h"
+#define square1( x ) ( x * x )
+
+inline float FastSqrt1(float x)
+{
+	unsigned int i = *(unsigned int*)&x;
+	i += 127 << 23;
+	i >>= 1;
+	return *(float*)&i;
+}
+
+
+
+void SlowWalk(UserCmd* get_cmd, float get_speed)
+{
+	if (get_speed <= 0.f)
+		return;
+
+	float min_speed = (float)(FastSqrt1(square1(get_cmd->forwardmove) + square1(get_cmd->sidemove) + square1(get_cmd->upmove)));
+	if (min_speed <= 0.f)
+		return;
+
+	get_cmd->buttons &= ~IN_WALK;
+
+	if (get_cmd->buttons & UserCmd::IN_DUCK)
+	{
+		const auto state = localPlayer->getAnimstate();
+
+		if (state)
+			get_speed *= 2.94117647f - (2.94117647f * state->duckAmount);
+		else
+			get_speed *= 2.94117647f;
+
+	}
+
+	if (min_speed <= get_speed)
+		return;
+
+	float speed = get_speed / min_speed;
+
+	get_cmd->forwardmove *= speed;
+	get_cmd->sidemove *= speed;
+	get_cmd->upmove *= speed;
+}
+
 
 void AntiAim::run(UserCmd* cmd, const Vector& previousViewAngles, const Vector& currentViewAngles, bool& sendPacket) noexcept
 {
+    if (config->antiAim.Slowwalk && GetAsyncKeyState(config->antiAim.slowwalkkey))
+	{
+		SlowWalk(cmd, config->antiAim.slowwalkspeed);
+	}
+    
     if (config->antiAim.enabled) {
         if (!localPlayer)
             return;
