bd9223fc23727ca64097bd43339840e0bd047759
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2020-06-04 16:18:28 -0300

    Update Entity.h

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -24,7 +24,72 @@
 #include <functional>
 
 struct AnimState;
+class CCSGOPlayerAnimState
+{
+public:
+    void* pThis;
+    char pad2[91];
+    void* pBaseEntity; //0x60
+    void* pActiveWeapon; //0x64
+    void* pLastActiveWeapon; //0x68
+    float m_flLastClientSideAnimationUpdateTime; //0x6C
+    int m_iLastClientSideAnimationUpdateFramecount; //0x70
+    float m_flEyePitch; //0x74
+    float m_flEyeYaw; //0x78
+    float m_flPitch; //0x7C
+    float m_flGoalFeetYaw; //0x80
+    float m_flCurrentFeetYaw; //0x84
+    float m_flCurrentTorsoYaw; //0x88
+    float m_flUnknownVelocityLean; //0x8C //changes when moving/jumping/hitting ground
+    float m_flLeanAmount; //0x90
+    char pad4[4]; //NaN
+    float m_flFeetCycle; //0x98 0 to 1
+    float m_flFeetYawRate; //0x9C 0 to 1
+    float m_fUnknown2;
+    float m_fDuckAmount; //0xA4
+    float m_fLandingDuckAdditiveSomething; //0xA8
+    float m_fUnknown3; //0xAC
+    Vector m_vOrigin; //0xB0, 0xB4, 0xB8
+    Vector m_vLastOrigin; //0xBC, 0xC0, 0xC4
+    float m_vVelocityX; //0xC8
+    float m_vVelocityY; //0xCC
+    char pad5[4];
+    float m_flUnknownFloat1; //0xD4 Affected by movement and direction
+    char pad6[8];
+    float m_flUnknownFloat2; //0xE0 //from -1 to 1 when moving and affected by direction
+    float m_flUnknownFloat3; //0xE4 //from -1 to 1 when moving and affected by direction
+    float m_unknown; //0xE8
+    float speed_2d; //0xEC
+    float flUpVelocity; //0xF0
+    float m_flSpeedNormalized; //0xF4 //from 0 to 1
+    float m_flFeetSpeedForwardsOrSideWays; //0xF8 //from 0 to 2. something  is 1 when walking, 2.something when running, 0.653 when crouch walking
+    float m_flFeetSpeedUnknownForwardOrSideways; //0xFC //from 0 to 3. something
+    float m_flTimeSinceStartedMoving; //0x100
+    float m_flTimeSinceStoppedMoving; //0x104
+    unsigned char m_bOnGround; //0x108
+    unsigned char m_bInHitGroundAnimation; //0x109
+    char pad7[10];
+    float m_flLastOriginZ; //0x114
+    float m_flHeadHeightOrOffsetFromHittingGroundAnimation; //0x118 from 0 to 1, is 1 when standing
+    float stopToFullRunningFractio1n; //0x11C from 0 to 1, doesnt change when walking or crouching, only running
+    char pad8[4]; //NaN
+    float m_flUnknownFraction; //0x124 affected while jumping and running, or when just jumping, 0 to 1
+    char pad9[4]; //NaN
+    float m_flUnknown3;
+    char pad10[528];
+}; //Size=0x344
 
+struct AnimationLayer
+{
+public:
+    std::byte pad[20];
+    unsigned int order;
+    unsigned int sequence;
+    std::byte pad2[4];
+    float weight;
+    std::byte pad3[8];
+    float cycle;
+};
 enum class MoveType {
     NOCLIP = 8,
     LADDER = 9
@@ -59,6 +124,7 @@ public:
     VIRTUAL_METHOD(Entity*, getObserverTarget, 294, (), (this))
     VIRTUAL_METHOD(WeaponInfo*, getWeaponData, 460, (), (this))
     VIRTUAL_METHOD(float, getInaccuracy, 482, (), (this))
+    VIRTUAL_METHOD(void, UpdateClientSideAnimation, 223, (), (this))
 
     constexpr auto getWeaponType() noexcept
     {
@@ -146,6 +212,11 @@ public:
         return *reinterpret_cast<AnimState**>(this + 0x3900);
     }
 
+    CCSGOPlayerAnimState* getAnimstate2() noexcept
+    {
+        return *reinterpret_cast<CCSGOPlayerAnimState**>(this + 0x3900);
+    }
+    
     float getMaxDesyncAngle() noexcept
     {
         const auto animState = getAnimstate();
@@ -208,7 +279,46 @@ public:
         playerName.erase(std::remove(playerName.begin(), playerName.end(), '\n'), playerName.cend());
         return playerName;
     }
+    
+    int getAnimationLayerCount() noexcept
+    {
+        return *reinterpret_cast<int*>(this + 0x298C);
+    }
 
+    AnimationLayer* animOverlays()
+    {
+        return *reinterpret_cast<AnimationLayer**>(uintptr_t(this) + 0x2980);
+    }
+
+    AnimationLayer* getAnimationLayer(int overlay) noexcept
+    {
+        return &(*reinterpret_cast<AnimationLayer**>(this + 0x2980))[overlay];
+    }
+    
+    std::array<float, 24>& pos_par()
+    {
+        return *reinterpret_cast<std::add_pointer_t<std::array<float, 24>>>((uintptr_t)this + netvars->operator[](fnv::hash("CBaseAnimating->m_flPoseParameter")));
+    }
+    
+    //can use memory for this
+    void UpdateState(CCSGOPlayerAnimState* state, Vector ang) {
+        using fn = void(__vectorcall*)(void*, void*, float, float, float, void*);
+        static auto ret = reinterpret_cast<fn>(Pattern::FindPattern("client.dll", "55 8B EC 83 E4 F8 83 EC 18 56 57 8B F9 F3 0F 11 54 24"));
+
+        if (!ret)
+            return;
+
+        ret(state, NULL, NULL, ang.y, ang.x, NULL);
+    }
+    
+    void setAbsAngle(Vector Angle)
+    {
+        typedef void(__thiscall* SetAngleFn)(void*, const Vector&);
+        static SetAngleFn SetAngle = (SetAngleFn)((DWORD)Pattern::FindPattern("client.dll", "55 8B EC 83 E4 F8 83 EC 64 53 56 57 8B F1"));
+        SetAngle(this, Angle);
+    }
+
+    NETVAR(animations, "CBaseAnimating", "m_bClientSideAnimation", bool)
     NETVAR(body, "CBaseAnimating", "m_nBody", int)
     NETVAR(hitboxSet, "CBaseAnimating", "m_nHitboxSet", int)
 
