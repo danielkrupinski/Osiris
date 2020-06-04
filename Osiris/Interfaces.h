efe8d16d0ea5d6e5cd45208a7f3d6ef2ff5526e6
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-04 09:10:21 +0200

    Update Interfaces.h to use "client.dll" module instead of "client_panorama.dll"

--- Osiris/Interfaces.h
+++ Osiris/Interfaces.h
@@ -32,14 +32,14 @@ type* name = reinterpret_cast<type*>(find(L##module, version));
 
 class Interfaces {
 public:
-    GAME_INTERFACE(Client, client, "client_panorama", "VClient018")
+    GAME_INTERFACE(Client, client, "client", "VClient018")
     GAME_INTERFACE(Cvar, cvar, "vstdlib", "VEngineCvar007")
     GAME_INTERFACE(Engine, engine, "engine", "VEngineClient014")
     GAME_INTERFACE(EngineTrace, engineTrace, "engine", "EngineTraceClient004")
-    GAME_INTERFACE(EntityList, entityList, "client_panorama", "VClientEntityList003")
+    GAME_INTERFACE(EntityList, entityList, "client", "VClientEntityList003")
     GAME_INTERFACE(GameEventManager, gameEventManager, "engine", "GAMEEVENTSMANAGER002")
-    GAME_INTERFACE(GameMovement, gameMovement, "client_panorama", "GameMovement001")
-    GAME_INTERFACE(GameUI, gameUI, "client_panorama", "GameUI011")
+    GAME_INTERFACE(GameMovement, gameMovement, "client", "GameMovement001")
+    GAME_INTERFACE(GameUI, gameUI, "client", "GameUI011")
     GAME_INTERFACE(InputSystem, inputSystem, "inputsystem", "InputSystemVersion001")
     GAME_INTERFACE(Localize, localize, "localize", "Localize_001")
     GAME_INTERFACE(MaterialSystem, materialSystem, "materialsystem", "VMaterialSystem080")
@@ -47,7 +47,7 @@ public:
     GAME_INTERFACE(ModelRender, modelRender, "engine", "VEngineModel016")
     GAME_INTERFACE(Panel, panel, "vgui2", "VGUI_Panel009")
     GAME_INTERFACE(PhysicsSurfaceProps, physicsSurfaceProps, "vphysics", "VPhysicsSurfaceProps001")
-    GAME_INTERFACE(Prediction, prediction, "client_panorama", "VClientPrediction001")
+    GAME_INTERFACE(Prediction, prediction, "client", "VClientPrediction001")
     GAME_INTERFACE(RenderView, renderView, "engine", "VEngineRenderView014")
     GAME_INTERFACE(Surface, surface, "vguimatsurface", "VGUI_Surface031")
     GAME_INTERFACE(Sound, sound, "engine", "IEngineSoundClient003")
