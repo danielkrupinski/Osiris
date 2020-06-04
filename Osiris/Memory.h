3121ca9641544b69df34a3db6436d6e9b66d7d78
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-04 09:20:38 +0200

    Fix offset argument not being added to resultant address in Memory::findPattern() method

--- Osiris/Memory.h
+++ Osiris/Memory.h
@@ -97,7 +97,7 @@ private:
                 }
 
                 if (!*second)
-                    return reinterpret_cast<std::uintptr_t>(start);
+                    return reinterpret_cast<std::uintptr_t>(const_cast<char*>(start) + offset);
             }
         }
         MessageBoxA(NULL, ("Failed to find pattern #" + std::to_string(id) + '!').c_str(), "Osiris", MB_OK | MB_ICONWARNING);
