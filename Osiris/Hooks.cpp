632fb0c4c179cc52db532073fbbecde54c4079f3
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-07 15:04:34 +0200

    Add debug assertion for return address validation in getDemoPlaybackParameters() hook

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -434,7 +434,15 @@ static DemoPlaybackParameters* __stdcall getDemoPlaybackParameters() noexcept
 {
     const auto params = hooks->engine.callOriginal<DemoPlaybackParameters*, 218>();
 
-    if (params && config->misc.revealSuspect && *static_cast<std::uint64_t*>(_ReturnAddress()) != 0x79801F74C985C88B)  // client.dll : 8B C8 85 C9 74 1F 80 79 10 00
+#ifdef _DEBUG
+    // Check if we always get the same return address
+    if (*static_cast<std::uint64_t*>(_ReturnAddress()) == 0x79801F74C985C88B) {
+        static const auto returnAddress = std::uintptr_t(_ReturnAddress());
+        assert(returnAddress == std::uintptr_t(_ReturnAddress()));
+    }
+#endif
+
+    if (params && config->misc.revealSuspect && *static_cast<std::uint64_t*>(_ReturnAddress()) != 0x79801F74C985C88B)  // client.dll : 8B C8 85 C9 74 1F 80 79 10 00 , there game decides whether to show overwatch panel
         params->anonymousPlayerIdentity = false;
 
     return params;
