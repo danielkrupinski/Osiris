5d9c7a913c9f9b04b7ef9db85c54007680b50bd6
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-09 17:19:01 +0200

    Add NetworkStringTable class

--- Osiris/SDK/NetworkStringTable.h
+++ Osiris/SDK/NetworkStringTable.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#include "VirtualMethod.h"
+
+class NetworkStringTable {
+public:
+    VIRTUAL_METHOD(int, addString, 8, (bool isServer, const char* value, int length = -1, const void* userdata = nullptr), (this, isServer, value, length, userdata))
+};
