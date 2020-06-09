19aa0c76e1557c332c6912f3b60111b5e68f6ac5
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-09 17:20:21 +0200

    Add NetworkStringTableContainer class

--- Osiris/SDK/NetworkStringTable.h
+++ Osiris/SDK/NetworkStringTable.h
@@ -6,3 +6,8 @@ class NetworkStringTable {
 public:
     VIRTUAL_METHOD(int, addString, 8, (bool isServer, const char* value, int length = -1, const void* userdata = nullptr), (this, isServer, value, length, userdata))
 };
+
+class NetworkStringTableContainer {
+public:
+    VIRTUAL_METHOD(NetworkStringTable*, findTable, 3, (const char* name), (this, name))
+};
