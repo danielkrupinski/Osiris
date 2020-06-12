59c62f87272befacb420137f8021e2c533e1b391
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-12 19:38:20 +0200

    Add ImGuiCustom::arrowButtonDisabled() function

--- Osiris/imguiCustom.h
+++ Osiris/imguiCustom.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "Config.h"
+#include "imgui/imgui.h"
 
-namespace ImGuiCustom {
+namespace ImGuiCustom
+{
     void colorPicker(const char* name, float color[3], bool* enable = nullptr, bool* rainbow = nullptr, float* rainbowSpeed = nullptr) noexcept;
     void colorPicker(const char* name, Config::ColorToggle& colorConfig) noexcept;
+    void arrowButtonDisabled(const char* id, ImGuiDir dir) noexcept;
 }
