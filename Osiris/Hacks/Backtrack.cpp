725e3e4e1f50bac79a5eba6815acd09e430534e6
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2020-06-17 08:51:08 -0300

    Add extra ms to backtrack

--- Osiris/Hacks/Backtrack.cpp
+++ Osiris/Hacks/Backtrack.cpp
@@ -37,7 +37,9 @@ void Backtrack::update(FrameStage stage) noexcept
 
             records[i].push_front(record);
 
-            while (records[i].size() > 3 && records[i].size() > static_cast<size_t>(timeToTicks(static_cast<float>(config->backtrack.timeLimit) / 1000.f)))
+            int delta = std::clamp(config->backtrack.fakeLatency ? config->backtrack.fakeLatencyAmmount : 0,0,200);
+            
+            while (records[i].size() > 3 && records[i].size() > static_cast<size_t>(timeToTicks(static_cast<float>(config->backtrack.timeLimit + delta) / 1000.f)))
                 records[i].pop_back();
 
             if (auto invalid = std::find_if(std::cbegin(records[i]), std::cend(records[i]), [](const Record & rec) { return !valid(rec.simulationTime); }); invalid != std::cend(records[i]))
