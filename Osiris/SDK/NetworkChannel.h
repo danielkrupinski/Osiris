3c32cf84fd4b5663e887f4dfecd8ef67153009b8
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2020-06-17 08:48:40 -0300

    Added network variables

--- Osiris/SDK/NetworkChannel.h
+++ Osiris/SDK/NetworkChannel.h
@@ -6,6 +6,11 @@ class NetworkChannel {
 public:
     VIRTUAL_METHOD(float, getLatency, 9, (int flow), (this, flow))
 
-    std::byte pad[44];
+    std::byte pad[24];
+    int OutSequenceNr;
+    int InSequenceNr;
+    int OutSequenceNrAck;
+    int OutReliableState;
+    int InReliableState;
     int chokedPackets;
 };
