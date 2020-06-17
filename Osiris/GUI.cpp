103b44a33b9e695851e069e802578b264572349a
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-17 16:37:44 +0200

    Replace "Style" slider in "Glow" window with a combo box

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -446,8 +446,9 @@ void GUI::renderGlowWindow(bool contentOnly) noexcept
     ImGuiCustom::colorPopup("Color", config->glow[currentItem].color, &config->glow[currentItem].rainbow, &config->glow[currentItem].rainbowSpeed);
 
     ImGui::NextColumn();
-    ImGui::PushItemWidth(220.0f);
-    ImGui::SliderInt("Style", &config->glow[currentItem].style, 0, 3);
+    ImGui::SetNextItemWidth(100.0f);
+    ImGui::Combo("Style", &config->glow[currentItem].style, "Default\0Rim3d\0Edge\0Edge Pulse\0");
+   
     ImGui::Columns(1);
     if (!contentOnly)
         ImGui::End();
