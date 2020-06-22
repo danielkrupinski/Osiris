c0a045e58a516ddd6d7615b83bb3ea822831eeb5
notgoodusename <57305491+notgoodusename@users.noreply.github.com>
2020-06-22 11:46:43 -0300

    Add to config

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -81,6 +81,8 @@ public:
         bool ignoreSmoke{ false };
         bool recoilBasedFov{ false };
         int timeLimit{ 200 };
+        bool fakeLatency{ false };
+        int fakeLatencyAmmount{ 200 };
     } backtrack;
 
     struct {
@@ -273,6 +275,7 @@ public:
         std::string customKillSound;
         std::string customHitSound;
         PurchaseList purchaseList;
+        bool svpurebypass{ false };
     } misc;
 
     struct {
