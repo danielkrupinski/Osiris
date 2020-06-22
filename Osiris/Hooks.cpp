852b538b36f76ec638feb0788c6572e60d44897d
Daniel Krupiński <danielkrupinski@outlook.com>
2020-06-22 19:25:57 +0200

    Check return address in shouldDrawFog hook only when using MinHook hooking method

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -310,6 +310,7 @@ static void __stdcall emitSound(SoundData data) noexcept
 
 static bool __stdcall shouldDrawFog() noexcept
 {
+    if constexpr (std::is_same_v<HookType, MinHook>) {
 #ifdef _DEBUG
     // Check if we always get the same return address
     if (*static_cast<std::uint32_t*>(_ReturnAddress()) == 0x6274C084) {
@@ -320,6 +321,7 @@ static bool __stdcall shouldDrawFog() noexcept
 
     if (*static_cast<std::uint32_t*>(_ReturnAddress()) != 0x6274C084)
         return hooks->clientMode.callOriginal<bool, 17>();
+    }
 
     return !config->visuals.noFog;
 }
