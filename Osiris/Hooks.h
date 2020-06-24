8d51e320fa294a4bf29d8eb149c6f0b333cd659f
DoomFish <30623715+DoomFishWasTaken@users.noreply.github.com>
2020-06-24 19:47:36 +0300

    Update Hooks.h

--- Osiris/Hooks.h
+++ Osiris/Hooks.h
@@ -25,6 +25,44 @@ public:
     std::add_pointer_t<HRESULT __stdcall(IDirect3DDevice9*, D3DPRESENT_PARAMETERS*)> originalReset;
     std::add_pointer_t<int __fastcall(SoundInfo&)> originalDispatchSound;
 
+    class Vmt {
+    public:
+        Vmt(void* const base) noexcept
+        {
+            init(base);
+        }
+
+        Vmt() = default;
+        bool init(void* const) noexcept;
+        void restore() noexcept;
+
+        template<typename T>
+        void hookAt(size_t index, T fun) const noexcept
+        {
+            if (index <= length)
+                newVmt[index + 1] = reinterpret_cast<uintptr_t>(fun);
+        }
+
+        template<typename T, std::size_t Idx, typename ...Args>
+        constexpr auto callOriginal(Args... args) const noexcept
+        {
+            return reinterpret_cast<T(__thiscall*)(void*, Args...)>(oldVmt[Idx])(base, args...);
+        }
+
+        template<typename T, typename ...Args>
+        constexpr auto getOriginal(size_t index, Args... args) const noexcept
+        {
+            return reinterpret_cast<T(__thiscall*)(void*, Args...)>(oldVmt[index]);
+        }
+    private:
+        static uintptr_t* findFreeDataPage(void* const, size_t) noexcept;
+        static auto calculateLength(uintptr_t*) noexcept;
+        void* base = nullptr;
+        uintptr_t* oldVmt = nullptr;
+        uintptr_t* newVmt = nullptr;
+        size_t length = 0;
+    };
+    
     HookType bspQuery;
     HookType client;
     HookType clientMode;
