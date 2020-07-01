f6b8f207bd1b3e6c03c60026f8141d7eb89186f6
techzin <61809691+techzin@users.noreply.github.com>
2020-07-01 10:38:58 -0300

    Add files via upload

---
+++ Osiris/ImGuiConnect.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <tchar.h>
+#include <iostream>
+#include <d3d9.h>
+#include <D3dx9tex.h>
+#include <dinput.h>
+
+#include "Imgui/imgui.h"
+#include "Imgui/imgui_internal.h"
+#include "Imgui/imgui_impl_dx9.h"
+
+#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3dx9.lib")
