319658e25e971b6a296a5debf57f7cab4069b897
Daniel Krupiński <danielkrupinski@outlook.com>
2020-07-04 15:22:44 +0200

    Remove GameEventManager::fireEventClientSide hook

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -359,19 +359,6 @@ static void __stdcall setDrawColor(int r, int g, int b, int a) noexcept
     hooks->surface.callOriginal<void, 15>(r, g, b, a);
 }
 
-static bool __stdcall fireEventClientSide(GameEvent* event) noexcept
-{
-    // TODO: remove this hook after testing the new method of overriding knife icon
-    if (event) {
-        switch (fnv::hashRuntime(event->getName())) {
-        case fnv::hash("player_death"):
-            // SkinChanger::overrideHudIcon(*event);
-            break;
-        }
-    }
-    return hooks->gameEventManager.callOriginal<bool, 9>(event);
-}
-
 struct ViewSetup {
     std::byte pad[176];
     float fov;
@@ -575,7 +562,6 @@ void Hooks::install() noexcept
     engine.hookAt(82, isPlayingDemo);
     engine.hookAt(101, getScreenAspectRatio);
     engine.hookAt(218, getDemoPlaybackParameters);
-    gameEventManager.hookAt(9, fireEventClientSide);
     modelRender.hookAt(21, drawModelExecute);
     panel.hookAt(41, paintTraverse);
     sound.hookAt(5, emitSound);
