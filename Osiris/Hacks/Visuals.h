e6ea56981e352b9814ef5a44ee984206a5304b8b
Matthew <31341992+NekoRem@users.noreply.github.com>
2020-07-07 20:38:19 +0100

    Update Visuals.h

--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -25,5 +25,6 @@ namespace Visuals
     bool removeSleeves(const char* modelName) noexcept;
     bool removeWeapons(const char* modelName) noexcept;
     void skybox() noexcept;
+    void skybox(FrameStage stage);
     void showVelocity() noexcept;
 }
