204d64d4296e54a4640d1d54cd3f69d6c7726bf0
Caillou <60858720+cailloubr@users.noreply.github.com>
2020-07-07 14:39:27 -0300

    Revert "Merge commit '06a4402b81fab9bc4ce9c4bec9874bb8bba70784'"
    This reverts commit ac4c3afa4f2c7298f9ffe59378399a1da4cae0af, reversing
    changes made to a701c1e7ac87f4f8bba1775f0770e81596965138.

--- Osiris/SDK/Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils.h"
-
-std::tuple<float, float, float> rainbowColor(float time, float speed) noexcept
-{
-    return std::make_tuple(std::sin(speed * time) * 0.5f + 0.5f,
-                           std::sin(speed * time + static_cast<float>(2 * M_PI / 3)) * 0.5f + 0.5f,
-                           std::sin(speed * time + static_cast<float>(4 * M_PI / 3)) * 0.5f + 0.5f);
-}
