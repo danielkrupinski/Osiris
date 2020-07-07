8c0551bf30f09ddaa8910d266c2bc2e60d5cb22f
Caillou <60858720+cailloubr@users.noreply.github.com>
2020-07-07 14:39:25 -0300

    Revert "Merge commit '432ea3ee6e9a8c8b22d40ed76b566791332f37b0'"
    This reverts commit 0a5c4096bcc566d5f73673e5a75ca21ee025ce37, reversing
    changes made to ac4c3afa4f2c7298f9ffe59378399a1da4cae0af.

--- Osiris/SDK/Utils.h
+++ Osiris/SDK/Utils.h
@@ -6,4 +6,4 @@
 constexpr auto degreesToRadians = [](float degrees) constexpr noexcept { return degrees * static_cast<float>(M_PI) / 180.0f; };
 constexpr auto radiansToDegrees = [](float radians) constexpr noexcept { return radians * 180.0f / static_cast<float>(M_PI); };
 
-std::tuple<float, float, float> rainbowColor(float speed) noexcept;
+std::tuple<float, float, float> rainbowColor(float time, float speed) noexcept;
