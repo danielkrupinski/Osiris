1cf207f001406c581c3f24df9f6c4121d8867d05
Daniel Krupiński <danielkrupinski@outlook.com>
2020-07-09 11:15:48 +0200

    Add Localize::convertAnsiToUnicode() virtual method

--- Osiris/SDK/Localize.h
+++ Osiris/SDK/Localize.h
@@ -5,5 +5,6 @@
 class Localize {
 public:
     VIRTUAL_METHOD(const wchar_t*, find, 12, (const char* tokenName), (this, tokenName))
+    VIRTUAL_METHOD(int, convertAnsiToUnicode, 15, (const char* ansi, wchar_t* unicode, int unicodeBufferSizeInBytes), (this, ansi, unicode, unicodeBufferSizeInBytes))
     VIRTUAL_METHOD(const char*, findAsUTF8, 47, (const char* tokenName), (this, tokenName))
 };
