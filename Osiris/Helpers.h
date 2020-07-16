2ccd0a257af915af7bb29ee0146f2537bdcfac38
Daniel Krupiński <danielkrupinski@outlook.com>
2020-07-16 15:08:20 +0200

    Add Helpers::utf8SeqLen() function

--- Osiris/Helpers.h
+++ Osiris/Helpers.h
@@ -14,4 +14,13 @@ namespace Helpers
     }
 
     ImWchar* getFontGlyphRanges() noexcept;
+
+    constexpr int utf8SeqLen(char firstByte) noexcept
+    {
+        return (firstByte & 0x80) == 0x00 ? 1 :
+               (firstByte & 0xE0) == 0xC0 ? 2 :
+               (firstByte & 0xF0) == 0xE0 ? 3 :
+               (firstByte & 0xF8) == 0xF0 ? 4 :
+               -1;
+    }
 }
