3382fb0db81d5329b2bbcb2923c135d49db10541
Daniel Krupiński <danielkrupinski@outlook.com>
2020-07-23 21:18:35 +0200

    Make HookType::callOriginal() invoke HookType::getOriginal()

--- Osiris/Hooks/VmtSwap.h
+++ Osiris/Hooks/VmtSwap.h
@@ -18,16 +18,17 @@ public:
     }
 
     template<typename T, std::size_t Idx, typename ...Args>
-    constexpr auto callOriginal(Args... args) const noexcept
-    {
-        return reinterpret_cast<T(__thiscall*)(void*, Args...)>(oldVmt[Idx])(base, args...);
-    }
-
-    template<typename T, std::size_t Idx, typename ...Args>
     constexpr auto getOriginal(Args... args) const noexcept
     {
         return reinterpret_cast<T(__thiscall*)(void*, Args...)>(oldVmt[Idx]);
     }
+
+    template<typename T, std::size_t Idx, typename ...Args>
+    constexpr auto callOriginal(Args... args) const noexcept
+    {
+        return getOriginal<T, Idx>(args...)(base, args...);
+    }
+
 private:
     void* base = nullptr;
     uintptr_t* oldVmt = nullptr;
