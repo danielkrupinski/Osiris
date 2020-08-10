danielkrupinski/Osiris

commit c0dc6ce4e6fdb8b8a11cb2184317c837b6ead6a5

Convert skin names to wide uppercase string only once to improve skin search performance

Osiris/GUI.cpp
@@ -1022,10 +1022,8 @@ void GUI::renderSkinChangerWindow(bool contentOnly) noexcept
         if (ImGui::ListBoxHeader("Paint Kit")) {
             const auto& kits = itemIndex == 1 ? SkinChanger::gloveKits : SkinChanger::skinKits;
 
-            // Case-insensitive UTF-8 compatible text filtering, when compiled in Debug mode it drops fps grately (toupper()), in Release only a bit
             const std::locale original;
-            if (!filter.empty())
-                std::locale::global(std::locale{ "en_US.utf8" });
+            std::locale::global(std::locale{ "en_US.utf8" });
 
             const auto& facet = std::use_facet<std::ctype<wchar_t>>(std::locale{});
             std::wstring filterWide(filter.length(), L'\0');
@@ -1034,34 +1032,16 @@ void GUI::renderSkinChangerWindow(bool contentOnly) noexcept
                 filterWide.resize(newLen);
             std::transform(filterWide.begin(), filterWide.end(), filterWide.begin(), [&facet](wchar_t w) { return facet.toupper(w); });
 
+            std::locale::global(original);
+
             for (std::size_t i = 0; i < kits.size(); ++i) {
-                bool passedTheFilter = filter.empty();
-
-                if (!passedTheFilter) {
-                    for (std::size_t j1 = 0, j2 = 0; j1 < kits[i].name.length() && j2 < filterWide.length();) {
-                        wchar_t w;
-                        mbstowcs(&w, kits[i].name.c_str() + j1, 1);
-                        j1 += Helpers::utf8SeqLen(kits[i].name[j1]);
-
-                        if (facet.toupper(w) != filterWide[j2])
-                            j2 = 0;
-                        else
-                            ++j2;
-
-                        if (j2 >= filterWide.length())
-                            passedTheFilter = true;
-                    }
-                }
-
-                if (passedTheFilter) {
+                if (filter.empty() || wcsstr(kits[i].nameUpperCase.c_str(), filterWide.c_str())) {
                     ImGui::PushID(i);
                     if (ImGui::Selectable(kits[i].name.c_str(), i == selected_entry.paint_kit_vector_index))
                         selected_entry.paint_kit_vector_index = i;
                     ImGui::PopID();
                 }
             }
-
-            std::locale::global(original);
             ImGui::ListBoxFooter();
         }
 
@@ -1125,10 +1105,8 @@ void GUI::renderSkinChangerWindow(bool contentOnly) noexcept
         if (ImGui::ListBoxHeader("Sticker")) {
             const auto& kits = SkinChanger::stickerKits;
 
-            // Case-insensitive UTF-8 compatible text filtering, when compiled in Debug mode it drops fps grately (toupper()), in Release only a bit
             const std::locale original;
-            if (!filter.empty())
-                std::locale::global(std::locale{ "en_US.utf8" });
+            std::locale::global(std::locale{ "en_US.utf8" });
 
             const auto& facet = std::use_facet<std::ctype<wchar_t>>(std::locale{});
             std::wstring filterWide(filter.length(), L'\0');
@@ -1137,34 +1115,16 @@ void GUI::renderSkinChangerWindow(bool contentOnly) noexcept
                 filterWide.resize(newLen);
             std::transform(filterWide.begin(), filterWide.end(), filterWide.begin(), [&facet](wchar_t w) { return facet.toupper(w); });
 
+            std::locale::global(original);
+
             for (std::size_t i = 0; i < kits.size(); ++i) {
-                bool passedTheFilter = filter.empty();
-
-                if (!passedTheFilter) {
-                    for (std::size_t j1 = 0, j2 = 0; j1 < kits[i].name.length() && j2 < filterWide.length();) {
-                        wchar_t w;
-                        mbstowcs(&w, kits[i].name.c_str() + j1, 1);
-                        j1 += Helpers::utf8SeqLen(kits[i].name[j1]);
-
-                        if (facet.toupper(w) != filterWide[j2])
-                            j2 = 0;
-                        else
-                            ++j2;
-
-                        if (j2 >= filterWide.length())
-                            passedTheFilter = true;
-                    }
-                }
-
-                if (passedTheFilter) {
+                if (filter.empty() || wcsstr(kits[i].nameUpperCase.c_str(), filterWide.c_str())) {
                     ImGui::PushID(i);
-                    if (ImGui::Selectable(kits[i].name.c_str(), i == selected_sticker.kit_vector_index))
-                        selected_sticker.kit_vector_index = i;
+                    if (ImGui::Selectable(kits[i].name.c_str(), i == selected_entry.paint_kit_vector_index))
+                        selected_entry.paint_kit_vector_index = i;
                     ImGui::PopID();
                 }
             }
-
-            std::locale::global(original);
             ImGui::ListBoxFooter();
         }
 

Osiris/Hacks/SkinChanger.cpp
@@ -26,6 +26,11 @@ void SkinChanger::initializeKits() noexcept
     const std::string gameItems{ std::istreambuf_iterator<char>{ items }, std::istreambuf_iterator<char>{ } };
     items.close();
 
+    const std::locale original;
+    std::locale::global(std::locale{ "en_US.utf8" });
+
+    const auto& facet = std::use_facet<std::ctype<wchar_t>>(std::locale{});
+  
     for (int i = 0; i <= memory->itemSystem()->getItemSchema()->paintKits.lastElement; i++) {
         const auto paintKit = memory->itemSystem()->getItemSchema()->paintKits.memory[i].value;
 
@@ -40,12 +45,22 @@ void SkinChanger::initializeKits() noexcept
                     name += '(' + gameItems.substr(weaponName + 7, pos - weaponName - 7) + ')';
                 }
             }
-            skinKits.emplace_back(paintKit->id, std::move(name));
+            std::wstring nameUpperCase(name.length(), L'\0');
+            const auto newLen = mbstowcs(nameUpperCase.data(), name.c_str(), name.length());
+            if (newLen != static_cast<std::size_t>(-1))
+                nameUpperCase.resize(newLen);
+            std::transform(nameUpperCase.begin(), nameUpperCase.end(), nameUpperCase.begin(), [&facet](wchar_t w) { return facet.toupper(w); });
+            skinKits.emplace_back(paintKit->id, std::move(name), std::move(nameUpperCase));
         } else {
             std::string_view gloveName{ paintKit->name.buffer };
             name += ' ';
             name += '(' + std::string{ gloveName.substr(0, gloveName.find('_')) } +')';
-            gloveKits.emplace_back(paintKit->id, std::move(name));
+            std::wstring nameUpperCase(name.length(), L'\0');
+            const auto newLen = mbstowcs(nameUpperCase.data(), name.c_str(), name.length());
+            if (newLen != static_cast<std::size_t>(-1))
+                nameUpperCase.resize(newLen);
+            std::transform(nameUpperCase.begin(), nameUpperCase.end(), nameUpperCase.begin(), [&facet](wchar_t w) { return facet.toupper(w); });
+            gloveKits.emplace_back(paintKit->id, std::move(name), std::move(nameUpperCase));
         }
     }
 
@@ -54,9 +69,17 @@ void SkinChanger::initializeKits() noexcept
 
     for (int i = 0; i <= memory->itemSystem()->getItemSchema()->stickerKits.lastElement; i++) {
         const auto stickerKit = memory->itemSystem()->getItemSchema()->stickerKits.memory[i].value;
-        stickerKits.emplace_back(stickerKit->id, interfaces->localize->findAsUTF8(stickerKit->id != 242 ? stickerKit->itemName.buffer + 1 : "StickerKit_dhw2014_teamdignitas_gold"));
+        std::string name = interfaces->localize->findAsUTF8(stickerKit->id != 242 ? stickerKit->itemName.buffer + 1 : "StickerKit_dhw2014_teamdignitas_gold");
+        std::wstring nameUpperCase(name.length(), L'\0');
+        const auto newLen = mbstowcs(nameUpperCase.data(), name.c_str(), name.length());
+        if (newLen != static_cast<std::size_t>(-1))
+            nameUpperCase.resize(newLen);
+        std::transform(nameUpperCase.begin(), nameUpperCase.end(), nameUpperCase.begin(), [&facet](wchar_t w) { return facet.toupper(w); });
+        stickerKits.emplace_back(stickerKit->id, std::move(name), std::move(nameUpperCase));
     }
+
     std::sort(std::next(stickerKits.begin()), stickerKits.end());
+    std::locale::global(original);
 }
 
 static std::unordered_map<std::string, const char*> iconOverrides;

Osiris/Hacks/SkinChanger.h
@@ -15,9 +15,10 @@ namespace SkinChanger
     void updateStatTrak(GameEvent& event) noexcept;
 
     struct PaintKit {
-        PaintKit(int id, std::string&& name) noexcept : id(id), name(name) { }
+        PaintKit(int id, std::string&& name, std::wstring&& nameUpperCase) noexcept : id(id), name(name), nameUpperCase(nameUpperCase) { }
         int id;
         std::string name;
+        std::wstring nameUpperCase;
 
         auto operator<(const PaintKit& other) const noexcept
         {
@@ -27,5 +28,5 @@ namespace SkinChanger
 
     inline std::vector<PaintKit> skinKits;
     inline std::vector<PaintKit> gloveKits;
-    inline std::vector<PaintKit> stickerKits{ {0, "None"} };
+    inline std::vector<PaintKit> stickerKits{ { 0, "None", L"NONE" } };
 }

commit 4744a675fc1905be089bf429ca8b5efec7dd1776

Fix sticker selectable bug introduced in c0dc6ce4e6fdb8b8a11cb2184317c837b6ead6a5

Osiris/GUI.cpp
@@ -1120,8 +1120,8 @@ void GUI::renderSkinChangerWindow(bool contentOnly) noexcept
             for (std::size_t i = 0; i < kits.size(); ++i) {
                 if (filter.empty() || wcsstr(kits[i].nameUpperCase.c_str(), filterWide.c_str())) {
                     ImGui::PushID(i);
-                    if (ImGui::Selectable(kits[i].name.c_str(), i == selected_entry.paint_kit_vector_index))
-                        selected_entry.paint_kit_vector_index = i;
+                    if (ImGui::Selectable(kits[i].name.c_str(), i == selected_sticker.kit_vector_index))
+                        selected_sticker.kit_vector_index = i;
                     ImGui::PopID();
                 }
             }