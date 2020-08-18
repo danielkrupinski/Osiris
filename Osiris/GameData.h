fcb7dd6a79164895aeb23493e14e8f0c2b0103fb
Daniel Krupiński <danielkrupinski@outlook.com>
2020-08-18 20:40:29 +0200

    Store player health in PlayerData struct

--- Osiris/GameData.h
+++ Osiris/GameData.h
@@ -106,6 +106,7 @@ struct PlayerData : BaseData {
     bool audible;
     bool spotted;
     float flashDuration;
+    int health;
     char name[128];
     std::string activeWeapon;
     std::vector<std::pair<Vector, Vector>> bones;
