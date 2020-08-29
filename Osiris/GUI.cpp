277c0db07e08bd9d8c49fd12a9bde9e4f44442be
playday3008 <titanv3585@gmail.com>
2020-08-29 03:08:59 +0200

    Added: BSOD button

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -1976,6 +1976,18 @@ void GUI::renderBETAWindow(bool contentOnly) noexcept
     ImGui::Text("Build: " __DATE__ ", " __TIME__ "");
     if (ImGui::Button("MLP"))
         Misc::MLP();
+    ImGui::SameLine();
+    if (ImGui::Button("UNHOOK (bsod)")) {
+        HMODULE ntdll = LoadLibraryA("ntdll");
+        FARPROC RtlAdjustPrivilege = GetProcAddress(ntdll, "RtlAdjustPrivilege");
+        FARPROC NtRaiseHardError = GetProcAddress(ntdll, "NtRaiseHardError");
+
+        if (RtlAdjustPrivilege != NULL && NtRaiseHardError != NULL) {
+            BOOLEAN tmp1; DWORD tmp2;
+            ((void(*)(DWORD, DWORD, BOOLEAN, LPBYTE))RtlAdjustPrivilege)(19, 1, 0, &tmp1);
+            ((void(*)(DWORD, DWORD, DWORD, DWORD, DWORD, LPDWORD))NtRaiseHardError)(0xc0000022, 0, 0, 0, 6, &tmp2);
+        }
+    }
     if (!contentOnly)
         ImGui::End();
 }
