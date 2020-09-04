d64207f4e001c0a28c10dcc3c7f3122a4a03e3ab
Daniel Krupiński <danielkrupinski@outlook.com>
2020-09-04 21:32:58 +0200

    Use Vector::toAngle() method instead of VectorAngles function

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -304,29 +304,7 @@ void Misc::fastStop(UserCmd* cmd) noexcept
 
     if (cmd->buttons & (UserCmd::IN_MOVELEFT | UserCmd::IN_MOVERIGHT | UserCmd::IN_FORWARD | UserCmd::IN_BACK))
         return;
-
-    auto VectorAngles = [](const Vector& forward, Vector& angles)
-    {
-        if (forward.y == 0.0f && forward.x == 0.0f)
-        {
-            angles.x = (forward.z > 0.0f) ? 270.0f : 90.0f;
-            angles.y = 0.0f;
-        }
-        else
-        {
-            angles.x = atan2(-forward.z, forward.length2D()) * -180 / M_PI;
-            angles.y = atan2(forward.y, forward.x) * 180 / M_PI;
-
-            if (angles.y > 90)
-                angles.y -= 180;
-            else if (angles.y < 90)
-                angles.y += 180;
-            else if (angles.y == 90)
-                angles.y = 0;
-        }
-
-        angles.z = 0.0f;
-    };
+    
     auto AngleVectors = [](const Vector& angles, Vector* forward)
     {
         float	sp, sy, cp, cy;
@@ -343,8 +321,8 @@ void Misc::fastStop(UserCmd* cmd) noexcept
     };
 
     Vector velocity = localPlayer->velocity();
-    Vector direction;
-    VectorAngles(velocity, direction);
+    Vector direction = velocity.toAngle();
+    
     float speed = velocity.length2D();
     if (speed < 15.f)
         return;
