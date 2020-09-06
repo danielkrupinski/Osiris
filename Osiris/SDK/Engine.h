0417c3169fe2578c70bcde8dcb6b0d736fd9c0ab
MrDumox <57267520+MrDumox@users.noreply.github.com>
2020-09-06 08:01:31 +0200

    Revert "KnifeBot"

--- Osiris/SDK/Engine.h
+++ Osiris/SDK/Engine.h
@@ -38,7 +38,8 @@ struct PlayerInfo {
     bool fakeplayer;
     bool hltv;
     int customfiles[4];
-    unsigned char filesDownloaded;
+    unsigned char filesdownloaded;
+    int entityIndex;
 };
 
 struct DemoPlaybackParameters {
@@ -61,7 +62,7 @@ public:
     VIRTUAL_METHOD(void*, getBSPTreeQuery, 43, (), (this))
     VIRTUAL_METHOD(const char*, getLevelName, 53, (), (this))
     VIRTUAL_METHOD(NetworkChannel*, getNetworkChannel, 78, (), (this))
-    VIRTUAL_METHOD(void, clientCmdUnrestricted, 114, (const char* cmd, bool fromConsoleOrKeybind = false), (this, cmd, fromConsoleOrKeybind))
+    VIRTUAL_METHOD(void, clientCmdUnrestricted, 114, (const char* cmd), (this, cmd, false))
     VIRTUAL_METHOD(const Matrix4x4&, worldToScreenMatrix, 37, (), (this))
 
     auto getViewAngles() noexcept
