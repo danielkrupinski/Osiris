330b1dcffab1ca659ed45322c6b34da3f7ddf5fe
Daniel Krupiński <danielkrupinski@outlook.com>
2020-10-22 20:32:59 +0200

    Define __STDCALL as a cross-platform stdcall calling convention

--- Osiris/SDK/Platform.h
+++ Osiris/SDK/Platform.h
@@ -5,12 +5,14 @@
 #define __THISCALL __thiscall
 #define __FASTCALL __fastcall
 #define __CDECL __cdecl
+#define __STDCALL __stdcall
 
 #else
 
 #define __THISCALL
 #define __FASTCALL
 #define __CDECL
+#define __STDCALL
 
 #endif
 
