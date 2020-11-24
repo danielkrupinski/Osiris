1e6650cd35e633f1acf9df084e911133a032f404
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-24 10:50:57 +0100

    Implement Memory::submitReport for linux

--- Osiris/Memory.cpp
+++ Osiris/Memory.cpp
@@ -82,6 +82,7 @@ Memory::Memory() noexcept
     findHudElement = relativeToAbsolute<decltype(findHudElement)>(findPattern(CLIENT_DLL, "\xE8????\x48\x8D\x50\xE0") + 1);
     
     disablePostProcessing = relativeToAbsolute<decltype(disablePostProcessing)>(findPattern(CLIENT_DLL, "\x80\x3D?????\x89\xB5") + 2);
+    submitReport = reinterpret_cast<decltype(submitReport)>(findPattern(CLIENT_DLL, "\x55\x48\x89\xF7\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x41\x54\x53\x48\x89\xD3\x48\x83\xEC\x58"));
 
     localPlayer.init(relativeToAbsolute<Entity**>(findPattern(CLIENT_DLL, "\x83\xFF\xFF\x48\x8B\x05") + 6));
 #endif
