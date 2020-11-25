a028a9a6f308fd9abb98e9a4802dc48d7af23058
Khajiitos <58109276+Khajiitos@users.noreply.github.com>
2020-11-25 15:35:36 +0100

    Update Config.cpp

--- Osiris/Config.cpp
+++ Osiris/Config.cpp
@@ -583,6 +583,7 @@ static void from_json(const json& j, Config::Misc& m)
     read<value_t::object>(j, "Reportbot", m.reportbot);
     read(j, "Opposite Hand Knife", m.oppositeHandKnife);
     read<value_t::object>(j, "Preserve Killfeed", m.preserveKillfeed);
+    read(j, "Auto disconnect", m.autoDisconnect);
 }
 
 static void from_json(const json& j, Config::Misc::Reportbot& r)
@@ -968,6 +969,7 @@ static void to_json(json& j, const Config::Misc& o)
     WRITE("Reportbot", reportbot);
     WRITE("Opposite Hand Knife", oppositeHandKnife);
     WRITE("Preserve Killfeed", preserveKillfeed);
+    WRITE("Auto disconnect", autoDisconnect);
 }
 
 static void to_json(json& j, const Config::Visuals::ColorCorrection& o, const Config::Visuals::ColorCorrection& dummy)
