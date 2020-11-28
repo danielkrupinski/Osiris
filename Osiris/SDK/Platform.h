ab4d4ba007c13d015ef24feb00c1ae07d72a09a8
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-28 13:27:54 +0100

    Add IS_WIN32() preprocessor macro

--- Osiris/SDK/Platform.h
+++ Osiris/SDK/Platform.h
@@ -51,7 +51,9 @@
 #ifdef _WIN32
 #define LINUX_ARGS(...)
 #define RETURN_ADDRESS(level) _ReturnAddress()
+#define IS_WIN32() true
 #else
 #define LINUX_ARGS(...) __VA_ARGS__
 #define RETURN_ADDRESS(level) __builtin_return_address(level)
+#define IS_WIN32() false
 #endif
