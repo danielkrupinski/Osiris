a54576059466cc172268ad6b19ab2dac3fe4ec72
Daniel Krupiński <danielkrupinski@outlook.com>
2020-11-30 16:37:19 +0100

    Implement "Reveal suspect" for linux

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -475,22 +475,12 @@ static const DemoPlaybackParameters* __STDCALL getDemoPlaybackParameters(LINUX_A
 {
     const auto params = hooks->engine.callOriginal<const DemoPlaybackParameters*, IS_WIN32() ? 218 : 219>();
 
-#ifdef _DEBUG
-    // Check if we always get the same return address
-    if (*static_cast<std::uint64_t*>(_ReturnAddress()) == 0x79801F74C985C88B) {
-        static const auto returnAddress = std::uintptr_t(_ReturnAddress());
-        assert(returnAddress == std::uintptr_t(_ReturnAddress()));
-    }
-#endif
-
-#ifdef _WIN32
-    if (params && config->misc.revealSuspect && *static_cast<std::uint64_t*>(_ReturnAddress()) != 0x79801F74C985C88B) { // client.dll : 8B C8 85 C9 74 1F 80 79 10 00 , there game decides whether to show overwatch panel
+    if (params && config->misc.revealSuspect && RETURN_ADDRESS() != memory->demoFileEndReached) {
         static DemoPlaybackParameters customParams;
         customParams = *params;
         customParams.anonymousPlayerIdentity = false;
         return &customParams;
     }
-#endif
 
     return params;
 }
