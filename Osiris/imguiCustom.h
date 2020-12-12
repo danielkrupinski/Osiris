17bef89b4148218db4714cf23cfadfd2d6a4bee1
Daniel Krupiński <danielkrupinski@outlook.com>
2020-12-12 18:37:20 +0100

    Add ImGui::progressBarFullWidth() function

--- Osiris/imguiCustom.h
+++ Osiris/imguiCustom.h
@@ -21,3 +21,8 @@ namespace ImGuiCustom
     void colorPicker(const char* name, ColorToggleThicknessRounding& colorConfig) noexcept;
     void arrowButtonDisabled(const char* id, ImGuiDir dir) noexcept;
 }
+
+namespace ImGui
+{
+    void progressBarFullWidth(float fraction, float height) noexcept;
+}
