ebf572bb0afb1046f70d733ef69978a65a4ad240
Daniel Krupiński <danielkrupinski@outlook.com>
2020-12-14 14:27:19 +0100

    Implement PlayerResource class

--- Osiris/SDK/PlayerResource.h
+++ Osiris/SDK/PlayerResource.h
@@ -8,3 +8,11 @@ public:
     VIRTUAL_METHOD_V(const char*, getPlayerName, 8, (int index), (this, index))
     VIRTUAL_METHOD_V(int, getPlayerHealth, 14, (int index), (this, index))
 };
+
+class PlayerResource {
+public:
+    auto getIPlayerResource() noexcept
+    {
+        return reinterpret_cast<IPlayerResource*>(uintptr_t(this) + WIN32_LINUX(0x9D8, 0xF68));
+    }
+};
