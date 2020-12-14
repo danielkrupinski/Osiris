f5d44a60ecc4cbdc49d38c5e43b43f6b25608e2e
Daniel Krupiński <danielkrupinski@outlook.com>
2020-12-14 14:26:52 +0100

    Add IPlayerResource class

--- Osiris/SDK/PlayerResource.h
+++ Osiris/SDK/PlayerResource.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include "VirtualMethod.h"
+
+class IPlayerResource {
+public:
+    VIRTUAL_METHOD_V(bool, isAlive, 5, (int index), (this, index))
+    VIRTUAL_METHOD_V(const char*, getPlayerName, 8, (int index), (this, index))
+    VIRTUAL_METHOD_V(int, getPlayerHealth, 14, (int index), (this, index))
+};
