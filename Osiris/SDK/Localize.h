7936d0664ee05bb4936d080ebb47ecd0f8c37515
Daniel Krupiński <danielkrupinski@outlook.com>
2020-12-22 17:24:08 +0100

    Add Localize::convertUnicodeToAnsi() overload returning std::string

--- Osiris/SDK/Localize.h
+++ Osiris/SDK/Localize.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include "VirtualMethod.h"
 
 class Localize {
@@ -8,4 +10,11 @@ public:
     VIRTUAL_METHOD(int, convertAnsiToUnicode, 15, (const char* ansi, wchar_t* unicode, int unicodeBufferSizeInBytes), (this, ansi, unicode, unicodeBufferSizeInBytes))
     VIRTUAL_METHOD(int, convertUnicodeToAnsi, 16, (const wchar_t* unicode, char* ansi, int ansiBufferSize), (this, unicode, ansi, ansiBufferSize))
     VIRTUAL_METHOD(const char*, findAsUTF8, 47, (const char* tokenName), (this, tokenName))
+
+    std::string convertUnicodeToAnsi(const wchar_t* unicode) noexcept
+    {
+        char buffer[4096];
+        convertUnicodeToAnsi(unicode, buffer, sizeof(buffer));
+        return buffer;
+    }
 };
