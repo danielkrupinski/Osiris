25bea9f23fb993b64e1d5716e7cdd42c1240f679
grey-stack <71586606+grey-stack@users.noreply.github.com>
2020-12-29 10:54:28 +1300

    Update Misc.cpp
    Added volume slider to hit sound

--- Osiris/Hacks/Misc.cpp
+++ Osiris/Hacks/Misc.cpp
@@ -731,6 +731,8 @@ void Misc::moonwalk(UserCmd* cmd) noexcept
 
 void Misc::playHitSound(GameEvent& event) noexcept
 {
+    char sndbuffer[128] = "";
+    
     if (!config->misc.hitSound)
         return;
 
@@ -741,16 +743,19 @@ void Misc::playHitSound(GameEvent& event) noexcept
         return;
 
     constexpr std::array hitSounds{
-        "play physics/metal/metal_solid_impact_bullet2",
-        "play buttons/arena_switch_press_02",
-        "play training/timer_bell",
-        "play physics/glass/glass_impact_bullet1"
+        "physics/metal/metal_solid_impact_bullet2",
+        "buttons/arena_switch_press_02",
+        "training/timer_bell",
+        "physics/glass/glass_impact_bullet1"
     };
-
+    
     if (static_cast<std::size_t>(config->misc.hitSound - 1) < hitSounds.size())
-        interfaces->engine->clientCmdUnrestricted(hitSounds[config->misc.hitSound - 1]);
+        sprintf_s(sndbuffer, "playvol \"%s.wav\" \"%.2f\"", hitSounds[config->misc.hitSound - 1], config->misc.hitSoundVolume);
+
     else if (config->misc.hitSound == 5)
-        interfaces->engine->clientCmdUnrestricted(("play " + config->misc.customHitSound).c_str());
+        sprintf_s(sndbuffer, "playvol \"%s.wav\" \"%.2f\"", config->misc.customHitSound.c_str(), config->misc.hitSoundVolume);
+
+    interfaces->engine->clientCmdUnrestricted(sndbuffer);
 }
 
 void Misc::killSound(GameEvent& event) noexcept
