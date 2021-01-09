0915720cd6ff23905ead5d8e39d45a97cebd4eba
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-09 13:19:43 +0100

    Combine Windows and Linux game dlls definitions using WIN32_LINUX() macro

--- Osiris/SDK/Platform.h
+++ Osiris/SDK/Platform.h
@@ -18,46 +18,31 @@
 
 #ifdef _WIN32
 
-constexpr auto CLIENT_DLL = "client";
-constexpr auto ENGINE_DLL = "engine";
-constexpr auto INPUTSYSTEM_DLL = "inputsystem";
-constexpr auto LOCALIZE_DLL = "localize";
-constexpr auto MATERIALSYSTEM_DLL = "materialsystem";
-constexpr auto SOUNDEMITTERSYSTEM_DLL = "soundemittersystem";
-constexpr auto STUDIORENDER_DLL = "studiorender";
-constexpr auto TIER0_DLL = "tier0";
-constexpr auto VGUI2_DLL = "vgui2";
-constexpr auto VGUIMATSURFACE_DLL = "vguimatsurface";
-constexpr auto VPHYSICS_DLL = "vphysics";
-constexpr auto VSTDLIB_DLL = "vstdlib";
-
-#elif __linux__
-
-constexpr auto CLIENT_DLL = "csgo/bin/linux64/client_client.so";
-constexpr auto ENGINE_DLL = "engine_client.so";
-constexpr auto INPUTSYSTEM_DLL = "inputsystem_client.so";
-constexpr auto LOCALIZE_DLL = "localize_client.so";
-constexpr auto MATERIALSYSTEM_DLL = "materialsystem_client.so";
-constexpr auto SOUNDEMITTERSYSTEM_DLL = "soundemittersystem_client.so";
-constexpr auto STUDIORENDER_DLL = "studiorender_client.so";
-constexpr auto TIER0_DLL = "libtier0_client.so";
-constexpr auto VGUI2_DLL = "vgui2_client.so";
-constexpr auto VGUIMATSURFACE_DLL = "vguimatsurface_client.so";
-constexpr auto VPHYSICS_DLL = "vphysics_client.so";
-constexpr auto VSTDLIB_DLL = "libvstdlib_client.so";
-
-#endif
-
-#ifdef _WIN32
 #define LINUX_ARGS(...)
 #define RETURN_ADDRESS() std::uintptr_t(_ReturnAddress())
 #define FRAME_ADDRESS() (std::uintptr_t(_AddressOfReturnAddress()) - sizeof(std::uintptr_t))
 #define IS_WIN32() true
 #define WIN32_LINUX(win32, linux) win32
+
 #else
+
 #define LINUX_ARGS(...) __VA_ARGS__
 #define RETURN_ADDRESS() std::uintptr_t(__builtin_return_address(0))
 #define FRAME_ADDRESS() std::uintptr_t(__builtin_frame_address(0))
 #define IS_WIN32() false
 #define WIN32_LINUX(win32, linux) linux
+
 #endif
+
+constexpr auto CLIENT_DLL = WIN32_LINUX("client", "csgo/bin/linux64/client_client.so");
+constexpr auto ENGINE_DLL = WIN32_LINUX("engine", "engine_client.so");
+constexpr auto INPUTSYSTEM_DLL = WIN32_LINUX("inputsystem", "inputsystem_client.so");
+constexpr auto LOCALIZE_DLL = WIN32_LINUX("localize", "localize_client.so");
+constexpr auto MATERIALSYSTEM_DLL = WIN32_LINUX("materialsystem", "materialsystem_client.so");
+constexpr auto SOUNDEMITTERSYSTEM_DLL = WIN32_LINUX("soundemittersystem", "soundemittersystem_client.so");
+constexpr auto STUDIORENDER_DLL = WIN32_LINUX("studiorender", "studiorender_client.so");
+constexpr auto TIER0_DLL = WIN32_LINUX("tier0", "libtier0_client.so");
+constexpr auto VGUI2_DLL = WIN32_LINUX("vgui2", "vgui2_client.so");
+constexpr auto VGUIMATSURFACE_DLL = WIN32_LINUX("vguimatsurface", "vguimatsurface_client.so");
+constexpr auto VPHYSICS_DLL = WIN32_LINUX("vphysics", "vphysics_client.so");
+constexpr auto VSTDLIB_DLL = WIN32_LINUX("vstdlib", "libvstdlib_client.so");
