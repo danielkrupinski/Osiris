92c763d366f98d135417acc89aeeffd9df9c3478
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-11 15:37:10 +0100

    Deprecate InputSystem::isButtonDown() method

--- Osiris/SDK/InputSystem.h
+++ Osiris/SDK/InputSystem.h
@@ -5,7 +5,7 @@
 class InputSystem {
 public:
     VIRTUAL_METHOD(void, enableInput, 11, (bool enable), (this, enable))
-    VIRTUAL_METHOD(bool, isButtonDown, 15, (int buttonCode), (this, buttonCode))
+    [[deprecated]] VIRTUAL_METHOD(bool, isButtonDown, 15, (int buttonCode), (this, buttonCode))
     VIRTUAL_METHOD(void, resetInputState, 39, (), (this))
     [[deprecated]] VIRTUAL_METHOD(const char*, buttonCodeToString, 40, (int buttonCode), (this, buttonCode))
 };
