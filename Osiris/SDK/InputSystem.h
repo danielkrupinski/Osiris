6732e25dbb7b6483a447f83accc4aeda6f5eaa7a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-11 15:34:47 +0100

    Remove deprecated InputSystem::virtualKeyToButtonCode() method

--- Osiris/SDK/InputSystem.h
+++ Osiris/SDK/InputSystem.h
@@ -8,15 +8,4 @@ public:
     VIRTUAL_METHOD(bool, isButtonDown, 15, (int buttonCode), (this, buttonCode))
     VIRTUAL_METHOD(void, resetInputState, 39, (), (this))
     VIRTUAL_METHOD(const char*, buttonCodeToString, 40, (int buttonCode), (this, buttonCode))
-
-    [[deprecated]] constexpr auto virtualKeyToButtonCode(int virtualKey) noexcept
-    {
-#ifdef _WIN32
-        if (virtualKey <= VK_XBUTTON2) {
-            if (virtualKey > VK_CANCEL) virtualKey--;
-            return virtualKey + 106;
-        }
-#endif
-        return VirtualMethod::call<int, 45>(this, virtualKey);
-    }
 };
