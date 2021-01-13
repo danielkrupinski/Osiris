b18a4c31248dcee5d28ec9e3e93fd174498fe7bf
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-13 14:33:53 +0100

    Add BeamInfo struct

--- Osiris/SDK/ViewRenderBeams.h
+++ Osiris/SDK/ViewRenderBeams.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include "Vector.h"
+
+class Entity;
+
+struct BeamInfo {
+    int	type;
+    Entity* startEnt;
+    int startAttachment;
+    Entity* endEnt;
+    int	endAttachment;
+    Vector start;
+    Vector end;
+    int modelIndex;
+    const char* modelName;
+    int haloIndex;
+    const char* haloName;
+    float haloScale;
+    float life;
+    float width;
+    float endWidth;
+    float fadeLength;
+    float amplitude;
+    float brightness;
+    float speed;
+    int	startFrame;
+    float frameRate;
+    float red;
+    float green;
+    float blue;
+    bool renderable;
+    int segments;
+    int	flags;
+    Vector ringCenter;
+    float ringStartRadius;
+    float ringEndRadius;
+};
