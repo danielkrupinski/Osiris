7d47c68624587aadfb51891ba30d35421a6b676e
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-13 13:04:34 +0100

    Add PlantedC4 class inheriting from Entity

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -277,3 +277,14 @@ public:
         return *reinterpret_cast<bool*>(this + 0x29E8);
     }
 };
+
+class PlantedC4 : public Entity {
+public:
+    NETVAR(c4BlowTime, "CPlantedC4", "m_flC4Blow", float)
+    NETVAR(c4TimerLength, "CPlantedC4", "m_flTimerLength", float)
+    NETVAR(c4BombSite, "CPlantedC4", "m_nBombSite", int)
+    NETVAR(c4Ticking, "CPlantedC4", "m_bBombTicking", bool)
+    NETVAR(c4DefuseCountDown, "CPlantedC4", "m_flDefuseCountDown", float)
+    NETVAR(c4DefuseLength, "CPlantedC4", "m_flDefuseLength", float)
+    NETVAR(c4Defuser, "CPlantedC4", "m_hBombDefuser", int)
+};
