53a161c3e881b1cd241b44b8f44d2e1e703707e4
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-14 19:15:43 +0100

    Listen for "bullet_impact" event

--- Osiris/EventListener.cpp
+++ Osiris/EventListener.cpp
@@ -17,6 +17,7 @@ EventListener::EventListener() noexcept
     interfaces->gameEventManager->addListener(this, "round_start");
     interfaces->gameEventManager->addListener(this, "round_freeze_end");
     interfaces->gameEventManager->addListener(this, "player_hurt");
+    interfaces->gameEventManager->addListener(this, "bullet_impact");
 
     interfaces->gameEventManager->addListener(this, "player_death");
 
