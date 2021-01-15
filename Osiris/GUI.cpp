831a73a029f565da2d272bd67ee7392af3085676
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-15 13:40:17 +0100

    Add "Bullet Tracers" color picker to "Visuals" window

--- Osiris/GUI.cpp
+++ Osiris/GUI.cpp
@@ -1006,6 +1006,8 @@ void GUI::renderVisualsWindow(bool contentOnly) noexcept
     ImGui::SliderFloat("Hit effect time", &config->visuals.hitEffectTime, 0.1f, 1.5f, "%.2fs");
     ImGui::Combo("Hit marker", &config->visuals.hitMarker, "None\0Default (Cross)\0");
     ImGui::SliderFloat("Hit marker time", &config->visuals.hitMarkerTime, 0.1f, 1.5f, "%.2fs");
+    ImGuiCustom::colorPicker("Bullet Tracers", config->visuals.bulletTracers.color.color.data(), &config->visuals.bulletTracers.enabled);
+
     ImGui::Checkbox("Color correction", &config->visuals.colorCorrection.enabled);
     ImGui::SameLine();
     bool ccPopup = ImGui::Button("Edit");
