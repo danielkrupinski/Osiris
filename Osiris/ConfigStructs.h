d42102a50192e441ba450b6951e5e1d96668e6fd
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-17 16:44:57 +0100

    Implement new format of storing colors in config file

--- Osiris/ConfigStructs.h
+++ Osiris/ConfigStructs.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <sstream>
 #include <string>
 
 #include "nlohmann/json.hpp"
@@ -167,7 +168,12 @@ static void to_json(json& j, const T& o, const T& dummy)
 
 static void to_json(json& j, const Color4& o, const Color4& dummy = {})
 {
-    WRITE("Color", color);
+    if (o.color != dummy.color) {
+        std::ostringstream s;
+        s << '#' << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(o.color[0] * 255) << static_cast<int>(o.color[1] * 255) << static_cast<int>(o.color[2] * 255);
+        j["Color"] = s.str();
+        j["Alpha"] = o.color[3];
+    }
     WRITE("Rainbow", rainbow);
     WRITE("Rainbow Speed", rainbowSpeed);
 }
@@ -281,7 +287,27 @@ static void read(const json& j, const char* key, std::unordered_map<std::string,
 
 static void from_json(const json& j, Color4& c)
 {
-    read(j, "Color", c.color);
+    if (j.contains("Color")) {
+        const auto& val = j["Color"];
+        // old format -> [1.0f, 0.0f, 0.0f, 1.0f]
+        // new format -> #ff0000 + alpha as float
+        if (val.type() == value_t::array && val.size() == c.color.size()) {
+            for (std::size_t i = 0; i < val.size(); ++i) {
+                if (!val[i].empty())
+                    val[i].get_to(c.color[i]);
+            }
+        } else if (val.type() == value_t::string) {
+            const auto str = val.get<std::string>();
+            if (str.length() == 7 && str[0] == '#') {
+                const auto color = std::strtol(str.c_str() + 1, nullptr, 16);
+                c.color[0] = ((color >> 16) & 0xFF) / 255.0f;
+                c.color[1] = ((color >> 8) & 0xFF) / 255.0f;
+                c.color[2] = (color & 0xFF) / 255.0f;
+            }
+            read(j, "Alpha", c.color[3]);
+        }
+    }
+    
     read(j, "Rainbow", c.rainbow);
     read(j, "Rainbow Speed", c.rainbowSpeed);
 }
