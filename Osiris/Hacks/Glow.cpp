0d6ef12a0f8f98ff9e74a852b8be473fabdf94b3
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-17 12:54:21 +0100

    Store glow config in a map

--- Osiris/Hacks/Glow.cpp
+++ Osiris/Hacks/Glow.cpp
@@ -18,7 +18,13 @@ struct GlowItem : ColorA {
     bool healthBased = false;
     int style = 0;
 };
-static std::array<GlowItem, 21> glowConfig;
+
+struct PlayerGlow {
+    GlowItem all, visible, occluded;
+};
+
+static std::unordered_map<std::string, PlayerGlow> playerGlowConfig;
+static std::unordered_map<std::string, GlowItem> glowConfig;
 
 static std::vector<std::pair<int, int>> customGlowEntities;
 
@@ -27,7 +33,7 @@ void Glow::render() noexcept
     if (!localPlayer)
         return;
 
-    const auto& glow = glowConfig;
+    auto& glow = glowConfig;
 
     Glow::clearCustomObjects();
 
@@ -63,7 +69,7 @@ void Glow::render() noexcept
         if (glowobject.isUnused() || !entity || entity->isDormant())
             continue;
 
-        auto applyGlow = [&glowobject](decltype(glow[0])& glow, int health = 0) noexcept
+        auto applyGlow = [&glowobject](const GlowItem& glow, int health = 0) noexcept
         {
             if (glow.enabled) {
                 glowobject.renderWhenOccluded = true;
@@ -81,13 +87,14 @@ void Glow::render() noexcept
             }
         };
 
-        auto applyPlayerGlow = [applyGlow](decltype(glow[0])& glowAll, decltype(glow[0])& glowVisible, decltype(glow[0])& glowOccluded, Entity* entity) noexcept {
-            if (glowAll.enabled)
-                applyGlow(glowAll, entity->health());
-            else if (glowVisible.enabled && entity->visibleTo(localPlayer.get()))
-                applyGlow(glowVisible, entity->health());
-            else if (glowOccluded.enabled)
-                applyGlow(glowOccluded, entity->health());
+        auto applyPlayerGlow = [applyGlow](const std::string& name, Entity* entity) noexcept {
+            const auto& cfg = playerGlowConfig[name];
+            if (cfg.all.enabled)
+                applyGlow(cfg.all, entity->health());
+            else if (cfg.visible.enabled && entity->visibleTo(localPlayer.get()))
+                applyGlow(cfg.visible, entity->health());
+            else if (cfg.occluded.enabled)
+                applyGlow(cfg.occluded, entity->health());
         };
 
         switch (entity->getClientClass()->classId) {
@@ -95,20 +102,20 @@ void Glow::render() noexcept
             if (!entity->isAlive())
                 break;
             if (auto activeWeapon{ entity->getActiveWeapon() }; activeWeapon && activeWeapon->getClientClass()->classId == ClassId::C4 && activeWeapon->c4StartedArming())
-                applyPlayerGlow(glow[6], glow[7], glow[8], entity);
+                applyPlayerGlow("Planting", entity);
             else if (entity->isDefusing())
-                applyPlayerGlow(glow[9], glow[10], glow[11], entity);
+                applyPlayerGlow("Defusing", entity);
             else if (entity == localPlayer.get())
-                applyGlow(glow[12], entity->health());
+                applyGlow(glow["Local Player"], entity->health());
             else if (entity->isOtherEnemy(localPlayer.get()))
-                applyPlayerGlow(glow[3], glow[4], glow[5], entity);
+                applyPlayerGlow("Enemies", entity);
             else
-                applyPlayerGlow(glow[0], glow[1], glow[2], entity);
+                applyPlayerGlow("Allies", entity);
             break;
-        case ClassId::C4: applyGlow(glow[14]); break;
-        case ClassId::PlantedC4: applyGlow(glow[15]); break;
-        case ClassId::Chicken: applyGlow(glow[16]); break;
-        case ClassId::EconEntity: applyGlow(glow[17]); break;
+        case ClassId::C4: applyGlow(glow["C4"]); break;
+        case ClassId::PlantedC4: applyGlow(glow["Planted C4"]); break;
+        case ClassId::Chicken: applyGlow(glow["Chickens"]); break;
+        case ClassId::EconEntity: applyGlow(glow["Defuse Kits"]); break;
 
         case ClassId::BaseCSGrenadeProjectile:
         case ClassId::BreachChargeProjectile:
@@ -118,14 +125,14 @@ void Glow::render() noexcept
         case ClassId::SensorGrenadeProjectile:
         case ClassId::SmokeGrenadeProjectile:
         case ClassId::SnowballProjectile:
-            applyGlow(glow[18]); break;
+            applyGlow(glow["Projectiles"]); break;
 
-        case ClassId::Hostage: applyGlow(glow[19]); break;
-        case ClassId::CSRagdoll: applyGlow(glow[20]); break;
+        case ClassId::Hostage: applyGlow(glow["Hostages"]); break;
+        case ClassId::CSRagdoll: applyGlow(glow["Ragdolls"]); break;
         default:
            if (entity->isWeapon()) {
-                applyGlow(glow[13]);
-                if (!glow[13].enabled) glowobject.renderWhenOccluded = false;
+                applyGlow(glow["Weapons"]);
+                if (!glow["Weapons"].enabled) glowobject.renderWhenOccluded = false;
             }
         }
     }
@@ -169,32 +176,38 @@ void Glow::drawGUI(bool contentOnly) noexcept
     static int currentCategory{ 0 };
     ImGui::PushItemWidth(110.0f);
     ImGui::PushID(0);
-    ImGui::Combo("", &currentCategory, "Allies\0Enemies\0Planting\0Defusing\0Local player\0Weapons\0C4\0Planted C4\0Chickens\0Defuse kits\0Projectiles\0Hostages\0Ragdolls\0");
+    constexpr std::array categories{ "Allies", "Enemies", "Planting", "Defusing", "Local Player", "Weapons", "C4", "Planted C4", "Chickens", "Defuse Kits", "Projectiles", "Hostages", "Ragdolls" };
+    ImGui::Combo("", &currentCategory, categories.data(), categories.size());
     ImGui::PopID();
-    static int currentItem{ 0 };
+    GlowItem* currentItem;
     if (currentCategory <= 3) {
         ImGui::SameLine();
         static int currentType{ 0 };
         ImGui::PushID(1);
         ImGui::Combo("", &currentType, "All\0Visible\0Occluded\0");
         ImGui::PopID();
-        currentItem = currentCategory * 3 + currentType;
+        auto& cfg = playerGlowConfig[categories[currentCategory]];
+        switch (currentType) {
+        case 0: currentItem = &cfg.all; break;
+        case 1: currentItem = &cfg.visible; break;
+        case 2: currentItem = &cfg.occluded; break;
+        }
     } else {
-        currentItem = currentCategory + 8;
+        currentItem = &glowConfig[categories[currentCategory]];
     }
 
     ImGui::SameLine();
-    ImGui::Checkbox("Enabled", &glowConfig[currentItem].enabled);
+    ImGui::Checkbox("Enabled", &currentItem->enabled);
     ImGui::Separator();
     ImGui::Columns(2, nullptr, false);
     ImGui::SetColumnOffset(1, 150.0f);
-    ImGui::Checkbox("Health based", &glowConfig[currentItem].healthBased);
+    ImGui::Checkbox("Health based", &currentItem->healthBased);
 
-    ImGuiCustom::colorPopup("Color", glowConfig[currentItem].color, &glowConfig[currentItem].rainbow, &glowConfig[currentItem].rainbowSpeed);
+    ImGuiCustom::colorPopup("Color", currentItem->color, &currentItem->rainbow, &currentItem->rainbowSpeed);
 
     ImGui::NextColumn();
     ImGui::SetNextItemWidth(100.0f);
-    ImGui::Combo("Style", &glowConfig[currentItem].style, "Default\0Rim3d\0Edge\0Edge Pulse\0");
+    ImGui::Combo("Style", &currentItem->style, "Default\0Rim3d\0Edge\0Edge Pulse\0");
 
     ImGui::Columns(1);
     if (!contentOnly)
@@ -209,11 +222,18 @@ static void to_json(json& j, const GlowItem& o, const GlowItem& dummy = {})
     WRITE("Style", style);
 }
 
+static void to_json(json& j, const PlayerGlow& o, const PlayerGlow& dummy = {})
+{
+    WRITE("All", all);
+    WRITE("Visible", visible);
+    WRITE("Occluded", occluded);
+}
+
 json Glow::toJson() noexcept
 {
     json j;
-    for (const auto& glowItem : glowConfig)
-        j.push_back(glowItem);
+    j["Items"] = glowConfig;
+    j["Players"] = playerGlowConfig;
     return j;
 }
 
@@ -226,17 +246,21 @@ static void from_json(const json& j, GlowItem& g)
     read(j, "Style", g.style);
 }
 
+static void from_json(const json& j, PlayerGlow& g)
+{
+    from_json(j["All"], g.all);
+    from_json(j["Visible"], g.visible);
+    from_json(j["Occluded"], g.occluded);
+}
+
 void Glow::fromJson(const json& j) noexcept
 {
-    if (j.type() == value_t::array && j.size() == glowConfig.size()) {
-        for (std::size_t i = 0; i < j.size(); ++i) {
-            if (!j[i].empty())
-                j[i].get_to(glowConfig[i]);
-        }
-    }
+    read(j, "Items", glowConfig);
+    read(j, "Players", playerGlowConfig);
 }
 
 void Glow::resetConfig() noexcept
 {
     glowConfig = {};
+    playerGlowConfig = {};
 }
