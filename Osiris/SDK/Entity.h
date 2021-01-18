98fe04e9b8ac2c547760b9fb70f624a22959369a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-18 21:13:07 +0100

    Add Entity::shouldDraw() virtual method

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -56,9 +56,9 @@ public:
     VIRTUAL_METHOD(int, index, 10, (), (this + sizeof(uintptr_t) * 2))
     VIRTUAL_METHOD(void, setDestroyedOnRecreateEntities, 13, (), (this + sizeof(uintptr_t) * 2))
 
+    VIRTUAL_METHOD(bool, shouldDraw, WIN32_LINUX(3, 149), (), (this + WIN32_LINUX(sizeof(uintptr_t), 0)))
     VIRTUAL_METHOD(const Model*, getModel, 8, (), (this + sizeof(uintptr_t)))
     VIRTUAL_METHOD(const matrix3x4&, toWorldTransform, 32, (), (this + sizeof(uintptr_t)))
-
     VIRTUAL_METHOD(bool, getAttachment, WIN32_LINUX(35, 122), (int index, Vector& origin, Vector& angles), (this + WIN32_LINUX(sizeof(uintptr_t), 0), index, std::ref(origin), std::ref(angles)))
 
     VIRTUAL_METHOD_V(int&, handle, 2, (), (this))
