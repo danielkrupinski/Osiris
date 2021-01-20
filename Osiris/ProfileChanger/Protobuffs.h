ec6cb40bf799aba2d8515cb65aa6b905b01fcca9
caiocinel <60858720+caiocinel@users.noreply.github.com>
2021-01-19 21:16:46 -0300

    Delete PreSendMessage Funciton

--- Osiris/ProfileChanger/Protobuffs.h
+++ Osiris/ProfileChanger/Protobuffs.h
@@ -7,7 +7,6 @@ class Protobuffs
 public:
 	static void WritePacket(std::string packet, void* thisPtr, void* oldEBP, void* pubDest, uint32_t cubDest, uint32_t* pcubMsgSize);
 	void ReceiveMessage(void* thisPtr, void* oldEBP, uint32_t messageType, void* pubDest, uint32_t cubDest, uint32_t* pcubMsgSize);
-	bool PreSendMessage(uint32_t& unMsgType, void* pubData, uint32_t& cubData);
 	bool SendClientHello();
 	bool SendMatchmakingClient2GCHello();
 };
