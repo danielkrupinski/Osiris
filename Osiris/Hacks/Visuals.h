78c2011f7dd0790fcfaaffdce811faa46320e653
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-25 17:22:13 +0100

    Implement Visuals::drawMolotovHull() function

--- Osiris/Hacks/Visuals.h
+++ Osiris/Hacks/Visuals.h
@@ -27,7 +27,8 @@ namespace Visuals
     bool removeWeapons(const char* modelName) noexcept;
     void skybox(FrameStage stage) noexcept;
     void bulletTracer(GameEvent& event) noexcept;
-    
+    void drawMolotovHull(ImDrawList* drawList) noexcept;
+
     inline constexpr std::array skyboxList{ "Default", "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam", "sky_lunacy", "sky_hr_aztec" };
 
     void updateInput() noexcept;
