4ad983237480424d880094e83ae35209ec1e03c3
Daniel Krupiński <danielkrupinski@outlook.com>
2021-01-27 20:38:41 +0100

    Correct BaseFileSystem::size() index on linux

--- Osiris/SDK/FileSystem.h
+++ Osiris/SDK/FileSystem.h
@@ -7,5 +7,5 @@ public:
     VIRTUAL_METHOD(int, read, 0, (void* output, int size, void* handle), (this, output, size, handle))
     VIRTUAL_METHOD(void*, open, 2, (const char* fileName, const char* options, const char* pathID), (this, fileName, options, pathID))
     VIRTUAL_METHOD(void, close, 3, (void* handle), (this, handle))
-    VIRTUAL_METHOD(unsigned int, size, 7, (void* handle), (this, handle))
+    VIRTUAL_METHOD(unsigned int, size, WIN32_LINUX(7, 6), (void* handle), (this, handle))
 };
