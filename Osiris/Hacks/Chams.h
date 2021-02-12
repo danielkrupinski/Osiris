7375adeb34d2ecde7a0803013dfa030ea638f280
SuperK2-OS <67206656+SuperK2-OS@users.noreply.github.com>
2021-02-12 21:33:57 +0100

    Update Chams.h

--- Osiris/Hacks/Chams.h
+++ Osiris/Hacks/Chams.h
@@ -11,47 +11,13 @@ class Material;
 
 class Chams {
 public:
-    Chams() noexcept;
     bool render(void*, void*, const ModelRenderInfo&, matrix3x4*) noexcept;
+    static void updateInput() noexcept;
 private:
     void renderPlayer(Entity* player) noexcept;
     void renderWeapons() noexcept;
     void renderHands() noexcept;
     void renderSleeves() noexcept;
-
-    Material* normal;
-    Material* flat;
-    Material* animated;
-    Material* platinum;
-    Material* glass;
-    Material* crystal;
-    Material* chrome;
-    Material* silver;
-    Material* gold;
-    Material* plastic;
-    Material* glow;
-    Material* pearlescent;
-    Material* metallic;
-
-    constexpr auto dispatchMaterial(int id) const noexcept
-    {
-        switch (id) {
-        default:
-        case 0: return normal;
-        case 1: return flat;
-        case 2: return animated;
-        case 3: return platinum;
-        case 4: return glass;
-        case 5: return chrome;
-        case 6: return crystal;
-        case 7: return silver;
-        case 8: return gold;
-        case 9: return plastic;
-        case 10: return glow;
-        case 11: return pearlescent;
-        case 12: return metallic;
-        }
-    }
 
     bool appliedChams;
     void* ctx;
