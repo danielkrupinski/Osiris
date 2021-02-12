26fe74710c60536640b2837b1309080b7b1cd9f3
SuperK2-OS <67206656+SuperK2-OS@users.noreply.github.com>
2021-02-12 21:30:37 +0100

    Update Backtrack.cpp

--- Osiris/Hacks/Backtrack.cpp
+++ Osiris/Hacks/Backtrack.cpp
@@ -9,6 +9,15 @@
 #include "../SDK/NetworkChannel.h"
 #include "../SDK/UserCmd.h"
 
+#if OSIRIS_BACKTRACK()
+
+struct BacktrackConfig {
+    bool enabled = false;
+    bool ignoreSmoke = false;
+    bool recoilBasedFov = false;
+    int timeLimit = 200;
+} backtrackConfig;
+
 static std::array<std::deque<Backtrack::Record>, 65> records;
 
 struct Cvars {
@@ -23,10 +32,15 @@ struct Cvars {
 
 static Cvars cvars;
 
+static auto timeToTicks(float time) noexcept
+{
+    return static_cast<int>(0.5f + time / memory->globalVars->intervalPerTick);
+}
+
 void Backtrack::update(FrameStage stage) noexcept
 {
     if (stage == FrameStage::RENDER_START) {
-        if (!config->backtrack.enabled || !localPlayer || !localPlayer->isAlive()) {
+        if (!backtrackConfig.enabled || !localPlayer || !localPlayer->isAlive()) {
             for (auto& record : records)
                 record.clear();
             return;
@@ -50,7 +64,7 @@ void Backtrack::update(FrameStage stage) noexcept
 
             records[i].push_front(record);
 
-            while (records[i].size() > 3 && records[i].size() > static_cast<size_t>(timeToTicks(static_cast<float>(config->backtrack.timeLimit) / 1000.f)))
+            while (records[i].size() > 3 && records[i].size() > static_cast<size_t>(timeToTicks(static_cast<float>(backtrackConfig.timeLimit) / 1000.f)))
                 records[i].pop_back();
 
             if (auto invalid = std::find_if(std::cbegin(records[i]), std::cend(records[i]), [](const Record & rec) { return !valid(rec.simulationTime); }); invalid != std::cend(records[i]))
@@ -59,9 +73,15 @@ void Backtrack::update(FrameStage stage) noexcept
     }
 }
 
+static float getLerp() noexcept
+{
+    auto ratio = std::clamp(cvars.interpRatio->getFloat(), cvars.minInterpRatio->getFloat(), cvars.maxInterpRatio->getFloat());
+    return (std::max)(cvars.interp->getFloat(), (ratio / ((cvars.maxUpdateRate) ? cvars.maxUpdateRate->getFloat() : cvars.updateRate->getFloat())));
+}
+
 void Backtrack::run(UserCmd* cmd) noexcept
 {
-    if (!config->backtrack.enabled)
+    if (!backtrackConfig.enabled)
         return;
 
     if (!(cmd->buttons & UserCmd::IN_ATTACK))
@@ -88,7 +108,7 @@ void Backtrack::run(UserCmd* cmd) noexcept
 
         const auto& origin = entity->getAbsOrigin();
 
-        auto angle = Aimbot::calculateRelativeAngle(localPlayerEyePosition, origin, cmd->viewangles + (config->backtrack.recoilBasedFov ? aimPunch : Vector{ }));
+        auto angle = Aimbot::calculateRelativeAngle(localPlayerEyePosition, origin, cmd->viewangles + (backtrackConfig.recoilBasedFov ? aimPunch : Vector{ }));
         auto fov = std::hypotf(angle.x, angle.y);
         if (fov < bestFov) {
             bestFov = fov;
@@ -99,7 +119,7 @@ void Backtrack::run(UserCmd* cmd) noexcept
     }
 
     if (bestTarget) {
-        if (records[bestTargetIndex].size() <= 3 || (!config->backtrack.ignoreSmoke && memory->lineGoesThroughSmoke(localPlayer->getEyePosition(), bestTargetOrigin, 1)))
+        if (records[bestTargetIndex].size() <= 3 || (!backtrackConfig.ignoreSmoke && memory->lineGoesThroughSmoke(localPlayer->getEyePosition(), bestTargetOrigin, 1)))
             return;
 
         bestFov = 255.f;
@@ -109,7 +129,7 @@ void Backtrack::run(UserCmd* cmd) noexcept
             if (!valid(record.simulationTime))
                 continue;
 
-            auto angle = Aimbot::calculateRelativeAngle(localPlayerEyePosition, record.origin, cmd->viewangles + (config->backtrack.recoilBasedFov ? aimPunch : Vector{ }));
+            auto angle = Aimbot::calculateRelativeAngle(localPlayerEyePosition, record.origin, cmd->viewangles + (backtrackConfig.recoilBasedFov ? aimPunch : Vector{ }));
             auto fov = std::hypotf(angle.x, angle.y);
             if (fov < bestFov) {
                 bestFov = fov;
@@ -125,15 +145,11 @@ void Backtrack::run(UserCmd* cmd) noexcept
     }
 }
 
-const std::deque<Backtrack::Record>& Backtrack::getRecords(std::size_t index) noexcept
+const std::deque<Backtrack::Record>* Backtrack::getRecords(std::size_t index) noexcept
 {
-    return records[index];
-}
-
-float Backtrack::getLerp() noexcept
-{
-    auto ratio = std::clamp(cvars.interpRatio->getFloat(), cvars.minInterpRatio->getFloat(), cvars.maxInterpRatio->getFloat());
-    return (std::max)(cvars.interp->getFloat(), (ratio / ((cvars.maxUpdateRate) ? cvars.maxUpdateRate->getFloat() : cvars.updateRate->getFloat())));
+    if (!backtrackConfig.enabled)
+        return nullptr;
+    return &records[index];
 }
 
 bool Backtrack::valid(float simtime) noexcept
@@ -146,11 +162,6 @@ bool Backtrack::valid(float simtime) noexcept
     return std::abs(delta) <= 0.2f;
 }
 
-int Backtrack::timeToTicks(float time) noexcept
-{
-    return static_cast<int>(0.5f + time / memory->globalVars->intervalPerTick);
-}
-
 void Backtrack::init() noexcept
 {
     cvars.updateRate = interfaces->cvar->findVar("cl_updaterate");
@@ -161,3 +172,97 @@ void Backtrack::init() noexcept
     cvars.maxInterpRatio = interfaces->cvar->findVar("sv_client_max_interp_ratio");
     cvars.maxUnlag = interfaces->cvar->findVar("sv_maxunlag");
 }
+
+static bool backtrackWindowOpen = false;
+
+void Backtrack::menuBarItem() noexcept
+{
+    if (ImGui::MenuItem("Backtrack")) {
+        backtrackWindowOpen = true;
+        ImGui::SetWindowFocus("Backtrack");
+        ImGui::SetWindowPos("Backtrack", { 100.0f, 100.0f });
+    }
+}
+
+void Backtrack::tabItem() noexcept
+{
+    if (ImGui::BeginTabItem("Backtrack")) {
+        drawGUI(true);
+        ImGui::EndTabItem();
+    }
+}
+
+void Backtrack::drawGUI(bool contentOnly) noexcept
+{
+    if (!contentOnly) {
+        if (!backtrackWindowOpen)
+            return;
+        ImGui::SetNextWindowSize({ 0.0f, 0.0f });
+        ImGui::Begin("Backtrack", &backtrackWindowOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+    }
+    ImGui::Checkbox("Enabled", &backtrackConfig.enabled);
+    ImGui::Checkbox("Ignore smoke", &backtrackConfig.ignoreSmoke);
+    ImGui::Checkbox("Recoil based fov", &backtrackConfig.recoilBasedFov);
+    ImGui::PushItemWidth(220.0f);
+    ImGui::SliderInt("Time limit", &backtrackConfig.timeLimit, 1, 200, "%d ms");
+    ImGui::PopItemWidth();
+    if (!contentOnly)
+        ImGui::End();
+}
+
+static void to_json(json& j, const BacktrackConfig& o, const BacktrackConfig& dummy = {})
+{
+    WRITE("Enabled", enabled);
+    WRITE("Ignore smoke", ignoreSmoke);
+    WRITE("Recoil based fov", recoilBasedFov);
+    WRITE("Time limit", timeLimit);
+}
+
+json Backtrack::toJson() noexcept
+{
+    json j;
+    to_json(j, backtrackConfig);
+    return j;
+}
+
+static void from_json(const json& j, BacktrackConfig& b)
+{
+    read(j, "Enabled", b.enabled);
+    read(j, "Ignore smoke", b.ignoreSmoke);
+    read(j, "Recoil based fov", b.recoilBasedFov);
+    read(j, "Time limit", b.timeLimit);
+}
+
+void Backtrack::fromJson(const json& j) noexcept
+{
+    from_json(j, backtrackConfig);
+}
+
+void Backtrack::resetConfig() noexcept
+{
+    backtrackConfig = {};
+}
+
+#else
+
+namespace Backtrack
+{
+    void update(FrameStage) noexcept {}
+    void run(UserCmd*) noexcept {}
+
+    const std::deque<Record>* getRecords(std::size_t index) noexcept { return nullptr; }
+    bool valid(float simtime) noexcept { return false; }
+    void init() noexcept {}
+
+    // GUI
+    void menuBarItem() noexcept {}
+    void tabItem() noexcept {}
+    void drawGUI(bool contentOnly) noexcept {}
+
+    // Config
+    json toJson() noexcept { return {}; }
+    void fromJson(const json& j) noexcept {}
+    void resetConfig() noexcept {}
+}
+
+#endif
