9c283e06ae737cb24fb0944093778252f9564129
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-02 20:14:34 +0100

    OpenGL3: Improve quality of skin preview images by generating mipmaps

--- Osiris/imgui/imgui_impl_opengl3.cpp
+++ Osiris/imgui/imgui_impl_opengl3.cpp
@@ -649,13 +649,14 @@ ImTextureID ImGui_CreateTextureRGBA(int width, int height, const unsigned char* 
     glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 #ifdef GL_UNPACK_ROW_LENGTH
     glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
 #endif
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
     glBindTexture(GL_TEXTURE_2D, last_texture);
 
     return reinterpret_cast<ImTextureID>(texture);
