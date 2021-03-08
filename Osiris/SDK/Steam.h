97291674c2149a2cd68a362caa74b68615bc7e6a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-08 18:17:29 +0100

    Add SteamUtils class

--- Osiris/SDK/Steam.h
+++ Osiris/SDK/Steam.h
@@ -8,3 +8,8 @@ class SteamFriends {
 public:
 	VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
 };
+
+class SteamUtils {
+public:
+	VIRTUAL_METHOD(bool, getImageRGBA, 6, (int image, std::uint8_t* buff, int buffSize), (this, image, buff, buffSize))
+};
