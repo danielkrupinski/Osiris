787299cde3915767cb5841173196a6b3daf3ee8e
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-08 18:17:04 +0100

    Add SteamFriends class

--- Osiris/SDK/Steam.h
+++ Osiris/SDK/Steam.h
@@ -1,1 +1,10 @@
 #pragma once
+
+#include <cstdint>
+
+#include "VirtualMethod.h"
+
+class SteamFriends {
+public:
+	VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
+};
