6877daf4ad40d8c29a074ccfdec37ff1dcd36fa7
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-10 12:44:24 +0100

    Convert tabs to spaces in SDK/Steam.h

--- Osiris/SDK/Steam.h
+++ Osiris/SDK/Steam.h
@@ -6,17 +6,17 @@
 
 class SteamFriends {
 public:
-	VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
+    VIRTUAL_METHOD(int, getSmallFriendAvatar, 34, (std::uint64_t steamID), (this, steamID))
 };
 
 class SteamUtils {
 public:
-	VIRTUAL_METHOD(bool, getImageRGBA, 6, (int image, std::uint8_t* buff, int buffSize), (this, image, buff, buffSize))
+    VIRTUAL_METHOD(bool, getImageRGBA, 6, (int image, std::uint8_t* buff, int buffSize), (this, image, buff, buffSize))
 };
 
 struct SteamAPIContext {
-	void* steamClient;
-	void* steamUser;
-	SteamFriends* steamFriends;
-	SteamUtils* steamUtils;
+    void* steamClient;
+    void* steamUser;
+    SteamFriends* steamFriends;
+    SteamUtils* steamUtils;
 };
