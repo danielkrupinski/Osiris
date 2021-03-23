432101d2aa98295ade0770468601fde2ca15157a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-23 19:50:27 +0100

    Add ClientMode class

--- Osiris/SDK/ClientMode.h
+++ Osiris/SDK/ClientMode.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <cstdint>
+
+#include "Platform.h"
+
+class HudChat {};
+
+class ClientMode {
+public:
+    auto getHudChat() noexcept
+    {
+        return reinterpret_cast<HudChat**>(std::uintptr_t(this) + WIN32_LINUX(28, 48));
+    }
+};
