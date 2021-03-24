86dd5ea865e8b29b5942caebe9991253f736617f
Daniel Krupiński <danielkrupinski@outlook.com>
2021-03-24 20:43:08 +0100

    Add HudChat class

--- Osiris/SDK/ClientMode.h
+++ Osiris/SDK/ClientMode.h
@@ -4,12 +4,19 @@
 
 #include "Platform.h"
 
-class HudChat {};
+class HudChat {
+public:
+    template <typename... Args>
+    void printf(int filter, const char* fmt, Args... args) noexcept
+    {
+        (*reinterpret_cast<void(__CDECL***)(void*, int, const char*, ...)>(this))[WIN32_LINUX(26, 29)](this, filter, fmt, args...);
+    }
+};
 
 class ClientMode {
 public:
     auto getHudChat() noexcept
     {
-        return reinterpret_cast<HudChat**>(std::uintptr_t(this) + WIN32_LINUX(28, 48));
+        return *reinterpret_cast<HudChat**>(std::uintptr_t(this) + WIN32_LINUX(28, 48));
     }
 };
