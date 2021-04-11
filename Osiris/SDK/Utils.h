01dfa2961fea2bd9108978326af7a241528d8c4a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-04-11 11:34:39 +0200

    Deprecate degreesToRadians() function

--- Osiris/SDK/Utils.h
+++ Osiris/SDK/Utils.h
@@ -3,7 +3,7 @@
 #include <numbers>
 #include <tuple>
 
-constexpr auto degreesToRadians = [](float degrees) constexpr noexcept { return degrees * std::numbers::pi_v<float> / 180.0f; };
+[[deprecated]] constexpr auto degreesToRadians = [](float degrees) constexpr noexcept { return degrees * std::numbers::pi_v<float> / 180.0f; };
 constexpr auto radiansToDegrees = [](float radians) constexpr noexcept { return radians * 180.0f / std::numbers::pi_v<float>; };
 
 std::tuple<float, float, float> rainbowColor(float speed) noexcept;
