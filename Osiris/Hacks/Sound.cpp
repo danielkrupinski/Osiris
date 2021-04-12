1a5d31c4568d415478971cb7dc32d5aadedd0c0c
Daniel Krupiński <danielkrupinski@outlook.com>
2021-04-12 14:06:13 +0200

    Fix widget id collision (fixes #2784)

--- Osiris/Hacks/Sound.cpp
+++ Osiris/Hacks/Sound.cpp
@@ -77,6 +77,8 @@ void Sound::drawGUI(bool contentOnly) noexcept
         ImGui::SetNextWindowSize({ 0.0f, 0.0f });
         ImGui::Begin("Sound", &soundWindowOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
     }
+
+    ImGui::PushID("Sound");
     ImGui::SliderInt("Chicken volume", &soundConfig.chickenVolume, 0, 200, "%d%%");
 
     static int currentCategory{ 0 };
@@ -87,6 +89,7 @@ void Sound::drawGUI(bool contentOnly) noexcept
     ImGui::SliderInt("Headshot volume", &soundConfig.players[currentCategory].headshotVolume, 0, 200, "%d%%");
     ImGui::SliderInt("Weapon volume", &soundConfig.players[currentCategory].weaponVolume, 0, 200, "%d%%");
     ImGui::SliderInt("Footstep volume", &soundConfig.players[currentCategory].footstepVolume, 0, 200, "%d%%");
+    ImGui::PopID();
 
     if (!contentOnly)
         ImGui::End();
