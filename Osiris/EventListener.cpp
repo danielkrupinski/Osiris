3851c4c08ad32ebdbcbfe2f63f581aafc0c1cea4
Daniel Krupiński <danielkrupinski@outlook.com>
2021-04-14 10:14:18 +0200

    Add a notice about AntiDLL to EventListener.cpp

--- Osiris/EventListener.cpp
+++ Osiris/EventListener.cpp
@@ -13,8 +13,9 @@ EventListener::EventListener() noexcept
 {
     assert(interfaces);
 
-    //interfaces->gameEventManager->addListener(this, "item_purchase");
-    //interfaces->gameEventManager->addListener(this, "bullet_impact");
+    // If you add here listeners which aren't used by client.dll (e.g., item_purchase, bullet_impact), the cheat will be detected by AntiDLL (community anticheat).
+    // Instead, register listeners dynamically and only when certain functions are enabled - see Misc::updateEventListeners(), Visuals::updateEventListeners()
+
     interfaces->gameEventManager->addListener(this, "round_start");
     interfaces->gameEventManager->addListener(this, "round_freeze_end");
     interfaces->gameEventManager->addListener(this, "player_hurt");
@@ -42,7 +43,6 @@ void EventListener::fireGameEvent(GameEvent* event)
         GameData::clearProjectileList();
         Misc::preserveKillfeed(true);
         [[fallthrough]];
-   // case fnv::hash("item_purchase"):
     case fnv::hash("round_freeze_end"):
         Misc::purchaseList(event);
         break;
@@ -57,9 +57,6 @@ void EventListener::fireGameEvent(GameEvent* event)
         Visuals::hitEffect(event);
         Visuals::hitMarker(event);
         break;
-   // case fnv::hash("bullet_impact"):
-   //     Visuals::bulletTracer(*event);
-   //     break;
     case fnv::hash("vote_cast"):
         Misc::voteRevealer(*event);
         break;
