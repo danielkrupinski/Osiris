70c8ec0c28c281ccf6ad4d086b5430ab534bc8b3
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-12 15:25:15 +0200

    Update SharedObjectTypeCache class definition

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -221,7 +221,11 @@ public:
     VIRTUAL_METHOD(void, addObject, 1, (void* object), (this, object))
     VIRTUAL_METHOD(void, removeObject, 3, (void* object), (this, object))
 
-    PAD(WIN32_LINUX(32, 56))
+    PAD(4)
+    void** objects;
+    PAD(16)
+    int objectCount;
+    PAD(4)
     int classID; // https://github.com/perilouswithadollarsign/cstrike15_src/blob/f82112a2388b841d72cb62ca48ab1846dfcc11c8/game/shared/econ/econ_item_constants.h#L39
 };
 
