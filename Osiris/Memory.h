60d145c80fd2b2a30683711e22e855d7cd4b14a9
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-12 19:21:49 +0200

    Add Memory::getInventoryItemByItemID function pointer

--- Osiris/Memory.h
+++ Osiris/Memory.h
@@ -109,6 +109,7 @@ public:
     std::uintptr_t setStickerToolSlotGetArgAsStringReturnAddress;
     std::uintptr_t initItemCustomizationNotification;
     std::add_pointer_t<void* __CDECL(std::uint64_t itemID)> findOrCreateEconItemViewForItemID;
+    void*(__THISCALL* getInventoryItemByItemID)(CSPlayerInventory* _this, std::uint64_t itemID);
 
     short makePanoramaSymbol(const char* name) const noexcept
     {
