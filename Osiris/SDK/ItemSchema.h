7ec6d796023000b31f568b33a29ec28ce6fd264c
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-16 14:59:31 +0200

    Add ItemSchema::musicKits field

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -183,6 +183,8 @@ public:
     PAD(WIN32_LINUX(0x48, 0x60))
     UtlMap<int, PaintKit*> paintKits;
     UtlMap<int, StickerKit*> stickerKits;
+    PAD(WIN32_LINUX(0x11C, ))
+    UtlMap<int, EconMusicDefinition*> musicKits; // linux offsetof = 0x570
 
     VIRTUAL_METHOD(EconItemDefinition*, getItemDefinitionInterface, 4, (WeaponId id), (this, id))
     VIRTUAL_METHOD(const char*, getRarityName, 19, (uint8_t rarity), (this, rarity))
