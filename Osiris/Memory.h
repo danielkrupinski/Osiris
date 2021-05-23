9a8016acdc16b38726db791f41460bb256c59550
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-23 18:53:29 +0200

    Remove Memory::initItemCustomizationNotification field

--- Osiris/Memory.h
+++ Osiris/Memory.h
@@ -112,7 +112,6 @@ public:
     std::uintptr_t setNameToolStringGetArgAsStringReturnAddress;
     std::uintptr_t clearCustomNameGetArgAsStringReturnAddress;
 
-    std::uintptr_t initItemCustomizationNotification;
     std::add_pointer_t<void* __CDECL(std::uint64_t itemID)> findOrCreateEconItemViewForItemID;
     void*(__THISCALL* getInventoryItemByItemID)(CSPlayerInventory* _this, std::uint64_t itemID);
     std::uintptr_t useToolStickerGetArgAsStringReturnAddress;
