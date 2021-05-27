427b172fb52236a2d361cf47aea6170b000e850a
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-27 10:38:35 +0200

    Define EconItemView class

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -56,6 +56,16 @@ public:
     VIRTUAL_METHOD(const Vector&, obbMaxs, 2, (), (this))
 };
 
+class EconItemView {
+public:
+    INCONSTRUCTIBLE(EconItemView)
+
+    std::uintptr_t getAttributeList() noexcept
+    {
+        return std::uintptr_t(this) + WIN32_LINUX(0x244, 0x2F8);
+    }
+};
+
 class Entity {
 public:
     INCONSTRUCTIBLE(Entity)
