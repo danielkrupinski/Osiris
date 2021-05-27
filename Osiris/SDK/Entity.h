1f691a52f5249659525dd6045ca429c5d21f6332
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-27 10:39:01 +0200

    Add Entity::econItemView netvar

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -296,6 +296,7 @@ public:
     NETVAR(fallbackWear, "CBaseAttributableItem", "m_flFallbackWear", float)
     NETVAR(fallbackStatTrak, "CBaseAttributableItem", "m_nFallbackStatTrak", unsigned)
     NETVAR(initialized, "CBaseAttributableItem", "m_bInitialized", bool)
+    NETVAR(econItemView, "CBaseAttributableItem", "m_Item", EconItemView)
 
     NETVAR(owner, "CBaseViewModel", "m_hOwner", int)
     NETVAR(weapon, "CBaseViewModel", "m_hWeapon", int)
