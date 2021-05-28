dbf6e07a0d5ba8ca81d8f413e66b2771e68ab952
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-28 17:51:29 +0200

    Draw vertical separator between weapon and paint kit name using ImDrawList::AddRectFilled() function instead of redering it as text

--- Osiris/Hacks/InventoryChanger.cpp
+++ Osiris/Hacks/InventoryChanger.cpp
@@ -143,7 +143,7 @@ private:
             if (paintKit->id == 0 || paintKit->id == 9001) // ignore workshop_default
                 continue;
 
-            _paintKits.emplace_back(paintKit->id, std::move(std::wstring{ L" | " }.append(interfaces->localize->findSafe(paintKit->itemName.data()))));
+            _paintKits.emplace_back(paintKit->id, interfaces->localize->findSafe(paintKit->itemName.data()));
 
             const auto isGlove = (paintKit->id >= 10000);
             for (auto it = std::ranges::lower_bound(kitsWeapons, paintKit->id, {}, &KitWeapon::paintKit); it != kitsWeapons.end() && it->paintKit == paintKit->id; ++it) {
@@ -169,7 +169,7 @@ private:
             if (std::string_view name{ stickerKit->name.data() }; name.starts_with("spray") || name.starts_with("patch") || name.ends_with("graffiti"))
                 continue;
 
-            std::wstring name = std::wstring{ L" | " } + interfaces->localize->findSafe(stickerKit->id != 242 ? stickerKit->itemName.data() + 1 : "StickerKit_dhw2014_teamdignitas_gold");
+            std::wstring name = interfaces->localize->findSafe(stickerKit->id != 242 ? stickerKit->itemName.data() + 1 : "StickerKit_dhw2014_teamdignitas_gold");
             _paintKits.emplace_back(stickerKit->id, std::move(name));
             _gameItems.emplace_back(Type::Sticker, stickerKit->rarity, WeaponId::Sticker, _paintKits.size() - 1, stickerKit->inventoryImage.data());
         }
@@ -180,7 +180,7 @@ private:
             if (musicKit->id == 1 || musicKit->id == 2)
                 continue;
 
-            std::wstring name = std::wstring{ L" | " } + interfaces->localize->findSafe(musicKit->nameLocalized);
+            std::wstring name = interfaces->localize->findSafe(musicKit->nameLocalized);
             _paintKits.emplace_back(musicKit->id, std::move(name));
             _gameItems.emplace_back(Type::Music, 3, WeaponId::MusicKit, _paintKits.size() - 1, musicKit->inventoryImage);
         }
@@ -961,7 +961,7 @@ namespace ImGui
         const auto itemName = StaticData::getWeaponName(item.weaponID).c_str();
         const auto itemNameSize = CalcTextSize(itemName, nullptr);
 
-        auto paintKitName = item.hasPaintKit() ? StaticData::paintKits()[item.dataIndex].name.c_str() : "";
+        const auto paintKitName = item.hasPaintKit() ? StaticData::paintKits()[item.dataIndex].name.c_str() : "";
         const auto paintKitNameSize = CalcTextSize(paintKitName, nullptr);
 
         PushID(itemName);
@@ -972,7 +972,7 @@ namespace ImGui
 
         const auto height = ImMax(paintKitNameSize.y, ImMax(itemNameSize.y, iconSizeSmall.y));
         const auto rarityBulletRadius = IM_FLOOR(height * 0.2f);
-        const auto size = ImVec2{ iconSizeSmall.x + paintKitNameSize.x + rarityBulletRadius + itemNameSize.x, height };
+        const auto size = ImVec2{ iconSizeSmall.x + rarityBulletRadius * 2.0f + itemNameSize.x + paintKitNameSize.x, height };
         
         ImVec2 pos = window->DC.CursorPos;
         pos.y += window->DC.CurrLineTextBaseOffset;
@@ -985,8 +985,12 @@ namespace ImGui
 
         const auto itemNameMin = ImVec2{ rarityBulletPos.x + rarityBulletRadius + 5.0f, pos.y };
         const auto itemNameMax = itemNameMin + ImVec2{ itemNameSize.x, size.y };
+        
+        const auto separatorHeightInv = IM_FLOOR(height * 0.2f);
+        const auto separatorMin = ImVec2{ itemNameMax.x + 5.0f, pos.y + separatorHeightInv };
+        const auto separatorMax = separatorMin + ImVec2{ 1.0f, height - 2.0f * separatorHeightInv };
 
-        const auto paintKitNameMin = ImVec2{ itemNameMax.x, pos.y };
+        const auto paintKitNameMin = ImVec2{ separatorMax.x + 5.0f, pos.y };
         const auto paintKitNameMax = paintKitNameMin + ImVec2{ paintKitNameSize.x, size.y };
 
         // Selectables are meant to be tightly packed together with no click-gap, so we extend their box to cover spacing between selectable.
@@ -1043,6 +1047,8 @@ namespace ImGui
         window->DrawList->AddCircleFilled(rarityBulletPos, rarityBulletRadius, rarityColor, 12);
 
         RenderTextClipped(itemNameMin, itemNameMax, itemName, nullptr, &itemNameSize, { 0.0f, 0.5f }, &bb);
+        if (item.hasPaintKit())
+            window->DrawList->AddRectFilled(separatorMin, separatorMax, GetColorU32(ImGuiCol_Text));
         RenderTextClipped(paintKitNameMin, paintKitNameMax, paintKitName, nullptr, &paintKitNameSize, { 0.0f, 0.5f }, &bb);
 
         if (pressed && (window->Flags & ImGuiWindowFlags_Popup) && !(window->DC.ItemFlags & ImGuiItemFlags_SelectableDontClosePopup))
