810bb634919481da62dedbbd217df5c070936054
Daniel Krupiński <danielkrupinski@outlook.com>
2021-05-28 21:16:10 +0200

    Add WeaponId::Patch enum value

--- Osiris/SDK/WeaponId.h
+++ Osiris/SDK/WeaponId.h
@@ -82,6 +82,7 @@ enum class WeaponId : short {
     NameTag = 1200,
     Sticker = 1209,
     MusicKit = 1314,
+    Patch = 4609,
     GloveStuddedBrokenfang = 4725,
     GloveStuddedBloodhound = 5027,
     GloveT,
