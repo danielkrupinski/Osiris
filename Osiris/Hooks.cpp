fb985d6a3fd4810db03ce796e5b5236c6e188d4b
Daniel Krupiński <danielkrupinski@outlook.com>
2021-06-04 20:18:01 +0200

    Remove "Fake prime" option (made obsolete by game update https://blog.counter-strike.net/index.php/2021/06/34389/)

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -283,7 +283,6 @@ static void __STDCALL frameStageNotify(LINUX_ARGS(void* thisptr,) FrameStage sta
         Misc::preserveKillfeed();
         Misc::disablePanoramablur();
         Visuals::colorWorld();
-        Misc::fakePrime();
         Misc::updateEventListeners();
         Visuals::updateEventListeners();
     }
