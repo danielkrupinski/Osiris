cdccba06f40b5f5410bd91ab97a4afa8b3526903
techzin <61809691+techzin@users.noreply.github.com>
2021-06-13 11:40:28 -0300

    Add XorStr Enc to Osiris.cpp

--- Osiris/Osiris.cpp
+++ Osiris/Osiris.cpp
@@ -9,17 +9,19 @@
 #include "VMP/def.h"
 #ifdef _WIN32
 
+#include "xorstr.hpp"
+
 extern "C" BOOL WINAPI _CRT_INIT(HMODULE moduleHandle, DWORD reason, LPVOID reserved);
 
 BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 {
-     VMP_ULTRA("DllMain");
+     VMP_ULTRA(xorstr_("DllMain"));
     if (!_CRT_INIT(moduleHandle, reason, reserved))
         return FALSE;
 
     if (reason == DLL_PROCESS_ATTACH) {
-        VMP_ULTRA("OnDllAttach");
-        std::setlocale(LC_CTYPE, ".utf8");
+        VMP_ULTRA(xorstr_("OnDllAttach"));
+        std::setlocale(LC_CTYPE, xorstr_(".utf8"));
         hooks = std::make_unique<Hooks>(moduleHandle);
     }
     return TRUE;
@@ -30,7 +32,7 @@ BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 
 void __attribute__((constructor)) DllEntryPoint()
 {
-    VMP_ULTRA("DllMain");
+    VMP_ULTRA(xorstr_("DllMain"));
     hooks = std::make_unique<Hooks>();
     VMP_END;
 }
