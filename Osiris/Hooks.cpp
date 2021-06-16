019a1502ab89cdda6f270179f58c2a65cf8faa8c
Daniel Krupiński <danielkrupinski@outlook.com>
2021-06-16 21:00:39 +0200

    Call InventoryChanger::onSoUpdated() in soUpdated hook

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -505,6 +505,7 @@ static bool __STDCALL equipItemInLoadout(LINUX_ARGS(void* thisptr, ) Team team, 
 
 static void __STDCALL soUpdated(LINUX_ARGS(void* thisptr, ) SOID owner, SharedObject* object, int event) noexcept
 {
+    InventoryChanger::onSoUpdated(object, event);
     hooks->inventory.callOriginal<void, 1>(owner, object, event);
 }
 
