c5947053f835b49b4a74e85a14bfb9c330105ff4
Daniel Krupiński <danielkrupinski@outlook.com>
2021-06-18 18:25:06 +0200

    Create EventListener namespace with functions replacing std::unique_ptr<EventListener> object

--- Osiris/Hooks.cpp
+++ Osiris/Hooks.cpp
@@ -74,7 +74,7 @@ static LRESULT __stdcall wndProc(HWND window, UINT msg, WPARAM wParam, LPARAM lP
 {
     [[maybe_unused]] static const auto once = [](HWND window) noexcept {
         netvars = std::make_unique<Netvars>();
-        eventListener = std::make_unique<EventListener>();
+        EventListener::init();
 
         ImGui::CreateContext();
         ImGui_ImplWin32_Init(window);
@@ -669,7 +669,7 @@ static DWORD WINAPI unload(HMODULE moduleHandle) noexcept
     Sleep(100);
 
     interfaces->inputSystem->enableInput(true);
-    eventListener->remove();
+    EventListener::remove();
 
     ImGui_ImplDX9_Shutdown();
     ImGui_ImplWin32_Shutdown();
@@ -741,7 +741,7 @@ static int pollEvent(SDL_Event* event) noexcept
 {
     [[maybe_unused]] static const auto once = []() noexcept {
         netvars = std::make_unique<Netvars>();
-        eventListener = std::make_unique<EventListener>();
+        EventListener::init();
 
         ImGui::CreateContext();
         config = std::make_unique<Config>();
