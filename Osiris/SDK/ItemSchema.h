0cc93c99273048e52f423a4b42402bd8335c2388
Daniel Krupiński <danielkrupinski@outlook.com>
2021-06-21 16:57:12 +0200

    Reveal ItemSchema::revolvingLootLists field

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -221,7 +221,9 @@ public:
     UtlMap<int, EconItemQualityDefinition> qualities;
     PAD(WIN32_LINUX(0x48, 0x60))
     UtlMap<int, EconItemDefinition*> itemsSorted;
-    PAD(WIN32_LINUX(0x104, 0x168))
+    PAD(WIN32_LINUX(0x60, 0x88))
+    UtlMap<int, const char*> revolvingLootLists;
+    PAD(WIN32_LINUX(0x80, 0xB0))
     UtlMap<std::uint64_t, AlternateIconData> alternateIcons;
     PAD(WIN32_LINUX(0x48, 0x60))
     UtlMap<int, PaintKit*> paintKits;
