481fb4a65e2a42e04441f49d2a14264b76f09c93
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-05 17:32:48 +0200

    Add ToolUser::setStatTrakSwapItem1() method

--- Osiris/InventoryChanger/InventoryChanger.cpp
+++ Osiris/InventoryChanger/InventoryChanger.cpp
@@ -322,6 +322,7 @@ public:
     static void setTool(std::uint64_t itemID) noexcept { instance().toolItemID = itemID; }
     static void setNameTag(const char* nameTag) noexcept { instance().nameTag = nameTag; }
     static void setStickerSlot(int slot) noexcept { instance().stickerSlot = slot; }
+    static void setStatTrakSwapItem1(std::uint64_t itemID) noexcept { instance().statTrakSwapItem1 = itemID; }
 
     static void preAddItems(CSPlayerInventory& localInventory) noexcept { instance()._preAddItems(localInventory); }
 private:
@@ -497,6 +498,7 @@ private:
 
     std::uint64_t toolItemID = 0;
     std::uint64_t destItemID = 0;
+    std::uint64_t statTrakSwapItem1 = 0;
     [[deprecated]] std::uint64_t recreatedItemID = 0;
     Action action;
     float useTime = 0.0f;
