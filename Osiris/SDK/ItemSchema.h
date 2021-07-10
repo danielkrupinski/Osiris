1844a65bdb552dba6820a3e205bfeff0db016155
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-10 17:58:03 +0200

    Make CSPlayerInventory::getItemInLoadout() virtual method return a pointer to EconItemView

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -389,13 +389,15 @@ struct SOID {
     std::uint32_t padding;
 };
 
+class EconItemView;
+
 class CSPlayerInventory {
 public:
     INCONSTRUCTIBLE(CSPlayerInventory)
 
     VIRTUAL_METHOD(void, soUpdated, 1, (SOID owner, SharedObject* object, int event), (this, owner, object, event))
     VIRTUAL_METHOD(void, soDestroyed, 2, (SOID owner, SharedObject* object, int event), (this, owner, object, event))
-    VIRTUAL_METHOD_V(void*, getItemInLoadout, 8, (Team team, int slot), (this, team, slot))
+    VIRTUAL_METHOD_V(EconItemView*, getItemInLoadout, 8, (Team team, int slot), (this, team, slot))
     VIRTUAL_METHOD_V(void, removeItem, 15, (std::uint64_t itemID), (this, itemID))
 
     auto getSOC() noexcept
