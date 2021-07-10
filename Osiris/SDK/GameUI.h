6e1bb02eeb7ce00a044f4c4002605c47dfa4254f
Maksym Dilai <mdilai@users.noreply.github.com>
2021-07-10 23:04:19 +0300

    Fix GameUI::createCommandMsgBox() index (#3079)

--- Osiris/SDK/GameUI.h
+++ Osiris/SDK/GameUI.h
@@ -7,5 +7,5 @@ class GameUI {
 public:
     INCONSTRUCTIBLE(GameUI)
 
-    VIRTUAL_METHOD(void, createCommandMsgBox, 20, (const char* title, const char* message, bool showOk = true, bool showCancel = false, const char* okCommand = nullptr, const char* cancelCommand = nullptr, const char* closedCommand = nullptr, const char* legend = nullptr, const char* unknown = nullptr), (this, title, message, showOk, showCancel, okCommand, cancelCommand, closedCommand, legend, unknown))
+    VIRTUAL_METHOD(void, createCommandMsgBox, 19, (const char* title, const char* message, bool showOk = true, bool showCancel = false, const char* okCommand = nullptr, const char* cancelCommand = nullptr, const char* closedCommand = nullptr, const char* legend = nullptr, const char* unknown = nullptr), (this, title, message, showOk, showCancel, okCommand, cancelCommand, closedCommand, legend, unknown))
 };
