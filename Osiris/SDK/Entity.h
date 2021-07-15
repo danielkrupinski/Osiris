46c5808257a2f733b0a5d2fe7cd6c07de10cb6c4
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-15 17:24:19 +0200

    Remove Entity::fallbackPaintKit netvar

--- Osiris/SDK/Entity.h
+++ Osiris/SDK/Entity.h
@@ -229,7 +229,6 @@ public:
     NETVAR(itemIDLow, "CBaseAttributableItem", "m_iItemIDLow", std::uint32_t)
     NETVAR(entityQuality, "CBaseAttributableItem", "m_iEntityQuality", int)
     NETVAR(customName, "CBaseAttributableItem", "m_szCustomName", char[32])
-    NETVAR(fallbackPaintKit, "CBaseAttributableItem", "m_nFallbackPaintKit", unsigned)
     NETVAR(fallbackSeed, "CBaseAttributableItem", "m_nFallbackSeed", unsigned)
     NETVAR(fallbackWear, "CBaseAttributableItem", "m_flFallbackWear", float)
     NETVAR(fallbackStatTrak, "CBaseAttributableItem", "m_nFallbackStatTrak", unsigned)
