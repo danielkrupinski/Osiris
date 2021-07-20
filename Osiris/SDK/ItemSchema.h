04d225953931db96a53afe306ba8a19428fe8754
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-20 14:51:05 +0200

    Move implementation of Node and UtlMap structs to SDK/UtlMap.h

--- Osiris/SDK/ItemSchema.h
+++ Osiris/SDK/ItemSchema.h
@@ -5,6 +5,7 @@
 #include "Inconstructible.h"
 #include "Pad.h"
 #include "Entity.h"
+#include "UtlMap.h"
 #include "UtlMemory.h"
 #include "UtlString.h"
 #include "UtlVector.h"
@@ -13,47 +14,6 @@
 #include "../Memory.h"
 
 enum class WeaponId : short;
-
-template <typename Key, typename Value>
-struct Node {
-    int left;
-    int right;
-    int parent;
-    int type;
-    Key key;
-    Value value;
-};
-
-template <typename Key, typename Value>
-struct UtlMap {
-    auto begin() const noexcept { return memory.memory; }
-    auto end() const noexcept { return memory.memory + numElements; }
-    
-    int find(Key key) const noexcept
-    {
-        auto curr = root;
-
-        while (curr != -1) {
-            const auto el = memory[curr];
-
-            if (el.key < key)
-                curr = el.right;
-            else if (el.key > key)
-                curr = el.left;
-            else
-                break;
-        }
-        return curr;
-    }
-
-    void* lessFunc;
-    UtlMemory<Node<Key, Value>> memory;
-    int root;
-    int numElements;
-    int firstFree;
-    int lastAlloc;
-    Node<Key, Value>* elements;
-};
 
 struct PaintKit {
     int id;
