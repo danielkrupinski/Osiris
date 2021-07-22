00ee7ec8d0dc9114750e036a979df416350c890e
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-22 15:12:29 +0200

    Declare 'items' variable in for loop init-statement in InventoryChanger::toJson()

--- Osiris/InventoryChanger/InventoryChanger.cpp
+++ Osiris/InventoryChanger/InventoryChanger.cpp
@@ -887,8 +887,7 @@ json InventoryChanger::toJson() noexcept
 
     j["Version"] = CONFIG_VERSION;
 
-    auto& items = j["Items"];
-    for (const auto& item : Inventory::get()) {
+    for (auto& items = j["Items"]; const auto& item : Inventory::get()) {
         if (item.isDeleted())
             continue;
 
