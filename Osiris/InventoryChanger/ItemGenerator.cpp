b1ea48093831907a3b9ec7dac7b420b3c1eadd27
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-22 22:26:13 +0200

    Inventory Changer: Pick a random match a souvenir package could be dropped in when creating a new souvenir package item

--- Osiris/InventoryChanger/ItemGenerator.cpp
+++ Osiris/InventoryChanger/ItemGenerator.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <span>
 #include <vector>
 
 #include "../Helpers.h"
@@ -76,6 +77,47 @@ constexpr auto tournaments = std::to_array<Tournament>({
 
 static_assert(std::ranges::is_sorted(tournaments, {}, & Tournament::tournamentID));
 
+[[nodiscard]] static const std::vector<Match>* getTournamentMatches(std::uint32_t tournamentID) noexcept
+{
+    if (const auto it = std::ranges::lower_bound(tournaments, tournamentID, {}, &Tournament::tournamentID); it != tournaments.end())
+        return &it->matches;
+    assert(false && "Missing tournament match data!");
+    return nullptr;
+}
+
+[[nodiscard]] static std::span<const Match> filterMatchesToMap(const std::vector<Match>& matches, TournamentMap map) noexcept
+{
+    if (map == TournamentMap::None)
+        return matches;
+
+    struct Comp {
+        bool operator()(TournamentMap map, const Match& match) const noexcept { return map < match.map; }
+        bool operator()(const Match& match, TournamentMap map) const noexcept { return match.map < map; }
+    };
+
+    // not using std::ranges::equal_range() here because it requires extra operators
+    if (const auto [begin, end] = std::equal_range(matches.begin(), matches.end(), map, Comp{}); begin != end)
+        return { begin, end };
+    return {};
+}
+
+[[nodiscard]] static auto generateSouvenirPackageData(const StaticData::Case& caseData) noexcept
+{
+    assert(caseData.isSouvenirPackage());
+    DynamicSouvenirPackageData dynamicData;
+
+    if (const auto matches = getTournamentMatches(caseData.souvenirPackageTournamentID)) {
+        if (const auto matchesOnMap = filterMatchesToMap(*matches, caseData.tournamentMap); !matchesOnMap.empty()) {
+            const auto& randomMatch = matchesOnMap[Helpers::random(0, matchesOnMap.size() - 1)];
+            dynamicData.tournamentStage = randomMatch.stage;
+            dynamicData.tournamentTeam1 = randomMatch.team1;
+            dynamicData.tournamentTeam2 = randomMatch.team2;
+        }
+    }
+
+    return dynamicData;
+}
+
 std::size_t ItemGenerator::createDefaultDynamicData(std::size_t gameItemIndex) noexcept
 {
     std::size_t index = Inventory::INVALID_DYNAMIC_DATA_IDX;
@@ -96,6 +138,9 @@ std::size_t ItemGenerator::createDefaultDynamicData(std::size_t gameItemIndex) n
         index = Inventory::emplaceDynamicData(DynamicAgentData{});
     } else if (item.isMusic()) {
         index = Inventory::emplaceDynamicData(DynamicMusicData{});
+    } else if (item.isCase()) {
+        if (const auto& staticData = StaticData::cases()[item.dataIndex]; staticData.isSouvenirPackage())
+            index = Inventory::emplaceDynamicData(generateSouvenirPackageData(staticData));
     }
 
     return index;
