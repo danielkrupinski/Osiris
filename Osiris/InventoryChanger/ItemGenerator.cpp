6eb5a19d4f1fefec1b9b3e6f7c337b301ee1cd54
Daniel Krupiński <danielkrupinski@outlook.com>
2021-07-27 11:40:44 +0200

    Rename Match::mvpPlayersIDs field to Match::mvpPlayers

--- Osiris/InventoryChanger/ItemGenerator.cpp
+++ Osiris/InventoryChanger/ItemGenerator.cpp
@@ -72,7 +72,7 @@ struct Match {
     TournamentStage stage;
     TournamentTeam team1;
     TournamentTeam team2;
-    std::array<ProPlayer, 10> mvpPlayersIDs;
+    std::array<ProPlayer, 10> mvpPlayers;
 };
 
 struct Tournament {
