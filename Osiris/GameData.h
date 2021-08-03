1df26cc6144607a670cef59a9653e793e8b02c83
Daniel Krupiński <danielkrupinski@outlook.com>
2021-08-03 18:17:59 +0200

    Remove extra semicolons

--- Osiris/GameData.h
+++ Osiris/GameData.h
@@ -33,7 +33,7 @@ namespace GameData
 
     class Lock {
     public:
-        Lock() noexcept : lock{ mutex } {};
+        Lock() noexcept : lock{ mutex } {}
     private:
         std::scoped_lock<std::mutex> lock;
         static inline std::mutex mutex;
