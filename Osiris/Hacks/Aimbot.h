d2b962d9182607a7e5ff4212219f566d1a768514
blackMasterLord <76704816+blackMasterLord@users.noreply.github.com>
2021-08-06 18:27:22 +0500

    Update Aimbot.h

--- Osiris/Hacks/Aimbot.h
+++ Osiris/Hacks/Aimbot.h
@@ -7,6 +7,7 @@ namespace Aimbot
 {
     Vector calculateRelativeAngle(const Vector& source, const Vector& destination, const Vector& viewAngles) noexcept;
     void run(UserCmd*) noexcept;
+    void drawFov(ImDrawList* drawList) noexcept;
 
     void updateInput() noexcept;
 }
