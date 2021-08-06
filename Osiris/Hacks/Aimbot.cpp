bc63f428670a8bc291f2646dffd1bc4c183529c3
blackMasterLord <76704816+blackMasterLord@users.noreply.github.com>
2021-08-06 18:53:29 +0500

    Update Aimbot.cpp

--- Osiris/Hacks/Aimbot.cpp
+++ Osiris/Hacks/Aimbot.cpp
@@ -4,6 +4,10 @@
 #include <initializer_list>
 #include <memory>
 
+#include "../imgui/imgui.h"
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "../imgui/imgui_internal.h"
+
 #include "Aimbot.h"
 #include "../Config.h"
 #include "../InputUtil.h"
