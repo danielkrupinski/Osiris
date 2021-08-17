cb5021d5af3897f77e96f32e7abf3f1cf5d8c36e
Daniel Krupiński <danielkrupinski@outlook.com>
2021-08-17 21:10:51 +0200

    Add a unit test for Helpers::rad2deg() for negative angles

--- Tests/HelpersTests.cpp
+++ Tests/HelpersTests.cpp
@@ -33,3 +33,16 @@ TEST_CASE("degrees to radians", "[Helpers]") {
         CHECK(Helpers::deg2rad(9.0f) == Approx(0.05f * pi).epsilon(floatEpsilon));
     }
 }
+
+TEST_CASE("radians to degrees", "[Helpers]") {
+    SECTION("negative angle") {
+        CHECK(Helpers::rad2deg(-4*pi) == Approx(-720.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-3*pi) == Approx(-540.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-2*pi) == Approx(-360.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-1*pi) == Approx(-180.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-0.5f * pi) == Approx(-90.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-0.25f * pi) == Approx(-45.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-0.05f * pi) == Approx(-9.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(-0.0f * pi) == Approx(-0.0f).epsilon(floatEpsilon));
+    }
+}
