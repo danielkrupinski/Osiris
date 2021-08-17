42100734b4200f051c4b674e33a5a8d9bc0289f1
Daniel Krupiński <danielkrupinski@outlook.com>
2021-08-17 21:14:05 +0200

    Add a unit test for Helpers::rad2deg() for positive angles

--- Tests/HelpersTests.cpp
+++ Tests/HelpersTests.cpp
@@ -49,4 +49,14 @@ TEST_CASE("radians to degrees", "[Helpers]") {
     SECTION("zero angle") {
         CHECK(Helpers::rad2deg(0.0f) == Approx(0.0f).epsilon(floatEpsilon));
     }
+
+    SECTION("positive angle") {
+        CHECK(Helpers::rad2deg(4*pi) == Approx(720.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(3*pi) == Approx(540.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(2*pi) == Approx(360.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(1*pi) == Approx(180.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(0.5f * pi) == Approx(90.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(0.25f * pi) == Approx(45.0f).epsilon(floatEpsilon));
+        CHECK(Helpers::rad2deg(0.05f * pi) == Approx(9.0f).epsilon(floatEpsilon));
+    }
 }
