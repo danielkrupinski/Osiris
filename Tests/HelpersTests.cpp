2c103a921f8c9cb6173d0773026c5efd20e802a5
Daniel Krupiński <danielkrupinski@outlook.com>
2021-08-17 20:47:04 +0200

    Add a unit test for Helpers::deg2rad() for negative angles

---
+++ Tests/HelpersTests.cpp
@@ -0,0 +1,21 @@
+#include <numbers>
+
+#include "catch.hpp"
+
+#include "../Osiris/Helpers.h"
+
+constexpr auto pi = std::numbers::pi_v<float>;
+constexpr auto floatEpsilon = std::numeric_limits<float>::epsilon();
+
+TEST_CASE("degrees to radians", "[Helpers]") {
+    SECTION("negative angle") {
+        CHECK(Helpers::deg2rad(-720.0f) == Approx(-4*pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-540.0f) == Approx(-3*pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-360.0f) == Approx(-2*pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-180.0f) == Approx(-1*pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-90.0f) == Approx(-0.5f * pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-45.0f) == Approx(-0.25f * pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-9.0f) == Approx(-0.05f * pi).epsilon(floatEpsilon));
+        CHECK(Helpers::deg2rad(-0.0f) == Approx(-0.0f * pi).epsilon(floatEpsilon));
+    }
+}
