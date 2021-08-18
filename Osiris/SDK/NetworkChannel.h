d2a3e60c5a5b47e5696a7e976313d717c634c61e
blackMasterLord <76704816+blackMasterLord@users.noreply.github.com>
2021-08-18 16:54:08 +0500

    Update NetworkChannel.h

--- Osiris/SDK/NetworkChannel.h
+++ Osiris/SDK/NetworkChannel.h
@@ -7,6 +7,7 @@ class NetworkChannel {
 public:
     INCONSTRUCTIBLE(NetworkChannel)
 
+    VIRTUAL_METHOD(const char*, getAddress, 1, (), (this))
     VIRTUAL_METHOD(float, getLatency, 9, (int flow), (this, flow))
 
     std::byte pad[44];
