272b24f92802e3a96bd4d7b91fec9e1238ead6db
blackMasterLord <76704816+blackMasterLord@users.noreply.github.com>
2021-08-29 15:29:52 +0500

    Update GameData.cpp

--- Osiris/GameData.cpp
+++ Osiris/GameData.cpp
@@ -51,6 +51,7 @@ static std::vector<LootCrateData> lootCrateData;
 static std::forward_list<ProjectileData> projectileData;
 static BombData bombData;
 static std::vector<InfernoData> infernoData;
+static std::vector<SmokeData> smokeGrenades;
 static std::atomic_int netOutgoingLatency;
 
 static auto playerByHandleWritable(int handle) noexcept
@@ -97,7 +98,19 @@ void GameData::update() noexcept
     if (!localPlayer) {
         playerData.clear();
         projectileData.clear();
+        smokeGrenades.clear();
         return;
+    }
+
+    std::erase_if(smokeGrenades, [](const auto& smoke) { return (interfaces->entityList->getEntityFromHandle(smoke.handle) == nullptr && smoke.fadingAlpha() == 0.f); });
+    for (int i = 0; i < memory->smokeHandles->size; ++i) {
+        const auto handle = memory->smokeHandles->memory[i];
+        const auto smoke = interfaces->entityList->getEntityFromHandle(handle);
+        if (!smoke)
+            continue;
+
+        if (std::ranges::find(std::as_const(smokeGrenades), handle, &SmokeData::handle) == smokeGrenades.cend())
+            smokeGrenades.push_back(SmokeData(smoke->getAbsOrigin(), handle));
     }
 
     viewMatrix = interfaces->engine->worldToScreenMatrix();
@@ -286,6 +299,11 @@ const BombData& GameData::plantedC4() noexcept
 const std::vector<InfernoData>& GameData::infernos() noexcept
 {
     return infernoData;
+}
+
+const std::vector<SmokeData>& GameData::smokes() noexcept
+{
+    return smokeGrenades;
 }
 
 void LocalPlayerData::update() noexcept
@@ -701,3 +719,11 @@ InfernoData::InfernoData(Entity* inferno) noexcept
             points.emplace_back(inferno->fireXDelta()[i] + origin.x, inferno->fireYDelta()[i] + origin.y, inferno->fireZDelta()[i] + origin.z);
     }
 }
+
+float SmokeData::fadingAlpha() const noexcept
+{
+    constexpr float fadeTime = 1.f;
+    return std::clamp(1.0f - (memory->globalVars->realtime - (explosionTime + SMOKEGRENADE_LIFETIME)) / fadeTime, 0.0f, 1.0f);
+}
+
+SmokeData::SmokeData(const Vector& origin, int handle) noexcept : origin{ origin }, handle{ handle }, explosionTime{ memory->globalVars->realtime } {}
