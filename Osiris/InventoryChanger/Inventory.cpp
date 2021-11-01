12f80989703284ce31676791271c2a91c5c4d5c5
Daniel Krupiński <danielkrupinski@outlook.com>
2021-11-01 18:16:37 +0100

    Inventory Changer: Set drops awarded (from dynamic data) when creating a tournament coin

--- Osiris/InventoryChanger/Inventory.cpp
+++ Osiris/InventoryChanger/Inventory.cpp
@@ -203,6 +203,7 @@ private:
             if (const auto& dynamicData = dynamicServiceMedalData[inventoryItem.getDynamicDataIndex()]; dynamicData.issueDateTimestamp != 0)
                 econItem->setIssueDate(dynamicData.issueDateTimestamp);
         } else if (item.isTournamentCoin()) {
+            econItem->setDropsAwarded(dynamicTournamentCoinData[inventoryItem.getDynamicDataIndex()].dropsAwarded);
             econItem->setDropsRedeemed(0);
         } else if (item.isCase() && StaticData::cases()[item.dataIndex].isSouvenirPackage()) {
             if (const auto& dynamicData = dynamicSouvenirPackageData[inventoryItem.getDynamicDataIndex()]; dynamicData.tournamentStage != TournamentStage{ 0 }) {
