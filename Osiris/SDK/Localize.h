544028425dc952b37b58442ec23751086d951132
Daniel Krupiński <danielkrupinski@outlook.com>
2022-02-17 19:38:13 +0100

    Remove deprecated Localize::convertUnicodeToAnsi() overload

--- Osiris/SDK/Localize.h
+++ Osiris/SDK/Localize.h
@@ -14,13 +14,6 @@ public:
     VIRTUAL_METHOD(int, convertAnsiToUnicode, 15, (const char* ansi, wchar_t* unicode, int unicodeBufferSizeInBytes), (this, ansi, unicode, unicodeBufferSizeInBytes))
     VIRTUAL_METHOD(int, convertUnicodeToAnsi, 16, (const wchar_t* unicode, char* ansi, int ansiBufferSize), (this, unicode, ansi, ansiBufferSize))
     VIRTUAL_METHOD(const char*, findAsUTF8, 47, (const char* tokenName), (this, tokenName))
-
-    [[deprecated]] std::string convertUnicodeToAnsi(const wchar_t* unicode) noexcept
-    {
-        char buffer[4096];
-        convertUnicodeToAnsi(unicode, buffer, sizeof(buffer));
-        return buffer;
-    }
 };
 
 template <std::size_t BufferSize = 4096>
