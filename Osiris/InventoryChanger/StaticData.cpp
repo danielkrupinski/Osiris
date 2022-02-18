266a2b57041e70dc9845e94919c2f804da3eb0f1
Daniel Krupiński <danielkrupinski@outlook.com>
2022-02-18 22:55:53 +0100

    Remove ServiceMedal struct

--- Osiris/InventoryChanger/StaticData.cpp
+++ Osiris/InventoryChanger/StaticData.cpp
@@ -22,16 +22,6 @@ constexpr auto operator<=>(WeaponId a, WeaponId b) noexcept
     return static_cast<std::underlying_type_t<WeaponId>>(a) <=> static_cast<std::underlying_type_t<WeaponId>>(b);
 }
 
-struct ServiceMedal {
-    explicit ServiceMedal(std::uint32_t year) : yearsSince2015{ static_cast<std::uint8_t>((std::max)(year, 2015u) - 2015) } {}
-    std::uint16_t getServiceYear() const noexcept
-    {
-        return yearsSince2015 + 2015;
-    }
-private:
-    std::uint8_t yearsSince2015;
-};
-
 class WeaponNames {
 public:
     [[nodiscard]] std::string_view getWeaponName(WeaponId weaponID) const
