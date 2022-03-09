b3b8ce536f3a774e54c1f5e6aa573c7433cddf8a
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-09 22:01:19 +0100

    Make game_items::Item::is...() methods [[nodiscard]]

--- Osiris/InventoryChanger/GameItems/Item.h
+++ Osiris/InventoryChanger/GameItems/Item.h
@@ -16,24 +16,24 @@ public:
     [[nodiscard]] constexpr std::size_t getDataIndex() const noexcept { return dataIndex; }
     [[nodiscard]] constexpr std::string_view getIconPath() const noexcept { return iconPath; }
 
-    constexpr bool isSticker() const noexcept { return type == Type::Sticker; }
-    constexpr bool isSkin() const noexcept { return type == Type::Skin; }
-    constexpr bool isGloves() const noexcept { return type == Type::Gloves; }
-    constexpr bool isMusic() const noexcept { return type == Type::Music; }
-    constexpr bool isCollectible() const noexcept { return type == Type::Collectible; }
-    constexpr bool isNameTag() const noexcept { return type == Type::NameTag; }
-    constexpr bool isPatch() const noexcept { return type == Type::Patch; }
-    constexpr bool isGraffiti() const noexcept { return type == Type::Graffiti; }
-    constexpr bool isSealedGraffiti() const noexcept { return type == Type::SealedGraffiti; }
-    constexpr bool isAgent() const noexcept { return type == Type::Agent; }
-    constexpr bool isCase() const noexcept { return type == Type::Case; }
-    constexpr bool isCaseKey() const noexcept { return type == Type::CaseKey; }
-    constexpr bool isOperationPass() const noexcept { return type == Type::OperationPass; }
-    constexpr bool isStatTrakSwapTool() const noexcept { return type == Type::StatTrakSwapTool; }
-    constexpr bool isViewerPass() const noexcept { return type == Type::ViewerPass; }
-    constexpr bool isServiceMedal() const noexcept { return type == Type::ServiceMedal; }
-    constexpr bool isSouvenirToken() const noexcept { return type == Type::SouvenirToken; }
-    constexpr bool isTournamentCoin() const noexcept { return type == Type::TournamentCoin; }
+    [[nodiscard]] constexpr bool isSticker() const noexcept { return type == Type::Sticker; }
+    [[nodiscard]] constexpr bool isSkin() const noexcept { return type == Type::Skin; }
+    [[nodiscard]] constexpr bool isGloves() const noexcept { return type == Type::Gloves; }
+    [[nodiscard]] constexpr bool isMusic() const noexcept { return type == Type::Music; }
+    [[nodiscard]] constexpr bool isCollectible() const noexcept { return type == Type::Collectible; }
+    [[nodiscard]] constexpr bool isNameTag() const noexcept { return type == Type::NameTag; }
+    [[nodiscard]] constexpr bool isPatch() const noexcept { return type == Type::Patch; }
+    [[nodiscard]] constexpr bool isGraffiti() const noexcept { return type == Type::Graffiti; }
+    [[nodiscard]] constexpr bool isSealedGraffiti() const noexcept { return type == Type::SealedGraffiti; }
+    [[nodiscard]] constexpr bool isAgent() const noexcept { return type == Type::Agent; }
+    [[nodiscard]] constexpr bool isCase() const noexcept { return type == Type::Case; }
+    [[nodiscard]] constexpr bool isCaseKey() const noexcept { return type == Type::CaseKey; }
+    [[nodiscard]] constexpr bool isOperationPass() const noexcept { return type == Type::OperationPass; }
+    [[nodiscard]] constexpr bool isStatTrakSwapTool() const noexcept { return type == Type::StatTrakSwapTool; }
+    [[nodiscard]] constexpr bool isViewerPass() const noexcept { return type == Type::ViewerPass; }
+    [[nodiscard]] constexpr bool isServiceMedal() const noexcept { return type == Type::ServiceMedal; }
+    [[nodiscard]] constexpr bool isSouvenirToken() const noexcept { return type == Type::SouvenirToken; }
+    [[nodiscard]] constexpr bool isTournamentCoin() const noexcept { return type == Type::TournamentCoin; }
 
     enum class Type : std::uint8_t {
         Gloves,
