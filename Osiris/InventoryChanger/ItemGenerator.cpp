d70e0c6aaee6ead5169caef36a1645cb28c1702d
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-16 20:21:11 +0100

    Enable special rare items drops for Spectrum Case and Spectrum 2 Case

--- Osiris/InventoryChanger/ItemGenerator.cpp
+++ Osiris/InventoryChanger/ItemGenerator.cpp
@@ -535,6 +535,8 @@ constexpr auto crateRareSpecialItems = std::to_array<CrateRareSpecialItems>({
     { WeaponId::ChromaCase, chromaKnives },
     { WeaponId::Chroma2Case, chromaKnives },
     { WeaponId::Chroma3Case, chromaKnives },
+    { WeaponId::SpectrumCase, spectrumKnives },
+    { WeaponId::Spectrum2Case, spectrumKnives },
     { WeaponId::ClutchCase, clutchGloves },
     { WeaponId::HorizonCase, horizonKnives },
     { WeaponId::DangerZoneCase, horizonKnives },
