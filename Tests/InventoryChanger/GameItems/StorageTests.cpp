ece176b37a2433a05a45434345f93ef6d9096ca5
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-18 18:10:52 +0100

    Add ItemType enum class for game_items::Storage unit tests

--- Tests/InventoryChanger/GameItems/StorageTests.cpp
+++ Tests/InventoryChanger/GameItems/StorageTests.cpp
@@ -2,65 +2,87 @@
 
 #include <gtest/gtest.h>
 
-#include "../../../Osiris/InventoryChanger/GameItems/Storage.h"
-#include "../../../Osiris/InventoryChanger/GameItems/Lookup.h"
+#include <InventoryChanger/GameItems/Storage.h>
+#include <InventoryChanger/GameItems/Lookup.h>
 
 namespace game_items
 {
 namespace
 {
 
-Item& addToStorage(Storage& storage, Item::Type type, EconRarity rarity, WeaponId weaponID, std::size_t dataIndex, std::string_view iconPath)
+enum class ItemType {
+    Gloves,
+    Skin,
+    Patch,
+    Sticker,
+    Music,
+    Graffiti,
+    SealedGraffiti,
+    Collectible,
+    NameTag,
+    Agent,
+    Case,
+    CaseKey,
+    OperationPass,
+    StatTrakSwapTool,
+    ViewerPass,
+    ServiceMedal,
+    SouvenirToken,
+    TournamentCoin,
+    VanillaKnife
+};
+
+Item& addToStorage(Storage& storage, ItemType type, EconRarity rarity, WeaponId weaponID, std::size_t dataIndex, std::string_view iconPath)
 {
     switch (type) {
-    case Item::Type::Gloves:
+    case ItemType::Gloves:
         storage.addPaintKit(0, {}, 0.0f, 0.0f);
         storage.addGlovesWithLastPaintKit(rarity, weaponID, iconPath);
         break;
-    case Item::Type::Skin:
+    case ItemType::Skin:
         storage.addPaintKit(0, {}, 0.0f, 0.0f);
         storage.addSkinWithLastPaintKit(rarity, weaponID, iconPath);
         break;
-    case Item::Type::Patch:
+    case ItemType::Patch:
         storage.addPatch(0, {}, rarity, iconPath);
         break;
-    case Item::Type::Sticker:
+    case ItemType::Sticker:
         storage.addSticker(0, {}, rarity, iconPath, 0, {}, 0, false);
         break;
-    case Item::Type::Music:
+    case ItemType::Music:
         storage.addMusic(0, {}, iconPath);
         break;
-    case Item::Type::Collectible:
+    case ItemType::Collectible:
         storage.addCollectible(rarity, weaponID, false, iconPath);
         break;
-    case Item::Type::NameTag:
+    case ItemType::NameTag:
         storage.addNameTag(rarity, weaponID, iconPath);
         break;
-    case Item::Type::Agent:
+    case ItemType::Agent:
         storage.addAgent(rarity, weaponID, iconPath);
         break;
-    case Item::Type::Case:
+    case ItemType::Case:
         storage.addCase(rarity, weaponID, 0, iconPath);
         break;
-    case Item::Type::CaseKey:
+    case ItemType::CaseKey:
         storage.addCaseKey(rarity, weaponID, iconPath);
         break;
-    case Item::Type::OperationPass:
+    case ItemType::OperationPass:
         storage.addOperationPass(rarity, weaponID, iconPath);
         break;
-    case Item::Type::StatTrakSwapTool:
+    case ItemType::StatTrakSwapTool:
         storage.addStatTrakSwapTool(rarity, weaponID, iconPath);
         break;
-    case Item::Type::ViewerPass:
+    case ItemType::ViewerPass:
         storage.addViewerPass(rarity, weaponID, 0, iconPath);
         break;
-    case Item::Type::ServiceMedal:
+    case ItemType::ServiceMedal:
         storage.addServiceMedal(rarity, 0, weaponID, iconPath);
         break;
-    case Item::Type::SouvenirToken:
+    case ItemType::SouvenirToken:
         storage.addSouvenirToken(rarity, weaponID, 0, iconPath);
         break;
-    case Item::Type::TournamentCoin:
+    case ItemType::TournamentCoin:
         storage.addTournamentCoin(rarity, weaponID, 0, iconPath);
         break;
     default:
@@ -70,27 +92,27 @@ Item& addToStorage(Storage& storage, Item::Type type, EconRarity rarity, WeaponI
     return storage.getItems().back();
 }
 
-constexpr bool validateItemType(const Item& item, Item::Type desiredType) noexcept
+constexpr bool validateItemType(const Item& item, ItemType desiredType) noexcept
 {
     switch (desiredType) {
-    case Item::Type::Gloves: return item.isGloves();
-    case Item::Type::Skin: return item.isSkin();
-    case Item::Type::Patch: return item.isPatch();
-    case Item::Type::Sticker: return item.isSticker();
-    case Item::Type::Music: return item.isMusic();
-    case Item::Type::Graffiti: return item.isGraffiti();
-    case Item::Type::SealedGraffiti: return item.isSealedGraffiti();
-    case Item::Type::Collectible: return item.isCollectible();
-    case Item::Type::NameTag: return item.isNameTag();
-    case Item::Type::Agent: return item.isAgent();
-    case Item::Type::Case: return item.isCase();
-    case Item::Type::CaseKey: return item.isCaseKey();
-    case Item::Type::OperationPass: return item.isOperationPass();
-    case Item::Type::StatTrakSwapTool: return item.isStatTrakSwapTool();
-    case Item::Type::ViewerPass: return item.isViewerPass();
-    case Item::Type::ServiceMedal: return item.isServiceMedal();
-    case Item::Type::SouvenirToken: return item.isSouvenirToken();
-    case Item::Type::TournamentCoin: return item.isTournamentCoin();
+    case ItemType::Gloves: return item.isGloves();
+    case ItemType::Skin: return item.isSkin();
+    case ItemType::Patch: return item.isPatch();
+    case ItemType::Sticker: return item.isSticker();
+    case ItemType::Music: return item.isMusic();
+    case ItemType::Graffiti: return item.isGraffiti();
+    case ItemType::SealedGraffiti: return item.isSealedGraffiti();
+    case ItemType::Collectible: return item.isCollectible();
+    case ItemType::NameTag: return item.isNameTag();
+    case ItemType::Agent: return item.isAgent();
+    case ItemType::Case: return item.isCase();
+    case ItemType::CaseKey: return item.isCaseKey();
+    case ItemType::OperationPass: return item.isOperationPass();
+    case ItemType::StatTrakSwapTool: return item.isStatTrakSwapTool();
+    case ItemType::ViewerPass: return item.isViewerPass();
+    case ItemType::ServiceMedal: return item.isServiceMedal();
+    case ItemType::SouvenirToken: return item.isSouvenirToken();
+    case ItemType::TournamentCoin: return item.isTournamentCoin();
     default: return false;
     }
 }
@@ -99,7 +121,7 @@ TEST(StorageTest, EmptyStorageHasNoItems) {
     ASSERT_TRUE(Storage{}.getItems().empty());
 }
 
-class GameItemsStorageTest : public testing::TestWithParam<Item::Type> {};
+class GameItemsStorageTest : public testing::TestWithParam<ItemType> {};
 
 TEST_P(GameItemsStorageTest, AddingItemIncrementsItemCount) {
     Storage storage;
@@ -114,41 +136,41 @@ TEST_P(GameItemsStorageTest, AddedItemHasCorrectType) {
 }
 
 const auto typesToTest = testing::Values(
-    Item::Type::Gloves,
-    Item::Type::Skin,
-    Item::Type::Patch,
-    Item::Type::Sticker,
-    Item::Type::Music,
-    // Item::Type::Graffiti,
-    // Item::Type::SealedGraffiti,
-    Item::Type::Collectible,
-    Item::Type::NameTag,
-    Item::Type::Agent,
-    Item::Type::Case,
-    Item::Type::CaseKey,
-    Item::Type::OperationPass,
-    Item::Type::StatTrakSwapTool,
-    Item::Type::ViewerPass,
-    Item::Type::ServiceMedal,
-    Item::Type::SouvenirToken,
-    Item::Type::TournamentCoin
+    ItemType::Gloves,
+    ItemType::Skin,
+    ItemType::Patch,
+    ItemType::Sticker,
+    ItemType::Music,
+    // ItemType::Graffiti,
+    // ItemType::SealedGraffiti,
+    ItemType::Collectible,
+    ItemType::NameTag,
+    ItemType::Agent,
+    ItemType::Case,
+    ItemType::CaseKey,
+    ItemType::OperationPass,
+    ItemType::StatTrakSwapTool,
+    ItemType::ViewerPass,
+    ItemType::ServiceMedal,
+    ItemType::SouvenirToken,
+    ItemType::TournamentCoin
 );
 
 INSTANTIATE_TEST_SUITE_P(InventoryChanger, GameItemsStorageTest, typesToTest);
 
-class GameItemsStorageItemRarityTest : public testing::TestWithParam<std::tuple<Item::Type, EconRarity>> {};
+class GameItemsStorageItemRarityTest : public testing::TestWithParam<std::tuple<ItemType, EconRarity>> {};
 
 TEST_P(GameItemsStorageItemRarityTest, AddedItemHasCorrectRarity) {
     Storage storage;
     const auto [type, rarity] = GetParam();
     const auto& item = addToStorage(storage, type, rarity, WeaponId::None, 0, {});
-    ASSERT_EQ(item.getRarity(), type == Item::Type::Music ? EconRarity::Blue : rarity);
+    ASSERT_EQ(item.getRarity(), type == ItemType::Music ? EconRarity::Blue : rarity);
 }
 
 INSTANTIATE_TEST_SUITE_P(InventoryChanger, GameItemsStorageItemRarityTest,
     testing::Combine(typesToTest, testing::Values(EconRarity::Default, EconRarity::Blue, EconRarity::Gold)));
 
-class GameItemsStorageItemImagePathTest : public testing::TestWithParam<std::tuple<Item::Type, std::string_view>> {};
+class GameItemsStorageItemImagePathTest : public testing::TestWithParam<std::tuple<ItemType, std::string_view>> {};
 
 TEST_P(GameItemsStorageItemImagePathTest, AddedItemHasCorrectImagePath) {
     Storage storage;
