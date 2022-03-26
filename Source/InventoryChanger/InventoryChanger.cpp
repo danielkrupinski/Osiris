32ad3c20b94d987e79d9e237d93ccdfba062a1f7
unknown <amir30354@gmail.com>
2022-03-26 18:39:14 +0100

    Fixed sequence for falchion knife

--- Source/InventoryChanger/InventoryChanger.cpp
+++ Source/InventoryChanger/InventoryChanger.cpp
@@ -1220,19 +1220,19 @@ static int remapKnifeAnim(WeaponId weaponID, const int sequence) noexcept
             return sequence + 1;
         }
     case WeaponId::Falchion:
-        switch (sequence) {
-        case SEQUENCE_DEFAULT_IDLE2:
-            return SEQUENCE_FALCHION_IDLE1;
-        case SEQUENCE_DEFAULT_HEAVY_MISS1:
-            return Helpers::random(SEQUENCE_FALCHION_HEAVY_MISS1, SEQUENCE_FALCHION_HEAVY_MISS1_NOFLIP);
-        case SEQUENCE_DEFAULT_LOOKAT01:
-            return Helpers::random(SEQUENCE_FALCHION_LOOKAT01, SEQUENCE_FALCHION_LOOKAT02);
-        case SEQUENCE_DEFAULT_DRAW:
-        case SEQUENCE_DEFAULT_IDLE1:
-            return sequence;
-        default:
-            return sequence - 1;
-        }
+	switch (sequence) {
+	case SEQUENCE_DEFAULT_DRAW:
+	case SEQUENCE_DEFAULT_IDLE1:
+	case SEQUENCE_DEFAULT_HEAVY_HIT1:
+	case SEQUENCE_DEFAULT_HEAVY_BACKSTAB:
+	    return sequence;
+	case SEQUENCE_DEFAULT_HEAVY_MISS1:
+	    return Helpers::random(SEQUENCE_FALCHION_HEAVY_MISS1, SEQUENCE_FALCHION_HEAVY_MISS1_NOFLIP);
+	case SEQUENCE_DEFAULT_LOOKAT01:
+	    return Helpers::random(SEQUENCE_FALCHION_LOOKAT01, SEQUENCE_FALCHION_LOOKAT02);
+	default:
+	    return sequence - 1;
+	}
     case WeaponId::Daggers:
         switch (sequence) {
         case SEQUENCE_DEFAULT_IDLE2:
