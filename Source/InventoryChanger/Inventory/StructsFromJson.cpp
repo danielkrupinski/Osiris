a86a9241b72fc672185f04d47f5a40eb9e706854
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-31 00:14:55 +0200

    Refactor inventory::gloveFromJson() function

--- Source/InventoryChanger/Inventory/StructsFromJson.cpp
+++ Source/InventoryChanger/Inventory/StructsFromJson.cpp
@@ -10,15 +10,11 @@ Glove gloveFromJson(const json& j)
 {
     Glove glove;
 
-    if (j.contains("Wear")) {
-        if (const auto& wear = j["Wear"]; wear.is_number_float())
-            glove.wear = wear;
-    }
+    if (const auto wear = j.find("Wear"); wear != j.end() && wear->is_number_float())
+        glove.wear = wear->get<float>();
 
-    if (j.contains("Seed")) {
-        if (const auto& seed = j["Seed"]; seed.is_number_integer())
-            glove.seed = seed;
-    }
+    if (const auto seed = j.find("Seed"); seed != j.end() && seed->is_number_integer())
+        glove.seed = seed->get<int>();
 
     return glove;
 }
