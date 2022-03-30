91c024e7939b2394fe44a859de39a0354671f93c
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-30 21:24:33 +0200

    Implement more inventory::gloveFromJson() unit tests

--- Tests/InventoryChanger/Inventory/StructsFromJsonTests.cpp
+++ Tests/InventoryChanger/Inventory/StructsFromJsonTests.cpp
@@ -12,17 +12,36 @@ TEST(InventoryChanger_Inventory_GloveFromJsonTests, GloveFromEmptyJsonHasDefault
     ASSERT_FLOAT_EQ(gloveFromJson({}).wear, Glove{}.wear);
 }
 
-TEST(InventoryChanger_Inventory_GloveFromJsonTests, GloveFromEmptyJsonHasDefaultSeed) {
-    ASSERT_EQ(gloveFromJson({}).seed, Glove{}.seed);
-}
-
 TEST(InventoryChanger_Inventory_GloveFromJsonTests, GloveFromJsonWithoutWearHasDefaultWear) {
     ASSERT_FLOAT_EQ(gloveFromJson({ { "key", "value" } }).wear, Glove{}.wear);
 }
 
-TEST(InventoryChanger_Inventory_GloveFromJsonTests, GloveFromJsonWithoutSeedHasDefaultSeed) {
-    ASSERT_EQ(gloveFromJson({ { "key", "value" } }).seed, Glove{}.seed);
+class InventoryChanger_Inventory_GloveFromJsonWearTypeTest : public testing::TestWithParam<json::value_t> {};
+
+TEST_P(InventoryChanger_Inventory_GloveFromJsonWearTypeTest, GloveFromJsonWithWearNotAsFloatHasDefaultWear) {
+    ASSERT_FLOAT_EQ(gloveFromJson({ { "Wear", GetParam() } }).wear, Glove{}.wear);
 }
+
+INSTANTIATE_TEST_SUITE_P(,
+    InventoryChanger_Inventory_GloveFromJsonWearTypeTest,
+    testing::Values(json::value_t::array,
+                    json::value_t::binary,
+                    json::value_t::boolean,
+                    json::value_t::discarded,
+                    json::value_t::null,
+                    json::value_t::number_integer,
+                    json::value_t::number_unsigned,
+                    json::value_t::object,
+                    json::value_t::string)
+);
+
+class InventoryChanger_Inventory_GloveFromJsonWearTest : public testing::TestWithParam<float> {};
+
+TEST_P(InventoryChanger_Inventory_GloveFromJsonWearTest, GloveFromJsonHasCorrectWear) {
+    ASSERT_FLOAT_EQ(gloveFromJson({ { "Wear", GetParam() } }).wear, GetParam());
+}
+
+INSTANTIATE_TEST_SUITE_P(, InventoryChanger_Inventory_GloveFromJsonWearTest, testing::Values(0.0f, 0.123f, 1.0f));
 
 }
 }
