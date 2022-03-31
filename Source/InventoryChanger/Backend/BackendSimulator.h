adb36948ca3cef991e2668a4c78c7010c57669ff
Daniel Krupiński <danielkrupinski@outlook.com>
2022-03-31 21:24:49 +0200

    Implement inventory_changer::backend::BackendSimulator class

--- Source/InventoryChanger/Backend/BackendSimulator.h
+++ Source/InventoryChanger/Backend/BackendSimulator.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "Loadout.h"
+
+namespace inventory_changer::backend
+{
+
+class BackendSimulator {
+public:
+    [[nodiscard]] const Loadout& getLoadout() const noexcept
+    {
+        return loadout;
+    }
+
+    [[nodiscard]] static BackendSimulator& instance()
+    {
+        static BackendSimulator backendSimulator;
+        return backendSimulator;
+    }
+
+private:
+    Loadout loadout;
+};
+
+}
