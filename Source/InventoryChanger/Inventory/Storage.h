34ca3de11f4b1ca8614c539b3737cab2c98a2b71
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-02 16:29:29 +0200

    Change inventory::Storage::removeItem() method to take const iterator

--- Source/InventoryChanger/Inventory/Storage.h
+++ Source/InventoryChanger/Inventory/Storage.h
@@ -82,7 +82,7 @@ public:
         return items.end();
     }
 
-    std::vector<Item_v2>::iterator removeItem(std::vector<Item_v2>::iterator it)
+    std::vector<Item_v2>::iterator removeItem(std::vector<Item_v2>::const_iterator it)
     {
         return items.erase(it);
     }
