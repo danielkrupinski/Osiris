5b3eeb80759eaee0f42fc566a263807199a96957
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-04 21:20:35 +0200

    Move implementation of inventory_changer::backend::ToolUser::applySticker() to ToolUser.cpp

--- Source/InventoryChanger/Backend/ToolUser.h
+++ Source/InventoryChanger/Backend/ToolUser.h
@@ -6,27 +6,16 @@
 #include <InventoryChanger/StaticData.h>
 #include <InventoryChanger/GameItems/Lookup.h>
 
-#include "BackendSimulator.h"
 #include "Response.h"
 
 namespace inventory_changer::backend
 {
 
+class BackendSimulator;
+
 class ToolUser {
 public:
-    std::optional<Response> applySticker(BackendSimulator backend, std::list<inventory::Item_v2>::iterator item, std::list<inventory::Item_v2>::const_iterator sticker, std::uint8_t slot)
-    {
-        const auto skin = item->get<inventory::Skin>();
-        if (!skin)
-            return {};
-
-        skin->stickers[slot].stickerID = StaticData::lookup().getStorage().getStickerKit(sticker->gameItem()).id;
-        skin->stickers[slot].wear = 0.0f;
-
-        backend.moveToFront(item);
-        backend.removeItem(sticker);
-        return Response{ Response::Type::StickerApplied, Response::StickerApplied{ item, slot } };
-    }
+    std::optional<Response> applySticker(BackendSimulator backend, std::list<inventory::Item_v2>::iterator item, std::list<inventory::Item_v2>::const_iterator sticker, std::uint8_t slot);
 };
 
 }
