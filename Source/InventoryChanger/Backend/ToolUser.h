91e481c1f46b1e8e5fd427e8b0d62367635119ee
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-04 21:09:51 +0200

    Add inventory_changer::backend::ToolUser class

--- Source/InventoryChanger/Backend/ToolUser.h
+++ Source/InventoryChanger/Backend/ToolUser.h
@@ -12,18 +12,21 @@
 namespace inventory_changer::backend
 {
 
-std::optional<Response> applySticker(BackendSimulator backend, std::list<inventory::Item_v2>::iterator item, std::list<inventory::Item_v2>::const_iterator sticker, std::uint8_t slot)
-{
-    const auto skin = item->get<inventory::Skin>();
-    if (!skin)
-        return {};
+class ToolUser {
+public:
+    std::optional<Response> applySticker(BackendSimulator backend, std::list<inventory::Item_v2>::iterator item, std::list<inventory::Item_v2>::const_iterator sticker, std::uint8_t slot)
+    {
+        const auto skin = item->get<inventory::Skin>();
+        if (!skin)
+            return {};
 
-    skin->stickers[slot].stickerID = StaticData::lookup().getStorage().getStickerKit(sticker->gameItem()).id;
-    skin->stickers[slot].wear = 0.0f;
+        skin->stickers[slot].stickerID = StaticData::lookup().getStorage().getStickerKit(sticker->gameItem()).id;
+        skin->stickers[slot].wear = 0.0f;
 
-    backend.moveToFront(item);
-    backend.removeItem(sticker);
-    return Response{ Response::Type::StickerApplied, Response::StickerApplied{ item, slot } };
-}
+        backend.moveToFront(item);
+        backend.removeItem(sticker);
+        return Response{ Response::Type::StickerApplied, Response::StickerApplied{ item, slot } };
+    }
+};
 
 }
