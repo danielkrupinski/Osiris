1658c4fa868f682114147b9fe763b2894acd5f9f
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-06 21:54:29 +0200

    Use an additional std::unordered_map in inventory_changer::backend::ItemIDMap class for lookup by iterator

--- Source/InventoryChanger/Backend/ItemIDMap.h
+++ Source/InventoryChanger/Backend/ItemIDMap.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <functional>
 #include <list>
 #include <optional>
 #include <unordered_map>
@@ -13,34 +14,43 @@ class ItemIDMap {
 public:
     [[nodiscard]] std::optional<std::list<inventory::Item_v2>::const_iterator> get(std::uint64_t itemID) const
     {
-        const auto it = itemIDs.find(itemID);
-        return it != itemIDs.end() ? std::make_optional(it->second) : std::nullopt;
+        const auto it = itemIDsIterators.find(itemID);
+        return it != itemIDsIterators.end() ? std::make_optional(it->second) : std::nullopt;
     }
 
     [[nodiscard]] std::optional<std::uint64_t> getItemID(std::list<inventory::Item_v2>::const_iterator iterator) const
     {
-        if (const auto it = std::ranges::find(itemIDs, iterator, &decltype(itemIDs)::value_type::second); it != itemIDs.end())
-            return it->first;
-        return {};
+        const auto it = iteratorsItemIDs.find(iterator);
+        return it != iteratorsItemIDs.end() ? std::make_optional(it->second) : std::nullopt;
     }
 
     void add(std::uint64_t itemID, std::list<inventory::Item_v2>::const_iterator iterator)
     {
-        itemIDs.emplace(itemID, iterator);
+        if (const bool inserted = iteratorsItemIDs.try_emplace(iterator, itemID).second)
+            itemIDsIterators.try_emplace(itemID, iterator);
     }
 
     std::optional<std::uint64_t> remove(std::list<inventory::Item_v2>::const_iterator iterator)
     {
-        if (const auto it = std::ranges::find(itemIDs, iterator, &decltype(itemIDs)::value_type::second); it != itemIDs.end()) {
-            const auto itemID = it->first;
-            itemIDs.erase(it);
+        if (const auto it = iteratorsItemIDs.find(iterator); it != iteratorsItemIDs.end()) {
+            const auto itemID = it->second;
+            iteratorsItemIDs.erase(it);
+            itemIDsIterators.erase(itemID);
             return itemID;
         }
         return {};
     }
 
 private:
-    std::unordered_map<std::uint64_t, std::list<inventory::Item_v2>::const_iterator> itemIDs;
+    struct ItemIteratorHasher {
+        [[nodiscard]] std::size_t operator()(std::list<inventory::Item_v2>::const_iterator iterator) const noexcept
+        {
+            return std::hash<const inventory::Item_v2*>{}(std::to_address(iterator));
+        }
+    };
+
+    std::unordered_map<std::uint64_t, std::list<inventory::Item_v2>::const_iterator> itemIDsIterators;
+    std::unordered_map<std::list<inventory::Item_v2>::const_iterator, std::uint64_t, ItemIteratorHasher> iteratorsItemIDs;
 };
 
 }
