405c8a7c3ac6bf6c7046e23dbc3ae2e70334589a
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-09 11:09:37 +0200

    Move the implementation of handling backend responses to BackendResponseHandler.cpp

--- Source/InventoryChanger/BackendResponseHandler.cpp
+++ Source/InventoryChanger/BackendResponseHandler.cpp
@@ -1,1 +1,344 @@
 #include "BackendResponseHandler.h"
+
+#include "StaticData.h"
+#include "GameItems/Lookup.h"
+#include <SDK/EconItemView.h>
+#include <SDK/Panorama.h>
+#include <Interfaces.h>
+
+namespace
+{
+
+void initSkinEconItem(const inventory::Item_v2& inventoryItem, EconItem& econItem) noexcept
+{
+    EconItemAttributeSetter attributeSetter{ *memory->itemSystem()->getItemSchema() };
+
+    const auto paintKit = StaticData::lookup().getStorage().getPaintKit(inventoryItem.gameItem()).id;
+    attributeSetter.setPaintKit(econItem, static_cast<float>(paintKit));
+
+    const auto skin = inventoryItem.get<inventory::Skin>();
+    if (!skin)
+        return;
+
+    const auto& dynamicData = *skin;
+    const auto isMP5LabRats = Helpers::isMP5LabRats(inventoryItem.gameItem().getWeaponID(), paintKit);
+    if (dynamicData.isSouvenir() || isMP5LabRats) {
+        econItem.quality = 12;
+    } else {
+        if (dynamicData.statTrak > -1) {
+            attributeSetter.setStatTrak(econItem, dynamicData.statTrak);
+            attributeSetter.setStatTrakType(econItem, 0);
+            econItem.quality = 9;
+        }
+        if (Helpers::isKnife(econItem.weaponId))
+            econItem.quality = 3;
+    }
+
+    if (isMP5LabRats) {
+        attributeSetter.setSpecialEventID(econItem, 1);
+    } else {
+        if (dynamicData.tournamentID != 0)
+            attributeSetter.setTournamentID(econItem, dynamicData.tournamentID);
+
+        if (dynamicData.tournamentStage != TournamentStage{ 0 }) {
+            attributeSetter.setTournamentStage(econItem, static_cast<int>(dynamicData.tournamentStage));
+            attributeSetter.setTournamentTeam1(econItem, static_cast<int>(dynamicData.tournamentTeam1));
+            attributeSetter.setTournamentTeam2(econItem, static_cast<int>(dynamicData.tournamentTeam2));
+            if (dynamicData.proPlayer != static_cast<ProPlayer>(0))
+                attributeSetter.setTournamentPlayer(econItem, static_cast<int>(dynamicData.proPlayer));
+        }
+    }
+
+    attributeSetter.setWear(econItem, dynamicData.wear);
+    attributeSetter.setSeed(econItem, static_cast<float>(dynamicData.seed));
+    memory->setCustomName(&econItem, dynamicData.nameTag.c_str());
+
+    for (std::size_t j = 0; j < dynamicData.stickers.size(); ++j) {
+        const auto& sticker = dynamicData.stickers[j];
+        if (sticker.stickerID == 0)
+            continue;
+
+        attributeSetter.setStickerID(econItem, j, sticker.stickerID);
+        attributeSetter.setStickerWear(econItem, j, sticker.wear);
+    }
+}
+
+std::uint64_t createSOCItem(const inventory::Item_v2& inventoryItem, bool asUnacknowledged)
+{
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return 0;
+
+    const auto baseTypeCache = localInventory->getItemBaseTypeCache();
+    if (!baseTypeCache)
+        return 0;
+
+    const auto econItem = memory->createEconItemSharedObject();
+    econItem->itemID = localInventory->getHighestIDs().first + 1;
+    econItem->originalID = 0;
+    econItem->accountID = localInventory->getAccountID();
+    econItem->inventory = asUnacknowledged ? 0 : localInventory->getHighestIDs().second + 1;
+
+    const auto& item = inventoryItem.gameItem();
+    econItem->rarity = static_cast<std::uint16_t>(item.getRarity());
+    econItem->quality = 4;
+    econItem->weaponId = item.getWeaponID();
+
+    const auto& storage = StaticData::lookup().getStorage();
+
+    EconItemAttributeSetter attributeSetter{ *memory->itemSystem()->getItemSchema() };
+
+    if (item.isSticker()) {
+        attributeSetter.setStickerID(*econItem, 0, storage.getStickerKit(item).id);
+    } else if (item.isPatch()) {
+        attributeSetter.setStickerID(*econItem, 0, storage.getPatch(item).id);
+    } else if (item.isGraffiti()) {
+        attributeSetter.setStickerID(*econItem, 0, storage.getGraffitiKit(item).id);
+        const auto& dynamicData = *inventoryItem.get<inventory::Graffiti>();
+        if (dynamicData.usesLeft >= 0) {
+            econItem->weaponId = WeaponId::Graffiti;
+            attributeSetter.setSpraysRemaining(*econItem, dynamicData.usesLeft);
+        }
+    } else if (item.isMusic()) {
+        attributeSetter.setMusicID(*econItem, storage.getMusicKit(item).id);
+        const auto& dynamicData = *inventoryItem.get<inventory::Music>();
+        if (dynamicData.statTrak > -1) {
+            attributeSetter.setStatTrak(*econItem, dynamicData.statTrak);
+            attributeSetter.setStatTrakType(*econItem, 1);
+            econItem->quality = 9;
+        }
+    } else if (item.isSkin()) {
+        initSkinEconItem(inventoryItem, *econItem);
+    } else if (item.isGloves()) {
+        econItem->quality = 3;
+        attributeSetter.setPaintKit(*econItem, static_cast<float>(storage.getPaintKit(item).id));
+
+        const auto& dynamicData = *inventoryItem.get<inventory::Glove>();
+        attributeSetter.setWear(*econItem, dynamicData.wear);
+        attributeSetter.setSeed(*econItem, static_cast<float>(dynamicData.seed));
+    } else if (item.isCollectible()) {
+        if (storage.isCollectibleGenuine(item))
+            econItem->quality = 1;
+    } else if (item.isAgent()) {
+        if (const auto agent = inventoryItem.get<inventory::Agent>()) {
+            for (std::size_t j = 0; j < agent->patches.size(); ++j) {
+                const auto& patch = agent->patches[j];
+                if (patch.patchID == 0)
+                    continue;
+
+                attributeSetter.setStickerID(*econItem, j, patch.patchID);
+            }
+        }
+    } else if (item.isServiceMedal()) {
+        if (const auto& dynamicData = *inventoryItem.get<inventory::ServiceMedal>(); dynamicData.issueDateTimestamp != 0)
+            attributeSetter.setIssueDate(*econItem, dynamicData.issueDateTimestamp);
+    } else if (item.isTournamentCoin()) {
+        if (const auto tournamentCoin = inventoryItem.get<inventory::TournamentCoin>())
+            attributeSetter.setDropsAwarded(*econItem, tournamentCoin->dropsAwarded);
+        attributeSetter.setDropsRedeemed(*econItem, 0);
+    } else if (item.isCase() && StaticData::isSouvenirPackage(item)) {
+        if (const auto& dynamicData = *inventoryItem.get<inventory::SouvenirPackage>(); dynamicData.tournamentStage != TournamentStage{ 0 }) {
+            attributeSetter.setTournamentStage(*econItem, static_cast<int>(dynamicData.tournamentStage));
+            attributeSetter.setTournamentTeam1(*econItem, static_cast<int>(dynamicData.tournamentTeam1));
+            attributeSetter.setTournamentTeam2(*econItem, static_cast<int>(dynamicData.tournamentTeam2));
+            if (dynamicData.proPlayer != static_cast<ProPlayer>(0))
+                attributeSetter.setTournamentPlayer(*econItem, static_cast<int>(dynamicData.proPlayer));
+        }
+    }
+
+    baseTypeCache->addObject(econItem);
+    localInventory->soCreated(localInventory->getSOID(), (SharedObject*)econItem, 4);
+
+    if (const auto inventoryComponent = *memory->uiComponentInventory) {
+        memory->setItemSessionPropertyValue(inventoryComponent, econItem->itemID, "recent", "0");
+        memory->setItemSessionPropertyValue(inventoryComponent, econItem->itemID, "updated", "0");
+    }
+
+    if (const auto view = memory->findOrCreateEconItemViewForItemID(econItem->itemID))
+        view->clearInventoryImageRGBA();
+
+    return econItem->itemID;
+}
+
+[[nodiscard]] std::uint64_t assingNewItemID(std::uint64_t itemID)
+{
+    const auto view = memory->findOrCreateEconItemViewForItemID(itemID);
+    if (!view)
+        return itemID;
+
+    const auto econItem = memory->getSOCData(view);
+    if (!econItem)
+        return itemID;
+
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return itemID;
+
+    localInventory->soDestroyed(localInventory->getSOID(), (SharedObject*)econItem, 4);
+    const auto newItemID = localInventory->getHighestIDs().first + 1;
+    econItem->itemID = newItemID;
+    localInventory->soCreated(localInventory->getSOID(), (SharedObject*)econItem, 4);
+
+    if (const auto newView = memory->findOrCreateEconItemViewForItemID(newItemID))
+        newView->clearInventoryImageRGBA();
+
+    return newItemID;
+}
+
+static void initItemCustomizationNotification(std::string_view typeStr, std::uint64_t itemID)
+{
+    const auto idx = memory->registeredPanoramaEvents->find(memory->makePanoramaSymbol("PanoramaComponent_Inventory_ItemCustomizationNotification"));
+    if (idx == -1)
+        return;
+
+    using namespace std::string_view_literals;
+    std::string args{ "0,'" }; args += typeStr; args += "','"sv; args += std::to_string(itemID); args += '\'';
+    const char* dummy;
+    if (const auto event = memory->registeredPanoramaEvents->memory[idx].value.createEventFromString(nullptr, args.c_str(), &dummy))
+        interfaces->panoramaUIEngine->accessUIEngine()->dispatchEvent(event);
+}
+
+void applySticker(std::uint64_t itemID, int stickerID, std::uint8_t slot)
+{
+    const auto view = memory->findOrCreateEconItemViewForItemID(itemID);
+    if (!view)
+        return;
+
+    const auto econItem = memory->getSOCData(view);
+    if (!econItem)
+        return;
+
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return;
+
+    EconItemAttributeSetter attributeSetter{ *memory->itemSystem()->getItemSchema() };
+    attributeSetter.setStickerID(*econItem, slot, stickerID);
+    attributeSetter.setStickerWear(*econItem, slot, 0.0f);
+
+    localInventory->soUpdated(localInventory->getSOID(), (SharedObject*)econItem, 4);
+    initItemCustomizationNotification("sticker_apply", itemID);
+}
+
+void updateNameTag(std::uint64_t itemID, const char* newNameTag)
+{
+    const auto view = memory->findOrCreateEconItemViewForItemID(itemID);
+    if (!view)
+        return;
+
+    const auto econItem = memory->getSOCData(view);
+    if (!econItem)
+        return;
+
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return;
+
+    memory->setCustomName(econItem, newNameTag);
+    localInventory->soUpdated(localInventory->getSOID(), (SharedObject*)econItem, 4);
+}
+
+void deleteItem(std::uint64_t itemID)
+{
+    const auto view = memory->findOrCreateEconItemViewForItemID(itemID);
+    if (!view)
+        return;
+
+    const auto econItem = memory->getSOCData(view);
+    if (!econItem)
+        return;
+
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return;
+
+    localInventory->soDestroyed(localInventory->getSOID(), (SharedObject*)econItem, 4);
+
+    if (const auto baseTypeCache = localInventory->getItemBaseTypeCache())
+        baseTypeCache->removeObject(econItem);
+
+    econItem->destructor();
+}
+
+void updateStatTrak(std::uint64_t itemID, int newStatTrakValue)
+{
+    const auto view = memory->findOrCreateEconItemViewForItemID(itemID);
+    if (!view)
+        return;
+
+    const auto econItem = memory->getSOCData(view);
+    if (!econItem)
+        return;
+
+    const auto localInventory = memory->inventoryManager->getLocalInventory();
+    if (!localInventory)
+        return;
+
+    EconItemAttributeSetter attributeSetter{ *memory->itemSystem()->getItemSchema() };
+    attributeSetter.setStatTrak(*econItem, newStatTrakValue);
+    localInventory->soUpdated(localInventory->getSOID(), (SharedObject*)econItem, 4);
+}
+
+}
+
+namespace inventory_changer
+{
+
+void BackendResponseHandler::operator()(const backend::Response::ItemAdded& response) const
+{
+    const auto itemID = createSOCItem(*response.item, true);
+    backend.assignItemID(response.item, itemID);
+}
+
+void BackendResponseHandler::operator()(const backend::Response::ItemMovedToFront& response) const
+{
+    backend.updateItemID(response.itemID, assingNewItemID(response.itemID));
+}
+
+void BackendResponseHandler::operator()(const backend::Response::ItemRemoved& response) const
+{
+    deleteItem(response.itemID);
+}
+
+void BackendResponseHandler::operator()(const backend::Response::StickerApplied& response) const
+{
+    if (const auto itemID = backend.getItemID(response.skinItem); itemID.has_value()) {
+        if (const auto skin = response.skinItem->get<inventory::Skin>())
+            applySticker(*itemID, skin->stickers[response.stickerSlot].stickerID, response.stickerSlot);
+    }
+}
+
+void BackendResponseHandler::operator()(const backend::Response::StatTrakUpdated& response) const
+{
+    updateStatTrak(response.itemID, response.newStatTrakValue);
+}
+
+void BackendResponseHandler::operator()(const backend::Response::ViewerPassActivated& response) const
+{
+    if (const auto itemID = backend.getItemID(response.createdEventCoin); itemID.has_value())
+        initItemCustomizationNotification("ticket_activated", *itemID);
+}
+
+void BackendResponseHandler::operator()(const backend::Response::NameTagAdded& response) const
+{
+    if (const auto itemID = backend.getItemID(response.skinItem); itemID.has_value()) {
+        if (const auto skin = response.skinItem->get<inventory::Skin>()) {
+            updateNameTag(*itemID, skin->nameTag.c_str());
+            initItemCustomizationNotification("nametag_add", *itemID);
+        }
+    }
+}
+
+void BackendResponseHandler::operator()(const backend::Response::NameTagRemoved& response) const
+{
+    if (const auto itemID = backend.getItemID(response.skinItem); itemID.has_value())
+        updateNameTag(*itemID, "");
+}
+
+void BackendResponseHandler::operator()(const backend::Response::ContainerOpened& response) const
+{
+    if (const auto itemID = backend.getItemID(response.receivedItem); itemID.has_value())
+        initItemCustomizationNotification("crate_unlock", *itemID);
+}
+
+}
--- Source/InventoryChanger/BackendResponseHandler.h
+++ Source/InventoryChanger/BackendResponseHandler.h
@@ -13,14 +13,14 @@ struct BackendResponseHandler {
     void operator()(const backend::Response::ItemMovedToFront& response) const;
     void operator()(const backend::Response::ItemRemoved& response) const;
     void operator()(const backend::Response::StickerApplied& response) const;
-    void operator()(const backend::Response::StickerScraped& response) const;
-    void operator()(const backend::Response::StickerRemoved& response) const;
+    void operator()(const backend::Response::StickerScraped& response) const {}
+    void operator()(const backend::Response::StickerRemoved& response) const {}
     void operator()(const backend::Response::StatTrakUpdated& response) const;
     void operator()(const backend::Response::ViewerPassActivated& response) const;
     void operator()(const backend::Response::NameTagAdded& response) const;
     void operator()(const backend::Response::NameTagRemoved& response) const;
     void operator()(const backend::Response::ContainerOpened& response) const;
-    void operator()(const backend::Response::PatchApplied& response) const;
+    void operator()(const backend::Response::PatchApplied& response) const {}
 
 private:
     backend::BackendSimulator& backend;
