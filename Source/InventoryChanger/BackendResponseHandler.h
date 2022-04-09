b6574d8e0b7b954215d45c7ec4f4e8369f3ecaa1
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-09 10:51:37 +0200

    Add inventory_changer::BackendResponseHandler struct

--- Source/InventoryChanger/BackendResponseHandler.h
+++ Source/InventoryChanger/BackendResponseHandler.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include "Backend/BackendSimulator.h"
+#include "Backend/Response.h"
+
+namespace inventory_changer
+{
+
+struct BackendResponseHandler {
+    explicit BackendResponseHandler(backend::BackendSimulator& backend) : backend{ backend } {}
+
+    void operator()(const backend::Response::ItemAdded& response) const;
+    void operator()(const backend::Response::ItemMovedToFront& response) const;
+    void operator()(const backend::Response::ItemRemoved& response) const;
+    void operator()(const backend::Response::StickerApplied& response) const;
+    void operator()(const backend::Response::StickerScraped& response) const;
+    void operator()(const backend::Response::StickerRemoved& response) const;
+    void operator()(const backend::Response::StatTrakUpdated& response) const;
+    void operator()(const backend::Response::ViewerPassActivated& response) const;
+    void operator()(const backend::Response::NameTagAdded& response) const;
+    void operator()(const backend::Response::NameTagRemoved& response) const;
+    void operator()(const backend::Response::ContainerOpened& response) const;
+    void operator()(const backend::Response::PatchApplied& response) const;
+
+private:
+    backend::BackendSimulator& backend;
+};
+
+}
