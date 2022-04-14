124ce021d74402f60d87024bf9099f10720a8e6a
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-14 19:45:55 +0200

    Define ItemList, ItemIterator and ItemConstIterator type aliases in Backend/Item.h

--- Source/InventoryChanger/Backend/Item.h
+++ Source/InventoryChanger/Backend/Item.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <list>
+
+namespace inventory
+{
+    class Item;
+}
+
+namespace inventory_changer::backend
+{
+
+using ItemList = std::list<inventory::Item>;
+using ItemIterator = ItemList::iterator;
+using ItemConstIterator = ItemList::const_iterator;
+
+}
