7b1803b3097f5b86d4ecb56ad0ebee932f9ab8e1
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-20 22:00:47 +0200

    Spoof return address when calling the original function in MinHook::callOriginal()

--- Source/Hooks/MinHook.h
+++ Source/Hooks/MinHook.h
@@ -3,6 +3,11 @@
 #include <cstddef>
 #include <memory>
 
+#ifdef _WIN32
+#include <x86RetSpoof.h>
+#endif
+
+#include "../Memory.h"
 #include "../SDK/Platform.h"
 
 class MinHook {
@@ -20,7 +25,11 @@ public:
     template<typename T, std::size_t Idx, typename ...Args>
     constexpr auto callOriginal(Args... args) const noexcept
     {
+#ifdef _WIN32
+        return x86RetSpoof::invokeThiscall<T, Args...>(std::uintptr_t(base), originals[Idx], memory->jmpEbxGadgetInClient, args...);
+#else
         return getOriginal<T, Idx>(args...)(base, args...);
+#endif
     }
 
 private:
