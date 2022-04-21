844fbeba6eee294ab7e5e62979e42b42d334e576
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-21 23:20:18 +0200

    Add inventory_changer::backend::RequestHandler struct

--- Source/InventoryChanger/Backend/RequestHandler.h
+++ Source/InventoryChanger/Backend/RequestHandler.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "RequestTypes.h"
+#include "Response.h"
+
+namespace game_items
+{
+    class Lookup;
+}
+
+namespace inventory_changer::backend
+{
+
+class BackendSimulator;
+
+struct RequestHandler {
+    RequestHandler(BackendSimulator& backend, const game_items::Lookup& gameItemLookup, ItemConstRemover constRemover) : backend{ backend }, gameItemLookup{ gameItemLookup }, constRemover{ constRemover } {}
+
+private:
+    BackendSimulator& backend;
+    const game_items::Lookup& gameItemLookup;
+    ItemConstRemover constRemover;
+};
+
+}
