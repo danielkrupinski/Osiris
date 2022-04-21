5b5dbc1b9e07cdf42791d294e4887738eaa010ac
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-21 22:55:59 +0200

    Add inventory_changer::backend::request::ApplySticker struct

--- Source/InventoryChanger/Backend/RequestTypes.h
+++ Source/InventoryChanger/Backend/RequestTypes.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <cstdint>
+
+#include "Item.h"
+
+namespace inventory_changer::backend::request
+{
+
+struct ApplySticker {
+    ItemConstIterator item;
+    ItemConstIterator sticker;
+    std::uint8_t slot;
+};
+
+}
