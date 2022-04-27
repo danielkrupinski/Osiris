658513f7be966d251e96469b7a65d90b162dbf22
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-27 20:52:02 +0200

    Add unit tests for game_items::Lookup created from empty game_items::Storage

---
+++ Tests/InventoryChanger/GameItems/LookupTests.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include <InventoryChanger/GameItems/Lookup.h>
+#include <InventoryChanger/GameItems/Storage.h>
+
+namespace game_items
+{
+namespace
+{
+
+class InventoryChanger_GameItems_Lookup_EmptyStorageTest : public testing::Test {
+protected:
+    Lookup lookup{ Storage{} };
+};
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, ItemIsNotFound) {    
+    ASSERT_FALSE(lookup.findItem(WeaponId::BronzeOperationHydraCoin).has_value());
+}
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, ItemWithPaintkitIsNotFound) {
+    ASSERT_FALSE(lookup.findItem(WeaponId::Ak47, 14).has_value());
+}
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, MusicIsNotFound) {
+    ASSERT_FALSE(lookup.findMusic(3).has_value());
+}
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, StickerIsNotFound) {
+    ASSERT_FALSE(lookup.findSticker(3).has_value());
+}
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, GraffitiIsNotFound) {
+    ASSERT_FALSE(lookup.findGraffiti(3).has_value());
+}
+
+TEST_F(InventoryChanger_GameItems_Lookup_EmptyStorageTest, PatchIsNotFound) {
+    ASSERT_FALSE(lookup.findPatch(3).has_value());
+}
+
+}
+}
