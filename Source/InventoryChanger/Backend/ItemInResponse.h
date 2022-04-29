12dbe1fec9c9f527fb5b7c5ea21e8b00f884d876
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-29 21:04:45 +0200

    Implement invoke operators of ItemInResponse to check if the item is referenced in a response

--- Source/InventoryChanger/Backend/ItemInResponse.h
+++ Source/InventoryChanger/Backend/ItemInResponse.h
@@ -3,6 +3,7 @@
 #include <variant>
 
 #include "Item.h"
+#include "Response.h"
 #include "ResponseTypes.h"
 
 namespace inventory_changer::backend
@@ -11,24 +12,24 @@ namespace inventory_changer::backend
 struct ItemInResponse {
     explicit ItemInResponse(ItemConstIterator item) : item{ item } {}
 
-    void operator()(std::monostate) const;
-    void operator()(const response::ItemAdded& response) const;
-    void operator()(const response::ItemMovedToFront& response) const;
-    void operator()(const response::ItemEquipped& response) const;
-    void operator()(const response::ItemRemoved& response) const;
-    void operator()(const response::StickerApplied& response) const;
-    void operator()(const response::StickerScraped& response) const;
-    void operator()(const response::StickerRemoved& response) const;
-    void operator()(const response::StatTrakUpdated& response) const;
-    void operator()(const response::ViewerPassActivated& response) const;
-    void operator()(const response::NameTagAdded& response) const;
-    void operator()(const response::NameTagRemoved& response) const;
-    void operator()(const response::ContainerOpened& response) const;
-    void operator()(const response::PatchApplied& response) const;
-    void operator()(const response::PatchRemoved& response) const;
-    void operator()(const response::SouvenirTokenActivated& response) const;
-    void operator()(const response::GraffitiUnsealed& response) const;
-    void operator()(const response::StatTrakSwapped& response) const;
+    bool operator()(std::monostate) const { return false; }
+    bool operator()(const response::ItemAdded& response) const { return response.item == item; }
+    bool operator()(const response::ItemMovedToFront& response) const { return response.item == item; }
+    bool operator()(const response::ItemEquipped& response) const { return response.item == item; }
+    bool operator()(const response::ItemRemoved&) const { return false; }
+    bool operator()(const response::StickerApplied& response) const { return response.skinItem == item; }
+    bool operator()(const response::StickerScraped& response) const { return response.skinItem == item; }
+    bool operator()(const response::StickerRemoved& response) const { return response.skinItem == item; }
+    bool operator()(const response::StatTrakUpdated&) const { return false; }
+    bool operator()(const response::ViewerPassActivated& response) const { return response.createdEventCoin == item; }
+    bool operator()(const response::NameTagAdded& response) const { return response.skinItem == item; }
+    bool operator()(const response::NameTagRemoved& response) const { return response.skinItem == item; }
+    bool operator()(const response::ContainerOpened& response) const { return response.receivedItem == item; }
+    bool operator()(const response::PatchApplied& response) const { return response.agentItem == item; }
+    bool operator()(const response::PatchRemoved& response) const { return response.agentItem == item; }
+    bool operator()(const response::SouvenirTokenActivated& response) const { return response.tournamentCoin == item; }
+    bool operator()(const response::GraffitiUnsealed& response) const { return response.graffitiItem == item; }
+    bool operator()(const response::StatTrakSwapped& response) const { return response.swapDestinationItem == item || response.swapSourceItem == item; }
 
 private:
     ItemConstIterator item;
