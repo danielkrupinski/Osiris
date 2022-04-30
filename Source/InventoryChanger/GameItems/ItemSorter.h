efc38d9c6c75a0ce952a9cfae0d60964cb2b6d31
Daniel Krupiński <danielkrupinski@outlook.com>
2022-04-30 11:08:11 +0200

    Add game_items::ItemSorter struct

--- Source/InventoryChanger/GameItems/ItemSorter.h
+++ Source/InventoryChanger/GameItems/ItemSorter.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include "Item.h"
+#include "Storage.h"
+
+namespace game_items
+{
+
+struct ItemSorter {
+    explicit ItemSorter(const Storage& storage) : storage{ storage } {}
+
+    [[nodiscard]] bool operator()(const Item& a, const Item& b) const
+    {
+        if (a.getWeaponID() == b.getWeaponID() && (a.isSkin() || a.isGloves()) && (b.isSkin() || b.isGloves()))
+            return storage.getPaintKit(a).id < storage.getPaintKit(b).id;
+        return a.getWeaponID() < b.getWeaponID();
+    }
+
+private:
+    const Storage& storage;
+};
+
+}
