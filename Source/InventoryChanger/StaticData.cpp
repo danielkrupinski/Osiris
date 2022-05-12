984db8fc48e538d71493ae96d87544b02f670365
Daniel Krupiński <danielkrupinski@outlook.com>
2022-05-12 21:51:26 +0200

    Use Items::getStickers() instead of StaticDataImpl::initStickerData()

--- Source/InventoryChanger/StaticData.cpp
+++ Source/InventoryChanger/StaticData.cpp
@@ -137,33 +137,6 @@ private:
         }
     }
 
-    void initStickerData(ItemSchema* itemSchema, game_items::Storage& storage, ToUtf8Converter<>& converter, Helpers::ToUpperConverter& toUpperConverter) noexcept
-    {
-        const auto& stickerMap = itemSchema->stickerKits;
-        for (const auto& node : stickerMap) {
-            const auto stickerKit = node.value;
-            if (stickerKit->id == 0)
-                continue;
-
-            const auto name = std::string_view{ stickerKit->name.data(), static_cast<std::size_t>(stickerKit->name.length - 1) };
-            const auto isPatch = name.starts_with("patch") || name.starts_with("stockh2021_teampatch");
-            const auto isGraffiti = !isPatch && (name.starts_with("spray") || name.ends_with("graffiti"));
-            const auto isSticker = !isPatch && !isGraffiti;
-            
-            if (isSticker) {
-                const auto isGolden = name.ends_with("gold");
-                const auto stickerName = interfaces->localize->findSafe(stickerKit->id != 242 ? stickerKit->itemName.data() : "StickerKit_dhw2014_teamdignitas_gold");
-                storage.addSticker(stickerKit->id, game_items::ItemName{ converter.convertUnicodeToAnsi(stickerName), toUpperConverter.toUpper(stickerName) }, static_cast<EconRarity>(stickerKit->rarity),stickerKit->inventoryImage.data(), stickerKit->tournamentID, static_cast<TournamentTeam>(stickerKit->tournamentTeamID), stickerKit->tournamentPlayerID, isGolden);
-            } else if (isPatch) {
-                const auto patchName = interfaces->localize->findSafe(stickerKit->itemName.data());
-                storage.addPatch(stickerKit->id, game_items::ItemName{ converter.convertUnicodeToAnsi(patchName), toUpperConverter.toUpper(patchName) }, static_cast<EconRarity>(stickerKit->rarity), stickerKit->inventoryImage.data());
-            } else if (isGraffiti) {
-                const auto paintName = interfaces->localize->findSafe(stickerKit->itemName.data());
-                storage.addGraffiti(stickerKit->id, game_items::ItemName{ converter.convertUnicodeToAnsi(paintName), toUpperConverter.toUpper(paintName) }, static_cast<EconRarity>(stickerKit->rarity), stickerKit->inventoryImage.data());
-            }
-        }
-    }
-
     void initItemData(ItemSchema* itemSchema, game_items::Storage& storage, std::vector<int>& lootListIndices) noexcept
     {
         for (const auto& node : itemSchema->itemsSorted) {
@@ -312,8 +285,8 @@ private:
         Helpers::ToUpperConverter toUpperConverter;
 
         initSkinData(itemSchema, storage, converter, toUpperConverter);
-        initStickerData(itemSchema, storage, converter, toUpperConverter);
         inventory_changer::game_integration::Items items{ *itemSchema, *interfaces->localize };
+        items.getStickers(storage);
         items.getMusicKits(storage);
         std::vector<int> lootListIndices;
         initItemData(itemSchema, storage, lootListIndices);
