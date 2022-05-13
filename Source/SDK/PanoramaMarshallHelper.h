c3d87c857336e71214128fdc1a14cc9fe8c806dc
Daniel Krupiński <danielkrupinski@outlook.com>
2022-05-13 22:17:26 +0200

    Add PanoramaMarshallHelper class

--- Source/SDK/PanoramaMarshallHelper.h
+++ Source/SDK/PanoramaMarshallHelper.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "Inconstructible.h"
+#include "VirtualMethod.h"
+
+class PanoramaMarshallHelper {
+public:
+    INCONSTRUCTIBLE(PanoramaMarshallHelper)
+
+    VIRTUAL_METHOD(void, setResult, 14, (void* params, int result), (this, params, result))
+};
