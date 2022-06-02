ec28020d3c8c9afc024fb5e3c55af19e62df0f76
Daniel Krupiński <danielkrupinski@outlook.com>
2022-06-02 20:45:39 +0200

    Add inventory_changer::GameItemList class

--- Source/InventoryChanger/InventoryChanger.cpp
+++ Source/InventoryChanger/InventoryChanger.cpp
@@ -1,11 +1,17 @@
 #include <algorithm>
+#include <cassert>
 #include <charconv>
 #include <chrono>
 #include <cstdint>
 #include <fstream>
+#include <span>
 #include <string_view>
 #include <type_traits>
 #include <utility>
+#include <vector>
+
+#include <range/v3/algorithm/sort.hpp>
+#include <range/v3/view/zip.hpp>
 
 #define STBI_ONLY_PNG
 #define STBI_NO_FAILURE_STRINGS
@@ -684,6 +690,60 @@ namespace ImGui
         window->DC.CursorPosPrevLine.y = cursorPosBackup;
         window->DC.CursorPos.y = cursorPosNext;
     }
+}
+
+namespace inventory_changer
+{
+
+class GameItemList {
+public:
+    explicit GameItemList(std::span<const game_items::Item> items)
+        : items{ items.begin(), items.end() }, passesFilter(items.size(), true) {}
+
+    template <typename Predicate>
+    void sort(Predicate&& predicate)
+    {
+        ranges::sort(ranges::views::zip(items, passesFilter), std::forward<Predicate>(predicate), [](const auto& pair) { return std::get<0>(pair); });
+    }
+
+    template <typename Predicate>
+    void filter(Predicate predicate)
+    {
+        assert(items.size() == passesFilter.size());
+
+        for (std::size_t i = 0; i < items.size(); ++i)
+            passesFilter[i] = predicate(items[i]);
+    }
+
+    [[nodiscard]] std::span<const std::reference_wrapper<const game_items::Item>> getItems() noexcept
+    {
+        return items;
+    }
+
+    [[nodiscard]] bool itemPassesFilter(std::size_t itemIndex) noexcept
+    {
+        return passesFilter[itemIndex];
+    }
+
+private:
+    // wrapper around bool to not use std::vector<bool>
+    // which isn't compatible with ranges::sort()
+    struct Boolean {
+        explicit(false) Boolean(bool b) : b{ b } {}
+
+        explicit(false) operator bool() const noexcept
+        {
+            return b;
+        }
+
+    private:
+        bool b;
+    };
+
+    std::vector<std::reference_wrapper<const game_items::Item>> items;
+    std::vector<Boolean> passesFilter;
+};
+
 }
 
 void InventoryChanger::drawGUI(bool contentOnly) noexcept
