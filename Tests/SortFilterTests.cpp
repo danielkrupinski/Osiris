80a1b85c44e730d54b28ba13c65bf78cdfdee154
Daniel Krupiński <danielkrupinski@outlook.com>
2022-06-09 19:59:19 +0200

    Add SortFilter unit tests for non-copyable and non-movable types

--- Tests/SortFilterTests.cpp
+++ Tests/SortFilterTests.cpp
@@ -26,15 +26,51 @@ TEST(SortFilterTest, HasTotalNumberOfItemsEqualToSizeOfSpanPassedToConstructor) 
 }
 
 TEST(SortFilterTest, ItemsPassFilterAndPreserveOrderAfterConstruction) {
-    const std::vector<int> items = ranges::view::values(SortFilter<int>{ numbers }.getItems()) | ranges::to<std::vector>();
+    const std::vector<int> items = ranges::views::values(SortFilter<int>{ numbers }.getItems()) | ranges::to<std::vector>();
     ASSERT_THAT(items, testing::ElementsAreArray(numbers));
 }
 
 TEST(SortFilterTest, FilteringExcludesItemsNotSatisfyingPredicate) {
     SortFilter<int> sortFilter{ numbers };
     sortFilter.filter([](int number) { return number % 2 != 0; });
-    const std::vector<int> items = ranges::view::values(sortFilter.getItems()) | ranges::to<std::vector>();
+    const std::vector<int> items = ranges::views::values(sortFilter.getItems()) | ranges::to<std::vector>();
     ASSERT_THAT(items, testing::ElementsAre(1, 3));
+}
+
+struct NonCopyableNonMovable {
+    constexpr NonCopyableNonMovable(int value) : value{ value } {}
+
+    NonCopyableNonMovable(NonCopyableNonMovable&&) = delete;
+    NonCopyableNonMovable(const NonCopyableNonMovable&) = delete;
+    NonCopyableNonMovable& operator=(NonCopyableNonMovable&&) = delete;
+    NonCopyableNonMovable& operator=(const NonCopyableNonMovable&) = delete;
+
+    int value;
+};
+
+class SortFilter_NonCopyableNonMovableTest : public testing::Test {
+protected:
+    static constexpr std::array<NonCopyableNonMovable, 4> objects{ 1, 3, 4, 2 };
+};
+
+TEST_F(SortFilter_NonCopyableNonMovableTest, CanBeFiltered) {
+    SortFilter<NonCopyableNonMovable> sortFilter{ objects };
+    sortFilter.filter([](const auto& object) { return object.value > 2; });
+    const std::vector<int> values = sortFilter.getItems() |
+                                    ranges::views::values |
+                                    ranges::views::transform(&NonCopyableNonMovable::value) |
+                                    ranges::to<std::vector>();
+    ASSERT_THAT(values, testing::ElementsAre(3, 4));
+}
+
+TEST_F(SortFilter_NonCopyableNonMovableTest, CanBeSorted) {
+    SortFilter<NonCopyableNonMovable> sortFilter{ objects };
+    sortFilter.sort([](const auto& a, const auto& b) { return a.value > b.value; });
+    const std::vector<int> values = sortFilter.getItems() |
+                                    ranges::views::values |
+                                    ranges::views::transform(&NonCopyableNonMovable::value) |
+                                    ranges::to<std::vector>();
+    ASSERT_THAT(values, testing::ElementsAre(4, 3, 2, 1));
 }
 
 }
