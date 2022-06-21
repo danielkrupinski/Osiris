5e7691d4c286a3f4693eb4ed702e6c2687c59061
Daniel Krupiński <danielkrupinski@outlook.com>
2022-06-21 23:28:13 +0200

    Add more RequestBuilder unit tests

--- Tests/InventoryChanger/Backend/Request/RequestBuilderTests.cpp
+++ Tests/InventoryChanger/Backend/Request/RequestBuilderTests.cpp
@@ -16,6 +16,17 @@ struct MockRequestor {
     MOCK_METHOD(void, request, (const request::RemoveNameTag&));
     MOCK_METHOD(void, request, (const request::WearSticker&));
     MOCK_METHOD(void, request, (const request::RemovePatch&));
+    MOCK_METHOD(void, request, (const request::OpenContainer&));
+    MOCK_METHOD(void, request, (const request::PerformXRayScan&));
+    MOCK_METHOD(void, request, (const request::ClaimXRayScannedItem&));
+    MOCK_METHOD(void, request, (const request::SwapStatTrak&));
+    MOCK_METHOD(void, request, (const request::ActivateOperationPass&));
+    MOCK_METHOD(void, request, (const request::ActivateViewerPass&));
+    MOCK_METHOD(void, request, (const request::ActivateSouvenirToken&));
+    MOCK_METHOD(void, request, (const request::UnsealGraffiti&));
+    MOCK_METHOD(void, request, (const request::ApplySticker&));
+    MOCK_METHOD(void, request, (const request::ApplyPatch&));
+    MOCK_METHOD(void, request, (const request::AddNameTag&));
 };
 
 struct MockRequestorWrapper {
@@ -60,7 +71,7 @@ TEST_F(InventoryChanger_Backend_RequestBuilderTest, NameTagRemovalRequestedWhenI
     const auto dummyItem = createDummyItem<ItemType::Skin>();
     constexpr auto dummyItemID = 123;
 
-    EXPECT_CALL(requestor, request(testing::An<const request::RemoveNameTag&>()));
+    EXPECT_CALL(requestor, request(testing::Matcher<const request::RemoveNameTag&>(testing::FieldsAre(dummyItem))));
     itemIDMap.add(dummyItemID, dummyItem);
 
     requestBuilder.removeNameTagFrom(dummyItemID);
@@ -96,6 +107,22 @@ TEST_P(InventoryChanger_Backend_RequestBuilder_WearStickerTest, RemovingPatchIsR
 }
 
 INSTANTIATE_TEST_SUITE_P(, InventoryChanger_Backend_RequestBuilder_WearStickerTest, testing::Values(0, 1));
+
+TEST_F(InventoryChanger_Backend_RequestBuilderTest, NothingIsRequestedWhenIDsOfItemsAreInvalid) {
+    constexpr auto nonexistentItemID = 1234;
+    requestBuilder.useToolOn(nonexistentItemID, nonexistentItemID);
+}
+
+TEST_F(InventoryChanger_Backend_RequestBuilderTest, OpeningKeylessContainerCanBeRequested) {
+    const auto crate = createDummyItem<ItemType::Case>();
+    constexpr auto dummyItemID = 123;
+
+    EXPECT_CALL(requestor, request(testing::Matcher<const request::OpenContainer&>(testing::FieldsAre(crate, std::nullopt))));
+    itemIDMap.add(dummyItemID, crate);
+
+    constexpr auto nonexistentItemID = 1234;
+    requestBuilder.useToolOn(nonexistentItemID, dummyItemID);
+}
 
 }
 }
