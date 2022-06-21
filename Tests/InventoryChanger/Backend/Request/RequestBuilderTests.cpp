53d4421ac1a557bca69dfbe45d73e0d4f79fd603
Daniel Krupiński <danielkrupinski@outlook.com>
2022-06-21 22:39:27 +0200

    Implement unit tests for RequestBuilder class

---
+++ Tests/InventoryChanger/Backend/Request/RequestBuilderTests.cpp
@@ -0,0 +1,101 @@
+#include <list>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <InventoryChanger/Backend/Item.h>
+#include <InventoryChanger/Backend/ItemIDMap.h>
+#include <InventoryChanger/Backend/Request/RequestBuilder.h>
+
+namespace inventory_changer::backend
+{
+namespace
+{
+
+struct MockRequestor {
+    MOCK_METHOD(void, request, (const request::RemoveNameTag&));
+    MOCK_METHOD(void, request, (const request::WearSticker&));
+    MOCK_METHOD(void, request, (const request::RemovePatch&));
+};
+
+struct MockRequestorWrapper {
+    MockRequestorWrapper(MockRequestor& requestor) : requestor{ requestor } {}
+
+    template <typename RequestType, typename... Args>
+    void request(Args&&... args)
+    {
+        requestor.request(RequestType{ std::forward<Args>(args)... });
+    }
+
+private:
+    MockRequestor& requestor;
+};
+
+class InventoryChanger_Backend_RequestBuilderTest : public testing::Test {
+protected:
+    using ItemType = game_items::Item::Type;
+
+    template <ItemType type>
+    [[nodiscard]] ItemIterator createDummyItem()
+    {
+        static constexpr game_items::Item dummyGameItem{ type, EconRarity::Red, WeaponId::None, 0, {} };
+        itemList.emplace_back(dummyGameItem);
+        return std::prev(itemList.end());
+    }
+
+    testing::StrictMock<MockRequestor> requestor;
+    ItemIDMap itemIDMap;
+    RequestBuilder<MockRequestorWrapper> requestBuilder{ itemIDMap, MockRequestorWrapper{ requestor } };
+
+private:
+    ItemList itemList;
+};
+
+TEST_F(InventoryChanger_Backend_RequestBuilderTest, NameTagRemovalIsNotRequestedWhenItemIdIsInvalid) {
+    constexpr auto nonexistentItemID = 1234;
+    requestBuilder.removeNameTagFrom(nonexistentItemID);
+}
+
+TEST_F(InventoryChanger_Backend_RequestBuilderTest, NameTagRemovalRequestedWhenItemIdIsValid) {
+    const auto dummyItem = createDummyItem<ItemType::Skin>();
+    constexpr auto dummyItemID = 123;
+
+    EXPECT_CALL(requestor, request(testing::An<const request::RemoveNameTag&>()));
+    itemIDMap.add(dummyItemID, dummyItem);
+
+    requestBuilder.removeNameTagFrom(dummyItemID);
+}
+
+TEST_F(InventoryChanger_Backend_RequestBuilderTest, NothingIsRequestedWhenIdOfItemToWearStickerOfIsInvalid) {
+    constexpr auto nonexistentItemID = 1234;
+    requestBuilder.wearStickerOf(nonexistentItemID, 0);
+}
+
+class InventoryChanger_Backend_RequestBuilder_WearStickerTest
+    : public InventoryChanger_Backend_RequestBuilderTest,
+      public testing::WithParamInterface<std::uint8_t> {};
+
+TEST_P(InventoryChanger_Backend_RequestBuilder_WearStickerTest, WearingStickerIsRequestedWhenItemIsSkin) {
+    const auto skin = createDummyItem<ItemType::Skin>();
+    constexpr auto dummyItemID = 123;
+
+    EXPECT_CALL(requestor, request(testing::Matcher<const request::WearSticker&>(testing::FieldsAre(skin, GetParam()))));
+    itemIDMap.add(dummyItemID, skin);
+
+    requestBuilder.wearStickerOf(dummyItemID, GetParam());
+}
+
+TEST_P(InventoryChanger_Backend_RequestBuilder_WearStickerTest, RemovingPatchIsRequestedWhenItemIsAgent) {
+    const auto agent = createDummyItem<ItemType::Agent>();
+    constexpr auto dummyItemID = 123;
+
+    EXPECT_CALL(requestor, request(testing::Matcher<const request::RemovePatch&>(testing::FieldsAre(agent, GetParam()))));
+    itemIDMap.add(dummyItemID, agent);
+
+    requestBuilder.wearStickerOf(dummyItemID, GetParam());
+}
+
+INSTANTIATE_TEST_SUITE_P(, InventoryChanger_Backend_RequestBuilder_WearStickerTest, testing::Values(0, 1));
+
+}
+}
