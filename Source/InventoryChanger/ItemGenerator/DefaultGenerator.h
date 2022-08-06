fad023c84d411676c8b71647672f460056557103
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-06 20:34:49 +0200

    Move DefaultGenerator class definition to ItemGenerator/DefaultGenerator.h

--- Source/InventoryChanger/ItemGenerator/DefaultGenerator.h
+++ Source/InventoryChanger/ItemGenerator/DefaultGenerator.h
@@ -1,1 +1,88 @@
 #pragma once
+
+#include <InventoryChanger/GameItems/Storage.h>
+#include <InventoryChanger/Inventory/Item.h>
+#include <InventoryChanger/Inventory/Structs.h>
+
+#include "TournamentMatches.h"
+#include "Utils.h"
+
+namespace inventory_changer::item_generator
+{
+
+class DefaultGenerator {
+public:
+    explicit DefaultGenerator(const game_items::Storage& gameItemStorage) : gameItemStorage{ gameItemStorage } {}
+
+    [[nodiscard]] inventory::Skin createSkin(const game_items::Item& item) const
+    {
+        const auto& paintKit = gameItemStorage.getPaintKit(item);
+
+        inventory::Skin skin;
+        skin.wear = std::lerp(paintKit.wearRemapMin, paintKit.wearRemapMax, generateWear(FactoryNew));
+        skin.seed = randomSeed();
+
+        if (Helpers::isMP5LabRats(item.getWeaponID(), gameItemStorage.getPaintKit(item).id))
+            skin.stickers[3].stickerID = 28;
+
+        return skin;
+    }
+
+    [[nodiscard]] inventory::Glove createGloves(const game_items::Item& item) const
+    {
+        const auto& paintKit = gameItemStorage.getPaintKit(item);
+        return inventory::Glove{
+            .wear = std::lerp(paintKit.wearRemapMin, paintKit.wearRemapMax, generateWear(FactoryNew)),
+            .seed = randomSeed()
+        };
+    }
+
+    [[nodiscard]] inventory::ServiceMedal createServiceMedal(const game_items::Item& item) const
+    {
+        return inventory::ServiceMedal{
+            .issueDateTimestamp = getRandomDateTimestampOfYear(gameItemStorage.getServiceMedalYear(item))
+        };
+    }
+
+    [[nodiscard]] inventory::SouvenirPackage createSouvenirPackage(const game_items::Item& item) const
+    {
+        return std::visit([](const auto& matches) {
+            inventory::SouvenirPackage dynamicData;
+
+            if (matches.empty())
+                return dynamicData;
+
+            const auto& randomMatch = matches[Helpers::random(static_cast<std::size_t>(0), matches.size() - 1)];
+            dynamicData.tournamentStage = randomMatch.stage;
+            dynamicData.tournamentTeam1 = randomMatch.team1;
+            dynamicData.tournamentTeam2 = randomMatch.team2;
+
+            if constexpr (std::is_same_v<decltype(randomMatch), const MatchWithMVPs&>) {
+                dynamicData.proPlayer = randomMatch.getRandomMVP();
+            }
+
+            return dynamicData;
+        }, getTournamentMatchesOnMap(gameItemStorage.getTournamentEventID(item), gameItemStorage.getTournamentMap(item)));
+    }
+
+    [[nodiscard]] inventory::ItemData createItemData(const game_items::Item& item) const
+    {
+        if (item.isSkin()) {
+            return createSkin(item);
+        } else if (item.isGloves()) {
+            return createGloves(item);
+        } else if (item.isCase()) {
+            if (gameItemStorage.isSouvenirPackage(item))
+                return createSouvenirPackage(item);
+        } else if (item.isServiceMedal()) {
+            return createServiceMedal(item);
+        }
+
+        return {};
+    }
+
+private:
+    const game_items::Storage& gameItemStorage;
+};
+
+}
--- Source/InventoryChanger/ItemGenerator/ItemGenerator.cpp
+++ Source/InventoryChanger/ItemGenerator/ItemGenerator.cpp
@@ -16,6 +16,7 @@
 #include <InventoryChanger/Inventory/Structs.h>
 #include <InventoryChanger/GameIntegration/Misc.h>
 
+#include "DefaultGenerator.h"
 #include "Utils.h"
 
 [[nodiscard]] static std::array<inventory_changer::inventory::Skin::Sticker, 5> generateSouvenirStickers(const inventory_changer::game_items::Lookup& gameItemLookup, WeaponId weaponID, std::uint32_t tournamentID, inventory_changer::TournamentMap map, TournamentTeam team1, TournamentTeam team2, csgo::ProPlayer player) noexcept;
@@ -923,81 +924,6 @@ std::optional<inventory::Item> generateItemFromContainer(const game_items::Looku
     const auto& unlockedItem = getRandomItemIndexFromContainer(gameItemLookup, crateLootLookup, caseItem.gameItem().getWeaponID(), *lootList);
     return inventory::Item{ unlockedItem, generateItemData(gameItemLookup, unlockedItem, caseItem, lootList->willProduceStatTrak) };
 }
-
-class DefaultGenerator {
-public:
-    explicit DefaultGenerator(const game_items::Storage& gameItemStorage) : gameItemStorage{ gameItemStorage } {}
-
-    [[nodiscard]] inventory::Skin createSkin(const game_items::Item& item) const
-    {
-        const auto& paintKit = gameItemStorage.getPaintKit(item);
-
-        inventory::Skin skin;
-        skin.wear = std::lerp(paintKit.wearRemapMin, paintKit.wearRemapMax, generateWear(FactoryNew));
-        skin.seed = randomSeed();
-
-        if (Helpers::isMP5LabRats(item.getWeaponID(), gameItemStorage.getPaintKit(item).id))
-            skin.stickers[3].stickerID = 28;
-
-        return skin;
-    }
-
-    [[nodiscard]] inventory::Glove createGloves(const game_items::Item& item) const
-    {
-        const auto& paintKit = gameItemStorage.getPaintKit(item);
-        return inventory::Glove{
-            .wear = std::lerp(paintKit.wearRemapMin, paintKit.wearRemapMax, generateWear(FactoryNew)),
-            .seed = randomSeed()
-        };
-    }
-
-    [[nodiscard]] inventory::ServiceMedal createServiceMedal(const game_items::Item& item) const
-    {
-        return inventory::ServiceMedal{
-            .issueDateTimestamp = getRandomDateTimestampOfYear(gameItemStorage.getServiceMedalYear(item))
-        };
-    }
-
-    [[nodiscard]] inventory::SouvenirPackage createSouvenirPackage(const game_items::Item& item) const
-    {
-        return std::visit([](const auto& matches) {
-            inventory::SouvenirPackage dynamicData;
-
-            if (matches.empty())
-                return dynamicData;
-
-            const auto& randomMatch = matches[Helpers::random(static_cast<std::size_t>(0), matches.size() - 1)];
-            dynamicData.tournamentStage = randomMatch.stage;
-            dynamicData.tournamentTeam1 = randomMatch.team1;
-            dynamicData.tournamentTeam2 = randomMatch.team2;
-
-            if constexpr (std::is_same_v<decltype(randomMatch), const MatchWithMVPs&>) {
-                dynamicData.proPlayer = randomMatch.getRandomMVP();
-            }
-
-            return dynamicData;
-        }, getTournamentMatchesOnMap(gameItemStorage.getTournamentEventID(item), gameItemStorage.getTournamentMap(item)));
-    }
-
-    [[nodiscard]] inventory::ItemData createItemData(const game_items::Item& item) const
-    {
-        if (item.isSkin()) {
-            return createSkin(item);
-        } else if (item.isGloves()) {
-            return createGloves(item);
-        } else if (item.isCase()) {
-            if (gameItemStorage.isSouvenirPackage(item))
-                return createSouvenirPackage(item);
-        } else if (item.isServiceMedal()) {
-            return createServiceMedal(item);
-        }
-
-        return {};
-    }
-
-private:
-    const game_items::Storage& gameItemStorage;
-};
 
 inventory::ItemData createDefaultDynamicData(const game_items::Storage& gameItemStorage, const game_items::Item& item) noexcept
 {
