9a96200a28a88e30327b6d9969c3242012faea32
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-08 21:53:28 +0200

    Add inventory_changer::item_generator::AttributeGenerator class

--- Source/InventoryChanger/ItemGenerator/AttributeGenerator.h
+++ Source/InventoryChanger/ItemGenerator/AttributeGenerator.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include <array>
+#include <random>
+
+#include "Utils.h"
+
+namespace inventory_changer::item_generator
+{
+
+template <typename RandomEngine>
+class AttributeGenerator {
+public:
+    explicit AttributeGenerator(RandomEngine& randomEngine) : randomEngine{ randomEngine } {}
+
+    [[nodiscard]] SkinCondition generateSkinCondition() const
+    {
+        if (const auto condition = std::uniform_int_distribution{ 1, 10'000 }(randomEngine); condition <= 1471)
+            return FactoryNew;
+        else if (condition <= 3939)
+            return MinimalWear;
+        else if (condition <= 8257)
+            return FieldTested;
+        else if (condition <= 9049)
+            return WellWorn;
+        return BattleScarred;
+    }
+
+    [[nodiscard]] float generatePaintKitWear(SkinCondition condition) const
+    {
+        static constexpr auto wearRanges = std::to_array<float>({ 0.0f, 0.07f, 0.15f, 0.38f, 0.45f, 1.0f });
+        return std::uniform_real_distribution{ wearRanges[condition - 1], wearRanges[condition] }(randomEngine);
+    }
+
+    [[nodiscard]] int generatePaintKitSeed() const
+    {
+        return std::uniform_int_distribution{ 1, 1000 }(randomEngine);
+    }
+
+private:
+    RandomEngine& randomEngine;
+};
+
+}
