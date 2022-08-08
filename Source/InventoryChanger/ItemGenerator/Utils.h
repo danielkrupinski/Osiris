57a12e1b92603b582ec41f3cf2457772a075b586
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-08 22:34:35 +0200

    Remove functions from ItemGenerator/Utils.h that are implemented by AttributeGenerator class

--- Source/InventoryChanger/ItemGenerator/Utils.h
+++ Source/InventoryChanger/ItemGenerator/Utils.h
@@ -13,35 +13,6 @@ enum SkinCondition {
     BattleScarred
 };
 
-inline SkinCondition randomSkinCondition()
-{
-    if (const auto condition = Helpers::random(1, 10000); condition <= 1471)
-        return FactoryNew;
-    else if (condition <= 3939)
-        return MinimalWear;
-    else if (condition <= 8257)
-        return FieldTested;
-    else if (condition <= 9049)
-        return WellWorn;
-    return BattleScarred;
-}
-
-inline float generateWear(SkinCondition condition)
-{
-    static constexpr auto wearRanges = std::to_array<float>({ 0.0f, 0.07f, 0.15f, 0.38f, 0.45f, 1.0f });
-    return Helpers::random(wearRanges[condition - 1], wearRanges[condition]);
-}
-
-inline float generateWear()
-{
-    return generateWear(randomSkinCondition());
-}
-
-[[nodiscard]] inline int randomSeed()
-{
-    return Helpers::random(1, 1000);
-}
-
 [[nodiscard]] inline std::pair<std::time_t, std::time_t> clampTimespanToNow(std::time_t min, std::time_t max) noexcept
 {
     const auto now = std::time(nullptr);
