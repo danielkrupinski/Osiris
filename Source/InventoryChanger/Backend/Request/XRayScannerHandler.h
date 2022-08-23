cc8463f739051773d220f3f28054276e87d520fb
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-23 21:41:45 +0200

    Add inventory_changer::backend::XRayScannerHandler class

---
+++ Source/InventoryChanger/Backend/Request/XRayScannerHandler.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <InventoryChanger/Backend/Item.h>
+#include <InventoryChanger/ItemGenerator/ItemGenerator.h>
+
+namespace inventory_changer::backend
+{
+
+template <typename ResponseAccumulator>
+class XRayScannerHandler {
+public:
+    XRayScannerHandler(const game_items::Lookup& gameItemLookup, const game_items::CrateLootLookup& crateLootLookup, XRayScanner& xRayScanner, InventoryHandler<ResponseAccumulator> inventoryHandler, ItemRemovalHandler<ResponseAccumulator> itemRemovalHandler, ResponseAccumulator responseAccumulator, ItemConstRemover constRemover)
+        : gameItemLookup{ gameItemLookup }, crateLootLookup{ crateLootLookup }, xRayScanner{ xRayScanner }, inventoryHandler{ inventoryHandler }, itemRemovalHandler{ itemRemovalHandler }, responseAccumulator{responseAccumulator}, constRemover{constRemover} {}
+
+    void performXRayScan(ItemIterator crate) const
+    {
+        if (!crate->gameItem().isCrate())
+            return;
+
+        auto generatedItem = item_generator::generateItemFromContainer(gameItemLookup, crateLootLookup, *crate, nullptr);
+        if (!generatedItem.has_value())
+            return;
+
+        constRemover.removeConstness(crate)->setState(inventory::Item::State::InXrayScanner);
+        responseAccumulator(response::ItemHidden{ crate });
+
+        generatedItem->setState(inventory::Item::State::InXrayScanner);
+
+        const auto receivedItem = inventoryHandler.addItem(std::move(*generatedItem), true);
+        xRayScanner.storeItems(XRayScanner::Items{ receivedItem, crate });
+        responseAccumulator(response::XRayScannerUsed{ receivedItem });
+    }
+
+    void claimXRayScannedItem(ItemIterator crate, std::optional<ItemIterator> key) const
+    {
+        const auto scannerItems = xRayScanner.getItems();
+        if (!scannerItems.has_value())
+            return;
+
+        if (crate != scannerItems->crate)
+            return;
+
+        if (key.has_value()) {
+            if (const auto& keyItem = *key; keyItem->gameItem().isCaseKey()) {
+                constRemover.removeConstness(scannerItems->reward)->getProperties().common.tradableAfterDate = keyItem->getProperties().common.tradableAfterDate;
+                itemRemovalHandler.removeItem(keyItem);
+            }
+        }
+
+        itemRemovalHandler.removeItem(crate);
+        constRemover.removeConstness(scannerItems->reward)->setState(inventory::Item::State::Default);
+        responseAccumulator(response::ItemUnhidden{ scannerItems->reward });
+        responseAccumulator(response::XRayItemClaimed{ scannerItems->reward });
+    }
+
+private:
+    const game_items::Lookup& gameItemLookup;
+    const game_items::CrateLootLookup& crateLootLookup;
+    XRayScanner& xRayScanner;
+    InventoryHandler<ResponseAccumulator> inventoryHandler;
+    ItemRemovalHandler<ResponseAccumulator> itemRemovalHandler;
+    ResponseAccumulator responseAccumulator;
+    ItemConstRemover constRemover;
+};
+
+}
