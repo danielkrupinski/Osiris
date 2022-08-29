a7e534093fa155b90f770a72f2b00bd64516f140
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-29 21:51:28 +0200

    Add debug assertions to StorageUnitManager class

--- Source/InventoryChanger/Backend/StorageUnitManager.h
+++ Source/InventoryChanger/Backend/StorageUnitManager.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <cstdint>
 #include <optional>
 #include <unordered_map>
@@ -13,11 +14,14 @@ class StorageUnitManager {
 public:
     void addItemToStorageUnit(ItemIterator item, ItemIterator storageUnit)
     {
+        assert(!item->gameItem().isStorageUnit() && storageUnit->gameItem().isStorageUnit());
         itemsStorageUnits[item] = storageUnit;
     }
 
     bool removeItemFromStorageUnit(ItemIterator item, ItemIterator storageUnit)
-    { 
+    {
+        assert(!item->gameItem().isStorageUnit() && storageUnit->gameItem().isStorageUnit());
+
         if (const auto it = itemsStorageUnits.find(item); it != itemsStorageUnits.end() && it->second == storageUnit) {
             itemsStorageUnits.erase(it);
             return true;
@@ -46,6 +50,8 @@ public:
     template <typename Function>
     void forEachItemInStorageUnit(ItemIterator storageUnit, Function function) const
     {
+        assert(storageUnit->gameItem().isStorageUnit());
+
         for (const auto& [item, storageUnitContainingItem] : itemsStorageUnits) {
             if (storageUnitContainingItem == storageUnit)
                 function(item);
