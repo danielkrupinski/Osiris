0201974f7e6692addeecc9aee83dd58a72979018
Daniel Krupiński <danielkrupinski@outlook.com>
2022-08-30 21:15:00 +0200

    Implement tests for StorageUnitManager::getStorageUnitIDs() method

--- Tests/InventoryChanger/Backend/StorageUnitManagerTests.cpp
+++ Tests/InventoryChanger/Backend/StorageUnitManagerTests.cpp
@@ -1,3 +1,6 @@
+#include <array>
+#include <functional>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
@@ -11,6 +14,12 @@ namespace inventory_changer::backend
 {
 namespace
 {
+
+template <std::size_t N>
+struct StorageUnitWithItems {
+    ItemIterator storageUnit;
+    std::array<ItemIterator, N> items;
+};
 
 class InventoryChanger_Backend_StorageUnitManagerTest : public testing::Test {
 protected:
@@ -26,6 +35,21 @@ protected:
         static constexpr game_items::Item storageUnit{ game_items::Item::Type::StorageUnit, EconRarity::Red, WeaponId::None, 0, {} };
         itemList.emplace_back(storageUnit);
         return std::prev(itemList.end());
+    }
+
+    template <std::size_t N>
+    [[nodiscard]] StorageUnitWithItems<N> createStorageUnitWithItems()
+    {
+        StorageUnitWithItems<N> storageUnitWithItems;
+        storageUnitWithItems.storageUnit = createDummyStorageUnit();
+
+        std::generate(storageUnitWithItems.items.begin(), storageUnitWithItems.items.end(), [this, &storageUnit = storageUnitWithItems.storageUnit](){
+            const auto item = createDummyItem();
+            storageUnitManager.addItemToStorageUnit(item, storageUnit);
+            return item;
+        });
+        
+        return storageUnitWithItems;
     }
 
     StorageUnitManager storageUnitManager;
@@ -96,6 +120,43 @@ TEST_F(InventoryChanger_Backend_StorageUnitManagerTest, ItemCanBeRemovedFromStor
     const auto storageUnit = createDummyStorageUnit();
     storageUnitManager.addItemToStorageUnit(item, storageUnit);
     EXPECT_TRUE(storageUnitManager.removeItemFromStorageUnit(item, storageUnit));
+}
+
+struct StorageUnitIdProjection {
+    explicit StorageUnitIdProjection(const std::unordered_map<ItemIterator, std::uint32_t>& storageUnitIDs)
+        : storageUnitIDs{ storageUnitIDs } {}
+
+    [[nodiscard]] std::uint32_t operator()(ItemIterator item) const
+    {
+        return storageUnitIDs.at(item);
+    }
+
+private:
+    const std::unordered_map<ItemIterator, std::uint32_t>& storageUnitIDs;
+};
+
+class InventoryChanger_Backend_StorageUnitManager_StorageUnitIDsTest
+    : public InventoryChanger_Backend_StorageUnitManagerTest {
+protected:
+    std::array<StorageUnitWithItems<4>, 2> storageUnitItems{ createStorageUnitWithItems<4>(), createStorageUnitWithItems<4>() };
+    std::unordered_map<ItemIterator, std::uint32_t> storageUnitIDs = storageUnitManager.getStorageUnitIDs();
+    StorageUnitIdProjection getID{ storageUnitIDs };
+};
+
+TEST_F(InventoryChanger_Backend_StorageUnitManager_StorageUnitIDsTest, DifferentStorageUnitsHaveDifferentIDs) {
+    EXPECT_NE(getID(storageUnitItems[0].storageUnit), getID(storageUnitItems[1].storageUnit));
+}
+
+TEST_F(InventoryChanger_Backend_StorageUnitManager_StorageUnitIDsTest, ItemsInSameStorageUnitHaveSameID) {
+    EXPECT_EQ(std::ranges::adjacent_find(storageUnitItems[0].items, std::not_equal_to{}, getID), storageUnitItems[0].items.end());
+}
+
+TEST_F(InventoryChanger_Backend_StorageUnitManager_StorageUnitIDsTest, ItemsFromDifferentStorageUnitsHaveDifferentIDs) {
+    EXPECT_TRUE(std::ranges::equal(storageUnitItems[0].items, storageUnitItems[1].items, std::not_equal_to{}, getID, getID));
+}
+
+TEST_F(InventoryChanger_Backend_StorageUnitManager_StorageUnitIDsTest, ItemsHaveSameIdAsTheirStorageUnit) {
+    EXPECT_EQ(getID(storageUnitItems[0].storageUnit), getID(storageUnitItems[0].items[0]));
 }
 
 }
