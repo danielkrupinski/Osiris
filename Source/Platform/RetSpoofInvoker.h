b8c94f13b7f891a6523470333aaa3414a0c21d81
Daniel Krupiński <danielkrupinski@outlook.com>
2022-09-30 22:00:08 +0200

    Add linux_platform::RetSpoofInvoker class

--- Source/Platform/RetSpoofInvoker.h
+++ Source/Platform/RetSpoofInvoker.h
@@ -3,4 +3,7 @@
 #ifdef _WIN32
 #include "Windows/RetSpoofInvoker.h"
 using RetSpoofInvoker = windows_platform::RetSpoofInvoker;
+#else
+#include "Linux/RetSpoofInvoker.h"
+using RetSpoofInvoker = linux_platform::RetSpoofInvoker;
 #endif
