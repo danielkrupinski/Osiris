9da4e092c363145289c06ea63f09090cc6acc1c2
Daniel Krupiński <danielkrupinski@outlook.com>
2022-10-22 18:38:20 +0200

    Remove VIRTUAL_METHOD() and VIRTUAL_METHOD_V() macros

--- Source/SDK/VirtualMethod.h
+++ Source/SDK/VirtualMethod.h
@@ -98,15 +98,3 @@ returnType name args const noexcept \
 #else
 #define VIRTUAL_METHOD2_V(returnType, name, idx, args, argsRaw) VIRTUAL_METHOD2(returnType, name, idx + 1, args, argsRaw)
 #endif
-
-#define VIRTUAL_METHOD(returnType, name, idx, args, argsRaw) \
-returnType name args noexcept \
-{ \
-    return VirtualMethod::call<returnType, idx>argsRaw; \
-}
-
-#ifdef _WIN32
-#define VIRTUAL_METHOD_V(returnType, name, idx, args, argsRaw) VIRTUAL_METHOD(returnType, name, idx, args, argsRaw)
-#else
-#define VIRTUAL_METHOD_V(returnType, name, idx, args, argsRaw) VIRTUAL_METHOD(returnType, name, idx + 1, args, argsRaw)
-#endif
