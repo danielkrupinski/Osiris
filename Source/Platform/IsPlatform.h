d74c699ab2f24899d578f12d01e7ca9e8bf4c230
Daniel Krupiński <danielkrupinski@outlook.com>
2022-10-22 21:33:05 +0200

    Add IS_LINUX() macro

--- Source/Platform/IsPlatform.h
+++ Source/Platform/IsPlatform.h
@@ -3,13 +3,16 @@
 #if defined(_WIN32)
 
 #define IS_WIN32() true
+#define IS_LINUX() false
 
 #elif defined(__linux__)
 
 #define IS_WIN32() false
+#define IS_LINUX() true
 
 #else
 
 #define IS_WIN32() false
+#define IS_LINUX() false
 
 #endif
