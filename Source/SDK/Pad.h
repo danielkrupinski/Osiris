e9a2a25d2f29ff06f4d6a9d7d492679a80d73c9e
Daniel Krupiński <danielkrupinski@outlook.com>
2022-10-22 18:46:45 +0200

    Do not change access specifier in PAD() macro to allow structs using it to be standard-layout types

--- Source/SDK/Pad.h
+++ Source/SDK/Pad.h
@@ -5,7 +5,4 @@
 #define CONCAT(a, b) a##b
 #define PAD_NAME(n) CONCAT(pad, n)
 
-#define PAD(size) \
-private: \
-    std::byte PAD_NAME(__LINE__) [size]; \
-public:
+#define PAD(size) std::byte PAD_NAME(__LINE__) [size];
