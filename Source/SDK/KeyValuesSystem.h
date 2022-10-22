a810dc10b35c74b0c5445a1d0c75a71ed2b05dbc
Daniel Krupiński <danielkrupinski@outlook.com>
2022-10-22 18:16:18 +0200

    Remove KeyValuesSystem class definition

--- Source/SDK/KeyValuesSystem.h
+++ Source/SDK/KeyValuesSystem.h
@@ -1,8 +1,3 @@
 #pragma once
 
-#include "VirtualMethod.h"
-
-class KeyValuesSystem {
-public:
-    VIRTUAL_METHOD(const char*, getStringForSymbol, 4, (int symbol), (this, symbol))
-};
+class KeyValuesSystem;
