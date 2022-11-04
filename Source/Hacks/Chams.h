fe62ca633c949560bba5f16ace632ec7fd8be9d1
Daniel Krupiński <danielkrupinski@outlook.com>
2022-11-04 19:36:28 +0100

    Reduce dependencies in Chams class

--- Source/Hacks/Chams.h
+++ Source/Hacks/Chams.h
@@ -14,10 +14,10 @@ public:
     bool render(const Engine& engine, const ClientInterfaces& clientInterfaces, const Interfaces& interfaces, const Memory& memory, Config& config, void*, void*, const ModelRenderInfo&, matrix3x4*) noexcept;
     static void updateInput(Config& config) noexcept;
 private:
-    void renderPlayer(const Engine& engine, const Interfaces& interfaces, const Memory& memory, Config& config, const Entity& player) noexcept;
-    void renderWeapons(const Interfaces& interfaces, const Memory& memory, Config& config) noexcept;
-    void renderHands(const Interfaces& interfaces, const Memory& memory, Config& config) noexcept;
-    void renderSleeves(const Interfaces& interfaces, const Memory& memory, Config& config) noexcept;
+    void renderPlayer(const Engine& engine, const StudioRender& studioRender, const Memory& memory, Config& config, const Entity& player) noexcept;
+    void renderWeapons(const StudioRender& studioRender, const Memory& memory, Config& config) noexcept;
+    void renderHands(const StudioRender& studioRender, const Memory& memory, Config& config) noexcept;
+    void renderSleeves(const StudioRender& studioRender, const Memory& memory, Config& config) noexcept;
 
     bool appliedChams;
     void* ctx;
@@ -25,5 +25,5 @@ private:
     const ModelRenderInfo* info;
     matrix3x4* customBoneToWorld;
 
-    void applyChams(const Interfaces& interfaces, const Memory& memory, const std::array<Config::Chams::Material, 7>& chams, int health = 0, const matrix3x4* customMatrix = nullptr) noexcept;
+    void applyChams(const StudioRender& studioRender, const Memory& memory, const std::array<Config::Chams::Material, 7>& chams, int health = 0, const matrix3x4* customMatrix = nullptr) noexcept;
 };
