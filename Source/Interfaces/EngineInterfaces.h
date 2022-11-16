76e8346d40bbbb7136692fa8961f504394494e33
Daniel Krupiński <danielkrupinski@outlook.com>
2022-11-16 23:09:09 +0100

    Create Interfaces/EngineInterfacesPODs.h and move EngineInterfacesPODs struct and createEngineInterfacesPODs() function definition there

--- Source/Interfaces/EngineInterfaces.h
+++ Source/Interfaces/EngineInterfaces.h
@@ -1,35 +1,11 @@
 #pragma once
 
-#include "InterfaceFinderWithLog.h"
+#include "EngineInterfacesPODs.h"
 #include <Platform/RetSpoofInvoker.h>
 
 #include <SDK/Engine.h>
 #include <SDK/GameEvent.h>
 #include <SDK/ModelInfo.h>
-
-namespace csgo::pod { struct ModelRender; }
-namespace csgo::pod { struct EngineSound; }
-
-struct EngineInterfacesPODs {
-    csgo::pod::Engine* engine;
-    csgo::pod::EngineTrace* engineTrace;
-    csgo::pod::GameEventManager* gameEventManager;
-    csgo::pod::ModelInfo* modelInfo;
-    csgo::pod::ModelRender* modelRender;
-    csgo::pod::EngineSound* sound;
-};
-
-[[nodiscard]] inline EngineInterfacesPODs createEngineInterfacesPODs(InterfaceFinderWithLog engineInterfaceFinder)
-{
-    return EngineInterfacesPODs{
-        .engine = static_cast<csgo::pod::Engine*>(engineInterfaceFinder("VEngineClient014")),
-        .engineTrace = static_cast<csgo::pod::EngineTrace*>(engineInterfaceFinder("EngineTraceClient004")),
-        .gameEventManager = static_cast<csgo::pod::GameEventManager*>(engineInterfaceFinder("GAMEEVENTSMANAGER002")),
-        .modelInfo = static_cast<csgo::pod::ModelInfo*>(engineInterfaceFinder("VModelInfoClient004")),
-        .modelRender = static_cast<csgo::pod::ModelRender*>(engineInterfaceFinder("VEngineModel016")),
-        .sound = static_cast<csgo::pod::EngineSound*>(engineInterfaceFinder("IEngineSoundClient003"))
-    };
-}
 
 class EngineInterfaces {
 public:
