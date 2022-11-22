a8cece79a9dd425b4801c6b85c0cc65a35b57790
Daniel Krupiński <danielkrupinski@outlook.com>
2022-11-22 20:15:01 +0100

    Add csgo::MemAlloc class

---
+++ Source/SDK/MemAlloc.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstddef>
+
+#include "VirtualMethod.h"
+
+namespace csgo
+{
+
+namespace pod { struct MemAlloc; }
+
+class MemAlloc : public VirtualCallableFromPOD<MemAlloc, pod::MemAlloc> {
+public:
+    VIRTUAL_METHOD(void*, alloc, 1, (std::size_t size), (size))
+
+    [[nodiscard]] void* allocAligned(std::size_t size, std::size_t alignment) const noexcept
+    {
+        const auto padding = alignment - 1 + sizeof(void*);
+        const auto memory = alloc(size + padding);
+        const auto alignedMemory = reinterpret_cast<void*>((std::uintptr_t(memory) + padding) & ~(alignment - 1));
+        reinterpret_cast<void**>(alignedMemory)[-1] = memory;
+        return alignedMemory;
+    }
+};
+
+}
