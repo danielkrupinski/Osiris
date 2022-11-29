a16bd2ca1817a7aa6617e67875942fd6457fabdc
Daniel Krupiński <danielkrupinski@outlook.com>
2022-11-29 18:59:52 +0100

    Add Visuals::ColorCorrection::run() method

--- Source/Hacks/Visuals.h
+++ Source/Hacks/Visuals.h
@@ -96,6 +96,20 @@ private:
             configure(configurator);
         }
 
+        void run(ClientMode* clientMode) const noexcept
+        {
+            if (!enabled)
+                return;
+
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x49C, 0x908)) = blue;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4A4, 0x918)) = red;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4AC, 0x928)) = mono;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4B4, 0x938)) = saturation;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4C4, 0x958)) = ghost;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4CC, 0x968)) = green;
+            *reinterpret_cast<float*>(std::uintptr_t(clientMode) + WIN32_LINUX(0x4D4, 0x978)) = yellow;
+        }
+
         template <typename Configurator>
         void configure(Configurator& configurator)
         {
