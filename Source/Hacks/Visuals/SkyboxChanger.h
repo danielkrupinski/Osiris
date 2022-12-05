16005d5f767f8502ccc4f40e0bb6ab0116d525da
Daniel Krupiński <danielkrupinski@outlook.com>
2022-12-05 20:37:17 +0100

    Spoof the return address when calling loadSky function

--- Source/Hacks/Visuals/SkyboxChanger.h
+++ Source/Hacks/Visuals/SkyboxChanger.h
@@ -3,6 +3,7 @@
 #include <array>
 
 #include <Config/ResetConfigurator.h>
+#include <RetSpoof/FastcallFunctionInvoker.h>
 #include <SDK/Constants/ConVarNames.h>
 #include <SDK/Constants/FrameStage.h>
 #include <SDK/Cvar.h>
@@ -14,7 +15,7 @@ public:
 
     static constexpr std::array skyboxList{ "Default", "cs_baggage_skybox_", "cs_tibet", "embassy", "italy", "jungle", "nukeblank", "office", "sky_cs15_daylight01_hdr", "sky_cs15_daylight02_hdr", "sky_cs15_daylight03_hdr", "sky_cs15_daylight04_hdr", "sky_csgo_cloudy01", "sky_csgo_night_flat", "sky_csgo_night02", "sky_day02_05_hdr", "sky_day02_05", "sky_dust", "sky_l4d_rural02_ldr", "sky_venice", "vertigo_hdr", "vertigo", "vertigoblue_hdr", "vietnam", "sky_lunacy", "sky_hr_aztec" };
 
-    SkyboxChanger(Cvar cvar, std::add_pointer_t<void FASTCALL_CONV(const char*)> loadSky) : cvar{ cvar }, loadSky{ loadSky }
+    SkyboxChanger(Cvar cvar, FastcallFunctionInvoker<void, const char*> loadSky) : cvar{ cvar }, loadSky{ loadSky }
     {
         ResetConfigurator configurator;
         configure(configurator);
@@ -44,14 +45,14 @@ public:
 
 private:
     Cvar cvar;
-    std::add_pointer_t<void FASTCALL_CONV(const char*)> loadSky;
+    FastcallFunctionInvoker<void, const char*> loadSky;
 };
 
 [[nodiscard]] inline SkyboxChanger createSkyboxChanger(Cvar cvar, const helpers::PatternFinder& enginePatternFinder)
 {
 #if IS_WIN32()
-    return SkyboxChanger{ cvar, reinterpret_cast<void(FASTCALL_CONV*)(const char*)>(enginePatternFinder("\xE8????\x84\xC0\x74\x2D\xA1").add(1).relativeToAbsolute().get()) };
+    return SkyboxChanger{ cvar, FastcallFunctionInvoker<void, const char*>{ retSpoofGadgets->engine, enginePatternFinder("\xE8????\x84\xC0\x74\x2D\xA1").add(1).relativeToAbsolute().get() } };
 #elif IS_LINUX()
-    return SkyboxChanger{ cvar, reinterpret_cast<void(FASTCALL_CONV*)(const char*)>(enginePatternFinder("\xE8????\x84\xC0\x74\xAB").add(1).relativeToAbsolute().get()) };
+    return SkyboxChanger{ cvar, FastcallFunctionInvoker<void, const char*>{ retSpoofGadgets->engine, enginePatternFinder("\xE8????\x84\xC0\x74\xAB").add(1).relativeToAbsolute().get() } };
 #endif
 }
