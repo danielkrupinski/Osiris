4e47652aa8faa66d64a672759f04eb73a3d82ab2
Daniel Krupiński <danielkrupinski@outlook.com>
2022-12-19 20:59:18 +0100

    Unify all FunctionInvoker partial specializations to use function pointers

--- Source/CSGO/Functions.h
+++ Source/CSGO/Functions.h
@@ -6,17 +6,17 @@
 namespace csgo
 {
 
-using R_LoadNamedSkys = bool FASTCALL_CONV (const char* skyname);
-using SendClanTag = void FASTCALL_CONV (const char* tag, const char* name);
-using SubmitReport = bool STDCALL_CONV (LINUX_ARGS(void*, ) const char* xuid, const char* report);
+using R_LoadNamedSkys = bool (FASTCALL_CONV*)(const char* skyname);
+using SendClanTag = void (FASTCALL_CONV*)(const char* tag, const char* name);
+using SubmitReport = bool (STDCALL_CONV*)(LINUX_ARGS(void*, ) const char* xuid, const char* report);
 
 template <typename T>
-using CreateSharedObjectSubclass = T* STDCALL_CONV ();
+using CreateSharedObjectSubclass = T* (STDCALL_CONV*)();
 
 struct EconItemViewPOD;
-using FindOrCreateEconItemViewForItemID = EconItemViewPOD* CDECL_CONV (ItemId itemID);
+using FindOrCreateEconItemViewForItemID = EconItemViewPOD* (CDECL_CONV*)(ItemId itemID);
 
 struct Vector;
-using LineGoesThroughSmoke = bool CDECL_CONV (Vector from, Vector to, short);
+using LineGoesThroughSmoke = bool (CDECL_CONV*)(Vector from, Vector to, short);
 
 }
--- Source/RetSpoof/FunctionInvoker.h
+++ Source/RetSpoof/FunctionInvoker.h
@@ -12,19 +12,19 @@ template <typename FunctionType>
 struct FunctionInvoker {};
 
 template <typename ReturnType, typename... Args>
-struct FunctionInvoker<ReturnType FASTCALL_CONV (Args...)> : FastcallFunctionInvoker<ReturnType, Args...> {
+struct FunctionInvoker<ReturnType (FASTCALL_CONV*)(Args...)> : FastcallFunctionInvoker<ReturnType, Args...> {
     using FastcallFunctionInvoker<ReturnType, Args...>::FastcallFunctionInvoker;
 };
 
 #if IS_WIN32()
 
 template <typename ReturnType, typename... Args>
-struct FunctionInvoker<ReturnType STDCALL_CONV (Args...)> : StdcallFunctionInvoker<ReturnType, Args...> {
+struct FunctionInvoker<ReturnType (STDCALL_CONV*)(Args...)> : StdcallFunctionInvoker<ReturnType, Args...> {
     using StdcallFunctionInvoker<ReturnType, Args...>::StdcallFunctionInvoker;
 };
 
 template <typename ReturnType, typename... Args>
-struct FunctionInvoker<ReturnType CDECL_CONV (Args...)> : CdeclFunctionInvoker<ReturnType, Args...> {
+struct FunctionInvoker<ReturnType (CDECL_CONV*)(Args...)> : CdeclFunctionInvoker<ReturnType, Args...> {
     using CdeclFunctionInvoker<ReturnType, Args...>::CdeclFunctionInvoker;
 };
 
@@ -34,6 +34,6 @@ struct FunctionInvoker<ReturnType (THISCALL_CONV*)(Args...)> : ThiscallFunctionI
 };
 
 template <typename ReturnType, typename... Args>
-FunctionInvoker(RetSpoofInvoker, ReturnType(THISCALL_CONV*)(Args...)) -> FunctionInvoker<ReturnType(THISCALL_CONV*)(Args...)>;
+FunctionInvoker(RetSpoofInvoker, ReturnType (THISCALL_CONV*)(Args...)) -> FunctionInvoker<ReturnType (THISCALL_CONV*)(Args...)>;
 
 #endif
