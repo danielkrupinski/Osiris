517514a89e30e538b3dc7e16e98f35627b4fc4a7
Daniel Krupiński <danielkrupinski@outlook.com>
2023-02-03 19:25:58 +0100

    Add FASTCALL_THIS() macro

--- Source/Platform/Macros/PlatformSpecific.h
+++ Source/Platform/Macros/PlatformSpecific.h
@@ -7,17 +7,20 @@
 #define WIN32_ARGS(...) __VA_ARGS__
 #define LINUX_ARGS(...)
 #define WIN32_LINUX(win32, linux) win32
+#define FASTCALL_THIS(thisptr) thisptr, void*
 
 #elif IS_LINUX()
 
 #define WIN32_ARGS(...)
 #define LINUX_ARGS(...) __VA_ARGS__
 #define WIN32_LINUX(win32, linux) linux
+#define FASTCALL_THIS(thisptr) thisptr
 
 #else
 
 #define WIN32_ARGS(...)
 #define LINUX_ARGS(...)
 #define WIN32_LINUX(win32, linux)
+#define FASTCALL_THIS(thisptr)
 
 #endif
