55e2514e07ad5ec4ebfbaed0be30f93e80d2eded
Daniel Krupiński <danielkrupinski@outlook.com>
2023-03-15 18:33:07 +0100

    Add IS_WIN64() macro

--- Source/Platform/Macros/IsPlatform.h
+++ Source/Platform/Macros/IsPlatform.h
@@ -4,19 +4,28 @@
 #error "Conflicting platform macros defined"
 #endif
 
-#if defined(_WIN32)
+#if defined(_WIN64)
+
+#define IS_WIN32() false
+#define IS_WIN64() true
+#define IS_LINUX() false
+
+#elif defined(_WIN32)
 
 #define IS_WIN32() true
+#define IS_WIN64() false
 #define IS_LINUX() false
 
 #elif defined(__linux__)
 
 #define IS_WIN32() false
+#define IS_WIN64() false
 #define IS_LINUX() true
 
 #else
 
 #define IS_WIN32() false
+#define IS_WIN64() false
 #define IS_LINUX() false
 
 #endif
