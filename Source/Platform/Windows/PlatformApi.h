77f837dc13dafbaf06c655aea145a1d488ba566e
Daniel Krupiński <danielkrupinski@outlook.com>
2023-03-20 19:31:53 +0100

    Add windows_platform::PlatformApi::getPeb() method

--- Source/Platform/Windows/PlatformApi.h
+++ Source/Platform/Windows/PlatformApi.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <intrin.h>
 #include <Windows.h>
+
+#include "Win.h"
 
 namespace windows_platform
 {
@@ -9,6 +12,17 @@ struct PlatformApi {
     HMODULE GetModuleHandleA(LPCSTR moduleName) const noexcept
     {
         return ::GetModuleHandleA(moduleName);
+    }
+
+    [[nodiscard]] win::Peb* getPeb() const noexcept
+    {
+#if IS_WIN32()
+        return reinterpret_cast<win::Peb*>(__readfsdword(0x30));
+#elif IS_WIN64()
+        return reinterpret_cast<win::Peb*>(__readgsqword(0x60));
+#else
+#error "Unsupported platform"
+#endif
     }
 };
 
