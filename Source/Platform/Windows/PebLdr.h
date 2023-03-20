967b607cce21915a495e3685ecfd35ca4bc76b18
Daniel Krupiński <danielkrupinski@outlook.com>
2023-03-20 19:25:13 +0100

    Add PebLdr class

---
+++ Source/Platform/Windows/PebLdr.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Windows.h>
+
+#include "Win.h"
+
+class PebLdr {
+public:
+    explicit PebLdr(win::PebLdrData* ldr)
+        : ldr{ ldr }
+    {
+    }
+
+    [[nodiscard]] HMODULE getModuleHandle(const char* moduleName) const noexcept
+    {
+        const auto head = getInLoadOrderModuleListHead();
+        for (auto currentLink = head->fLink; currentLink != head; currentLink = currentLink->fLink) {
+            const auto& entry = *CONTAINING_RECORD(currentLink, win::LdrDataTableEntry, inLoadOrderLinks);
+
+            if (entry.baseDllName.equals(moduleName))
+                return HMODULE(entry.dllBase);
+        }
+        return {};
+    }
+
+private:
+    [[nodiscard]] win::ListEntry* getInLoadOrderModuleListHead() const noexcept
+    {
+        return &ldr->inLoadOrderModuleList;
+    }
+
+    win::PebLdrData* ldr;
+};
