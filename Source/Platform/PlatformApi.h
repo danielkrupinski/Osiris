fa2b3074de31f0b756e2efd24a0762eca35e1527
Daniel Krupiński <danielkrupinski@outlook.com>
2023-07-28 22:53:46 +0200

    Bring back PlatformApi alias

---
+++ Source/Platform/PlatformApi.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Macros/IsPlatform.h"
+
+#if IS_WIN32() || IS_WIN64()
+
+#include "Windows/WindowsPlatformApi.h"
+
+using PlatformApi = WindowsPlatformApi;
+
+#elif IS_LINUX()
+
+#include "Linux/LinuxPlatformApi.h"
+
+using PlatformApi = LinuxPlatformApi;
+
+#endif
