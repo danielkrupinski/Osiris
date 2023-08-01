d89e33bbcc747d3f3d79c3031ad3e70c70141c46
Daniel Krupiński <danielkrupinski@outlook.com>
2023-08-01 19:39:00 +0200

    Add UniquePtr type alias and mem::makeUniqueForOverwrite() function to be used with memory allocated by MemoryAllocator

---
+++ Source/MemoryAllocation/UniquePtr.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <memory>
+#include <type_traits>
+
+#include "MemoryAllocator.h"
+#include "MemoryDeleter.h"
+
+template <typename T>
+using UniquePtr = std::unique_ptr<T, MemoryDeleter<T>>;
+
+namespace mem
+{
+
+template <typename T>
+    requires (!std::is_array_v<T>)
+[[nodiscard]] auto makeUniqueForOverwrite() noexcept
+{
+    return UniquePtr<T>{ new (MemoryAllocator::allocate(MemoryAllocator::memoryFor<T>())) T };
+}
+
+template <typename T>
+    requires std::is_unbounded_array_v<T>
+[[nodiscard]] auto makeUniqueForOverwrite(std::size_t size) noexcept
+{
+    return UniquePtr<T>{ new (MemoryAllocator::allocate(MemoryAllocator::memoryFor<T>(size))) std::remove_extent_t<T>[size], MemoryDeleter<T>{ size } };
+}
+
+template <typename T, typename... Args>
+    requires std::is_bounded_array_v<T>
+void makeUniqueForOverwrite(Args&&...) = delete;
+
+}
