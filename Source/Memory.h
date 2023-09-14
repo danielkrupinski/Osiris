danielkrupinski/Osiris

commit c1502229d79341308b34b2c729cda970851ddd33

Use const void* instead of std::uintptr_t in SafeAddress

Source/Memory.h
@@ -203,9 +203,9 @@ inline Memory::Memory(const ClientPatternFinder& clientPatternFinder, const Engi
 #elif IS_LINUX()
     conColorMsg = tier0.getFunctionAddress("_Z11ConColorMsgRK5ColorPKcz").template as<decltype(conColorMsg)>();
 
-    globalVars = SafeAddress{ (*reinterpret_cast<std::uintptr_t**>(clientInterface))[11] + 16 }.abs().deref().as<csgo::GlobalVars*>();
-    clientMode = SafeAddress{ (*reinterpret_cast<uintptr_t**>(clientInterface))[10] }.add(12).abs().add(4).abs().deref().as<decltype(clientMode)>();
-    input = SafeAddress{ (*reinterpret_cast<uintptr_t**>(clientInterface))[16] }.add(3).abs().deref().as<csgo::Input*>();
+    globalVars = SafeAddress{ (*reinterpret_cast<std::uintptr_t***>(clientInterface))[11] + 16 }.abs().deref().as<csgo::GlobalVars*>();
+    clientMode = SafeAddress{ (*reinterpret_cast<uintptr_t***>(clientInterface))[10] }.add(12).abs().add(4).abs().deref().as<decltype(clientMode)>();
+    input = SafeAddress{ (*reinterpret_cast<uintptr_t***>(clientInterface))[16] }.add(3).abs().deref().as<csgo::Input*>();
 
     localPlayer.init(clientPatternFinder.localPlayer());
 #endif

Source/InventoryChanger/InventoryChanger.cpp
@@ -100,7 +100,7 @@ static csgo::EntityPOD* createGloves(const ClientInterfaces& clientInterfaces, c
 
     std::memset(econWearable, 0, sizeOfEconWearable);
 
-    const auto econWearableConstructor = SafeAddress{ std::uintptr_t(createWearable) + 61 }.abs().as<std::uintptr_t>();
+    const auto econWearableConstructor = SafeAddress{ reinterpret_cast<std::byte*>(createWearable) + 61 }.abs().as<std::uintptr_t>();
     retSpoofGadgets->client.invokeThiscall<void>(std::uintptr_t(econWearable), econWearableConstructor);
 
     csgo::Entity::from(retSpoofGadgets->client, static_cast<csgo::EntityPOD*>(econWearable)).initializeAsClientEntity(nullptr, false);

Source/MemorySearch/PatternFinder.h
@@ -25,14 +25,14 @@ public:
         const auto found = HybridPatternFinder{ bytes, pattern }();
         if (!found)
             notFoundHandler(pattern);
-        return SafeAddress{ std::uintptr_t(found) };
+        return SafeAddress{ found };
     }
 
     [[nodiscard]] SafeAddress operator()(BytePattern pattern, OffsetHint offsetHint) const noexcept
     {
         const auto foundWithHint = HybridPatternFinder{ getSliceForHint(offsetHint), pattern }();
         if (foundWithHint)
-            return SafeAddress{ std::uintptr_t(foundWithHint) };
+            return SafeAddress{ foundWithHint };
         return operator()(pattern);
     }
 

Source/Platform/Linux/LinuxDynamicLibrary.h
@@ -29,7 +29,7 @@ public:
     [[nodiscard]] SafeAddress getFunctionAddress(const char* functionName) const noexcept
     {
         if (handle)
-            return SafeAddress{ std::uintptr_t(LinuxPlatformApi::dlsym(handle, functionName)) };
+            return SafeAddress{ LinuxPlatformApi::dlsym(handle, functionName) };
         return SafeAddress{ 0 };
     }
 

Source/Platform/Windows/PortableExecutable.h
@@ -54,7 +54,7 @@ public:
                     assert(false && "Forwarded exports are not supported yet!");
                     return SafeAddress{ 0 };
                 }
-                return SafeAddress{ std::uintptr_t(base + functionRva) };
+                return SafeAddress{ base + functionRva };
             }
         }
 

Source/Utils/SafeAddress.h
@@ -9,15 +9,15 @@
 
 class SafeAddress {
 public:
-    explicit SafeAddress(std::uintptr_t address)
+    explicit SafeAddress(const void* address) noexcept
         : address{ address }
     {
     }
 
     SafeAddress& add(std::ptrdiff_t offset) noexcept
     {
-        if (address != 0)
-            address += offset;
+        if (address != nullptr)
+            address = static_cast<const std::byte*>(address) + offset;
         return *this;
     }
 
@@ -25,8 +25,8 @@ public:
     SafeAddress& deref() noexcept
     {
         if constexpr (N != 0) {
-            if (address != 0) {
-                address = derefAddress<std::uintptr_t>();
+            if (address != nullptr) {
+                address = derefAddress<const void*>();
                 return deref<N - 1>();
             }
         }
@@ -40,9 +40,9 @@ public:
 
     SafeAddress& abs() noexcept
     {
-        if (address != 0) {
+        if (address != nullptr) {
             using OffsetType = std::int32_t;
-            const auto addressOfNextInstruction = address + sizeof(OffsetType);
+            const auto addressOfNextInstruction = static_cast<const std::byte*>(address) + sizeof(OffsetType);
             const auto offset = derefAddress<OffsetType>();
             address = addressOfNextInstruction + offset;
         }
@@ -51,7 +51,7 @@ public:
 
     [[nodiscard]] ReturnAddress asReturnAddress() const noexcept
     {
-        return ReturnAddress{ address };
+        return ReturnAddress{ reinterpret_cast<std::uintptr_t>(address) };
     }
 
     template <typename T>
@@ -65,9 +65,9 @@ private:
     [[nodiscard]] T derefAddress() const noexcept
     {
         T value;
-        std::memcpy(&value, reinterpret_cast<const void*>(address), sizeof(T));
+        std::memcpy(&value, address, sizeof(T));
         return value;
     }
 
-    std::uintptr_t address;
+    const void* address;
 };

Tests/SafeAddressTests.cpp
@@ -13,15 +13,15 @@ constexpr auto maxAddress = (std::numeric_limits<std::uintptr_t>::max)();
 class SafeAddress_AddressTest : public testing::TestWithParam<std::uintptr_t> {};
 
 TEST_P(SafeAddress_AddressTest, AfterConstructionHasAddressPassedToTheConstructor) {
-    EXPECT_EQ(SafeAddress{ GetParam() }.as<std::uintptr_t>(), GetParam());
+    EXPECT_EQ(SafeAddress{ reinterpret_cast<const void*>(GetParam()) }.as<std::uintptr_t>(), GetParam());
 }
 
 TEST_P(SafeAddress_AddressTest, ReturnsCorrectAddressWhenConvertedToReturnAddress) {
-    EXPECT_EQ(static_cast<std::uintptr_t>(SafeAddress{ GetParam() }.asReturnAddress()), GetParam());
+    EXPECT_EQ(static_cast<std::uintptr_t>(SafeAddress{ reinterpret_cast<const void*>(GetParam()) }.asReturnAddress()), GetParam());
 }
 
 TEST_P(SafeAddress_AddressTest, ReturnsCorrectAddressWhenConvertedToPointer) {
-    EXPECT_EQ(reinterpret_cast<std::uintptr_t>(SafeAddress{ GetParam() }.as<int*>()), GetParam());
+    EXPECT_EQ(reinterpret_cast<std::uintptr_t>(SafeAddress{ reinterpret_cast<const void*>(GetParam()) }.as<int*>()), GetParam());
 }
 
 INSTANTIATE_TEST_SUITE_P(, SafeAddress_AddressTest, testing::Values(minAddress, 123, maxAddress));
@@ -39,15 +39,15 @@ TEST(SafeAddressTest, AddingNegativeOffsetToZeroAddressDoesNothing) {
 }
 
 TEST(SafeAddressTest, AddingZeroOffsetToAnAddressDoesNothing) {
-    EXPECT_EQ(SafeAddress{ 123'456 }.add(0).as<std::uintptr_t>(), 123'456);
+    EXPECT_EQ(SafeAddress{ reinterpret_cast<const void*>(123'456) }.add(0).as<std::uintptr_t>(), 123'456);
 }
 
 TEST(SafeAddressTest, AddingPositiveOffsetCanWrapAround) {
-    EXPECT_EQ(SafeAddress{ maxAddress }.add(1).as<std::uintptr_t>(), minAddress);
+    EXPECT_EQ(SafeAddress{ reinterpret_cast<const void*>(maxAddress) }.add(1).as<std::uintptr_t>(), minAddress);
 }
 
 TEST(SafeAddressTest, AddingNegativeOffsetCanWrapAround) {
-    EXPECT_EQ(SafeAddress{ 10 }.add(-11).as<std::uintptr_t>(), maxAddress);
+    EXPECT_EQ(SafeAddress{ reinterpret_cast<const void*>(10) }.add(-11).as<std::uintptr_t>(), maxAddress);
 }
 
 TEST(SafeAddressTest, CanBeDereferenced) {
@@ -56,7 +56,7 @@ TEST(SafeAddressTest, CanBeDereferenced) {
     int*** ptr3 = &ptr2;
     int**** ptr4 = &ptr3;
 
-    EXPECT_EQ(SafeAddress{ std::uintptr_t(ptr4) }.deref<2>().as<std::uintptr_t>(), std::uintptr_t(&ptr1));
+    EXPECT_EQ(SafeAddress{ ptr4 }.deref<2>().as<std::uintptr_t>(), std::uintptr_t(&ptr1));
 }
 
 TEST(SafeAddressTest, DereferenceStopsWhenReachedZeroAddress) {
@@ -64,7 +64,7 @@ TEST(SafeAddressTest, DereferenceStopsWhenReachedZeroAddress) {
     int**** ptr4 = &ptr3;
     int***** ptr5 = &ptr4;
 
-    EXPECT_EQ(SafeAddress{ std::uintptr_t(ptr5) }.deref<10>().as<std::uintptr_t>(), 0);
+    EXPECT_EQ(SafeAddress{ ptr5 }.deref<10>().as<std::uintptr_t>(), 0);
 }
 
 TEST(SafeAddressTest, DereferencingZeroAddressDoesNothing) {
@@ -77,7 +77,7 @@ TEST(SafeAddressTest, NonTemplateDerefMethodDereferencesOnce) {
     int** ptr2 = &ptr1;
     int*** ptr3 = &ptr2;
 
-    EXPECT_EQ(SafeAddress{ std::uintptr_t(ptr3) }.deref().as<std::uintptr_t>(), std::uintptr_t(&ptr1));
+    EXPECT_EQ(SafeAddress{ ptr3 }.deref().as<std::uintptr_t>(), std::uintptr_t(&ptr1));
 }
 
 TEST(SafeAddressTest, ConvertingZeroAddressToAbsoluteDoesNothing) {
@@ -88,7 +88,7 @@ class SafeAddress_RelativeToAbsoluteTest : public testing::TestWithParam<std::in
 
 TEST_P(SafeAddress_RelativeToAbsoluteTest, RelativeAddressIsCorrectlyConvertedToAbsolute) {
     const std::int32_t relativeJumpOffset = GetParam();
-    EXPECT_EQ(SafeAddress{ std::uintptr_t(&relativeJumpOffset) }.abs().as<std::uintptr_t>(), std::uintptr_t(&relativeJumpOffset) + sizeof(std::int32_t) + relativeJumpOffset);
+    EXPECT_EQ(SafeAddress{ &relativeJumpOffset }.abs().as<std::uintptr_t>(), std::uintptr_t(&relativeJumpOffset) + sizeof(std::int32_t) + relativeJumpOffset);
 }
 
 INSTANTIATE_TEST_SUITE_P(, SafeAddress_RelativeToAbsoluteTest, testing::Values(-1000, 0, 1000));

commit 3f91a62f374efebfd7a07a326e325d7b385d84cb

Fix crash on Linux (bug introduced in c1502229d79341308b34b2c729cda970851ddd33)

Source/Memory.h
@@ -205,7 +205,7 @@ inline Memory::Memory(const ClientPatternFinder& clientPatternFinder, const Engi
 #elif IS_LINUX()
     conColorMsg = tier0.getFunctionAddress("_Z11ConColorMsgRK5ColorPKcz").template as<decltype(conColorMsg)>();
 
-    globalVars = SafeAddress{ (*reinterpret_cast<std::uintptr_t***>(clientInterface))[11] + 16 }.abs().deref().as<csgo::GlobalVars*>();
+    globalVars = SafeAddress{ (*reinterpret_cast<std::uintptr_t***>(clientInterface))[11] + 2 }.abs().deref().as<csgo::GlobalVars*>();
     clientMode = SafeAddress{ (*reinterpret_cast<uintptr_t***>(clientInterface))[10] }.add(12).abs().add(4).abs().deref().as<decltype(clientMode)>();
     input = SafeAddress{ (*reinterpret_cast<uintptr_t***>(clientInterface))[16] }.add(3).abs().deref().as<csgo::Input*>();
 