0d9d1691cc8762279050bd71416ddaaa45687196
Daniel Krupiński <danielkrupinski@outlook.com>
2023-10-04 18:18:32 +0200

    Reimplement intrinsics

--- Source/Intrinsics.cpp
+++ Source/Intrinsics.cpp
@@ -8,35 +8,49 @@ namespace intrinsics
 
 [[nodiscard]] inline int memcmp(const unsigned char* lhs, const unsigned char* rhs, std::size_t count) noexcept
 {
-    while (count-- > 0) {
-        if (*lhs++ != *rhs++)
-            return lhs[-1] < rhs[-1] ? -1 : 1;
+    for (std::size_t i = 0; i < count; ++i) {
+        if (lhs[i] != rhs[i])
+            return lhs[i] < rhs[i] ? -1 : 1;
     }
     return 0;
 }
 
 inline std::byte* memcpy(std::byte* dest, const std::byte* src, std::size_t count) noexcept
 {
-    while (count--)
-        *dest++ = *src++;
+    for (std::size_t i = 0; i < count; ++i)
+        dest[i] = src[i];
     return dest;
 }
 
 inline unsigned char* memset(unsigned char* const dest, unsigned char ch, std::size_t count) noexcept
 {
-    unsigned char* ptr = dest;
-    while (count-- > 0)
-        *ptr++ = ch;
+    for (std::size_t i = 0; i < count; ++i)
+        dest[i] = ch;
     return dest;
 }
 
 [[nodiscard]] inline int strcmp(const unsigned char* s1, const unsigned char* s2) noexcept
 {
-    while (*s1 == *s2++) {
-        if (*s1++ == '\0')
+    while (*s1 == *s2) {
+        if (*s1 == '\0')
             return 0;
-    }   
-    return *s1 - s2[-1];
+        ++s1;
+        ++s2;
+    }
+
+    return *s1 - *s2;
+}
+
+inline unsigned char* memmove(unsigned char* dest, const unsigned char* src, std::size_t count) noexcept
+{
+    if (dest < src) {
+        for (std::size_t i = 0; i < count; ++i)
+            dest[i] = src[i];
+    } else {
+        for (std::size_t i = count; i > 0; --i)
+            dest[i - 1] = src[i - 1];
+    }
+    return dest;
 }
 
 }
@@ -50,21 +64,8 @@ void* memcpy(void* dest, const void* src, size_t count)
 #pragma function(memmove)
 void* memmove(void* dest, const void* src, size_t count)
 {
-    char* d = static_cast<char*>(dest);
-    const char* s = static_cast<const char*>(src);
-    if (d < s) {
-        while (count--)
-            *d++ = *s++;
-    }
-    else {
-        const char* lasts = s + (count - 1);
-        char* lastd = d + (count - 1);
-        while (count--)
-            *lastd-- = *lasts--;
-    }
-    return dest;
+    return intrinsics::memmove(static_cast<unsigned char*>(dest), static_cast<const unsigned char*>(src), count);
 }
-
 
 #pragma function(memcmp)
 int memcmp(const void* s1, const void* s2, std::size_t n)
