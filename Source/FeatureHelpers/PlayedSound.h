060643dbdf706628fdb9946ce39083bdf7c55634
Daniel Krupiński <danielkrupinski@outlook.com>
2023-10-25 20:59:35 +0200

    Add PlayedSound struct

---
+++ Source/FeatureHelpers/PlayedSound.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <limits>
+
+#include <CS2/Classes/Vector.h>
+
+struct PlayedSound {
+    int guid;
+    float spawnTime;
+    cs2::Vector origin;
+
+    [[nodiscard]] float getTimeAlive(float curtime) const noexcept
+    {
+        if (curtime >= spawnTime)
+            return curtime - spawnTime;
+        return (std::numeric_limits<float>::max)();
+    }
+
+    [[nodiscard]] bool isAlive(float curtime, float lifetime) const noexcept
+    {
+        return getTimeAlive(curtime) <= lifetime;
+    }
+};
