8be0af46be3353b7ff4abda64eac3f1369ff79ee
Daniel Krupiński <danielkrupinski@outlook.com>
2023-10-26 20:01:44 +0200

    Remove assertions from NormalizedDeviceCoordinates constructor

--- Source/FeatureHelpers/NormalizedDeviceCoordinates.h
+++ Source/FeatureHelpers/NormalizedDeviceCoordinates.h
@@ -1,15 +1,11 @@
 #pragma once
 
-#include <cassert>
-
 #include <CS2/Classes/Panorama.h>
 
 struct NormalizedDeviceCoordinates {
     NormalizedDeviceCoordinates(float x, float y) noexcept
         : x{ x }, y{ y }
     {
-        assert(x >= -1.0f && x <= 1.0f);
-        assert(y >= -1.0f && y <= 1.0f);
     }
 
     [[nodiscard]] cs2::CUILength getX() const noexcept
