5122123ca48a2f29c3de9a09740d28461ce7a327
Daniel Krupiński <danielkrupinski@outlook.com>
2023-11-17 18:40:26 +0100

    Update CImagePanel::setImage pattern for Linux

--- Source/MemoryPatterns/Linux/PanoramaImagePanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/PanoramaImagePanelPatternsLinux.h
@@ -6,5 +6,5 @@
 
 inline cs2::CImagePanel::setImage PanoramaImagePanelPatterns::setImage() noexcept
 {
-    return GlobalContext::instance().clientPatternFinder("48 85 F6 48 0F 44 F0 E8 ? ? ? ? 48 83 7D ? 00 0F 84 83"_pat).add(8).abs().as<cs2::CImagePanel::setImage>();
+    return GlobalContext::instance().clientPatternFinder("24 ? 48 85 F6 48 0F 44 F0 E8 ? ? ? ? 48 83 7D ? 00 0F 84"_pat).add(10).abs().as<cs2::CImagePanel::setImage>();
 }
