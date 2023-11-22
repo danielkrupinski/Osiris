fb4611ca4c249ae8e485e82278353f648df97acc
Daniel Krupiński <danielkrupinski@outlook.com>
2023-11-22 21:06:46 +0100

    Uncomment std::is_function_v check in GenericFunctionPointer

--- Source/Utils/GenericFunctionPointer.h
+++ Source/Utils/GenericFunctionPointer.h
@@ -11,7 +11,7 @@ struct GenericFunctionPointer {
     }
 
     template <typename FunctionPointer>
-        requires std::is_pointer_v<FunctionPointer> /* && std::is_function_v<std::remove_pointer_t<FunctionPointer>> */ // doesn't work with __thiscall*, todo: uncomment when x64 only
+        requires std::is_pointer_v<FunctionPointer> && std::is_function_v<std::remove_pointer_t<FunctionPointer>>
     explicit(false) operator FunctionPointer() noexcept
     {
         return reinterpret_cast<FunctionPointer>(value);
