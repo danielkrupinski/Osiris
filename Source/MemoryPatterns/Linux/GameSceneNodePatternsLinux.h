3f9bf3d14df87db048ae403884a872d22559995f
Daniel Krupiński <danielkrupinski@outlook.com>
2024-10-18 17:29:13 +0200

    Add compile-time check for missing .read() in pattern declaration

--- Source/MemoryPatterns/Linux/GameSceneNodePatternsLinux.h
+++ Source/MemoryPatterns/Linux/GameSceneNodePatternsLinux.h
@@ -7,7 +7,7 @@ struct GameSceneNodePatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<OffsetToAbsOrigin, CodePattern{"43 ? F3 0F 10 83 ? ? ? ? 66"}.add(6)>()
+            .template addPattern<OffsetToAbsOrigin, CodePattern{"43 ? F3 0F 10 83 ? ? ? ? 66"}.add(6).read()>()
             .template addPattern<OffsetToGameSceneNodeOwner, CodePattern{"4C 8B 6F ? ? F6 ? ? 0F"}.add(3).read()>()
             .template addPattern<OffsetToChildGameSceneNode, CodePattern{"? 48 85 DB 75 ? EB ? 0F B6"}.read()>()
             .template addPattern<OffsetToNextSiblingGameSceneNode, CodePattern{"50 ? 48 8B 5B ? 48 85 DB 0F"}.add(5).read()>();
