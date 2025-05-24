30c660ff5f34659555180a72ede598fc4aab4631
Daniel Krupiński <danielkrupinski@outlook.com>
2025-05-24 19:19:49 +0200

    Fix for the latest game update

--- Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
@@ -7,7 +7,7 @@ struct UiItem3dPanelPatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<SetItemItemIdFunction, CodePattern{"55 48 89 E5 41 57 41 56 49 89 D6 41 55 41 54 53 48 89 FB"}>()
-            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"75 ? 8B 85 ? ? ? ? ? 83 ? ? ? ? 0F"}.add(10).read()>();
+            .template addPattern<SetItemItemIdFunction, CodePattern{"55 48 89 E5 41 57 41 56 41 55 49 89 D5 41 54 49 89 FC 48 89 F7 53 48 81 EC ? ? ? ? E8"}>()
+            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"8B 85 ? ? ? ? 41 3B 84 24 ? ? ? ? 0F 84"}.add(10).read()>();
     }
 };
--- Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
+++ Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
@@ -7,7 +7,7 @@ struct UiItem3dPanelPatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<SetItemItemIdFunction, CodePattern{"4C 89 44 24 ? 48 89 4C 24 ? 55 56 57 41 54 48"}>()
-            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"75 ? 39 B7 ? ? ? ? 75"}.add(4).read()>();
+            .template addPattern<SetItemItemIdFunction, CodePattern{"48 89 4C 24 ? 55 56 57 41 54 41 56"}>()
+            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"75 ? 39 ? ? ? ? ? 75 ? 45"}.add(4).read()>();
     }
 };
