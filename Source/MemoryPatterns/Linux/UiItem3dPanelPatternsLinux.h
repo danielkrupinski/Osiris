5bcf52771707943cba7c39c11ddadd1c8c2b5031
Daniel Krupiński <danielkrupinski@outlook.com>
2025-09-25 17:31:57 +0200

    Fix for the latest game update on Linux

--- Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
@@ -7,7 +7,7 @@ struct UiItem3dPanelPatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<SetItemItemIdFunction, CodePattern{"55 48 89 E5 41 57 41 56 49 89 D6 41 55 41 54 49 89 F4 53 48 89 FB 48 89 F7 48 81"}>()
+            .template addPattern<SetItemItemIdFunction, CodePattern{"5E 5D E9 ? ? ? ? 31 C0"}.add(3).abs()>()
             .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"39 83 ? ? ? ? 0F 84 ? ? ? ? 48 8B B5"}.add(2).read()>();
     }
 };
