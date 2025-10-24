54a4be0e6ea5ee8f722fb5263416ddc6e42c9c68
Daniel Krupiński <danielkrupinski@outlook.com>
2025-10-24 18:08:05 +0200

    Fix for the latest game update on Linux

--- Source/MemoryPatterns/Linux/PortraitWorldPatternsLinux.h
+++ Source/MemoryPatterns/Linux/PortraitWorldPatternsLinux.h
@@ -8,6 +8,6 @@ struct PortraitWorldPatterns {
     {
         return clientPatterns
             .template addPattern<OffsetToPortraitWorldEntities, CodePattern{"48 89 FB 48 83 EC ? 4C 63 6F ? 48"}.add(10).read()>()
-            .template addPattern<OffsetToPortraitWorldMapEntities, CodePattern{"A3 ? ? ? ? 8B BB ? ? ? ? 44 8B"}.add(1).read()>();
+            .template addPattern<OffsetToPortraitWorldMapEntities, CodePattern{"4D 63 A6 ? ? ? ? 45"}.add(3).read()>();
     }
 };
