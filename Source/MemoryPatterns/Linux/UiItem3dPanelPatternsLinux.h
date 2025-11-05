0212bbe114549f2c1c243c81402cef208befb328
Daniel Krupiński <danielkrupinski@outlook.com>
2025-11-05 17:57:44 +0100

    Fix for the latest game update

--- Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/UiItem3dPanelPatternsLinux.h
@@ -10,6 +10,6 @@ struct UiItem3dPanelPatterns {
             .template addPattern<SetItemItemIdFunction, CodePattern{"5E 5D E9 ? ? ? ? 31 C0"}.add(3).abs()>()
             .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"41 39 44 24 ? 44"}.add(4).read()>()
             .template addPattern<OffsetToStartWeaponLookAt, CodePattern{"B8 01 00 00 00 66 89 87 ? ? ? ? C3"}.add(8).read()>()
-            .template addPattern<OffsetToItem3dPanelProperties, CodePattern{"8C 24 ? ? ? ? 4C 89 EF"}.add(2).read()>();
+            .template addPattern<OffsetToItem3dPanelProperties, CodePattern{"? ? ? ? 4C 89 ? 41 ? 48 8B 95 ? ? ? ? 4C"}.read()>();
     }
 };
