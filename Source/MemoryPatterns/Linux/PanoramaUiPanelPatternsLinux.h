06e31166a1a69f0f5147909b86e01949216b020f
Jerryyy <jerry@voltary.net>
2026-02-12 00:01:20 +0200

    Fix for the latest game update (14135)

--- Source/MemoryPatterns/Linux/PanoramaUiPanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/PanoramaUiPanelPatternsLinux.h
@@ -20,7 +20,7 @@ struct PanoramaUiPanelPatterns {
             .template addPattern<PanelClassesVectorOffset, CodePattern{"97 ? ? ? ? 85 D2 7E ? 48 8B 87"}.add(1).read()>()
             .template addPattern<PanelStyleOffset, CodePattern{"E8 ? ? ? ? 48 8D 43 ? 48 8B"}.add(8).read()>()
             .template addPattern<ParentWindowOffset, CodePattern{"? 48 85 D2 74 ? 48 89 53"}.read()>()
-            .template addPattern<OffsetToPanelId, CodePattern{"FF 50 ? 49 83 7C 24 ? 00 0F"}.add(7).read()>()
+            .template addPattern<OffsetToPanelId, CodePattern{"AD 49 ? FF 48 83 7B ? 00"}.add(7).read()>()
             .template addPattern<OffsetToPanelFlags, CodePattern{"3E 41 F6 87 ? ? ? ?"}.add(4).read()>()
             .template addPattern<OffsetToPanelHandle, CodePattern{"48 8B 86 ? ? ? ? C3"}.add(3).read()>();
     }
