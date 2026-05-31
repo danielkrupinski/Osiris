0120da3e8ba05997f5c5c31ee5d2bc840be9c82a
Daniel Krupiński <danielkrupinski@outlook.com>
2026-05-31 14:01:17 +0200

    Fix for the latest game update

--- Source/MemoryPatterns/Linux/WeaponServicesPatternsLinux.h
+++ Source/MemoryPatterns/Linux/WeaponServicesPatternsLinux.h
@@ -7,7 +7,7 @@ struct WeaponServicesPatterns {
     [[nodiscard]] static consteval auto addClientPatterns(auto clientPatterns) noexcept
     {
         return clientPatterns
-            .template addPattern<OffsetToActiveWeapon, CodePattern{"B7 4B ? 48 89 C8"}.add(2).read()>()
+            .template addPattern<OffsetToActiveWeapon, CodePattern{"09 D0 89 43 ? 48 85"}.add(4).read()>()
             .template addPattern<OffsetToWeapons, CodePattern{"8B 57 ? 48 8B 4F ? 8D"}.add(2).read()>();
     }
 };
--- Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
+++ Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
@@ -8,7 +8,7 @@ struct UiItem3dPanelPatterns {
     {
         return clientPatterns
             .template addPattern<SetItemItemIdFunction, CodePattern{"CF 48 8B D0 48 8B 5C 24 ? 48 83 C4 ? 5F E9 ? ? ? ?"}.add(15).abs()>()
-            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"7E ? 75 28"}.add(1).read()>()
+            .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"44 39 76 ? 0F 85"}.add(3).read()>()
             .template addPattern<OffsetToStartWeaponLookAt, CodePattern{"81 ? ? ? ? 01 00 C3"}.add(1).read()>()
             .template addPattern<OffsetToItem3dPanelProperties, CodePattern{"8E ? ? ? ? 48 8D 45 ? C6"}.add(1).read()>();
     }
