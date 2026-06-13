9e92e002332e5bd598e9070d10d38f83585d6849
Juchi <24779257+KebabLord@users.noreply.github.com>
2026-06-13 20:21:49 +0300

    fix linux pattern after latest game update

--- Source/MemoryPatterns/Linux/PanoramaUiPanelPatternsLinux.h
+++ Source/MemoryPatterns/Linux/PanoramaUiPanelPatternsLinux.h
@@ -10,7 +10,7 @@ struct PanoramaUiPanelPatterns {
             .template addPattern<SetParentFunctionOffset, CodePattern{"48 8B 90 ? ? ? ? 48 85 F6 74 16"}.add(3).read()>()
             .template addPattern<SetVisibleFunctionOffset, CodePattern{"78 ? 48 8B 07 FF 90 ? ? ? ? 4C 89 F7 E8 ? ? ? ? 31"}.add(7).read()>()
             .template addPattern<GetAttributeStringFunctionOffset, CodePattern{"FF 90 ? ? ? ? 80 BB 90 ? ? ? ? 48 89 C2"}.add(2).read()>()
-            .template addPattern<SetAttributeStringFunctionOffset, CodePattern{"78 ? 48 8B 07 FF 90 ? ? ? ? 44 8B 8D"}.add(7).read()>();
+            .template addPattern<SetAttributeStringFunctionOffset, CodePattern{"48 89 DA 0F B7 30 48 8B 07 FF 90 ? ? ? ? B8 01 00 00 00"}.add(11).read()>();
     }
 
     [[nodiscard]] static consteval auto addPanoramaPatterns(auto panoramaPatterns) noexcept
