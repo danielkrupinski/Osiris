2b85e721ff78bbb7a436401fd048279959968193
Kamuyin <56199686+Kamuyin@users.noreply.github.com>
2026-07-12 19:46:24 +0200

    fix patterns

--- Source/MemoryPatterns/Windows/EntityPatternsWindows.h
+++ Source/MemoryPatterns/Windows/EntityPatternsWindows.h
@@ -12,7 +12,7 @@ struct EntityPatterns {
             .template addPattern<OffsetToLifeState, CodePattern{"0F B6 81 ? ? ? ? 3B C2"}.add(3).read()>()
             .template addPattern<OffsetToTeamNumber, CodePattern{"44 0F B6 89 ? ? ? ? 41 3B"}.add(4).read()>()
             .template addPattern<OffsetToVData, CodePattern{"89 ? ? ? ? 48 85 C9 74 ? 48 8B 49 ? 48 8D"}.add(1).read()>()
-            .template addPattern<OffsetToRenderComponent, CodePattern{"8B ? ? ? ? ? 48 85 C0 74 ? 39 ? ? 7E"}.add(2).read()>()
+            .template addPattern<OffsetToRenderComponent, CodePattern{"F3 0F 10 00 F3 0F 11 87 10 12 00 00 48 83 C4 38 5F 5B C3 48 8B 87 ? ? ? ? 48 85 C0 74 27 39 58 38 7E 22 8B D3 48 8B C8 E8 B3 D0 A2 FF"}.add(22).read()>()
             .template addPattern<OffsetToOwnerEntity, CodePattern{"44 8B 81 ? ? ? ? 4C 8B DA 4C 8B D1"}.add(3).read()>()
             .template addPattern<GetAbsOriginFunction, CodePattern{"F8 ? 75 ? E8 ? ? ? ? F3"}.add(5).abs()>();
     }
--- Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
+++ Source/MemoryPatterns/Windows/UiItem3dPanelPatternsWindows.h
@@ -9,7 +9,7 @@ struct UiItem3dPanelPatterns {
         return clientPatterns
             .template addPattern<SetItemItemIdFunction, CodePattern{"CF 48 8B D0 48 8B 5C 24 ? 48 83 C4 ? 5F E9 ? ? ? ?"}.add(15).abs()>()
             .template addPattern<OffsetToItem3dPanelUnknownField, CodePattern{"48 8B 85 E0 00 00 00 48 8D 15 ? ? ? ? 49 8D 4E 60 49 89 86 80 00 00 00 41 C6 86 94 00 00 00 01 FF 15 ? ? ? ? 41 C7 46 ? FF FF FF FF 48 8D 54 24 60 49 8B 46 60 49 8B 5D 10 48 85 C0 48 8B CB"}.add(42).read()>()
-            .template addPattern<OffsetToStartWeaponLookAt, CodePattern{"81 ? ? ? ? 01 00 C3"}.add(1).read()>()
+            .template addPattern<OffsetToStartWeaponLookAt, CodePattern{"C3 CC CC CC CC CC CC CC CC CC CC CC CC CC CC CC 66 C7 81 ? ? ? ? 01 00 C3"}.add(19).read()>()
             .template addPattern<OffsetToItem3dPanelProperties, CodePattern{"8E ? ? ? ? 48 8D 45 ? C6"}.add(1).read()>();
     }
 };
