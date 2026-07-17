b1e7b4992ceac284157dd92fc5539f7f1be5ea0d
unknown <kuzeyerkan2009@gmail.com>
2026-07-17 20:39:51 +0300

    Add CRT-free geometry math

--- Source/Utils/Math.h
+++ Source/Utils/Math.h
@@ -3,6 +3,12 @@
 #include <xmmintrin.h>
 
 namespace Math {
+    [[nodiscard]] inline bool isFinite(float x) noexcept
+    {
+        constexpr float kMaxFinite = 3.402823466e+38F;
+        return x == x && x >= -kMaxFinite && x <= kMaxFinite;
+    }
+
     inline float abs(float x) noexcept
     {
         return x < 0.0f ? -x : x;
@@ -36,6 +42,38 @@ namespace Math {
         // Taylor terms through x^11/x^10 have sub-micro-unit error here.
         sine = x * (1.0f + x2 * (-1.0f / 6.0f + x2 * (1.0f / 120.0f + x2 * (-1.0f / 5040.0f + x2 * (1.0f / 362880.0f - x2 / 39916800.0f)))));
         cosine = cosineSign * (1.0f + x2 * (-0.5f + x2 * (1.0f / 24.0f + x2 * (-1.0f / 720.0f + x2 * (1.0f / 40320.0f - x2 / 3628800.0f)))));
+    }
+
+    [[nodiscard]] inline float sqrt(float x) noexcept
+    {
+        return _mm_cvtss_f32(_mm_sqrt_ss(_mm_set_ss(x)));
+    }
+
+    [[nodiscard]] inline float atan2(float y, float x) noexcept
+    {
+        constexpr float kPi = 3.14159265358979323846f;
+        constexpr float kHalfPi = 1.57079632679489661923f;
+        if (x == 0.0f) {
+            if (y > 0.0f)
+                return kHalfPi;
+            if (y < 0.0f)
+                return -kHalfPi;
+            return 0.0f;
+        }
+        if (y == 0.0f)
+            return x < 0.0f ? kPi : 0.0f;
+
+        const float absoluteX = abs(x);
+        const float absoluteY = abs(y);
+        const bool yIsDominant = absoluteY > absoluteX;
+        const float ratio = yIsDominant ? absoluteX / absoluteY : absoluteY / absoluteX;
+        const float ratioSquared = ratio * ratio;
+        float angle = ratio * (0.9998660f + ratioSquared * (-0.3302995f + ratioSquared * (0.1801410f + ratioSquared * (-0.0851330f + ratioSquared * 0.0208351f))));
+        if (yIsDominant)
+            angle = kHalfPi - angle;
+        if (x < 0.0f)
+            angle = kPi - angle;
+        return y < 0.0f ? -angle : angle;
     }
 
 }
