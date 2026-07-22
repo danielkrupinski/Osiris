970ae0f981d0d256d2a3cc03882fbc13be16a974
Daniel Krupiński <danielkrupinski@outlook.com>
2026-07-22 18:57:11 +0200

    Fix for the latest game update

--- Source/MemoryPatterns/Linux/PlantedC4PatternsLinux.h
+++ Source/MemoryPatterns/Linux/PlantedC4PatternsLinux.h
@@ -11,7 +11,7 @@ struct PlantedC4Patterns {
             .template addPattern<BombSiteOffset, CodePattern{"0F 86 ? ? ? ? 8B 83 ? ? ? ? 41 B9"}.add(8).read()>()
             .template addPattern<BombTickingOffset, CodePattern{"B6 00 84 C0 74 ? 80 BB ? ? ? ? 00 74 ? 80"}.add(8).read()>()
             .template addPattern<BombBlowTimeOffset, CodePattern{"F3 0F 10 83 ? ? ? ? F3 0F 5C C1 0F 2F E0"}.add(4).read()>()
-            .template addPattern<BombDefuserOffset, CodePattern{"EC ? ? ? ? E8 ? ? ? ? 48 8D BB ? ? ? ? E8 ? ? ? ? 48"}.add(13).read()>()
+            .template addPattern<BombDefuserOffset, CodePattern{"EC ? ? ? ? E8 ? ? ? ? 48 8D BB ? ? ? ? E8"}.add(13).read()>()
             .template addPattern<BombDefuseEndTimeOffset, CodePattern{"74 ? F3 0F 10 80 ? ? ? ? 4C"}.add(6).read()>();
     }
 };
--- Source/MemoryPatterns/Windows/PlantedC4PatternsWindows.h
+++ Source/MemoryPatterns/Windows/PlantedC4PatternsWindows.h
@@ -8,9 +8,9 @@ struct PlantedC4Patterns {
     {
         return clientPatterns
             .template addPattern<PlantedC4sPointer, CodePattern{"49 8B 0E 48 8B 1D ? ? ? ?"}.add(6).abs()>()
-            .template addPattern<BombSiteOffset, CodePattern{"41 0F 2F C2 0F 86 ? ? ? ? 8B 83 ? ? ? ?"}.add(12).read()>()
+            .template addPattern<BombSiteOffset, CodePattern{"41 0F 2F C2 0F 86 ? ? ? ? 8B ? ? ? ? ?"}.add(12).read()>()
             .template addPattern<BombTickingOffset, CodePattern{"44 38 ? ? ? ? ? 74 ? 49 8B ? E8 ? ? ? ? 83"}.add(3).read()>()
-            .template addPattern<BombBlowTimeOffset, CodePattern{"BB ? ? ? ? 48 8B CF 48 8D 55"}.add(1).read()>()
+            .template addPattern<BombBlowTimeOffset, CodePattern{"F3 0F 10 87 ? ? ? ? F3 0F 11 06"}.add(4).read()>()
             .template addPattern<BombDefuserOffset, CodePattern{"EC ? ? ? ? 48 8B ? E8 ? ? ? ? 8B ? ? ? ? ? 45 33 ? 4C"}.add(15).read()>()
             .template addPattern<BombDefuseEndTimeOffset, CodePattern{"8D 3D ? ? ? ? 48 8D 8B ? ? ? ?"}.add(9).read()>();
     }
